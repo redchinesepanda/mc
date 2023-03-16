dfda6efa3e925c04108167b77bed0a202d94d46b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-16 15:28:16 +0300

    style wp_head

--- template-parts/part-billet-title.php
+++ template-parts/part-billet-title.php
@@ -3,10 +3,12 @@
 // echo '<pre>part-billet-title.php:' . print_r( $args, true ) . '</pre>';
 
 ?>
-<h3>
-    <a href="<?php echo $args['billet-url'] ?>">
-        <?php echo $args['billet-title-text'] ?>
-    </a>
-</h3>
-<div class="billet-title-rating"><?php echo $args['billet-title-rating'] ?></div>
-<div class="billet-title-best"><?php echo $args['billet-title-best'] ?></div>
+<div class="billet-title">
+    <h3 class="billet-title-text">
+        <a href="<?php echo $args['billet-url'] ?>">
+            <?php echo $args['billet-title-text'] ?>
+        </a>
+    </h3>
+    <div class="billet-title-rating"><?php echo $args['billet-title-rating'] ?></div>
+    <div class="billet-title-best"><?php echo $args['billet-title-best'] ?></div>
+</div>
