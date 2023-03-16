5aaae5c2822e42d0164aa9864dfc322ce315c249
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-16 15:33:51 +0300

    style wp_head

--- template-parts/part-billet-list.php
+++ template-parts/part-billet-list.php
@@ -7,9 +7,9 @@
 <?php if ( !empty( $args ) ): ?>
     <?php foreach( $args as $part ) :?>
         <?php if ( !empty( $part['part-items'] ) ): ?>
-            <div class="part-items <?php echo $part['part-icon']; ?> <?php echo $part['part-direction']; ?>">
+            <div class="billet-list-part <?php echo $part['part-icon']; ?> <?php echo $part['part-direction']; ?>">
                 <?php foreach( $part['part-items'] as $item ) :?>
-                    <div class=""><?php echo $item; ?></div>
+                    <div class="billet-list-part-item"><?php echo $item; ?></div>
                 <?php endforeach;?>
             </div>
         <?php endif; ?>
