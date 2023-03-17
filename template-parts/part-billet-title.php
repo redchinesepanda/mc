063ecd0ab3d76e745f244e134adc904f081d2020
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-17 15:23:43 +0300

    style wp_head

--- template-parts/part-billet-title.php
+++ template-parts/part-billet-title.php
@@ -5,10 +5,10 @@ require_once( Template::LEGAL_PATH . '/lib/BilletAchievement.php' );
 ?>
 <div class="billet-title">
     <h3 class="billet-title-text">
-        <a href="<?php echo $args['billet-url'] ?>">
-            <?php echo $args['billet-title-text'] ?>
+        <a href="<?php echo $args['billet-url']; ?>">
+            <?php echo $args['billet-title-text']; ?>
         </a>
     </h3>
-    <div class="billet-title-rating"><?php echo $args['billet-title-rating'] ?></div>
+    <div class="billet-title-rating"><?php echo $args['billet-title-rating']; ?></div>
     <?php BilletAchievement::render(); ?>
 </div>
