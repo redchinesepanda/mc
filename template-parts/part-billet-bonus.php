c5023daa553e5a4c917726c83699a28db961b262
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-17 10:58:36 +0300

    style wp_head

--- template-parts/part-billet-bonus.php
+++ template-parts/part-billet-bonus.php
@@ -3,21 +3,19 @@
 // echo '<pre>part-billet-bonus.php:' . print_r( $args, true ) . '</pre>';
 
 ?>
-<div class="billet-bonus">
-    <div class="bonus-title">
-        <a href="<?php echo $args['bonus-url']; ?>" rel="nofollow">
-            <?php echo $args['bonus-title']; ?>
-        </a>
-    </div>
-    <div class="bonus-description">
-        <?php echo $args['bonus-description']; ?>
-    </div>
-    <div class="bonus-button">
-        <a href="<?php echo $args['bonus-url']; ?>" rel="nofollow">
-            <?php echo $args['bonus-button']; ?>
-        </a>
-    </div>
-    <div class="billet-spoiler-button">
-        <?php echo $args['spoiler-button']; ?>
-    </div>
+<div class="bonus-title">
+    <a href="<?php echo $args['bonus-url']; ?>" rel="nofollow">
+        <?php echo $args['bonus-title']; ?>
+    </a>
+</div>
+<div class="bonus-description">
+    <?php echo $args['bonus-description']; ?>
+</div>
+<div class="bonus-button">
+    <a href="<?php echo $args['bonus-url']; ?>" rel="nofollow">
+        <?php echo $args['bonus-button']; ?>
+    </a>
+</div>
+<div class="billet-spoiler-button">
+    <?php echo $args['spoiler-button']; ?>
 </div>
