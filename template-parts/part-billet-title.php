969eaa16f527d5b71ba9df128af99b8b0165610d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-17 14:24:50 +0300

    style wp_head

--- template-parts/part-billet-title.php
+++ template-parts/part-billet-title.php
@@ -4,7 +4,7 @@
 
 ?>
 <style type="text/css">
-    .<?php echo $args['achievement-selector']; ?> {
+    .<?php echo $args['billet-achievement']['achievement-selector']; ?> {
         background-color: <?php echo $args['billet-achievement']['achievement-color']; ?>;
         background-image: url('<?php echo $args['billet-achievement']['achievement-color']; ?>');
     }
@@ -16,7 +16,7 @@
         </a>
     </h3>
     <div class="billet-title-rating"><?php echo $args['billet-title-rating'] ?></div>
-    <div class="billet-title-achivement <?php echo $args['achievement-selector']; ?>">
+    <div class="billet-title-achivement <?php echo $args['billet-achievement']['achievement-selector']; ?>">
         <?php echo $args['billet-achievement']['achievement-name']; ?>
     </div>
 </div>
