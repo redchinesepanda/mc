eebf408d7ac77cc15f7fa773b63d84281356aeff
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-21 18:03:33 +0300

    billet spoiler

--- template-parts/part-billet-title.php
+++ template-parts/part-billet-title.php
@@ -4,11 +4,15 @@ require_once( Template::LEGAL_PATH . '/lib/BilletAchievement.php' );
 
 ?>
 <div class="billet-title">
-    <h3 class="billet-title-text">
-        <a href="<?php echo $args['url']['title']; ?>">
-            <?php echo $args['title']; ?>
-        </a>
+    <?php if ( !empty( $args['url']['title'] ) ): ?>
+        <a class="legal-title" href="<?php echo $args['url']['title']; ?>" rel="nofollow">
+    <?php endif; ?>
+    <h3 class="legal-title">
+        <?php echo $args['title']; ?>
     </h3>
+    <?php if ( !empty( $args['url']['title'] ) ): ?>
+        </a>
+    <?php endif; ?>
     <?php if ( !empty( $args['rating'] ) ): ?>
         <div class="billet-title-rating"><?php echo $args['rating']; ?></div>
     <?php endif; ?>
