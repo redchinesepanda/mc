490db0eb4eae2885bf8ea08c2cbb135d1d440bf3
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-22 14:38:34 +0300

    billet spoiler js

--- lib/wpml/WPMLLangSwitcher.php
+++ lib/wpml/WPMLLangSwitcher.php
@@ -14,17 +14,6 @@ class WPMLLangSwitcher
         self::debug( $message );
     }
 
-    public static function get_current()
-    {
-        $post = get_post();
-
-        $trid = apply_filters( 'wpml_element_trid', NULL, $post->ID, 'post_page' );
-
-        $translations = apply_filters( 'wpml_get_element_translations', NULL, $trid, 'post_page' );
-
-        return $translations;
-    }
-
     function get_all() {
         $languages = apply_filters(
             'wpml_active_languages',
@@ -42,13 +31,45 @@ class WPMLLangSwitcher
         return $languages;
     }
 
+    public static function get_active( &$args )
+    {
+        $args_active = array_column( $args, 'active' );
+
+        $key = array_search( true, $args_active );
+
+        $active = array_splice( $args, $key, 1 );
+
+        return self::map( $active[0] );
+    }
+
+    private static function map( $args )
+    {
+        $mapped['title'] = $args['native_name'];
+
+        $mapped['href'] = $args['url'];
+
+        $mapped['src'] = $args['country_flag_url'];
+
+        $mapped['alt'] = $args['translated_name'] . '-flag';
+
+        return $mapped;
+    }
+
     public function get()
     {
         $message['function'] = 'WPMLLangSwitcher::get';
 
-        // $message['translations'] = self::get_current();
+        $languages = self::get_all();
 
-        $message['languages'] = self::get_all();
+        // $message['languages'] = $languages;
+
+        $args['active'] = self::get_active( $languages );
+
+        foreach ( $languages as $lang ) {
+            $args['languages'][] = self::map( $lang );
+        }
+
+        $message['args'] = $args;
 
         self::debug( $message );
     }
