7de4bd1790703c3afc84e02b86b15402f84f815b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-22 10:39:50 +0300

    billet spoiler js

--- template-parts/part-billet-bonus.php
+++ template-parts/part-billet-bonus.php
@@ -32,6 +32,6 @@
 <?php if ( BilletSpoiler::check() ): ?>
     <div class="bonus-spoiler" data-id="<?php echo $args['data']['id']; ?>">
         <span class="spoiler-open"><?php echo $args['spoiler']['open']; ?></span>
-        <span class="spoiler-open"><?php echo $args['spoiler']['close']; ?></span>
+        <span class="spoiler-close"><?php echo $args['spoiler']['close']; ?></span>
     </div>
 <?php endif; ?>
