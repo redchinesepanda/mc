46ab2d83471a88a5e4d456340b8b20b830cc309e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-22 13:37:39 +0300

    billet spoiler js

--- lib/wpml/WPMLLangSwitcher.php
+++ lib/wpml/WPMLLangSwitcher.php
@@ -3,18 +3,31 @@
 class WPMLLangSwitcher
 {
     public static function register() {
+        $message[] = 'WPMLLangSwitcher::register';
+
         $handler = new self();
 
         // [legal-lang-switcher]
 
         add_shortcode( 'legal-lang-switcher', [ $handler, 'get' ] );
+
+        self::debug( $message );
     }
 
     public function get()
     {
+        $message[] = 'WPMLLangSwitcher::get';
+
         $translations = apply_filters( 'wpml_get_element_translations', NULL, 2, 'post_page' );
 
         var_dump( $translations );
+
+        self::debug( $message );
+    }
+
+    public static function debug( $message )
+    {
+        echo '<pre>WPMLLangSwitcher::debug: ' . print_r( $message, true ) . '<pre>';
     }
 }
 
