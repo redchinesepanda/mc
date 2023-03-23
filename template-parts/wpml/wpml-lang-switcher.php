8aa4a26c7a4695cec9f835a7efa297818db3d950
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-23 10:49:04 +0300

    billet spoiler js

--- template-parts/wpml/wpml-lang-switcher.php
+++ template-parts/wpml/wpml-lang-switcher.php
@@ -9,13 +9,15 @@
     <?php endforeach; ?>
 </style>
 <div class="lang-switcher">
-    <div class="lang-current lang-item">
+    <!-- <div class="lang-current lang-item"> -->
+    <a href="#lang-menu-1" class="lang-current lang-item">
         <div class="lang-image locale-<?php echo $args['active']['id']; ?>"></div>
         <div class="lang-title">
             <?php echo $args['active']['title']; ?>
         </div>
-    </div>
-    <div class="menu-avaible">
+    </a>
+    <!-- </div> -->
+    <div id="lang-menu-1" class="menu-avaible">
         <?php foreach( $args['languages'] as $lang ) : ?>
             <a class='lang-avaible lang-item' href="<?php echo $lang['href']; ?>" target="_blank">
                 <div class="lang-image locale-<?php echo $lang['id']; ?>"></div>
