cbcbab2bab9b4b34e5c49423b5a26a90f10acf67
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-23 09:43:08 +0300

    billet spoiler js

--- template-parts/wpml/wpml-lang-switcher.php
+++ template-parts/wpml/wpml-lang-switcher.php
@@ -1,33 +1,26 @@
 <style type="text/css">
+    .lang-switcher .locale-<?php echo $args['active']['id']; ?> {
+        background-image: url('<?php echo $args['active']['src']; ?>');
+    }
     <?php foreach( $args['languages'] as $lang ) : ?>
-        .container__menu-in-futter .img-before-text-block.locale-<?php echo $args['active']['id']; ?> {
-            background-image: url('<?php echo $args['active']['src']; ?>');
+        .lang-switcher .locale-<?php echo $lang['id']; ?> {
+            background-image: url('<?php echo $lang['src']; ?>');
         }
     <?php endforeach; ?>
 </style>
-<div class="container__menu-in-futter" style="position: relative;">
-    <section class="menu-in-futter">
-        <div class="buttton-country-whis-flag">
-            <div class="img-before-text-block locale-<?php echo $args['active']['id']; ?>"></div>
-            <div class="menu-in-futter__text-block">
-                <?php echo $args['active']['title']; ?>
-            </div>
-            <svg class="tcb-icon" viewBox="0 0 24 24" data-id="icon-arrow_drop_up-duotone" data-name="">
-                <path fill="none" d="M0 0h24v24H0V0z"></path>
-                <path d="M7 14l5-5 5 5H7z"></path>
-            </svg>
+<div class="lang-switcher">
+    <div class="lang-current">
+        <div class="lang-image locale-<?php echo $args['active']['id']; ?>"></div>
+        <div class="lang-title">
+            <?php echo $args['active']['title']; ?>
         </div>
-    </section>
-    <div class="drop-menu_content_Spoiler">
+    </div>
+    <div class="menu-avaible">
         <?php foreach( $args['languages'] as $lang ) : ?>
-            <a class='lang' href="<?php echo $lang['href']; ?>" target="_blank">
-                <div class="container__img-before-text-block">
-                    <div class="img-before-text-block">
-                        <img src="<?php echo $lang['src']; ?>" alt="<?php echo $lang['alt']; ?>" />
-                    </div>
-                    <div class="menu-in-futter__text-block">
-                        <?php echo $lang['title']; ?>
-                    </div>
+            <a class='lang-avaible' href="<?php echo $lang['href']; ?>" target="_blank">
+                <div class="lang-image locale-<?php echo $lang['id']; ?>"></div>
+                <div class="lang-title">
+                    <?php echo $lang['title']; ?>
                 </div>
             </a>
         <?php endforeach; ?>
