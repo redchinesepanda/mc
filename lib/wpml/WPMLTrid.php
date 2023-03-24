7508b2e9b429c8068fcb27b298bfb0a53fe11051
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-24 13:37:29 +0300

    wpml trid

--- lib/wpml/WPMLTrid.php
+++ lib/wpml/WPMLTrid.php
@@ -15,11 +15,11 @@ class WPMLTrid
 
         self::debug( $message );
     }
-    
+
     public static function get() {
         $post = get_post();
 
-        return apply_filters( 'wpml_element_trid', NULL, $post-ID, 'post_page' );  
+        return apply_filters( 'wpml_element_trid', NULL, $post->ID, 'post_page' );  
     }
 
     public static function debug( $message )
