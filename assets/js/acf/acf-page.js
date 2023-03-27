2cd93a1a5454a33e28ca4c421b0a6d8dc6bcc16f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-27 14:26:08 +0300

    wpml trid

--- assets/js/acf/acf-page.js
+++ assets/js/acf/acf-page.js
@@ -33,13 +33,28 @@ var ACFPage = ( function( $ )
             
             // var val = ( val = acf.getFields( { name : 'page-translation-group' } ).shift() ) ? val.val() : '';
             
-            var val = acf.getField( 'page-translation-group' ).val();
+            // var val = acf.getField( 'page-translation-group' ).val();
 
-            console.log( 'acf-page-js val: ' + JSON.stringify( val ) );
+            // console.log( 'acf-page-js val: ' + JSON.stringify( val ) );
 
-            $( '#acf-field_64213360cf905-input' ).select2('data');
+            // $( '#acf-field_64213360cf905-input' ).select2('data');
 
-            console.log( 'acf-page-js data: ' + JSON.stringify( data ) );
+            // console.log( 'acf-page-js data: ' + JSON.stringify( data ) );
+
+            acf.addAction('select2_init', function( $select, args, settings, field ){
+                // $select (jQuery) select element
+                // args (object) args given to the select2 function
+                // settings (object) settings given to the acf.select2 function
+                // field (object) field instance 
+
+                console.log( 'acf-page-js $select: ' + JSON.stringify( $select ) );
+
+                console.log( 'acf-page-js args: ' + JSON.stringify( args ) );
+
+                console.log( 'acf-page-js settings: ' + JSON.stringify( settings ) );
+
+                console.log( 'acf-page-js field: ' + JSON.stringify( field ) );
+            });
         }
     }
 } )( jQuery );
