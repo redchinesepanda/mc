140f28398b68d3b66e8acb49eb40787b93e05f9e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-27 11:48:02 +0300

    wpml trid

--- lib/tools/ToolPosts.php
+++ lib/tools/ToolPosts.php
@@ -2,7 +2,7 @@
 
 class ToolPosts
 {
-    const CSV = '/container/var/www/mc-wp/test/public_html/wp-content/uploads/csv/posts.csv';
+    const CSV = WP_CONTENT_DIR . '/uploads/csv/posts.csv';
 
     public static function register()
     {
