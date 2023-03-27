b3113dabd30326e7a30cb444a7f585f13ba0b347
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-27 10:52:59 +0300

    wpml trid

--- assets/js/acf/acf-page.js
+++ assets/js/acf/acf-page.js
@@ -33,7 +33,7 @@ var ACFPage = ( function( $ )
             
             // var val = ( val = acf.getFields( { name : 'page-translation-group' } ).shift() ) ? val.val() : '';
             
-            let val = ( val = acf.getField( 'page-translation-group' ) ) ? val.val() : '';
+            var val = ( val = acf.getField( 'page-translation-group' ) ) ? val.val() : '';
 
             console.log( 'acf-page-js val: ' + JSON.stringify( val ) );
         }
