0cd228d3ddefef4b9e5fd44b6d7ab86503a7c924
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-27 10:14:40 +0300

    wpml trid

--- assets/js/acf/acf-page.js
+++ assets/js/acf/acf-page.js
@@ -1,23 +1,35 @@
 // acf-page-js start
 
-document.addEventListener('DOMContentLoaded', function()
+// document.addEventListener('DOMContentLoaded', function()
+// {
+//     function option_changed( event )
+//     {
+//         const value = event.currentTarget.value;
+
+//         console.log( 'acf-page-js value: ' + value );
+
+//         const text = event.currentTarget.options[ event.currentTarget.selectedIndex ].text;
+
+//         console.log( 'acf-page-js text: ' + text );
+//     }
+
+// 	const pageTranslationGroups = document.querySelector( '.acf-field-select[data-name="page-translation-group"] select' );
+
+//     console.log( 'acf-page-js pageTranslationGroups: ' + pageTranslationGroups );
+
+//     pageTranslationGroups.addEventListener('change', option_changed, false );
+// } );
+
+var ACFPage = ( function( $ )
 {
-    function option_changed( event )
-    {
-        const value = event.currentTarget.value;
+    'use strict';
 
-        console.log( 'acf-page-js value: ' + value );
-
-        const text = event.currentTarget.options[ event.currentTarget.selectedIndex ].text;
-
-        console.log( 'acf-page-js text: ' + text );
+    return {
+        run: function ()
+        {
+            console.log( 'acf-page-js data: ' + $( '.acf-field-select[data-name="page-translation-group"] select' ).select2( 'data' ) );
+        }
     }
-
-	const pageTranslationGroups = document.querySelector( '.acf-field-select[data-name="page-translation-group"] select' );
-
-    console.log( 'acf-page-js pageTranslationGroups: ' + pageTranslationGroups );
-
-    pageTranslationGroups.addEventListener('change', option_changed, false );
-} );
+} )( jQuery );
 
 // acf-page-js end
--- lib/acf/ACFMain.php
+++ lib/acf/ACFMain.php
@@ -6,8 +6,21 @@ require_once( 'ACFPage.php' );
 
 class ACFMain
 {
+    const JS = Template::LEGAL_URL . '/assets/js/acf/acf-main.js';
+
+    public static function register_script()
+    {
+        wp_register_script( 'acf-main', self::JS, [], false, true);
+
+        wp_enqueue_script( 'acf-main' );
+    }
+
     public static function register()
     {
+        $handler = new self();
+
+        add_action( 'admin_enqueue_scripts', [ $handler, 'register_script' ] );
+
         ACFBilletCards::register();
 
         ACFPage::register();
