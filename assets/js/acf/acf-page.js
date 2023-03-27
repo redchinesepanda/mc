55b5aa0b13bbb52c568bbae1a09bbad8d126e812
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-27 10:20:38 +0300

    wpml trid

--- assets/js/acf/acf-page.js
+++ assets/js/acf/acf-page.js
@@ -1,37 +1,39 @@
 // acf-page-js start
 
-document.addEventListener('DOMContentLoaded', function()
-{
-    function option_changed( event )
-    {
-        const value = event.currentTarget.value;
-
-        console.log( 'acf-page-js value: ' + value );
-
-        const text = event.currentTarget.options[ event.currentTarget.selectedIndex ].text;
-
-        console.log( 'acf-page-js text: ' + text );
-    }
-
-    const field = acf.getField( 'page-translation-group' );
-
-	// const pageTranslationGroups = document.querySelector( '.acf-field-select[data-name="page-translation-group"] select' );
-
-    console.log( 'acf-page-js data: ' + field.select2('data') );
-
-    // pageTranslationGroups.addEventListener('change', option_changed, false );
-} );
-
-// var ACFPage = ( function( $ )
+// document.addEventListener('DOMContentLoaded', function()
 // {
-//     'use strict';
+//     function option_changed( event )
+//     {
+//         const value = event.currentTarget.value;
 
-//     return {
-//         run: function ()
-//         {
-//             console.log( 'acf-page-js data: ' + $( '.acf-field-select[data-name="page-translation-group"] select' ).select2( 'data' ) );
-//         }
+//         console.log( 'acf-page-js value: ' + value );
+
+//         const text = event.currentTarget.options[ event.currentTarget.selectedIndex ].text;
+
+//         console.log( 'acf-page-js text: ' + text );
 //     }
-// } )( jQuery );
+
+//     const field = acf.getField( 'page-translation-group' );
+
+// 	// const pageTranslationGroups = document.querySelector( '.acf-field-select[data-name="page-translation-group"] select' );
+
+//     console.log( 'acf-page-js data: ' + field.select2('data') );
+
+//     // pageTranslationGroups.addEventListener('change', option_changed, false );
+// } );
+
+var ACFPage = ( function( $ )
+{
+    'use strict';
+
+    return {
+        run: function ()
+        {
+            const field = acf.getField( 'page-translation-group' );
+            
+            console.log( 'acf-page-js data: ' + field.select2('data') );
+        }
+    }
+} )( jQuery );
 
 // acf-page-js end
--- lib/acf/ACFMain.php
+++ lib/acf/ACFMain.php
@@ -6,20 +6,20 @@ require_once( 'ACFPage.php' );
 
 class ACFMain
 {
-    // const JS = Template::LEGAL_URL . '/assets/js/acf/acf-main.js';
+    const JS = Template::LEGAL_URL . '/assets/js/acf/acf-main.js';
 
-    // public static function register_script()
-    // {
-    //     wp_register_script( 'acf-main', self::JS, [], false, true);
+    public static function register_script()
+    {
+        wp_register_script( 'acf-main', self::JS, [], false, true);
 
-    //     wp_enqueue_script( 'acf-main' );
-    // }
+        wp_enqueue_script( 'acf-main' );
+    }
 
     public static function register()
     {
-        // $handler = new self();
+        $handler = new self();
 
-        // add_action( 'admin_enqueue_scripts', [ $handler, 'register_script' ] );
+        add_action( 'admin_enqueue_scripts', [ $handler, 'register_script' ] );
 
         ACFBilletCards::register();
 
