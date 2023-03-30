a41b53f9a85db594136aa1c2317b95d0f4b02589
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-30 16:55:36 +0300

    tage type admin

--- template-parts/billet/center/part-billet-achievement.php
+++ template-parts/billet/center/part-billet-achievement.php
@@ -1,9 +1,11 @@
-<style type="text/css">
-    .<?php echo $args['selector']; ?> {
-        background-color: <?php echo $args['color']; ?>;
-        background-image: url('<?php echo $args['image']; ?>');
-    }
-</style>
+<?php if ( $args['class'] != BilletAchievement::TYPE_DISABLED ) : ?>
+    <style type="text/css">
+        .<?php echo $args['selector']; ?> {
+            background-color: <?php echo $args['color']; ?>;
+            background-image: url('<?php echo $args['image']; ?>');
+        }
+    </style>
+<?php endif; ?>
 <div class="billet-title-achivement <?php echo $args['selector'];?> <?php echo $args['class']; ?>">
     <?php echo $args['name']; ?>
 </div>
