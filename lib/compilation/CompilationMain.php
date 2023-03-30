98f5fe72f0ae51722a2b8dcf23ad5bb1fc7cc92d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-30 12:18:33 +0300

    tage type admin

--- lib/compilation/CompilationMain.php
+++ lib/compilation/CompilationMain.php
@@ -11,6 +11,19 @@ class CompilationMain
         BilletMain::print();
     }
 
+    // public static function get_settings( $post )
+    // {
+    //     $id = $post->ID;
+
+    //     $data['id'] = $id;
+
+    //     $data['order'] = get_field( 'billet-order-type', $id );
+
+    //     $data['spoiler'] = get_field( 'billet-spoiler-enabled', $id );
+
+    //     return $data;
+    // }
+
     public static function get()
     {
         $args = [
@@ -24,6 +37,8 @@ class CompilationMain
         $data = [];
 
         foreach ( $posts as $post ) {
+            // $data[] = self::get_settings( $post );
+
             $data[] = $post->ID;
         }
 
