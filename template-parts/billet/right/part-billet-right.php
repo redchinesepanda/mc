c994f0945c010b1f10caf051b6f65f572a067ddc
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-31 08:50:47 +0300

    compilation settings

--- template-parts/billet/right/part-billet-right.php
+++ template-parts/billet/right/part-billet-right.php
@@ -1,4 +1,5 @@
 <?php BilletBonus::render( $args ); ?>
+!!!
 <div class="bonus-button">
     <a class="legal-play <?php echo $args['play']['class']; ?>" href="<?php echo $args['play']['href']; ?>" rel="nofollow">
         <?php echo $args['play']['label']; ?>
