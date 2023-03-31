0400aea105bf260a07b579683a9f08f55c8f9e66
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-31 15:27:51 +0300

    compilation settings

--- lib/acf/ACFLocationRules.php
+++ lib/acf/ACFLocationRules.php
@@ -6,19 +6,21 @@ class ACFLocationRules
     {
         $handler = new self();
 
-        add_filter( 'acf/location/rule_types', [ $handler,'acf_location_rules_types' ] );
+        add_filter( 'acf/location/rule_types', [ $handler,'acf_location_rule_type' ] );
 
-        add_filter( 'acf/location/rule_values/current_locale', [ $handler,'acf_location_rule_values_user' ] );
+        add_filter( 'acf/location/rule_values/current_locale', [ $handler,'acf_location_rule_values_locale' ] );
+        
+        add_filter( 'acf/location/rule_match/current_locale', [ $handler, 'acf_location_rule_match_locale' ], 10, 4 );
     }
 
-    public static function acf_location_rules_types( $choices )
+    public static function acf_location_rule_type( $choices )
     {
         $choices['Language']['current_locale'] = 'Current Locale';
     
         return $choices;
     }
 
-    public static function acf_location_rule_values_user( $choices )
+    public static function acf_location_rule_values_locale( $choices )
     {
         $languages = WPMLLangSwitcher::choises();
 
@@ -27,6 +29,32 @@ class ACFLocationRules
         }
 
         return $choices;
+    }
+
+    public static function acf_location_rule_match_locale( $match, $rule, $options, $field_group )
+    {
+        $message['function'] = 'acf_location_rule_match_locale';
+
+        $current_language = apply_filters( 'wpml_current_language', NULL );
+
+        $message['current_language'] = $current_language;
+
+        $selected_language = $rule['value'];
+
+        $message['selected_language'] = $selected_language;
+
+        if ( $rule['operator'] == "==" ) {
+            $match = ( $current_language == $selected_language );
+        }
+
+        self::debug( $message );
+
+        return $match;
+    }
+
+    public static function debug( $message )
+    {
+        echo '<pre>' . get_class( self ) . '::debug: ' . print_r( $message, true ) . '</pre>';
     }
 }
 
