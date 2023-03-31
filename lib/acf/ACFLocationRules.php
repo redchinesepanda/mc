b7c2e1cd316a75e754f560be598eb757acd51086
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-31 16:07:21 +0300

    compilation settings

--- lib/acf/ACFLocationRules.php
+++ lib/acf/ACFLocationRules.php
@@ -9,12 +9,8 @@ class ACFLocationRules
         add_filter( 'acf/location/rule_types', [ $handler,'acf_location_rule_types' ] );
 
         add_filter( 'acf/location/rule_values/current_locale', [ $handler,'acf_location_rule_values_locale' ] );
-        
-        // add_filter( 'acf/location/rule_match/current_locale', [ $handler, 'acf_location_rule_match_locale' ], 10, 4 );
 
         add_filter( 'acf/location/rule_match/current_locale', [ $handler, 'acf_location_rule_match_user' ] );
-
-        // add_filter('acf/location/screen', [ $handler, 'acf_location_screen_options' ], 10, 1);
     }
 
     public static function acf_location_rule_types( $choices )
@@ -59,25 +55,35 @@ class ACFLocationRules
         return $match;
     }
 
-    // function acf_location_screen_options( $options )
-    // {
-    //     $message['function'] = 'acf_location_screen_options';
-
-    //     $message['options'] = $options;
-
-    //     $current_language = apply_filters( 'wpml_post_language_details', NULL, $options['post_id'] ) ;
-
-    //     $message['current_language'] = $current_language;
-
-    //     self::debug( $message );
-
-    //     return $options;
-    // }
-
     public static function debug( $message )
     {
         wp_die( '<pre>' . __CLASS__ . '::debug: ' . print_r( $message, true ) . '</pre>' );
     }
+}
+
+add_filter('acf/location/rule_match/current_locale', 'acf_location_rule_match_user', 10, 4);
+function acf_location_rule_match_user( $match, $rule, $options, $field_group )
+{
+    $message['function'] = 'acf_location_rule_match_user';
+
+    $current_language = apply_filters( 'wpml_post_language_details', NULL, $options['post_id'] ) ;
+
+    $message['current_language'] = $current_language;
+
+    $selected_language = $rule['value'];
+
+    $message['selected_language'] = $selected_language;
+
+    if ( $rule['operator'] == "==" ) {
+        $match = ( $current_language['locale'] == $selected_language );
+
+    } elseif( $rule['operator'] == "!=" ) {
+        $match = ( $current_language['locale'] != $selected_language );
+    }
+
+    self::debug( $message );
+
+    return $match;
 }
 
 ?>
