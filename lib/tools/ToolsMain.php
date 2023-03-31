2918fac5703a90cc7540ac0fca7d9338923039cd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-03-31 12:38:04 +0300

    compilation settings

--- lib/tools/ToolsMain.php
+++ lib/tools/ToolsMain.php
@@ -1,20 +1,20 @@
 <?php
 
+require_once( 'ToolLoco.php' );
+
 require_once( 'ToolDisable.php' );
 
 require_once( 'ToolPosts.php' );
-
-require_once( 'ToolLoco.php' );
 
 class ToolsMain
 {
     public static function register()
     {
+        ToolLoco::register();
+
         ToolDisable::register();
 
         ToolPosts::register();
-
-        ToolLoco::register();
     }
 }
 
