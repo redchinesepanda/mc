6d3de0380480f2d4fb72a79887d7d9f5413af19c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-04 18:12:25 +0300

    bonus separate

--- template-parts/yoast/part-yoast-main.php
+++ template-parts/yoast/part-yoast-main.php
@@ -1,6 +1,6 @@
-<title><?php args['title'] ?></title>
-<meta name="description" content="<?php args['description'] ?>" />
-<meta name="author" content="<?php args['author'] ?>">
-<meta name="keywords" content="<?php args['keywords'] ?>" />
+<title><?php $args['title'] ?></title>
+<meta name="description" content="<?php $args['description'] ?>" />
+<meta name="author" content="<?php $args['author'] ?>">
+<meta name="keywords" content="<?php $args['keywords'] ?>" />
 <!-- Shared. To move -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
