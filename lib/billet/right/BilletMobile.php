6c7d4ac5435e5c872d64b4a5c50e87078a00c38c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-07 13:52:18 +0300

    tabs compilations billet logo

--- lib/billet/right/BilletMobile.php
+++ lib/billet/right/BilletMobile.php
@@ -21,7 +21,7 @@ class BilletMobile
 
         $enabled = true;
 
-        if ( array_key_exists( 'filter', $billet ) ) {
+        if ( !empty( $billet[ 'filter' ] ) ) {
             $enabled = $billet['filter']['mobile'];
         }
 
