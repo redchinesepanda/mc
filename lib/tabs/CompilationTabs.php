ba33eaf7c27d514d3da2fdf7288f42ada3c21e53
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-10 09:47:33 +0300

    tabs

--- lib/tabs/CompilationTabs.php
+++ lib/tabs/CompilationTabs.php
@@ -66,7 +66,7 @@ class CompilationTabs
 
                     'active' => ( $key == 0 ? 'legal-active' : '' ),
 
-                    'class' => 'tab-' . $key,
+                    'class' => 'tab-' . $key . ( !empty( $tab[ self::TAB_IMAGE ] ) ? 'legal-image' : '' ),
                 ];
             }
         } else {
