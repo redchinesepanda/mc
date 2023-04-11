ae3d3a1ff2911fc7ba995046edfcc3761fb77196
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-11 16:19:33 +0300

    breadcrumbs

--- template-parts/breadcrumbs/part-breadcrumbs-main.php
+++ template-parts/breadcrumbs/part-breadcrumbs-main.php
@@ -2,7 +2,7 @@
     <?php foreach( $args as $arg ) : ?>
         <div class="legal-breadcrumbs-item">
             <?php if( !empty( $arg[ 'link' ] ) ) : ?>
-                <a class="legal-item-title" itemprop="<?php echo $arg[ 'link' ]['itemprop']; ?>" href="<?php echo $arg[ 'href' ]['itemprop']; ?>">
+                <a class="legal-item-title" itemprop="<?php echo $arg[ 'link' ]['itemprop']; ?>" href="<?php echo $arg[ 'link' ]['href']; ?>">
                     <span itemprop="<?php echo $arg[ 'title' ]['itemprop']; ?>"><?php echo $arg[ 'title' ]['text']; ?></span>
                 </a>
             <?php else : ?>
