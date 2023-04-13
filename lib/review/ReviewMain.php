1a0a71d7a7cd3a0e56610bdfc9a0268d86f0b981
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-13 16:02:03 +0300

    review-about

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -4,8 +4,23 @@ require_once( 'ReviewAbout.php' );
 
 class ReviewMain
 {
+    const CSS = [
+        'review-main' => LegalMain::LEGAL_URL . '/assets/css/review/review-main.css',
+    ];
+
+    public static function register_style()
+    {
+        foreach ( self::CSS as $name => $path ) {
+            wp_enqueue_style( $name, $path );
+        }
+    }
+
     public static function register()
     {
+        $handler = new self();
+
+        add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
+
         ReviewAbout::register();
     }
 }
