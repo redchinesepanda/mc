648718c52099b200aca9f4ae506de0e70ecf3993
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-14 12:18:45 +0300

    review-about

---
+++ lib/review/ReviewAnchors.php
@@ -0,0 +1,105 @@
+<?php
+
+class ReviewAnchors
+{
+    const CSS = [
+        'review-anchors' => LegalMain::LEGAL_URL . '/assets/css/review/review-anchors.css',
+    ];
+
+    public static function register_style()
+    {
+        foreach ( self::CSS as $name => $path ) {
+            wp_enqueue_style( $name, $path );
+        }
+    }
+
+    public static function register()
+    {
+        $handler = new self();
+
+        // [legal-anchors]
+
+        add_shortcode( 'legal-anchors', [ $handler, 'render' ] );
+
+        add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
+
+        add_filter( 'the_content', 'content', 1 );
+    }
+
+    const TERM = 'bookmaker-review';
+
+    public static function content( $content )
+    {
+        if ( has_term( self::TERM, 'page_type' ) ) {
+            return self::target( $content );
+        }
+    
+        return $content;
+    }
+
+    public static function target()
+    {
+        $dom = new DomDocument();
+
+        $dom->loadHTML( get_the_content() );
+
+        // foreach ( $dom->getElementsByTagName('a') as $item ) {
+        foreach ( $dom->getElementsByTagName( 'h2' ) as $key => $item ) {
+            $link = $dom->createElement( 'a' );
+
+            $link->setAttribute( 'class', 'legal-target' );
+
+            $link->setAttribute( 'name', 'target-' . $key );
+
+            $item->appendChild( $link );
+        }
+
+        return $dom->saveHTML();
+    }
+
+    // const FIELD = 'review-anchors';
+
+    public static function get()
+    {
+        $group = get_field( self::FIELD );
+        
+        if( $group ) {
+            return [
+                'title' => $group[ 'about-title' ],
+                
+                'bonus' => $group[ 'about-bonus' ],
+                
+                'description' => $group[ 'about-description' ],
+                
+                'logo' => $group[ 'about-logo' ],
+
+                'background' => $group[ 'about-background' ],
+                
+                'rating' => __( 'Rating', ToolLoco::TEXTDOMAIN ) . ' - ' . $group[ 'about-rating' ],
+
+                'afillate' => [
+                    'href' => $group[ 'about-afillate' ],
+
+                    'text' => __( 'Bet here', ToolLoco::TEXTDOMAIN ),
+                ],
+            ];
+        }
+
+        return [];
+    }
+
+    const TEMPLATE = LegalMain::LEGAL_PATH . '/template-parts/review/review-anchors.php';
+
+    public static function render()
+    {
+        ob_start();
+
+        load_template( self::TEMPLATE, false, self::get() );
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+}
+
+?>
