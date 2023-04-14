e83d75a5a49ab80fb99a0b87e31a00e83b2ce008
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-14 14:07:48 +0300

    review-about

--- template-parts/review/review-group.php
+++ template-parts/review/review-group.php
@@ -2,7 +2,7 @@
     <nav class="group">
         <div class="review-group">
             <span class="group-item">
-                <?php echo $item[ 'current' ][ 'label' ]; ?>
+                <?php echo $args[ 'current' ][ 'label' ]; ?>
             </span>
             <?php foreach( $args[ 'other' ] as $item ) : ?>
                 <a class="group-item" href="<?php echo $item[ 'href' ]; ?>">
