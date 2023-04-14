8bdbe06615ae380c80213ad6236157e8fec39d60
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-14 12:58:22 +0300

    review-about

--- template-parts/review/review-anchors.php
+++ template-parts/review/review-anchors.php
@@ -1,10 +1,12 @@
 <?php if( !empty( $args ) ) : ?>
-    <div class="review-anchors">
-        <div class="anchors-label"><?php echo $args[ 'label' ]; ?></div>
-        <?php foreach( $args[ 'items' ] as $item ) : ?>
-            <a class="anchors-item" href="<?php echo $item[ 'href' ]; ?>">
-                <?php echo $item[ 'label' ]; ?>
-            </a>
-        <?php endforeach; ?>
-    </div>
+    <nav class="anchors">
+        <div class="review-anchors">
+            <div class="anchors-label"><?php echo $args[ 'label' ]; ?></div>
+            <?php foreach( $args[ 'items' ] as $item ) : ?>
+                <a class="anchors-item" href="<?php echo $item[ 'href' ]; ?>">
+                    <?php echo $item[ 'label' ]; ?>
+                </a>
+            <?php endforeach; ?>
+        </div>
+    </nav>
 <?php endif; ?>
