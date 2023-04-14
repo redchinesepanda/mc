790ad9856209fb8ba1c307f84a9de0004f002090
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-14 13:38:48 +0300

    review-about

---
+++ lib/review/ReviewGroup.php
@@ -0,0 +1,95 @@
+<?php
+
+class ReviewGroup
+{
+    const CSS = [
+        'review-group' => LegalMain::LEGAL_URL . '/assets/css/review/review-group.css',
+    ];
+
+    public static function register_style()
+    {
+        foreach ( self::CSS as $name => $path ) {
+            wp_enqueue_style( $name, $path );
+        }
+    }
+
+    public static function register()
+    {
+        $handler = new self();
+
+        // [legal-group]
+
+        add_shortcode( 'legal-group', [ $handler, 'render' ] );
+
+        add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
+    }
+
+    const TAXONOMY = 'bookmaker-review';
+
+    public static function get()
+    {
+        LegalDebug::debug( [
+            'YoastSEO' => YoastSEO(),
+        ] );
+
+        $post = get_post();
+
+        if ( empty( $post ) )
+            return [];
+
+        $terms = wp_get_post_terms( $post->ID, self::TAXONOMY, [ 'fields' => 'ids' ] );
+
+        $posts = get_posts( [
+            'numberposts' => -1,
+
+            'post_type' => 'legal_billet',
+
+            'suppress_filters' => get_field( 'compilation-locale', $id ),
+
+            'tax_query' => [
+                [
+                    'taxonomy' => self::TAXONOMY,
+
+                    'field' => 'term_id',
+
+                    'terms' => $terms,
+
+                    'operator' => 'IN',
+                ]
+            ],
+
+            'orderby' => 'menu_order',
+
+            'order' => 'ASC',
+        ] );
+
+        if ( !empty( $posts ) ) {
+            foreach ( $posts as $post ) {
+                $items[] = [
+                    'title' => $post->post_title,
+    
+                    'href' => get_post_permalink( $post->ID ),
+                ];
+            }
+
+            return $items;
+        }
+
+        return [];
+    }
+
+    const TEMPLATE = LegalMain::LEGAL_PATH . '/template-parts/review/review-anchors.php';
+
+    public static function render()
+    {
+        ob_start();
+
+        load_template( self::TEMPLATE, false, self::get() );
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+}
+
+?>
