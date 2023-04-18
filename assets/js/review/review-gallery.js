bd13d1fd3236c142535fcb3aac2a6644a0621c5c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-18 18:45:43 +0300

    review-gallery

--- assets/js/review/review-gallery.js
+++ assets/js/review/review-gallery.js
@@ -78,7 +78,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		if ( figure.nextSibling !== null ) {
 			console.log( 'review-gallery figure.nextSibling.tagName: ' + figure.nextSibling.tagName );
 
-			if ( figure.nextSibling.tagName == 'figure' ) {
+			if ( figure.nextSibling.tagName == undefined ) {
 				right.dataset.next = figure.nextSibling.dataset.id;
 			}
 		}
