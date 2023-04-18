c226a9274f9075713d4d97e1ff1a05f13ccb2e3f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-18 16:36:13 +0300

    review-gallery

--- assets/js/review/review-gallery.js
+++ assets/js/review/review-gallery.js
@@ -1,87 +1,7 @@
-// Open the Modal
-// function openModal() {
-// 	document.getElementById( "myModal" ).style.display = "block";
-// }
-
-// Close the Modal
-// function closeModal() {
-// 	document.getElementById( "myModal" ).style.display = "none";
-// }
-
-// var slideIndex = 1;
-// showSlides( slideIndex );
-
-// Next/previous controls
-// function plusSlides( n ) {
-// 	showSlides(slideIndex += n);
-// }
-
-// Thumbnail image controls
-// function currentSlide( n ) {
-// 	showSlides( slideIndex = n );
-// }
-
-// function showSlides( n ) {
-// 	var i;
-
-// 	var slides = document.getElementsByClassName("mySlides");
-
-// 	var dots = document.getElementsByClassName("demo");
-
-// 	var captionText = document.getElementById("caption");
-
-// 	if (n > slides.length) {slideIndex = 1}
-
-// 	if (n < 1) {slideIndex = slides.length}
-
-// 	for (i = 0; i < slides.length; i++) {
-// 		slides[i].style.display = "none";
-// 	}
-
-// 	for (i = 0; i < dots.length; i++) {
-// 		dots[i].className = dots[i].className.replace(" active", "");
-// 	}
-
-// 	slides[slideIndex-1].style.display = "block";
-
-// 	dots[slideIndex-1].className += " active";
-
-// 	captionText.innerHTML = dots[slideIndex-1].alt;
-// }
-
 // review-gallery start
 
 document.addEventListener( 'DOMContentLoaded', function ()
 {
-    // function tabToggle( event )
-    // {
-    //     let tabs = document.getElementById( event.currentTarget.dataset.tabs );
-
-    //     tabs.querySelectorAll( '.legal-tab-title' ).forEach( ( title ) => {
-    //         title.classList.remove( 'legal-active' );
-    //     });
-
-    //     tabs.querySelectorAll( '.legal-tab-content' ).forEach( ( content ) => {
-    //         content.classList.remove( 'legal-active' );
-    //     });
-        
-    //     event.currentTarget.classList.add( 'legal-active' );
-
-    //     tabs.querySelector( '.legal-content-' + event.currentTarget.dataset.content ).classList.add( 'legal-active' );
-    // }
-
-    // Array.from( document.getElementsByClassName( 'legal-tabs' ) ).forEach( function callback( tabs, index ) {
-    //     tabs.id = "legal-tabs-" + index;
-
-    //     let titles = tabs.getElementsByClassName( 'legal-tab-title' );
-
-    //     for ( let title of titles ) {
-    //         title.dataset.tabs = tabs.id;
-
-    //         title.addEventListener( 'click', tabToggle, false );
-    //     }
-    // });
-
 	function popup( event )
     {
 		let lightroom = document.createElement( 'div' );
@@ -89,6 +9,26 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		lightroom.classList.add( 'legal-gallery' );
 
 		lightroom.style.backgroundImage = "url( '" + parse_srcset( event.currentTarget.getAttribute( 'srcset' ) ) + "' )"; 
+
+		// let left = document.createElement( 'div' );
+		
+		// left.classList.add( 'legal-left' );
+
+		// lightroom.appendChild( left );
+
+		lightroom.appendChild( document.createElement( 'div' ).classList.add( 'legal-left' ) );
+
+		let right = document.createElement( 'div' );
+		
+		right.classList.add( 'legal-right' );
+
+		lightroom.appendChild( right );
+
+		let close = document.createElement( 'div' );
+		
+		close.classList.add( 'legal-close' );
+
+		lightroom.appendChild( close );
 
 		document.getElementById( event.currentTarget.dataset.galleryID ).appendChild( lightroom );
 	}
