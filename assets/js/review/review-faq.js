ddc58659c76c313037fbdefe85b8b0fdf80d445f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-19 10:59:46 +0300

    review-gallery

--- assets/js/review/review-faq.js
+++ assets/js/review/review-faq.js
@@ -6,12 +6,6 @@ document.addEventListener( 'DOMContentLoaded', function ()
     {
 		event.currentTarget.classList.toggle( 'legal-active' );
 
-		console.log( 'review-faq event.target: ' + event.target );
-
-		console.log( 'review-faq event.currentTarget: ' + event.currentTarget );
-
-		console.log( 'review-faq event.currentTarget.nextElementSibling : ' + event.currentTarget.nextElementSibling  );
-
 		event.currentTarget.nextElementSibling.classList.toggle( 'legal-active' );
     }
 
