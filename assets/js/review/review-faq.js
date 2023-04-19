ae8a8b699621dbc00ba9ae4582f40b49482984cc
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-19 11:12:30 +0300

    review-gallery

--- assets/js/review/review-faq.js
+++ assets/js/review/review-faq.js
@@ -4,9 +4,29 @@ document.addEventListener( 'DOMContentLoaded', function ()
 {
     function toggle( event )
     {
-		event.currentTarget.classList.toggle( 'legal-active' );
+		// event.currentTarget.classList.toggle( 'legal-active' );
 
-		event.currentTarget.nextElementSibling.classList.toggle( 'legal-active' );
+		// event.currentTarget.nextElementSibling.classList.toggle( 'legal-active' );
+
+		let content = event.currentTarget.nextElementSibling;
+
+		if ( !content.classList.contains( 'legal-active' )) {
+			content.classList.add('legal-visibility');    
+
+			content.addEventListener( 'transitionend', function( e ) {
+				content.classList.add( 'legal-active' );
+			}, {
+				capture: false,
+				once: true,
+				passive: false
+			} );
+		} else {
+			content.classList.remove( 'legal-active' );
+
+			setTimeout(function () {
+				content.classList.remove( 'legal-visibility' );
+			}, 20);
+		}
     }
 
 	document.querySelectorAll( '.tcb-post-content > .faq' ).forEach( function ( faq ) {
