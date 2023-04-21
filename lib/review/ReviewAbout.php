1c7228fa17e89064a9aa1b5754dd8c98c2e943ee
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-21 14:09:19 +0300

    review-overview

--- lib/review/ReviewAbout.php
+++ lib/review/ReviewAbout.php
@@ -21,6 +21,10 @@ class ReviewAbout
 
         add_shortcode( 'legal-about', [ $handler, 'render' ] );
 
+        // [legal-button]
+
+        add_shortcode( 'legal-button', [ $handler, 'render_button' ] );
+
         add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
     }
 
@@ -72,6 +76,34 @@ class ReviewAbout
         ob_start();
 
         load_template( self::TEMPLATE, false, self::get( $args ) );
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+
+    public static function get( $args )
+    {
+        $group = get_field( self::FIELD );
+        
+        if( $group ) {
+            return [
+                'href' => $group[ 'about-afillate' ],
+
+                'text' => __( 'Bet here', ToolLoco::TEXTDOMAIN ),
+            ];
+        }
+
+        return [];
+    }
+
+    const TEMPLATE_BUTTON = LegalMain::LEGAL_PATH . '/template-parts/review/review-button.php';
+
+    public static function render( $args )
+    {
+        ob_start();
+
+        load_template( self::TEMPLATE_BUTTON, false, self::get_button( $args ) );
 
         $output = ob_get_clean();
 
---
+++ template-parts/review/review-button.php
@@ -0,0 +1,3 @@
+<a href="<?php echo $args[ 'href' ]; ?>" class="legal-afillate" target="_blank" rel="nofollow">
+	<?php echo $args[ 'text' ]; ?>
+</a>
