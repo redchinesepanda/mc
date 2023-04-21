aa6d31f9f7f27445dcc00a99c9b87a4fddb85558
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-21 16:04:35 +0300

    review-overview

--- template-parts/review/review-pros-cons.php
+++ template-parts/review/review-pros-cons.php
@@ -1,8 +1,8 @@
 <?php if( !empty( $args ) ) : ?>
     <div class="legal-pros-cons">
         <?php foreach( $args as $item ) : ?>
-            <div class="pros-cons-item">
-                <div class="pros-cons-title <?php echo $item[ 'type' ]; ?>">
+            <div class="pros-cons-item <?php echo $item[ 'type' ]; ?>">
+                <div class="pros-cons-title">
                     <?php echo $item[ 'title' ]; ?>
                 </div>
                 <div class="pros-cons-description">
