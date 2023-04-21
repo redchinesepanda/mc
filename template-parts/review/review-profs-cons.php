0d1a40e3b87822acf9a728eebcd889f9729e91d8
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-21 15:40:58 +0300

    review-overview

--- template-parts/review/review-profs-cons.php
+++ template-parts/review/review-profs-cons.php
@@ -1,11 +1,13 @@
 <?php if( !empty( $args ) ) : ?>
     <div class="legal-profs-cons">
         <?php foreach( $args as $item ) : ?>
-            <div class="profs-cons-title <?php echo $item[ 'type' ]; ?>">
-                <?php echo $item[ 'title' ]; ?>
-            </div>
-            <div class="profs-cons-description">
-                <?php echo $item[ 'description' ]; ?>
+            <div class="profs-cons-item">
+                <div class="profs-cons-title <?php echo $item[ 'type' ]; ?>">
+                    <?php echo $item[ 'title' ]; ?>
+                </div>
+                <div class="profs-cons-description">
+                    <?php echo $item[ 'description' ]; ?>
+                </div>
             </div>
         <?php endforeach; ?>
     </div>
