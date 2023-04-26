3e8eb68f760e0901ba803f9772906238cf11ae51
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-26 09:56:13 +0300

    review-overview

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -32,6 +32,8 @@ class ReviewBonus
 		$nodes = $finder->query( '//*[contains(@class, ' . self::BONUS_CLASS[ 'bonus' ] . ')]' );
 
 		LegalDebug::debug( [
+			'class' => self::BONUS_CLASS[ 'bonus' ],
+			
 			'$nodes' => $nodes,
 		] );
 
