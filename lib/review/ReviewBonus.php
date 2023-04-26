5eaea1b512e2ff099c7af5f8c88c006af04855b5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-26 11:26:20 +0300

    review-overview

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -25,7 +25,7 @@ class ReviewBonus
 	{
 		$dom = new DOMDocument();
 
-		$dom->loadHTML( $content, [ LIBXML_HTML_NODEFDTD, LIBXML_HTML_NOIMPLIED ] );
+		$dom->loadHTML( $content, [ LIBXML_HTML_NOIMPLIED ] );
 
 		$xpath = new DOMXPath( $dom );
 
