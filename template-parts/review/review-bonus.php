879b14903647c7ea024daf22e68e579b95de1458
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-26 13:11:06 +0300

    review-overview

--- template-parts/review/review-bonus.php
+++ template-parts/review/review-bonus.php
@@ -1,17 +1,17 @@
-<div class="bonus-title">
-	<?php if ( !empty( $args[ 'title' ] ) ) : ?>
+<?php if ( !empty( $args[ 'title' ] ) ) : ?>
+	<div class="bonus-title">
 		<?php echo $args[ 'title' ]; ?>
-	<?php endif; ?>
-</div>
-<div class="bonus-description">
-	<?php if ( !empty( $args[ 'description' ] ) ) : ?>
+	</div>
+<?php endif; ?>
+<?php if ( !empty( $args[ 'description' ] ) ) : ?>
+	<div class="bonus-description">
 		<?php echo $args[ 'description' ]; ?>
-	<?php endif; ?>
-</div>
-<div class="bonus-content">
-	<?php if ( !empty( $args[ 'content' ] ) ) : ?>
+	</div>
+<?php endif; ?>
+<?php if ( !empty( $args[ 'content' ] ) ) : ?>
+	<div class="bonus-content">
 		<?php foreach ( $args[ 'content' ] as $item ) : ?>
 			<?php echo $item; ?>
 		<?php endforeach; ?>
-	<?php endif; ?>
-</div>
+	</div>
+<?php endif; ?>
