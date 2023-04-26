50b878b932021b6cd898c63d39de1c7f30ae9307
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-26 11:08:24 +0300

    review-overview

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -31,7 +31,7 @@ class ReviewBonus
 
 		// $rawData = $xpath->query('//div[@id=\'products\']/ul/li[contains(@class, \'product\')]');
 
-		$expression = '//p[contains(@class, \'' . self::BONUS_CLASS[ 'bonus' ] . '\')]';
+		$expression = '//p[contains(@class, \'legal-bonus\')]';
 
 		$nodes = $xpath->query( $expression );
 
