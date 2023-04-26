8781a7431aba49e670a26904664a8f50a3e4f46f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-26 13:08:43 +0300

    review-overview

--- template-parts/review/review-bonus.php
+++ template-parts/review/review-bonus.php
@@ -1,3 +1,13 @@
 <div class="legal-bonus">
-	<?php echo $args[ 'value' ]; ?>
+	<?php if ( !empty( $args[ 'title' ] ) ) : ?>
+		<?php echo $args[ 'title' ]; ?>
+	<?php endif; ?>
+	<?php if ( !empty( $args[ 'description' ] ) ) : ?>
+		<?php echo $args[ 'description' ]; ?>
+	<?php endif; ?>
+	<?php if ( !empty( $args[ 'content' ] ) ) : ?>
+		<?php foreach ( $args[ 'content' ] as $item ) : ?>
+			<?php echo $item; ?>
+		<?php endforeach; ?>
+	<?php endif; ?>
 </div>
