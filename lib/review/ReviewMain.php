6e92588ec11f27bfe58bfa768efb1e420b9ca4e8
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-28 09:46:49 +0300

    review-overview

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -43,10 +43,6 @@ class ReviewMain
 
         add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
         
-        // add_action( 'save_post', [ $handler, 'character_replace' ], 10, 3 );
-
-        // add_action( 'pre_post_update', [ $handler, 'pre_post_update' ], 10, 2 );
-        
         add_filter( 'content_save_pre' , [ $handler, 'encoding' ], 10, 1);
 
         ReviewAbout::register();
@@ -66,63 +62,9 @@ class ReviewMain
         ReviewBonus::register();
     }
 
-    function encoding( $content ) {
-
-        $content = mb_convert_encoding( $content, 'HTML-ENTITIES','UTF-8' );
-
-        // wp_die( LegalDebug::debug( [
-        //     '$content1' => $content,
-
-        //     '$content2' => mb_convert_encoding( $content, 'HTML-ENTITIES','UTF-8' ),
-        // ] ) );
-    
-        return $content;
-    
-    }
-
-    // function pre_post_update( $post_id, $data )
-    // {
-    //     wp_die( LegalDebug::debug( [
-    //         '$post_id' => $post_id,
-
-    //         '$data' => $data,
-    //     ] ) );
-    // }
-
-    function character_replace( $post_id, $post, $update )
+    function encoding( $content )
     {
-        // 195, 8218, 194
-
-        // $post->post_content = str_replace( chr( 195 ).chr( 8218 ).chr( 194 ), '', $post->post_content );
-
-        // $post->post_content = preg_replace( '/[^a-z0-9$¢£€¥ ]+/ui', '', $post->post_content );
-
-
-        $post->post_content = mb_convert_encoding( $post->post_content, 'HTML-ENTITIES','UTF-8' );
-
-        // $handler = new self();
-
-        // remove_action( 'save_post', [ $handler, 'character_replace' ] );
-
-		// обновляем запись. В это время срабатывает событие save_post
-		// wp_update_post( $post );
-
-		// Ставим хук обратно
-		// add_action( 'save_post', [ $handler, 'character_replace' ] );
-
-        // wp_die( LegalDebug::debug( [
-        //     '$post->post_content' => $post->post_content,
-        // ] ) );
-
-        // wp_die( LegalDebug::debug( [
-        //     'iconv1' => iconv( 'ISO-8859-1', 'UTF-8', $post->post_content ),
-
-        //     'iconv2' => iconv( 'UTF-8', 'ISO-8859-1', $post->post_content ),
-
-        //     'mb_convert_encoding1' => mb_convert_encoding( $post->post_content, 'UTF-8', 'ISO-8859-1' ),
-
-        //     'mb_convert_encoding2' => mb_convert_encoding( $post->post_content, 'HTML-ENTITIES','UTF-8' ),
-        // ] ) );
+        return mb_convert_encoding( $content, 'HTML-ENTITIES','UTF-8' );
     }
 }
 
