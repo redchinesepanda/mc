f4d723387982ad819a6d46962224a67dede1e70a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-04-28 15:38:13 +0300

    review-overview

--- lib/review/ReviewAnchors.php
+++ lib/review/ReviewAnchors.php
@@ -27,7 +27,11 @@ class ReviewAnchors
     
         // acf/load_value/name={$field_name} - filter for a specific value load based on it's field name
 
-        add_filter('acf/load_value/key=review-anchors', [ $handler, 'set_repeater' ], 10, 3);
+        add_filter( 'acf/load_value/key=review-anchors', [ $handler, 'set_repeater' ], 10, 3 );
+
+        LegalDebug::debug( [
+            'ReviewAnchors' => 'register',
+        ] );
     }
 
     const TERM = 'bookmaker-review';
