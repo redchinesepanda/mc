5ab113e5de66290b7c5bb10bb8c8af516a8cdf82
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-02 12:04:39 +0300

    review-overview

--- template-parts/review/review-anchors.php
+++ template-parts/review/review-anchors.php
@@ -10,10 +10,10 @@ LegalDebug::debug( [
         <div class="review-anchors">
             <div class="anchors-label"><?php echo $args[ 'title' ]; ?></div>
             <?php foreach( $args[ 'items' ] as $item ) : ?>
-                <?php echo $item[ 'href' ]; ?>
-                <!-- <a class="anchors-item" href="<?php echo $item[ 'href' ]; ?>"> -->
+                <!-- <?php echo $item[ 'href' ]; ?> -->
+                <a class="anchors-item" href="<?php echo $item[ 'href' ]; ?>">
                     <?php echo $item[ 'label' ]; ?>
-                <!-- </a> -->
+                </a>
             <?php endforeach; ?>
         </div>
     </nav>
