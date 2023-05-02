e49d8b8d2f1f0f184256856e501460b25ed5b379
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-02 17:00:51 +0300

    review-overview

--- lib/review/ReviewProsCons.php
+++ lib/review/ReviewProsCons.php
@@ -99,13 +99,13 @@ class ReviewProsCons
 			}
 
             if ( $permission_last ) {
-                // $node->parentNode->replaceChild( $container, $node );
+                $node->parentNode->replaceChild( $container, $node );
                 
-                $node->insertBefore( $container );
-            }
-            // } else {
-                $node->parentNode->removeChild( $node );
+                // $node->insertBefore( $container );
             // }
+            } else {
+                $node->parentNode->removeChild( $node );
+            }
 
 			if ( $permission_title ) {
 
