87ad30c785a01e15400ba9186d33c6ac6d3a8e02
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-03 14:11:31 +0300

    review-overview

--- lib/oops/OopsMain.php
+++ lib/oops/OopsMain.php
@@ -2,55 +2,99 @@
 
 class OopsMain
 {
-    public static function check_oops()
+    public static function get_args()
     {
-        $current_language = ' ' . WPMLMain::current_language();
-
-        // LegalDebug::debug( [
-        //     '$current_language' => $current_language,
-        // ] );
-
-        $args = [
+        return [
             'numberposts' => -1,
-
-            // 'post_type' => 'legal_billet',
             
             'post_type' => 'affiliate-links',
 
             'suppress_filters' => 0,
 
-            's' => $current_language,
+            's' => ' ' . WPMLMain::current_language(),
 
             'sentence' => true,
+        ];
+    }
 
-            // 'meta_query' => [
-            //     [
-            //         'key' => 'billet-referal',
+    public static function get()
+    {
+        $posts = self::get_posts();
 
-            //         'value' => '',
+        $args = [
+            'title' => __( 'Ouch', ToolLoco::TEXTDOMAIN ) . '!',
 
-            //         'meta_compare' => '!=',
-            //     ],
-            // ]
+            'description' => __( "This bookie doesn't pay for the referral program. But here are the offers of Match.Center partners", ToolLoco::TEXTDOMAIN ) . ':',
+
+            'items' => [],
+            
+            'label' => __( 'Bet Now', ToolLoco::TEXTDOMAIN ),
+
         ];
 
-        $posts = new WP_Query( $args );
+        foreach ( $posts as $post ) {
+            $args['items'][] = [
+                'src' => '',
+
+                'href' => get_post_permalink( $post->ID ),
+            ];
+        }
+
+        return $args;
+    }
+
+    public static function get_posts()
+    {
+        $query = new WP_Query( self::get_args() );
         
-        return $posts->found_posts;
+        return $query->posts;
+    }
+
+    public static function check_oops()
+    {
+        $query = new WP_Query( self::get_args() );
+        
+        return $query->found_posts;
+    }
+
+    const TEMPLATE = [
+        'oops' => LegalMain::LEGAL_PATH . '/template-parts/oops/oops.php',
+    ];
+
+    public static function render_check()
+    {
+        if ( self::check_oops() ) {
+            self::render_button();
+        }
+    }
+
+    public static function render_button()
+    {
+        ob_start();
+
+        load_template( self::TEMPLATE[ 'oops' ], false, self::get() );
+
+        $output = ob_get_clean();
+
+        return $output;
     }
 
     public static function register()
     {
         $handler = new self();
 
-        add_action( 'edit_form_before_permalink', [ $handler, 'wp_kama_edit_form_before_permalink_action' ] );
+        // [legal-oops]
+
+        add_shortcode( 'legal-oops', [ $handler, 'render_check' ] );
+
+        // add_action( 'edit_form_before_permalink', [ $handler, 'wp_kama_edit_form_before_permalink_action' ] );
     }
 
-    public static function wp_kama_edit_form_before_permalink_action( $post ){
-        LegalDebug::debug( [
-            'get_post_meta' => get_post_meta( $post->ID ),
-        ] );
-    }
+    // public static function wp_kama_edit_form_before_permalink_action( $post ){
+    //     LegalDebug::debug( [
+    //         'get_post_meta' => get_post_meta( $post->ID ),
+    //     ] );
+    // }
 }
 
 ?>
---
+++ template-parts/oops/oops.php
@@ -0,0 +1,12 @@
+<div class="legal-oops">
+	<div class="oops-title"><?php echo $args[ 'title' ]; ?></div>
+	<div class="oops-description"><?php echo $args[ 'description' ]; ?></div>
+	<?php foreach( $args[ 'items' ] as $item ) : ?>
+		<div class="oops-item">
+			<img class="item-image" src="<?php echo $args[ 'src' ]; ?>" width="88" height="29" />
+			<a class="item-button" href="<?php echo $item[ 'href' ]; ?>">
+				<?php echo $args[ 'label' ]; ?>
+			</a>
+		</div>
+	<?php endforeach; ?>
+</div>
