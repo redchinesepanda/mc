9d8b1efc813664aead8f0c2e5218ba3a2a722438
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-03 11:43:54 +0300

    review-overview

--- lib/review/ReviewGallery.php
+++ lib/review/ReviewGallery.php
@@ -89,14 +89,14 @@ class ReviewGallery
 
         LegalDebug::debug( [
             'function' => 'wp_kama_calculate_image_srcset_filter',
-            
+
             '$sources' => $sources,
             
             '$size_array' => $size_array,
 
             '$image_src' => $image_src,
 
-            '$image_meta' => $image_meta,
+            // '$image_meta' => $image_meta,
             
             '$attachment_id' => $attachment_id,
         ] );
