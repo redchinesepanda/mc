189ac501b85d4e83350ef2b288fdc48bfc1218cb
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-04 13:31:35 +0300

    review-overview

--- template-parts/review/review-gallery.php
+++ template-parts/review/review-gallery.php
@@ -1,6 +1,6 @@
-<?php if( !empty( $images ) ): ?>
+<?php if( !empty( $args ) ): ?>
     <div class="legal-imageset">
-        <?php foreach( $images as $image ): ?>
+        <?php foreach( $args as $image ): ?>
             <div class="imageset-item">
                 <img class="item-image" src="<?php echo $image[ 'src' ]; ?>" alt="<?php echo $image[ 'alt' ]; ?>" width="<?php echo $image[ 'width' ]; ?>" height="<?php echo $image[ 'height' ]; ?>" />
                 <div class="item-caption"><?php echo esc_html( $image[ 'caption' ] ); ?></div>
