00602c553f20df1d7d85a6c26b3c1ca85564cb18
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-15 09:33:41 +0300

    review-overview

--- lib/review/ReviewHowTo.php
+++ lib/review/ReviewHowTo.php
@@ -35,64 +35,94 @@ class ReviewHowTo
 						[
 							"@type" => "HowToDirection",
 
+							"position" => "1",
+
 							"text" => "Successfully verify your account with the provider",
 						],
 					],
 				],
 
 				[
-					"@type" => "HowToSection",
+					"@type" => "HowToStep",
 
 					"name" => "Before placing your first bet, make a minimum deposit of £10 within 7 days of registering, using a debit card. Note: payment restrictions apply. ",
 
 					"position" => "3",
 
 					"itemListElement" => [
-					  	[
-							"@type" => "HowToStep",
+						[
+							"@type" => "HowToDirection",
 
 							"position" => "1",
 
-							"itemListElement" => [
-								[
-									"@type" => "HowToDirection",
-
-									"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar nibh id nibh molestie, scelerisque interdum metus venenatis"
-								],
-							]
+							"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar nibh id nibh molestie, scelerisque interdum metus venenatis.",
 						],
+					],
 
+					"itemListElement" => [
 						[
-							"@type" => "HowToStep",
+							"@type" => "HowToDirection",
 
 							"position" => "2",
 
-							"itemListElement" => [
-								[
-									"@type" => "HowToDirection",
-
-									"text" => "Nam pellentesque eu nisl id congue."
-								],
-							]
+							"text" => "Nam pellentesque eu nisl id congue.",
 						],
-
-						[
-							"@type" => "HowToStep",
-
-							"position" => "3",
-
-							"text" => "Maecenas in vulputate ipsum.",
-
-							// "itemListElement" => [
-							// 	[
-							// 		"@type" => "HowToDirection",
-
-							// 		"text" => "Maecenas in vulputate ipsum."
-							// 	]
-							// ]
-						],
-					]
+					],
 				],
+
+				// [
+				// 	"@type" => "HowToSection",
+
+				// 	"name" => "Before placing your first bet, make a minimum deposit of £10 within 7 days of registering, using a debit card. Note: payment restrictions apply. ",
+
+				// 	"position" => "3",
+
+				// 	"itemListElement" => [
+				// 	  	[
+				// 			"@type" => "HowToStep",
+
+				// 			"position" => "1",
+
+				// 			"itemListElement" => [
+				// 				[
+				// 					"@type" => "HowToDirection",
+
+				// 					"text" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar nibh id nibh molestie, scelerisque interdum metus venenatis"
+				// 				],
+				// 			]
+				// 		],
+
+				// 		[
+				// 			"@type" => "HowToStep",
+
+				// 			"position" => "2",
+
+				// 			"itemListElement" => [
+				// 				[
+				// 					"@type" => "HowToDirection",
+
+				// 					"text" => "Nam pellentesque eu nisl id congue."
+				// 				],
+				// 			]
+				// 		],
+
+				// 		[
+				// 			"@type" => "HowToStep",
+
+				// 			"position" => "3",
+
+				// 			"text" => "Maecenas in vulputate ipsum.",
+
+				// 			// "itemListElement" => [
+				// 			// 	[
+				// 			// 		"@type" => "HowToDirection",
+
+				// 			// 		"text" => "Maecenas in vulputate ipsum."
+				// 			// 	]
+				// 			// ]
+				// 		],
+				// 	]
+				// ],
 
 			// 	[
 			// 		"@type" => "HowToStep",
