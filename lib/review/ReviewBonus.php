cd18372cd255dbfbb8465e7ff2cc0e96af6fd68e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-23 15:33:39 +0300

    schema author

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -68,6 +68,10 @@ class ReviewBonus
 			return $content;
 		}
 
+		LegalDebug::debug( [
+			'content' => ( !empty( $content ) ? 'set' : 'unset' ),
+		] );
+
 		$dom = LegalDOM::get_dom( $content );
 
 		$nodes = self::get_nodes( $dom );
