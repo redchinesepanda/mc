2f12d37644d41554063af721be387ad833e57db2
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-23 18:06:44 +0300

    schema author

--- lib/LegalDebug.php
+++ lib/LegalDebug.php
@@ -4,7 +4,19 @@ class LegalDebug
 {
     public static function debug( $message )
     {
-        if( current_user_can( 'administrator' ) ) {
+        $permission = false;
+
+        $current_user = wp_get_current_user();
+
+        if( $current_user->exists() ){
+            if ( $current_user->user_login == 'redchinesepanda' ) {
+                $permission = true;
+            }
+        }
+
+        // if( current_user_can( 'administrator' ) ) {
+
+        if( $permission ) {
             echo ( '<pre>' . __CLASS__ . '::debug: ' . print_r( $message, true ) . '</pre>' );
         }
         
