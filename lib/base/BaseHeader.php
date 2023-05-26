82cc3e563047239bfa38ee42dc3149fd317df576
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-26 09:03:54 +0300

    header

--- lib/base/BaseHeader.php
+++ lib/base/BaseHeader.php
@@ -7,11 +7,26 @@ class BaseHeader
         $handler = new self();
 
         add_action( 'init', [ $handler, 'location' ] );
+
+		// [legal-menu]
+
+        add_shortcode( 'legal-menu', [ $handler, 'render' ] );
     }
 	
+	const LOCATION = 'legal-main';
+
 	public static function location() {
-		register_nav_menu( 'legal-main', __( 'Legal Review BK', ToolLoco::TEXTDOMAIN ) );
+		register_nav_menu( self::LOCATION, __( 'Legal Review BK', ToolLoco::TEXTDOMAIN ) );
 	}
+
+	public static function render() {
+		return wp_nav_menu( [
+			'theme_location' => self::LOCATION,
+
+			'echo' => fasle,
+		] );
+	}
+	
 }
 
 ?>
