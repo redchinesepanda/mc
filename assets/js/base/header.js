7aa00e5b174091e0401ec8e8d1ba18447ee60398
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-29 13:41:00 +0300

    header

---
+++ assets/js/base/header.js
@@ -0,0 +1,31 @@
+// header-js start
+
+document.addEventListener( 'DOMContentLoaded', function ()
+{
+    function toggleBlock( event )
+	{
+		event.preventDefault();
+
+		let element = event.currentTarget;
+
+		element.classList.toggle( 'legal-active' );
+		
+		// let oops = document.querySelector( '.legal-oops-background');
+
+		// if ( !oops.contains( event.currentTarget ) || oops == event.target ) {
+			
+		// }
+	}
+
+    document.querySelectorAll( '.legal-menu .menu-item').forEach( function ( element ) {
+		element.addEventListener( 'click', toggleBlock, false );
+	} );
+
+	// let oops = document.querySelector( '.legal-oops-background');
+
+	// if ( oops !== null ) {
+	// 	oops.addEventListener( 'click', toggleBlock, false );
+	// }
+} );
+
+// header-js end
