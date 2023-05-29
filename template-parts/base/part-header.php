8c0da6997967a32c23c04abf4b8e378742d08966
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-29 12:42:28 +0300

    header

--- template-parts/base/part-header.php
+++ template-parts/base/part-header.php
@@ -1,7 +1,7 @@
 <?php if ( !empty( $args ) ) : ?>
 	<div class="legal-header-menu">
-		<input id="toggle-state" type="checkbox" />
-		<label class="toggle-button" for="toggle-state"></label>
+		<!-- <input id="toggle-state" type="checkbox" />
+		<label class="toggle-button" for="toggle-state"></label> -->
 		<?php echo $args; ?>
 	</div>
 <?php endif; ?>
