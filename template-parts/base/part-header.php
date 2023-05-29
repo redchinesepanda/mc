0f0777ce4e1c34f13e81c757649239162a8f208d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-29 15:59:50 +0300

    header

--- template-parts/base/part-header.php
+++ template-parts/base/part-header.php
@@ -1,6 +1,6 @@
 <?php if ( !empty( $args ) ) : ?>
 	<div class="legal-header">
-		<a href="/">
+		<a class="legal-logo" href="/">
 			<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center UK" />
 		</a>
 		<div class="legal-header-control">
