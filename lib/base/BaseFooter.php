3120d54b8b30281e1a5e5984219c3749e9c04bc7
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-30 15:36:55 +0300

    header

--- lib/base/BaseFooter.php
+++ lib/base/BaseFooter.php
@@ -75,11 +75,11 @@ class BaseFooter
 
 			// 'menu_items' => $menu_items,
 
-			'menu_item_parents' => $menu_item_parents,
-
-			'items' => $items,
+			// 'menu_item_parents' => $menu_item_parents,
 			
 			'parents_top' => $parents_top,
+
+			'items' => $items,
 		] );
 
 		$items = [];
