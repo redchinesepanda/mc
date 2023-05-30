29f7ac5899be7a7bb217b2f42fc21abfd19969da
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-30 16:10:02 +0300

    header

--- template-parts/base/part-footer-main.php
+++ template-parts/base/part-footer-main.php
@@ -8,17 +8,23 @@ LegalDebug::debug( [
 <?php if ( !empty( $args ) ) : ?>
 	<div class="legal-footer">
 		<div class="footer-menu">
+			<?php foreach( $args[ 'items' ] as $item ) : ?>
+				<?php echo BaseFooter::render_item( $item ); ?>
+			<?php endforeach; ?>
 		</div>
 		<div class="footer-logo">
 			<a class="logo-item" href="/">
 				<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center UK" />
 			</a>
 		</div>
-		<div class="footer-copy">2021-2023 Match.Center © All rights reserved.</div>
+		<div class="footer-copy">
+			<?php echo $args[ 'copy' ][ 'year' ]; ?> <?php echo $args[ 'copy' ][ 'company' ]; ?> © <?php echo $args[ 'copy' ][ 'reserved' ]; ?>.
+		</div>
 		<?php echo WPMLLangSwitcher::render(); ?>
 		<div class="footer-text">
-			<p>Match.Center is not a gambling operator (we do not accept any bets). The content of this website is strictly for information purposes and does not constitute advice. We only review gambling operators who are licenced by their respective local and international regulators. We only claim information to be correct at the time of posting.</p>
-			<p>Always gamble responsibly and never risk money that you can not afford to lose!</p>
+			<?php foreach( $args[ 'text' ] as $text ) : ?>
+				<p><?php echo $text; ?></p>
+			<?php endforeach; ?>
 		</div>
 	</div>
 <?php endif; ?>
