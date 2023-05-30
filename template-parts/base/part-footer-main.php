1434fd99e8fa6802dd6ee80b05d4ad90f424b5d7
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-30 16:12:40 +0300

    header

--- template-parts/base/part-footer-main.php
+++ template-parts/base/part-footer-main.php
@@ -1,17 +1,12 @@
-<?php
-
-LegalDebug::debug( [
-	'args' => $args,
-] );
-
-?>
 <?php if ( !empty( $args ) ) : ?>
 	<div class="legal-footer">
-		<div class="footer-menu">
-			<?php foreach( $args[ 'items' ] as $item ) : ?>
-				<?php echo BaseFooter::render_item( $item ); ?>
-			<?php endforeach; ?>
-		</div>
+		<?php if ( !empty( $args[ 'items' ] ) ) : ?>
+			<div class="footer-menu">
+				<?php foreach( $args[ 'items' ] as $item ) : ?>
+					<?php echo BaseFooter::render_item( $item ); ?>
+				<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
 		<div class="footer-logo">
 			<a class="logo-item" href="/">
 				<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center UK" />
