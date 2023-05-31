2156f41ff1f9c744be29f1f8a5c554530be0ac9e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-31 14:54:53 +0300

    header

--- lib/base/BaseFooter.php
+++ lib/base/BaseFooter.php
@@ -162,9 +162,9 @@ class BaseFooter
 
 		$end = array_splice( $items, -2 );
 
-		$text_default = 'Match.Center is not a gambling operator (we do not accept any bets). The content of this website is strictly for information purposes and does not constitute advice. We only review gambling operators who are licenced by their respective local and international regulators. We only claim information to be correct at the time of posting.
+		$text_default = "Match.Center is not a gambling operator (we do not accept any bets). The content of this website is strictly for information purposes and does not constitute advice. We only review gambling operators who are licenced by their respective local and international regulators. We only claim information to be correct at the time of posting.
 
-		Always gamble responsibly and never risk money that you can not afford to lose!';
+		Always gamble responsibly and never risk money that you can not afford to lose!";
 
 		return  [
 			'end' => $end,
