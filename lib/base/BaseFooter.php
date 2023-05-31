8de8211b1b8ffa55a1d31c94fafb16d1fc9c63c2
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-05-31 14:50:06 +0300

    header

--- lib/base/BaseFooter.php
+++ lib/base/BaseFooter.php
@@ -181,13 +181,7 @@ class BaseFooter
 				'reserved' => 'All rights reserved'
 			],
 
-			'text' => __( $text_default, ToolLoco::TEXTDOMAIN ),
-
-			// 'text' => [
-			// 	'Match.Center is not a gambling operator (we do not accept any bets). The content of this website is strictly for information purposes and does not constitute advice. We only review gambling operators who are licenced by their respective local and international regulators. We only claim information to be correct at the time of posting.',
-
-			// 	'Always gamble responsibly and never risk money that you can not afford to lose!'
-			// ],
+			'text' => wpautop( __( $text_default, ToolLoco::TEXTDOMAIN ) ),
 		];
 	}
 
