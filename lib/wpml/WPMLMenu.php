99c367d50525ee21ef6337e476697a30c9ef45d1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-01 14:28:46 +0300

    header

--- lib/wpml/WPMLMenu.php
+++ lib/wpml/WPMLMenu.php
@@ -7,35 +7,15 @@ class WPMLMenu
         $handler = new self();
 
 		add_action( 'pre_get_posts', [ $handler, 'wpml_custom_query' ] );
-
-		// LegalDebug::debug( [
-		// 	'function' => 'WPMLMenu::register',
-		// ] );
     }
 
 	public static function wpml_custom_query( $query )
 	{
-		// LegalDebug::debug( [
-		// 	'function' => 'WPMLMenu::wpml_custom_query',
-
-		// 	'action' => $_POST[ 'action' ],
-		// ] );
-
-		// if( is_search() ) {
-		
 		if( !empty( $_POST[ 'action' ] ) ) {
 			if( $_POST[ 'action' ] == 'menu-quick-search' ) {
 				$query->query_vars[ 'suppress_filters' ] = true;
 
 				$query->query_vars[ 'posts_per_page' ] = -1;
-
-				LegalDebug::debug( [
-					'function' => 'WPMLMenu::wpml_custom_query',
-
-					'suppress_filters' => $query->query_vars['suppress_filters'],
-
-					'query_vars' => $query->query_vars,
-				] );
 			}
 		}
 	}
