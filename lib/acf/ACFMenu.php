97799d6487b7cb5d5effcb0b10777ed5f6171bdd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-01 14:41:31 +0300

    header

---
+++ lib/acf/ACFMenu.php
@@ -0,0 +1,48 @@
+<?php
+
+class ACFMenu
+{
+    const FIELD = [
+        'class' => 'menu-item-class',
+    ];
+
+    public static function register()
+    {
+        $handler = new self();
+
+        add_filter( 'acf/load_field/name=' . self::FIELD[ 'class' ], [ $handler, 'choices' ] );
+    }
+
+    function choices( $field )
+    {
+        $langs = WPMLLangSwitcher::get_all();
+
+        LegalDebug::debug( [
+            'function' => 'ACFMenu::choices',
+
+            'langs' => $langs,
+        ] );
+
+        // $post = get_post();
+
+        // $post_id = $post->ID;
+
+        // $args = [
+        //     'post_type' => 'page',
+
+        //     'page_type' => 'bookmaker-card'
+        // ];
+
+        // $posts = get_posts( $args );
+
+        // if ( !empty( $posts ) ) {
+        //     foreach( $posts as $post ) {
+        //         $field['choices'][$post->ID] = $post->post_title; 
+        //     }
+        // }
+
+        return $field;
+    }
+}
+
+?>
