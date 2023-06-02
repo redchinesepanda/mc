8d73143409713d8aa4a8adf5999b5091c70c1a1d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-02 13:25:05 +0300

    header

--- template-parts/notfound/part-notfound-main.php
+++ template-parts/notfound/part-notfound-main.php
@@ -13,166 +13,21 @@
 		<h1 class="block-groups_title"><?php echo $args[ 'title' ]; ?></h1>
 	</div>
 	<p><?php echo $args[ 'description' ]; ?></p>
-</div>
-<div class="menu-avaible-404">
-	<?php foreach( $args['languages'] as $lang ) : ?>
-		<a class='lang-avaible lang-item' href="<?php echo $lang['href']; ?>" target="_blank">
-			<div class="lang-image locale-<?php echo $lang['id']; ?>"></div>
-			<div class="lang-title">
-				<?php echo $lang['title']; ?>
+	<div class="menu-avaible-404">
+		<?php foreach( $args['languages'] as $lang ) : ?>
+			<a class='lang-avaible lang-item' href="<?php echo $lang['href']; ?>" target="_blank">
+				<div class="lang-image locale-<?php echo $lang['id']; ?>"></div>
+				<div class="lang-title">
+					<?php echo $lang['title']; ?>
+				</div>
+			</a>
+		<?php endforeach; ?>
+		<a class='lang-current lang-item' href="#" target="_blank">
+			<div class="lang-image locale-<?php echo $args['active']['id']; ?>"></div>
+				<div class="lang-title">
+					<?php echo $args['active']['title']; ?>
+				</div>
 			</div>
 		</a>
-	<?php endforeach; ?>
-	<a class='lang-current lang-item' href="#" target="_blank">
-		<div class="lang-image locale-<?php echo $args['active']['id']; ?>"></div>
-			<div class="lang-title">
-				<?php echo $args['active']['title']; ?>
-			</div>
-		</div>
-	</a>
-	<!-- <a class="lang-item" href="https://match.center/se/" target="_blank">
-		<div class="lang-image locale-88"></div>
-		<div class="lang-title">
-			Sverige </div>
-	</a>
-	<a class="lang-item" href="https://match.center/at/" target="_blank">
-		<div class="lang-image locale-87"></div>
-		<div class="lang-title">
-			Österreich </div>
-	</a>
-	<a class="lang-item" href="https://match.center/pt/" target="_blank">
-		<div class="lang-image locale-86"></div>
-		<div class="lang-title">
-			Portugal </div>
-	</a>
-	<a class="lang-item" href="https://match.center/br/" target="_blank">
-		<div class="lang-image locale-85"></div>
-		<div class="lang-title">
-			Brasil </div>
-	</a>
-	<a class="lang-item" href="https://match.center/rs/" target="_blank">
-		<div class="lang-image locale-90"></div>
-		<div class="lang-title">
-			Republika Srbija </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ph/" target="_blank">
-		<div class="lang-image locale-84"></div>
-		<div class="lang-title">
-			Philippines </div>
-	</a>
-	<a class="lang-item" href="https://match.center/py/" target="_blank">
-		<div class="lang-image locale-83"></div>
-		<div class="lang-title">
-			Paraguay </div>
-	</a>
-	<a class="lang-item" href="https://match.center/co/" target="_blank">
-		<div class="lang-image locale-80"></div>
-		<div class="lang-title">
-			Colombia </div>
-	</a>
-	<a class="lang-item" href="https://match.center/pe/" target="_blank">
-		<div class="lang-image locale-79"></div>
-		<div class="lang-title">
-			Perú </div>
-	</a>
-	<a class="lang-item" href="https://match.center/cz/" target="_blank">
-		<div class="lang-image locale-89"></div>
-		<div class="lang-title">
-			Česká Republika </div>
-	</a>
-	<a class="lang-item" href="https://match.center/nz/" target="_blank">
-		<div class="lang-image locale-78"></div>
-		<div class="lang-title">
-			New Zealand </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ke/" target="_blank">
-		<div class="lang-image locale-77"></div>
-		<div class="lang-title">
-			Kenya </div>
-	</a>
-	<a class="lang-item" href="https://match.center/za/" target="_blank">
-		<div class="lang-image locale-75"></div>
-		<div class="lang-title">
-			South Africa </div>
-	</a>
-	<a class="lang-item" href="https://match.center/au/" target="_blank">
-		<div class="lang-image locale-74"></div>
-		<div class="lang-title">
-			Australia </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ua/" target="_blank">
-		<div class="lang-image locale-72"></div>
-		<div class="lang-title">
-			Україна </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ua-ru/" target="_blank">
-		<div class="lang-image locale-72"></div>
-		<div class="lang-title">
-			Украина </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ie/" target="_blank">
-		<div class="lang-image locale-71"></div>
-		<div class="lang-title">
-			Ireland </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ng/" target="_blank">
-		<div class="lang-image locale-70"></div>
-		<div class="lang-title">
-			Nigeria </div>
-	</a>
-	<a class="lang-item" href="https://match.center/mx/" target="_blank">
-		<div class="lang-image locale-69"></div>
-		<div class="lang-title">
-			México </div>
-	</a>
-	<a class="lang-item" href="https://match.center/by/" target="_blank">
-		<div class="lang-image locale-67"></div>
-		<div class="lang-title">
-			Беларусь </div>
-	</a>
-	<a class="lang-item" href="https://match.center/kz/" target="_blank">
-		<div class="lang-image locale-66"></div>
-		<div class="lang-title">
-			Казахстан </div>
-	</a>
-	<a class="lang-item" href="https://match.center/pl/" target="_blank">
-		<div class="lang-image locale-40"></div>
-		<div class="lang-title">
-			polska </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ro/" target="_blank">
-		<div class="lang-image locale-44"></div>
-		<div class="lang-title">
-			Română </div>
-	</a>
-	<a class="lang-item" href="https://match.center/mt/" target="_blank">
-		<div class="lang-image locale-34"></div>
-		<div class="lang-title">
-			Malta </div>
-	</a>
-	<a class="lang-item" href="https://match.center/hr/" target="_blank">
-		<div class="lang-image locale-22"></div>
-		<div class="lang-title">
-			Hrvatska </div>
-	</a>
-	<a class="lang-item" href="https://match.center/ca/" target="_blank">
-		<div class="lang-image locale-8"></div>
-		<div class="lang-title">
-			Canada (En) </div>
-	</a>
-	<a class="lang-item" href="https://match.center/de/" target="_blank">
-		<div class="lang-image locale-3"></div>
-		<div class="lang-title">
-			Deutschland </div>
-	</a>
-	<a class="lang-item" href="https://match.center/es/" target="_blank">
-		<div class="lang-image locale-2"></div>
-		<div class="lang-title">
-			España </div>
-	</a>
-	<a class="lang-item" href="https://match.center" target="_blank">
-		<div class="lang-image locale-1"></div>
-		<div class="lang-title">
-			United Kingdom </div>
-	</a> -->
+	</div>
 </div>
