e2d914bc9cc53447bac069050d536d6e55697fb4
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-06 17:41:08 +0300

    header auto language

--- template-parts/base/part-header.php
+++ template-parts/base/part-header.php
@@ -1,4 +1,4 @@
-<?php if ( !empty( $args ) ) : ?>
+<!-- <?php if ( !empty( $args ) ) : ?>
 	<div class="legal-header">
 		<a class="legal-logo" href="/">
 			<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center UK" />
@@ -6,5 +6,21 @@
 		<div class="legal-header-control">
 		</div>
 		<?php echo $args; ?>
+	</div>
+<?php endif; ?> -->
+<?php if ( !empty( $args ) ) : ?>
+	<div class="legal-header">
+		<a class="legal-logo" href="/">
+			<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center UK" />
+		</a>
+		<div class="legal-header-control">
+		</div>
+		<?php if ( !empty( $args[ 'items' ] ) ) : ?>
+			<div class="header-menu">
+				<?php foreach( $args[ 'items' ] as $item ) : ?>
+					<?php echo BaseFooter::render_item( $item ); ?>
+				<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
 	</div>
 <?php endif; ?>
