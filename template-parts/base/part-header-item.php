71da565dc1388371760696db341500d1c6f1d56b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-06 18:06:42 +0300

    header auto language

--- template-parts/base/part-header-item.php
+++ template-parts/base/part-header-item.php
@@ -1,17 +1,10 @@
-<?php
-
-// LegalDebug::debug( [
-// 	'args' => $args,
-// ] );
-
-?>
 <?php if ( !empty( $args ) ) : ?>
 	<div class="menu-item">
 		<a class="item-title" href="<?php echo $args[ 'href' ]; ?>">
 			<?php echo $args[ 'title' ]; ?>
 		</a>
 		<?php if ( !empty( $args[ 'children' ] ) ) : ?>
-			<div class="item-children">
+			<div class="sub-menu">
 				<?php foreach( $args[ 'children' ] as $item ) : ?>
 					<?php echo BaseFooter::render_item( $item ); ?>
 				<?php endforeach; ?>
