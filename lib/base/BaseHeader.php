4f7e1ed90bb1c86c9dee919fa8a3550dc2fd0502
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-07 14:56:39 +0300

    header auto language

--- lib/base/BaseHeader.php
+++ lib/base/BaseHeader.php
@@ -99,7 +99,7 @@ class BaseHeader
 
 		try {
 			$details = WPMLMain::get_post_language_details();
-		} catch ( $e ) {
+		} catch ( Exception $e ) {
 			LegalDebug::debug( [
 				'action' => 'catch',
 			] );
