7f202d79983d6f2f327c055988b8048d6de1d1ed
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 18:00:47 +0300

    anchors

--- template-parts/review/review-banner.php
+++ template-parts/review/review-banner.php
@@ -1,3 +1,6 @@
 <div class="banner-image">
 	<img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" width="<?php echo $args[ 'image' ][ 'width' ]; ?>" height="<?php echo $args[ 'image' ][ 'height' ]; ?>" />
+	<?php if( !empty( $args[ 'caption' ] ) ) : ?>
+		<p><?php echo $args[ 'caption' ]; ?></p>
+	<?php endif; ?>
 </div>
