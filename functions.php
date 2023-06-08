e2528d17f337c549c94529919f2597769c126c98
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 18:02:47 +0300

    anchors

--- functions.php
+++ functions.php
@@ -10,6 +10,8 @@ add_action( 'wp_enqueue_scripts', function () {
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [ $parent_style ], wp_get_theme()->get( 'Version' ) );
 } );
 
+define ( 'CAPTIONS_OFF', true );
+
 require_once( 'lib/LegalMain.php' );
 
 LegalMain::register();
