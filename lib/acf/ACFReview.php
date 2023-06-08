c30db74fd35d3595f2432454d4ac831616de2c67
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 11:14:24 +0300

    anchors

--- lib/acf/ACFReview.php
+++ lib/acf/ACFReview.php
@@ -18,32 +18,14 @@ class ACFReview
     {
         $handler = new self();
 
-        add_filter( 'acf/load_field/name=' . self::FIELD[ 'anchors' ], [ $handler, 'choices' ] );
+        add_filter( 'acf/load_field/name=' . self::FIELD[ 'anchors' ], [ $handler, 'supply_field' ] );
     }
 
-    public static function choices( $field )
+    public static function supply_field( $field )
     {
-		$field[ 'instructions' ] = 'test';
+		$anchors = ReviewAnchors::get_labels();
 
-		// LegalDebug::debug( [
-		// 	'field' => $field,
-		// ] );
-
-        // $items = WPMLTrid::get();
-
-        // if ( !empty( $items ) ) {
-        //     foreach( $items as $item ) {
-        //         $title = get_post_meta( $item->legal_element_id, self::FIELD_LABEL, true );
-
-        //         if ( $title ) {
-        //             $item->legal_title .= ' ( ' . $title . ' )';
-        //         }
-
-        //         $field['choices'][$item->legal_trid] = $item->legal_title . ' [' . $item->legal_language_codes . ']'; 
-        //     }
-        // }
-
-        // $field['default_value'] = WPMLTrid::get_trid();
+		$field[ 'instructions' ] = implode( '<br />', $anchors );;
 
         return $field;
     }
