fc691c1ba7d63d648915107af12a174165cf2f63
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 16:39:59 +0300

    anchors

--- template-parts/review/review-banner.php
+++ template-parts/review/review-banner.php
@@ -1,3 +1,3 @@
 <div class="banner-image">
-	<img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" />
+	<img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" width="<?php echo $args[ 'image' ][ 'width' ]; ?>" height="<?php echo $args[ 'image' ][ 'height' ]; ?>" />
 </div>
