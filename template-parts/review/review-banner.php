18029b663100987650946c3cf2b2ba8566d64ca1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 14:23:12 +0300

    anchors

--- template-parts/review/review-banner.php
+++ template-parts/review/review-banner.php
@@ -1,5 +1,3 @@
 <div class="banner-image">
-	<div class="image-scroll">
-		<img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" width="<?php echo $args[ 'image' ][ 'width' ]; ?>" height="<?php echo $args[ 'image' ][ 'height' ]; ?>" />
-	</div>
+	<img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" width="<?php echo $args[ 'image' ][ 'width' ]; ?>" height="<?php echo $args[ 'image' ][ 'height' ]; ?>" />
 </div>
