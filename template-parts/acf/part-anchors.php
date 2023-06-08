9aff68e1711c842a643c86038cbc7b7ced52560e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 11:33:47 +0300

    anchors

--- template-parts/acf/part-anchors.php
+++ template-parts/acf/part-anchors.php
@@ -1,15 +1,17 @@
-<div><?php echo $args[ 'title' ] ?></div>
-<?php if ( !empty( $args[ 'items' ] ) ) : ?>
-	<table>
-		<tr>
-			<td><?php echo $args[ 'id' ] ?></td>
-			<td><?php echo $args[ 'label' ] ?></td>
-		</tr>
-		<?php foreach( $args[ 'items' ] as $item ) : ?>
+<div class="legal-instructions">
+	<div class="legal-title"><?php echo $args[ 'title' ] ?></div>
+	<?php if ( !empty( $args[ 'items' ] ) ) : ?>
+		<table class="legal-anchors">
 			<tr>
-				<td><?php echo $item[ 'id' ] ?></td>
-				<td><?php echo $item[ 'label' ] ?></td>
+				<td><?php echo $args[ 'id' ] ?></td>
+				<td><?php echo $args[ 'label' ] ?></td>
 			</tr>
-		<?php endforeach; ?>
-	<table>
-<?php endif; ?>
+			<?php foreach( $args[ 'items' ] as $item ) : ?>
+				<tr>
+					<td><?php echo $item[ 'id' ] ?></td>
+					<td><?php echo $item[ 'label' ] ?></td>
+				</tr>
+			<?php endforeach; ?>
+		</table>
+	<?php endif; ?>
+</div>
