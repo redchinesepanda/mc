86562422973975563fc89c6e355049cf15641801
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 14:19:06 +0300

    anchors

---
+++ template-parts/review/review-banner-backup.php
@@ -0,0 +1,16 @@
+<div class="banner-image">
+	<img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" width="<?php echo $args[ 'image' ][ 'width' ]; ?>" height="<?php echo $args[ 'image' ][ 'height' ]; ?>" />
+</div>
+<div class="banner-info">
+	<div class="banner-title">
+		<?php echo $args[ 'title' ]; ?>
+	</div>
+	<div class="banner-description">
+		<?php echo $args[ 'description' ]; ?>
+	</div>
+	<div class="banner-terms">
+		<a class="banner-terms-link check-oops" href="<?php echo $args['terms'][ 'href' ]; ?>">
+			<?php echo $args['terms'][ 'text' ]; ?>
+		</a>
+	</div>
+</div>
