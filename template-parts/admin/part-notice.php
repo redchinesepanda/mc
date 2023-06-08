653980291f480a03ca6c3cb19b51c017ca80b3e4
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-08 12:18:41 +0300

    anchors

--- template-parts/admin/part-notice.php
+++ template-parts/admin/part-notice.php
@@ -1,5 +1,5 @@
 <div class="notice notice-info">
 	<p><b><?php echo $args[ 'message' ] ?></b></p>
-	<p><b><?php echo $args[ 'name' ] ?></b></p>
-	<p><b><?php echo $args[ 'user' ] ?></b></p>
+	<p><?php echo $args[ 'name' ] ?></p>
+	<p><?php echo $args[ 'user' ] ?></p>
 </div>
