92feb98952466b7ad91c07e6f1d4f44d22fdd44d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-14 13:13:40 +0300

    oops

--- assets/js/tools/tool-tinymce.js
+++ assets/js/tools/tool-tinymce.js
@@ -4,10 +4,12 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	const observer = new MutationObserver( function( mutations_list ) {
 		mutations_list.forEach( function( mutation ) {
 			mutation.addedNodes.forEach( function( added_node ) {
-				console.log( 'added_node.id: ' + added_node.id );
+				// console.log( 'added_node.id: ' + added_node.id );
 
 				if( added_node.id == 'mce-modal-block' ) {
 					console.log( 'child has been added' );
+
+					console.log( 'added_node: ' + JSON.stringify( added_node ) );
 
 					observer.disconnect();
 				}
