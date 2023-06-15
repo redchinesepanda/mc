7fe92d4037d823ad12d44318c7039b0edf23c4c5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-15 09:41:11 +0300

    oops

--- assets/js/tools/tool-tinymce.js
+++ assets/js/tools/tool-tinymce.js
@@ -7,7 +7,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
 				mutation.addedNodes.forEach( function ( added_node, index, listObj ) {
 					added_node.querySelectorAll( 'label' ).forEach( function ( element ) {
 						if ( element.textContent == 'ID' ) {
-							observer.disconnect();
+							// observer.disconnect();
 
 							element.nextSibling.setAttribute( 'list', 'legal-anchor-choices' );
 
@@ -34,26 +34,6 @@ document.addEventListener( 'DOMContentLoaded', function () {
 	};
 
 	const observer = new MutationObserver( callback );
-
-	// const observer = new MutationObserver( function( mutations_list ) {
-	// 	mutations_list.forEach( function( mutation ) {
-	// 		mutation.addedNodes.forEach( function( added_node ) {
-	// 			// console.log( 'added_node.id: ' + added_node.id );
-
-	// 			if( added_node.id == 'mce-modal-block' ) {
-	// 				console.log( 'child has been added' );
-
-	// 				console.log( 'added_node.id: ' + added_node.id );
-
-	// 				added_node.querySelectorAll( '.mce-abs-end' ).forEach( function ( element ) {
-	// 					console.log( 'element.id: ' + element.id );
-	// 				} );
-
-	// 				observer.disconnect();
-	// 			}
-	// 		});
-	// 	});
-	// });
 
 	observer.observe( document.querySelector( 'body' ), { subtree: false, childList: true } );
 } );
