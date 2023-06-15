924f28d73674dbc96318998776a8e539db865d4d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-15 16:49:42 +0300

    oops

--- lib/yoast/YoastOG.php
+++ lib/yoast/YoastOG.php
@@ -6,7 +6,7 @@ class YoastOG
     {
         $handler = new self();
 
-		add_action( 'wpseo_add_opengraph_images', [ $handler, 'default_opengraph_images' ] );
+		// add_action( 'wpseo_add_opengraph_images', [ $handler, 'default_opengraph_images' ] );
 
 		add_filter( 'wpseo_twitter_image', [ $handler, 'current_image' ] );
 
@@ -15,17 +15,17 @@ class YoastOG
 
 	function current_image()
 	{
-		LegalDebug::debug( [
-			'current_image' => LegalMain::LEGAL_URL . '/assets/img/yoast/preview-' . WPMLMain::current_language() . '.webp',
-		] );
+		// LegalDebug::debug( [
+		// 	'current_image' => LegalMain::LEGAL_URL . '/assets/img/yoast/preview-' . WPMLMain::current_language() . '.webp',
+		// ] );
 
 		return LegalMain::LEGAL_URL . '/assets/img/yoast/preview-' . WPMLMain::current_language() . '.webp';
 	}
 
-	function default_opengraph_images( $object )
-	{
-		$object->add_image( self::current_image() );
-	}
+	// function default_opengraph_images( $object )
+	// {
+	// 	$object->add_image( self::current_image() );
+	// }
 }
 
 ?>
