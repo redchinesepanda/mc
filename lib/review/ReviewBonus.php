c0af3c1ee649b3f7347854f3e6674e1f3c99471d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-15 14:34:10 +0300

    oops

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -6,7 +6,7 @@ class ReviewBonus
         'review-bonus' => [
 			'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-bonus.css',
 
-			'ver' => '1.0.5',
+			'ver' => '1.0.6',
 		],
 
         'review-billet' => [
