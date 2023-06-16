481a4231bda98064e5ccdb2a2094c34bd6c0f9d8
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-16 09:03:05 +0300

    anchor js

--- template-parts/notfound/part-notfound-main.php
+++ template-parts/notfound/part-notfound-main.php
@@ -15,9 +15,8 @@
 		<?php endforeach; ?>
 		<a class='lang-current lang-item' href="<?php echo $args['active']['href']; ?>" target="_blank">
 			<div class="lang-image locale-<?php echo $args['active']['id']; ?>"></div>
-				<div class="lang-title">
-					<?php echo $args['active']['title']; ?>
-				</div>
+			<div class="lang-title">
+				<?php echo $args['active']['title']; ?>
 			</div>
 		</a>
 	</div>
