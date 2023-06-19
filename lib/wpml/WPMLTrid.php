cb7a86c1a09d52f6c9b3b58cc9885534a5ddf2b0
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-19 15:17:33 +0300

    anchor js

--- lib/wpml/WPMLTrid.php
+++ lib/wpml/WPMLTrid.php
@@ -26,7 +26,7 @@ class WPMLTrid
             FROM `wp_icl_translations`
             INNER JOIN `wp_posts` ON `element_id` = `ID`
             WHERE
-                `element_type` = 'post_page'
+                `element_type` IN ( 'post_page', 'post_legal_bk_review' )
                 AND `post_status` = 'publish'
             GROUP BY `trid`
             ORDER BY `legal_title`, `legal_elements`";
