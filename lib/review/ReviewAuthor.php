672eedda9ec6966727ffea9994a16f0d56d8af6f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-19 13:41:49 +0300

    anchor js

---
+++ lib/review/ReviewAuthor.php
@@ -0,0 +1,54 @@
+<?php
+
+class ReviewAuthor
+{
+    const CSS = [
+        'review-author' => [
+            'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-author.css',
+
+            'ver'=> '1.0.0',
+        ],
+    ];
+
+    public static function register_style()
+    {
+        ReviewMain::register_style( self::CSS );
+    }
+
+    public static function register()
+    {
+        $handler = new self();
+
+        // [legal-author]
+
+        add_shortcode( 'legal-author', [ $handler, 'render' ] );
+    }
+
+    public static function get( $args )
+    {
+        return [
+			'name' => __( 'Valentin Axani', ToolLoco::TEXTDOMAIN ),
+
+			'duty' => __( 'Website Manager', ToolLoco::TEXTDOMAIN ),
+
+			'file' => 'valentin-axani.webp',
+		];
+    }
+
+    const TEMPLATE = [
+		'review-author' =>  LegalMain::LEGAL_PATH . '/template-parts/review/review-button.php',
+	];
+
+    public static function render( $args )
+    {
+        ob_start();
+
+        load_template( self::TEMPLATE_BUTTON, false, self::get() );
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+}
+
+?>
---
+++ template-parts/review/review-author.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+	.legal-author .author-photo {
+		background-image: url( '../../review/author/<?php echo $args[ 'file' ]; ?>' );
+	}
+</style>
+<div class="legal-author">
+	<div class="author-photo"></div>
+	<div class="author-data">
+		<div class="author-name"><?php echo $args[ 'name' ]; ?></div>
+		<div class="author-duty"><?php echo $args[ 'duty' ]; ?></div>
+	</div>
+</div>
