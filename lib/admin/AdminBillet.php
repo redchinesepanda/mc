25df71198e5f00c61b05f35d14e298305b4d667a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-21 14:43:31 +0300

    compilation shortcode

--- lib/admin/AdminBillet.php
+++ lib/admin/AdminBillet.php
@@ -2,11 +2,9 @@
 
 class AdminBillet
 {
-    const POST_TYPE = 'legal_billet';
-
-    const META_KEY = '_wp_page_template';
-
-    const TEMPLATE = 'single-legal_billet.php';
+    const META_KEY = [
+        'page-template' => '_wp_page_template',
+    ];
 
     const META_VALUE = [
         'legal_billet' => 'single-legal_billet.php',
@@ -14,23 +12,20 @@ class AdminBillet
         'legal_compilation' => 'template-compilation.php',
     ];
 
+    // При создании нового поста POST_TYPE будет выбран шаблон TEMPLATE в блоке Post Attributes > Template
+
     public static function register()
     {
         $handler = new self();
 
-        // При создании нового поста POST_TYPE будет выбран шаблон TEMPLATE в блоке Post Attributes > Template
-
-        add_action( 'wp_insert_post', [ $handler, 'meta_template' ], 10, 3 );
+        // add_action( 'wp_insert_post', [ $handler, 'meta_template' ], 10, 3 );
     }
 
-    function meta_template( $post_ID, $post, $update ) {
+    function meta_template( $post_ID, $post, $update )
+    {
         if ( !empty( self::META_VALUE[ $post->post_type ] ) ) {
-            update_post_meta( $post_ID, self::META_KEY, self::META_VALUE[ $post->post_type ] );
+            update_post_meta( $post_ID, self::META_KEY[ 'page-template' ], self::META_VALUE[ $post->post_type ] );
         }
-
-        // if ( $post->post_type == self::POST_TYPE ) {
-        //     update_post_meta( $post_ID, self::META_KEY, self::TEMPLATE );
-        // }
     }
 }
 
