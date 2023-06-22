662b197744b558a5c08456b132f27d837e84a5bc
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-22 15:41:10 +0300

    compilation shortcode

--- lib/acf/ACFBillet.php
+++ lib/acf/ACFBillet.php
@@ -4,16 +4,20 @@ class ACFBillet
 {
 	const FIELD = [
         'icon' => 'billet-list-part-icon',
+
+        'direction' => 'billet-list-part-direction',
     ];
 
     public static function register()
     {
         $handler = new self();
 
-        add_filter( 'acf/load_field/name=' . self::FIELD[ 'icon' ], [ $handler, 'choices' ] );
+        add_filter( 'acf/load_field/name=' . self::FIELD[ 'icon' ], [ $handler, 'choices_icon' ] );
+
+        add_filter( 'acf/load_field/name=' . self::FIELD[ 'direction' ], [ $handler, 'choices_direction' ] );
     }
 
-    function choices( $field )
+    function choices_icon( $field )
     {
         $choices[ 'legal-default' ] = 'Без маркера';
 
@@ -28,6 +32,17 @@ class ACFBillet
         $choices[ 'legal-minus' ] = 'Минус';
 
         $choices[ 'legal-triangle' ] = 'Треугольник';
+
+        $field[ 'choices' ] = $choices;
+
+        return $field;
+    }
+
+    function choices_icon( $field )
+    {
+        $choices[ 'legal-row' ] = 'Строка';
+
+        $choices[ 'legal-column' ] = 'Столбец';
 
         $field[ 'choices' ] = $choices;
 
