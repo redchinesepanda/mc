0784de509fc885edcc2e2d3ce6299b1dad2bd691
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-22 13:08:42 +0300

    compilation shortcode

--- lib/base/BaseHeader.php
+++ lib/base/BaseHeader.php
@@ -159,12 +159,10 @@ class BaseHeader
 		$code = WPMLMain::current_language();
 		
 		$search[ 'avaible' ] = WPMLMain::search_language();
+		
+		$search[ 'current' ] = $search[ 'avaible' ][ $code ];
 
-		// if ( !empty( $search[ 'avaible' ][ $code ] ) ) {
-			$search[ 'current' ] = $search[ 'avaible' ][ $code ];
-
-			unset( $search[ 'avaible' ][ $code ] );
-		// }
+		unset( $search[ 'avaible' ][ $code ] );
 
 		$parse = self::parse_languages( $search );
 
