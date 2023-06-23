1b1d748e014588d95f448888e5cfaed82e3b7de8
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-23 11:57:30 +0300

    compilation shortcode

--- lib/tools/ToolSitemap.php
+++ lib/tools/ToolSitemap.php
@@ -11,8 +11,20 @@ class ToolSitemap
 		add_shortcode( 'legal-sitemap', [ $handler, 'render' ] );
     }
 
-	public static function get_args( $post_type )
+	public static function get_args( $post_type, $tax_query )
     {
+		$tax_query = [];
+
+		if ( !empty( $term ) ) {
+			$tax_query[] = [
+				'taxonomy' => $tax_query[ 'taxonomy' ],
+
+				'field' => 'slug',
+
+				'terms' => $tax_query[ 'terms' ],
+			];
+		}
+
         return [
             'numberposts' => -1,
             
@@ -21,6 +33,8 @@ class ToolSitemap
 			'suppress_filters' => 0,
             
             'orderby' => [ 'date ' => 'DESC', 'title' => 'ASC' ],
+
+			'tax_query' => $tax_query,
         ];
     }
 
@@ -30,16 +44,6 @@ class ToolSitemap
 
 		if ( !empty( $posts ) ) {
 			foreach ( $posts as $post ) {
-				// LegalDebug::debug( [
-				// 	'ID' => $post->ID,
-
-				// 	'post_title' => $post->post_title,
-
-				// 	'get_post_permalink' => get_post_permalink( $post->ID ),
-
-				// 	'wpml_post_language_details' => apply_filters( 'wpml_post_language_details', NULL, $post->ID ),
-				// ] );
-
 				$items[] = [
 					'label' => $post->post_title,
 
@@ -55,11 +59,17 @@ class ToolSitemap
 	{
 		$post_type = 'legal_bk_review';
 
+		$term = '';
+
 		if ( !empty( $atts[ 'post_type' ] ) ) {
 			$post_type = $atts[ 'post_type' ];
 		}
 
-		$args = self::get_args( $post_type );
+		if ( !empty( $atts[ 'term' ] ) ) {
+			$term = $atts[ 'term' ];
+		}
+
+		$args = self::get_args( $post_type, $term );
 
 		$posts = get_posts( $args );
 
