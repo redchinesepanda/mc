9fce6d3cf46d68e39cf5692bf70f6da70f911059
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-29 15:37:22 +0300

    compilation shortcode

--- lib/acf/ACFMenu.php
+++ lib/acf/ACFMenu.php
@@ -17,23 +17,25 @@ class ACFMenu
     {
         $langs = WPMLMain::get_all_languages();
 
-        $field[ 'choices' ][ 'legal-country-wide' ] = '[ wide ] 320px';
+        $choices[ 'legal-country-wide' ] = '[ wide ] 320px';
 
-        $field[ 'choices' ][ 'legal-country-rocket' ] = '[ rocket ] Betting sites';
+        $choices[ 'legal-country-rocket' ] = '[ rocket ] Betting sites';
 
-        $field[ 'choices' ][ 'legal-country-football' ] = '[ football ] Sports';
+        $choices[ 'legal-country-football' ] = '[ football ] Sports';
 
-        $field[ 'choices' ][ 'legal-country-bonus' ] = '[ bonus ] Bonuses';
+        $choices[ 'legal-country-bonus' ] = '[ bonus ] Bonuses';
 
-        $field[ 'choices' ][ 'legal-country-casino' ] = '[ casino ] Casino';
+        $choices[ 'legal-country-casino' ] = '[ casino ] Casino';
 
-        $field[ 'choices' ][ 'legal-country-all' ] = '[ all ] Choose your country';
+        $choices[ 'legal-country-all' ] = '[ all ] Choose your country';
 
         if ( !empty( $langs ) ) {
             foreach( $langs as $lang ) {
-                $field[ 'choices' ][ 'legal-country-' . $lang[ 'code' ] ] = '[ ' . $lang[ 'code' ] . ' ] ' . $lang[ 'native_name' ]; 
+                $choices[ 'legal-country-' . $lang[ 'code' ] ] = '[ ' . $lang[ 'code' ] . ' ] ' . $lang[ 'native_name' ]; 
             }
         }
+
+        $field[ 'choices' ] = $choices;
 
         return $field;
     }
