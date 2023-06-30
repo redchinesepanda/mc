b9748f7bae2eaa1ddca27b7b116ccbf1ac2db21e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-06-30 13:30:53 +0300

    compilation shortcode

--- template-parts/review/review-counter.php
+++ template-parts/review/review-counter.php
@@ -3,8 +3,8 @@
 	<div class="info-rating"><?php echo $args[ 'title' ]; ?> - <?php echo $args[ 'rating' ]; ?></div>
 </div>
 <div class="review-counter-set">
-	<?php foreach( $args[ 'items' ] as $item ) : ?>
-		<div class="set-item" >
+	<?php foreach( $args[ 'items' ] as $id => $item ) : ?>
+		<div class="set-item set-item-<?php echo $id; ?>" >
 			<div class="set-item-info">
 				<div class="item-value"><?php echo $item[ 'value' ]; ?></div>
 				<div class="item-label"><?php echo $item[ 'label' ]; ?></div>
