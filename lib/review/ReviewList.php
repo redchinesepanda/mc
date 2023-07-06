1e153d705261d3b943b91d4f3573a155d0c819c2
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-06 14:38:00 +0300

    review

--- lib/review/ReviewList.php
+++ lib/review/ReviewList.php
@@ -31,6 +31,8 @@ class ReviewList
         add_action( 'wp_enqueue_scripts', [ $handler, 'register_inline_style' ] );
 
         add_filter( 'tiny_mce_before_init', [ $handler, 'style_formats_list' ] );
+
+        add_filter( 'the_content', [ $handler, 'get_content' ] );
     }
 
     public static function inline_style()
@@ -105,6 +107,104 @@ class ReviewList
 
         return self::get_nodes( $dom );
     }
+
+    public static function get_content( $content )
+	{
+        if ( !ReviewMain::check() ) {
+			return $content;
+		}
+
+		$dom = LegalDOM::get_dom( $content );
+
+        $nodes = self::get_nodes( $dom );
+
+		if ( $nodes->length == 0 ) {
+			return $content;
+		}
+
+		// $item = null;
+
+		// $last = $nodes->length - 1;
+
+        // $args = [];
+
+        // $container = $dom->createElement( 'div' );
+
+        // $container->setAttribute( 'class', self::CSS_CLASS[ 'container' ] );
+
+		foreach ( $nodes as $node_id => $node )
+        {
+            $elements = $node->getElementsByTagName( 'li' );
+        
+            if ( $elements->length != 0 )
+            {
+                foreach ( $elements as $element_id => $element )
+                {
+                    $class = self::CSS_CLASS[ 'base' ] . '-' . $node_id . '-' . $element_id;
+
+                    $element->setAttribute( 'class', $class );
+
+                    // $label = strtolower( $element->textContent );
+    
+                    // $label = ToolLoco::__( $label, ToolLoco::TEXTDOMAIN, 'en' );
+    
+                    // $label = str_replace( ' ', '-', $label );
+    
+                    // $items[] = [
+                    //     'label' => $label,
+                    // ];
+                }
+            }
+
+            // $class = explode( ' ', $node->getAttribute( 'class' ) );
+
+			// $permission_title = ( in_array( self::CSS_CLASS[ 'title' ], $class ) );
+
+			// $permission_content = ( in_array( self::CSS_CLASS[ 'content' ], $class ) );
+
+			// $permission_last = ( $id == $last );
+
+			// if ( $permission_content ) {
+			// 	$node->removeAttribute( 'class' );
+
+            //     $args[ 'content' ] = ToolEncode::encode( $dom->saveHTML( $node ) );
+			// }
+
+			// if ( !empty( $item ) && ( $permission_title || $permission_last ) ) {
+				
+			// 	LegalDOM::appendHTML( $item, self::render( $args ) );
+
+            //     $container->appendChild( $item );
+
+            //     $item = null;
+			// }
+
+            // if ( $permission_last ) {
+            //     $node->parentNode->replaceChild( $container, $node );
+            // } else {
+            //     $node->parentNode->removeChild( $node );
+            // }
+
+			// if ( $permission_title ) {
+
+			// 	$item = $dom->createElement( 'div' );
+
+            //     $class = self::CSS_CLASS[ 'pros' ];
+
+            //     if ( strpos( $node->getAttribute( 'class' ), self::CSS_CLASS[ 'cons' ] ) !== false ) {
+            //         $class = self::CSS_CLASS[ 'cons' ];
+            //     }
+
+			// 	$item->setAttribute( 'class', self::CSS_CLASS[ 'pros-item' ] . ' ' . $class );
+
+			// 	$args = [];
+				
+			// 	$args[ 'title' ] = ToolEncode::encode( $node->textContent );
+			// }
+		}
+
+		return $dom->saveHTML();
+	}
 
     const CLASSES = [
         'list-3' => 'legal-list-3',
