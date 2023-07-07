ee6576b8dfaaf3a1540f19e3ef71df8054a32746
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-07 12:12:45 +0300

    review

--- template-parts/billet/part-billet-logo.php
+++ template-parts/billet/part-billet-logo.php
@@ -4,6 +4,6 @@
 <a class="legal-logo <?php echo $args['logo']['class']; ?> check-oops" href="<?php echo $args['logo']['href']; ?>" rel="nofollow">
     <img src="<?php echo $args['logo']['src'] ?>" alt="billet logo" />
 </a>
-<a class="legal-review <?php echo $args['review']['class']; echo $args['review']['font']; ?>" href="<?php echo $args['review']['href']; ?>">
+<a class="legal-review <?php echo $args['review']['class']; ?> <?php echo $args['review']['font']; ?>" href="<?php echo $args['review']['href']; ?>">
     <?php echo $args['review']['label']; ?>
 </a>
