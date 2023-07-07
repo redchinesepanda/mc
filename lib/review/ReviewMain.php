1b04616bebeb9b9e1dbc7015ca1d56774c51be35
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-07 11:06:08 +0300

    review

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -32,7 +32,7 @@ class ReviewMain
         'review-main' => [
             'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-main.css',
 
-            'ver' => '1.0.9',
+            'ver' => '1.1.0',
         ],
 
         'review-overview' => LegalMain::LEGAL_URL . '/assets/css/review/review-overview.css',
