86e434b85ed8ba7f0fec9d4a75f18ef7afdc7594
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-10 09:10:51 +0300

    review

--- template-parts/billet/center/part-billet-title.php
+++ template-parts/billet/center/part-billet-title.php
@@ -2,9 +2,7 @@
     <?php if ( $args['order'] == BilletTitle::ORDER_TYPE ) : ?>
         <div class="billet-order">#1</div>
     <?php endif; ?>
-    <a class="legal-title <?php echo $args['class']; ?>" href="<?php echo $args['href']; ?>" rel="nofollow">
-        <?php echo $args['label']; ?>
-    </a>
+    <a class="legal-title <?php echo $args['class']; ?>" href="<?php echo $args['href']; ?>"><?php echo $args['label']; ?></a>
     <?php if ( !empty( $args['rating'] ) ): ?>
         <div class="billet-title-rating"><?php echo $args['rating']; ?></div>
     <?php endif; ?>
