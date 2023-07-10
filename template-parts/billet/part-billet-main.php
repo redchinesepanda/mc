1847d529bce1c00fe741a8c9e2b83df4351bde7e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-10 10:50:27 +0300

    review

--- template-parts/billet/part-billet-main.php
+++ template-parts/billet/part-billet-main.php
@@ -1,21 +1,23 @@
-<style type="text/css">
-    #<?php echo $args['selector']; ?> .billet-left {
-        background-color: <?php echo $args['color']; ?>;
-    }
-</style>
-<div id="<?php echo $args['selector']; ?>" class="billet">
-    <div class="billet-left">
-        <?php BilletLogo::render( $args ); ?>
+<div class="billet-item">
+    <style type="text/css">
+        #<?php echo $args['selector']; ?> .billet-left {
+            background-color: <?php echo $args['color']; ?>;
+        }
+    </style>
+    <div id="<?php echo $args['selector']; ?>" class="billet">
+        <div class="billet-left">
+            <?php BilletLogo::render( $args ); ?>
+        </div>
+        <div class="billet-center">
+            <?php BilletTitle::render( $args ); ?>
+            <?php BilletList::render( $args ); ?>
+        </div>
+        <div class="billet-right">
+            <?php BilletRight::render( $args ); ?>
+        </div>
     </div>
-    <div class="billet-center">
-        <?php BilletTitle::render( $args ); ?>
-        <?php BilletList::render( $args ); ?>
+    <?php BilletSpoiler::render( $args ); ?>
+    <div class="billet-footer">
+        <?php echo $args['description'] ?>
     </div>
-    <div class="billet-right">
-        <?php BilletRight::render( $args ); ?>
-    </div>
-</div>
-<?php BilletSpoiler::render( $args ); ?>
-<div class="billet-footer">
-    <?php echo $args['description'] ?>
 </div>
