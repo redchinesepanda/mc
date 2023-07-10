fd5293cd79e94ea172f98e87ad98bf92f0739d79
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-10 13:27:09 +0300

    review

--- template-parts/review/review-offers.php
+++ template-parts/review/review-offers.php
@@ -6,7 +6,7 @@
 			<div class="offers-item offers-item-<?php echo $item_id; ?>">
 				<div class="item-bonus <?php echo $item[ 'font' ]; ?>"><?php echo $item[ 'bonus' ]; ?></div>
 				<div class="item-logo"></div>
-		s		<a href="<?php echo $item[ 'afillate' ][ 'href' ]; ?>" class="item-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $item[ 'afillate' ][ 'text' ]; ?></a>
+				<a href="<?php echo $item[ 'afillate' ][ 'href' ]; ?>" class="item-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $item[ 'afillate' ][ 'text' ]; ?></a>
 			</div>
 		<?php endforeach; ?>
 	</div>
