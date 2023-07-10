e89c9a6cda6a5b9259cdb7db1b070357ec74e731
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-10 15:18:33 +0300

    review

--- assets/js/review/review-offers.js
+++ assets/js/review/review-offers.js
@@ -2,12 +2,8 @@
 
 document.addEventListener( 'DOMContentLoaded', function ()
 {
-	// const slidesContainer = document.getElementById( 'legal-other-offers' );
-
 	document.querySelectorAll( '.legal-other-offers-wrapper' ).forEach( function ( wrapper, index ) {
 		const slidesContainer = wrapper.querySelector( '.legal-other-offers' );
-
-		console.log( 'review-offers slidesContainer: ' + slidesContainer.classList );
 
 		const slide = slidesContainer.querySelector( '.offers-item' );
 
@@ -15,7 +11,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 		const prevButton = wrapper.querySelector( '.offers-arrow-prev' );
 
-		const offset = 20;
+		const offset = 18;
 
 		nextButton.addEventListener( "click", () => {
 			const slideWidth = slide.clientWidth;
@@ -28,44 +24,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 	
 			slidesContainer.scrollLeft -= slideWidth + offset;
 		} );
-
-		// imageset.id = 'imageset-' + index;
-
-		// imageset.querySelectorAll( '.imageset-item' ).forEach( function ( item, index ) {
-
-		// 	item.dataset.imageset = imageset.id;
-
-		// 	item.dataset.id = index;
-
-		// 	item.addEventListener( 'click', popup, false );
-
-		// 	item.addEventListener( 'click', popupUpdate, false );
-		// } );
 	} );
-
-	
-
-	// console.log( 'review-offers slidesContainer: ' + slidesContainer.classList );
-	
-	// const slide = document.querySelector( ".slide" );
-
-	// const prevButton = document.getElementById( "slide-arrow-prev" );
-
-	// const nextButton = document.getElementById( "slide-arrow-next" );
-
-	// const offset = 20;
-
-	// nextButton.addEventListener( "click", () => {
-	// 	const slideWidth = slide.clientWidth;
-
-	// 	slidesContainer.scrollLeft += slideWidth + offset;
-	// } );
-
-	// prevButton.addEventListener( "click", () => {
-	// 	const slideWidth = slide.clientWidth;
-
-	// 	slidesContainer.scrollLeft -= slideWidth + offset;
-	// } );
 } );
 
 // review-offers-js end
