4a9e84161d52704564f4bf2cf757303feca6737e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-10 13:26:39 +0300

    review

--- template-parts/review/review-offers.php
+++ template-parts/review/review-offers.php
@@ -1,12 +1,12 @@
 <?php if( !empty( $args ) ) : ?>
 	<button class="slide-arrow" id="slide-arrow-prev">&#8249;</button>
   	<button class="slide-arrow" id="slide-arrow-next">&#8250;</button>
-  	<div class="offers-container" id="offers-container">
+  	<div class="legal-other-offers" id="legal-other-offers">
 		<?php foreach ( $args as $item_id => $item ) : ?>
 			<div class="offers-item offers-item-<?php echo $item_id; ?>">
 				<div class="item-bonus <?php echo $item[ 'font' ]; ?>"><?php echo $item[ 'bonus' ]; ?></div>
 				<div class="item-logo"></div>
-				<a href="<?php echo $item[ 'afillate' ][ 'href' ]; ?>" class="item-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $item[ 'afillate' ][ 'text' ]; ?></a>
+		s		<a href="<?php echo $item[ 'afillate' ][ 'href' ]; ?>" class="item-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $item[ 'afillate' ][ 'text' ]; ?></a>
 			</div>
 		<?php endforeach; ?>
 	</div>
