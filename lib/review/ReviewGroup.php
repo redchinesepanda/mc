41af1b838cfe43a8daf7291d245a4a4cf76f0c39
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-11 16:56:23 +0300

    review

--- lib/review/ReviewGroup.php
+++ lib/review/ReviewGroup.php
@@ -73,7 +73,7 @@ class ReviewGroup
         }
 
         if ( in_array( $post->post_type, [ 'legal_bk_review' ] ) ) {
-            $terms = wp_get_post_terms( $post->ID, self::TAXONOMY[ 'type' ] );
+            $terms = wp_get_post_terms( $post->ID, self::TAXONOMY[ 'type' ], [ 'fields' => 'names' ] );
 
             LegalDebug::debug( [
                 'ID' => $post->ID,
@@ -85,7 +85,7 @@ class ReviewGroup
 
             if ( !empty( $terms ) )
             {
-                
+                $term = array_pop( $terms );
             }
 
             $label[] = __( 'Review', ToolLoco::TEXTDOMAIN );
