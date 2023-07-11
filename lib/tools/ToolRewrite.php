20be80f2a714f685dc6032bca2cf9e25ee4f8267
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-11 13:42:29 +0300

    review

--- lib/tools/ToolRewrite.php
+++ lib/tools/ToolRewrite.php
@@ -39,22 +39,16 @@ class ToolRewrite
 		// ] );
 		
 		if ( in_array( $post->post_type, [ 'legal_bk_review' ] ) )
-		{
-			// $terms = get_the_terms( $post->ID, 'page_group' );
 
-			// $terms = wp_get_post_terms( $post->ID, self::TAXONOMY );
+			$term_id = get_post_meta( $post->ID, '_yoast_wpseo_primary_' . self::TAXONOMY, true );
 
-			// $term_id = get_post_meta( $post->ID, '_yoast_wpseo_primary_' . self::TAXONOMY, true );
-			
-			// $term_id = the_seo_framework()->get_primary_term( $post->ID, self::TAXONOMY );
-
-			$wpseo_primary_term = new WPSEO_Primary_Term( self::TAXONOMY, $post->ID );
+			// $wpseo_primary_term = new WPSEO_Primary_Term( self::TAXONOMY, $post->ID );
         	
-			$term_id = $wpseo_primary_term->get_primary_term();
+			// $term_id = $wpseo_primary_term->get_primary_term();
 
-			LegalDebug::debug( [
-				'term_id' => $term_id,
-			] );
+			// LegalDebug::debug( [
+			// 	'term_id' => $term_id,
+			// ] );
 
 			$page_group = '';
 
