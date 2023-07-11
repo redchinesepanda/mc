d4c1b8ab0fee8800b5f77473a703fc5bb7ec01d4
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-11 13:08:09 +0300

    review

--- lib/tools/ToolRewrite.php
+++ lib/tools/ToolRewrite.php
@@ -1,7 +1,7 @@
 <?php
 
-ini_set( 'error_reporting', -1 );
-ini_set( 'display_errors', 'On' );
+// ini_set( 'error_reporting', -1 );
+// ini_set( 'display_errors', 'On' );
 
 class ToolRewrite
 {
@@ -9,13 +9,13 @@ class ToolRewrite
 	{
 		$handler = new self();
 
-		add_action( 'parse_request', [ $handler, 'debug_404_rewrite_dump' ] );
+		// add_action( 'parse_request', [ $handler, 'debug_404_rewrite_dump' ] );
 
-		add_action( 'template_redirect', [ $handler, 'debug_404_template_redirect' ], 99999 );
+		// add_action( 'template_redirect', [ $handler, 'debug_404_template_redirect' ], 99999 );
 
-		add_filter ( 'template_include', [ $handler, 'debug_404_template_dump' ] );
+		// add_filter ( 'template_include', [ $handler, 'debug_404_template_dump' ] );
 
-		// add_filter( 'post_type_link', [ $handler, 'review_link' ], 10, 4 );
+		add_filter( 'post_type_link', [ $handler, 'review_link' ], 10, 4 );
 	}
 
 	public static function review_link( $post_link, $post, $leavename, $sample )
@@ -46,41 +46,41 @@ class ToolRewrite
 		return $post_link;
 	}
 
-	public static function debug_404_rewrite_dump( &$wp ) {
-		global $wp_rewrite;
+	// public static function debug_404_rewrite_dump( &$wp ) {
+	// 	global $wp_rewrite;
 
-		global $wp_the_query;
+	// 	global $wp_the_query;
 		
-		LegalDebug::debug( [
-			'rewrite rules' => var_export( $wp_rewrite->wp_rewrite_rules(), true ),
+	// 	LegalDebug::debug( [
+	// 		'rewrite rules' => var_export( $wp_rewrite->wp_rewrite_rules(), true ),
 
-			'permalink structure' => var_export( $wp_rewrite->permalink_structure, true ),
+	// 		'permalink structure' => var_export( $wp_rewrite->permalink_structure, true ),
 
-			'page permastruct' => var_export( $wp_rewrite->get_page_permastruct(), true ),
+	// 		'page permastruct' => var_export( $wp_rewrite->get_page_permastruct(), true ),
 
-			'matched rule and query' => var_export( $wp->matched_rule, true ),
+	// 		'matched rule and query' => var_export( $wp->matched_rule, true ),
 
-			'matched query' => var_export( $wp->matched_query, true ),
+	// 		'matched query' => var_export( $wp->matched_query, true ),
 
-			'request' => var_export( $wp->request, true ),
+	// 		'request' => var_export( $wp->request, true ),
 
-			'the query' => var_export( $wp_the_query, true ),
-		] );
-	}
+	// 		'the query' => var_export( $wp_the_query, true ),
+	// 	] );
+	// }
 	
-	public static  function debug_404_template_redirect() {
-		global $wp_filter;
+	// public static  function debug_404_template_redirect() {
+	// 	global $wp_filter;
 
-		LegalDebug::debug( [
-			'template redirect filters' => var_export( $wp_filter[current_filter()], true ),
-		] );
-	}
+	// 	LegalDebug::debug( [
+	// 		'template redirect filters' => var_export( $wp_filter[current_filter()], true ),
+	// 	] );
+	// }
 	
-	public static  function debug_404_template_dump( $template ) { 
-		LegalDebug::debug( [
-			'template file selected' => var_export( $template, true ),
-		] );
-	}
+	// public static  function debug_404_template_dump( $template ) { 
+	// 	LegalDebug::debug( [
+	// 		'template file selected' => var_export( $template, true ),
+	// 	] );
+	// }
 }
 
 ?>
