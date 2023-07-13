763be53efa2e59db01f5d11a68ad2cfb4a05be10
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-13 14:27:22 +0300

    review

--- lib/review/ReviewAbout.php
+++ lib/review/ReviewAbout.php
@@ -6,7 +6,7 @@ class ReviewAbout
         'review-about' => [
             'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-about.css',
 
-            'ver'=> '1.0.7',
+            'ver'=> '1.0.8',
         ],
     ];
 
@@ -25,6 +25,8 @@ class ReviewAbout
         $handler = new self();
 
         // [legal-about]
+
+        // [legal-about mode="footer"]
 
         add_shortcode( 'legal-about', [ $handler, 'render' ] );
 
