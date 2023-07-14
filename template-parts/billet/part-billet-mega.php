495754a88ed9d26a4e7211e8799d30e5a3878e09
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-14 13:05:12 +0300

    review

--- template-parts/billet/part-billet-mega.php
+++ template-parts/billet/part-billet-mega.php
@@ -1,3 +1,15 @@
 <div class="legal-billet-mega item-<?php echo $args[ 'id' ] ?>">
-	<?php echo $args[ 'content' ] ?>
+	<div class="billet-mega-about">
+		<div class="mega-about-logo"></div>
+		<a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="mega-about-afillate check-oops" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
+		<a href="<?php echo $args[ 'review' ][ 'href' ]; ?>" class="mega-about-review check-oops" rel="nofollow"><?php echo $args[ 'review' ][ 'text' ]; ?></a>
+	</div>
+	<div class="billet-mega-content">
+		<?php echo $args[ 'content' ] ?>
+	</div>
+	<?php if( !empty( $args[ 'footer' ] ) ) : ?>
+		<div class="billet-mega-footer">
+			<?php echo $args[ 'footer' ] ?>
+		</div>
+	<?php endif; ?>
 </div>
