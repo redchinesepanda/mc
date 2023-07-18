1ac83893efe8ba157f8c81ac6a1449907316cce9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-18 11:12:57 +0300

    review

--- template-parts/review/review-bonus.php
+++ template-parts/review/review-bonus.php
@@ -1,3 +1,10 @@
+<?php
+
+LegalDebug::debug( [
+	'name' => $args[ 'name' ],
+] );
+
+?>
 <?php if( !empty( $args ) ) : ?>
 	<div class="bonus-title">
 		<img src="<?php echo $args[ 'src' ]; ?>" width="53" height="53" />
