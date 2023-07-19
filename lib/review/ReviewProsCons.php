e4949be0d87ba75d11e03535724e08899eea2e80
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-19 09:06:56 +0300

    review

--- lib/review/ReviewProsCons.php
+++ lib/review/ReviewProsCons.php
@@ -52,13 +52,15 @@ class ReviewProsCons
 		}
 
 		return [
-			'title' => ( in_array( self::CSS_CLASS[ 'title' ], $class ) ),
+			'title' => in_array( self::CSS_CLASS[ 'title' ], $class ),
 
-			'pros_title' => ( in_array( self::CSS_CLASS[ 'pros' ], $class ) ),
+			'pros_title' => in_array( self::CSS_CLASS[ 'pros' ], $class ),
 
-			'cons_title' => ( in_array( self::CSS_CLASS[ 'cons' ], $class ) ),
+			'cons_title' => in_array( self::CSS_CLASS[ 'cons' ], $class ),
 
-			'content' => ( in_array( self::CSS_CLASS[ 'content' ], $class ) ),
+			'content' => in_array( self::CSS_CLASS[ 'content' ], $class ),
+
+			'not_empty' => !empty( $node->textContent ),
 		];
 	}
 
@@ -72,6 +74,8 @@ class ReviewProsCons
 
 		$last = !$next[ 'title' ] && !$next[ 'content' ];
 
+		$not_empty = $current[ 'not_empty' ];
+
 		// LegalDebug::debug( [
 		// 	'get_permission_replace' => self::permission_debug( [
 		// 		'default' => $default,
@@ -84,7 +88,7 @@ class ReviewProsCons
 		// 	] ),
 		// ] );
 
-		return $default || $half_pros || $half_cons || $last; 
+		return ( $default || $half_pros || $half_cons || $last ) && $not_empty; 
 	}
 
 	public static function get_content( $content )
@@ -188,120 +192,120 @@ class ReviewProsCons
 		return $dom->saveHTML();
 	}
 
-	public static function get_content_2( $content )
-	{
-        if ( !ReviewMain::check() ) {
-			return $content;
-		}
+	// public static function get_content_2( $content )
+	// {
+    //     if ( !ReviewMain::check() ) {
+	// 		return $content;
+	// 	}
 
-		$dom = LegalDOM::get_dom( $content );
+	// 	$dom = LegalDOM::get_dom( $content );
 
-        $nodes = self::get_nodes( $dom );
+    //     $nodes = self::get_nodes( $dom );
 
-		if ( $nodes->length == 0 ) {
-			return $content;
-		}
+	// 	if ( $nodes->length == 0 ) {
+	// 		return $content;
+	// 	}
 
-		$item = null;
+	// 	$item = null;
 
-		$last = $nodes->length - 1;
+	// 	$last = $nodes->length - 1;
 
-        $args = [];
+    //     $args = [];
 
-		$container = null;
+	// 	$container = null;
 
-        // $container = $dom->createElement( 'div' );
+    //     // $container = $dom->createElement( 'div' );
 
-        // $container->setAttribute( 'class', self::CSS_CLASS[ 'container' ] );
+    //     // $container->setAttribute( 'class', self::CSS_CLASS[ 'container' ] );
 
-		foreach ( $nodes as $id => $node ) {
-            $class = explode( ' ', $node->getAttribute( 'class' ) );
+	// 	foreach ( $nodes as $id => $node ) {
+    //         $class = explode( ' ', $node->getAttribute( 'class' ) );
 
-			$permission_title = ( in_array( self::CSS_CLASS[ 'title' ], $class ) );
+	// 		$permission_title = ( in_array( self::CSS_CLASS[ 'title' ], $class ) );
 
-			$permission_pros_title = ( in_array( self::CSS_CLASS[ 'pros' ], $class ) );
+	// 		$permission_pros_title = ( in_array( self::CSS_CLASS[ 'pros' ], $class ) );
 
-			$permission_content = ( in_array( self::CSS_CLASS[ 'content' ], $class ) );
+	// 		$permission_content = ( in_array( self::CSS_CLASS[ 'content' ], $class ) );
 
-			$permission_last = ( $id == $last || !empty( $node->nextSibling ) );
+	// 		$permission_last = ( $id == $last || !empty( $node->nextSibling ) );
 
-			// LegalDebug::debug( [
-			// 	'class' => $class,
+	// 		// LegalDebug::debug( [
+	// 		// 	'class' => $class,
 
-			// 	'permission' => self::permission_debug( [
-			// 		'permission_title' => $permission_title,
+	// 		// 	'permission' => self::permission_debug( [
+	// 		// 		'permission_title' => $permission_title,
 	
-			// 		'permission_pros_title' => $permission_pros_title,
+	// 		// 		'permission_pros_title' => $permission_pros_title,
 	
-			// 		'permission_content' => $permission_content,
-			// 	] ),
+	// 		// 		'permission_content' => $permission_content,
+	// 		// 	] ),
 
-			// 	'permission_title' => $permission_title ? 'true' : 'false',
+	// 		// 	'permission_title' => $permission_title ? 'true' : 'false',
 
-			// 	'permission_pros_title' => $permission_pros_title ? 'true' : 'false',
+	// 		// 	'permission_pros_title' => $permission_pros_title ? 'true' : 'false',
 
-			// 	'permission_content' => $permission_content ? 'true' : 'false',
+	// 		// 	'permission_content' => $permission_content ? 'true' : 'false',
 
-			// 	'permission_last' => $permission_last ? 'true' : 'false',
+	// 		// 	'permission_last' => $permission_last ? 'true' : 'false',
 
-			// 	// 'container' => $container,
-			// ] );
+	// 		// 	// 'container' => $container,
+	// 		// ] );
 
-			if ( $permission_content ) {
-				$node->removeAttribute( 'class' );
+	// 		if ( $permission_content ) {
+	// 			$node->removeAttribute( 'class' );
 
-                $args[ 'content' ] = ToolEncode::encode( $dom->saveHTML( $node ) );
-			}
+    //             $args[ 'content' ] = ToolEncode::encode( $dom->saveHTML( $node ) );
+	// 		}
 
-			if ( !empty( $item ) && ( $permission_title || $permission_last ) ) {
+	// 		if ( !empty( $item ) && ( $permission_title || $permission_last ) ) {
 				
-				LegalDOM::appendHTML( $item, self::render( $args ) );
+	// 			LegalDOM::appendHTML( $item, self::render( $args ) );
 
-                $container->appendChild( $item );
+    //             $container->appendChild( $item );
 
-                $item = null;
-			}
+    //             $item = null;
+	// 		}
 
-            if ( !empty( $container ) && $permission_last ) {
-				// LegalDebug::debug( [
-				// 	'action' => 'replace',
-				// ] );
+    //         if ( !empty( $container ) && $permission_last ) {
+	// 			// LegalDebug::debug( [
+	// 			// 	'action' => 'replace',
+	// 			// ] );
 
-                $node->parentNode->replaceChild( $container, $node );
-            } else {
-				// LegalDebug::debug( [
-				// 	'action' => 'remove',
-				// ] );
+    //             $node->parentNode->replaceChild( $container, $node );
+    //         } else {
+	// 			// LegalDebug::debug( [
+	// 			// 	'action' => 'remove',
+	// 			// ] );
 
-                $node->parentNode->removeChild( $node );
-            }
+    //             $node->parentNode->removeChild( $node );
+    //         }
 
-			if ( $permission_pros_title ) {
-				$container = $dom->createElement( 'div' );
+	// 		if ( $permission_pros_title ) {
+	// 			$container = $dom->createElement( 'div' );
 
-        		$container->setAttribute( 'class', self::CSS_CLASS[ 'container' ] );
-			}
+    //     		$container->setAttribute( 'class', self::CSS_CLASS[ 'container' ] );
+	// 		}
 
-			if ( $permission_title ) {
+	// 		if ( $permission_title ) {
 
-				$item = $dom->createElement( 'div' );
+	// 			$item = $dom->createElement( 'div' );
 
-                $class = self::CSS_CLASS[ 'pros' ];
+    //             $class = self::CSS_CLASS[ 'pros' ];
 
-                if ( strpos( $node->getAttribute( 'class' ), self::CSS_CLASS[ 'cons' ] ) !== false ) {
-                    $class = self::CSS_CLASS[ 'cons' ];
-                }
+    //             if ( strpos( $node->getAttribute( 'class' ), self::CSS_CLASS[ 'cons' ] ) !== false ) {
+    //                 $class = self::CSS_CLASS[ 'cons' ];
+    //             }
 
-				$item->setAttribute( 'class', self::CSS_CLASS[ 'pros-item' ] . ' ' . $class );
+	// 			$item->setAttribute( 'class', self::CSS_CLASS[ 'pros-item' ] . ' ' . $class );
 
-				$args = [];
+	// 			$args = [];
 				
-				$args[ 'title' ] = ToolEncode::encode( $node->textContent );
-			}
-		}
+	// 			$args[ 'title' ] = ToolEncode::encode( $node->textContent );
+	// 		}
+	// 	}
 
-		return $dom->saveHTML();
-	}
+	// 	return $dom->saveHTML();
+	// }
 
 	// public static function get_content( $content )
 	// {
