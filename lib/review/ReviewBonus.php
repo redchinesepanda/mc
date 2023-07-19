c7b15a096f78531fdf2bb346da84d75ead6a9a35
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-19 12:25:38 +0300

    review

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -107,11 +107,11 @@ class ReviewBonus
 		$last = $nodes->length - 1;
 
 		foreach ( $nodes as $id => $node ) {
-			LegalDebug::debug(
+			LegalDebug::debug( [
 				'parentNode' => $node->parentNode,
 
 				'previousSibling' => $node->previousSibling,
-			);
+			] );
 
 			$class = $node->getAttribute( 'class' );
 
