741a67842d9c9f31a6fc779c4d331f0eb28a5d60
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-20 12:21:12 +0300

    review

--- lib/billet/BilletLogo.php
+++ lib/billet/BilletLogo.php
@@ -2,19 +2,45 @@
 
 class BilletLogo
 {
-    const TEMPLATE = LegalMain::LEGAL_PATH . '/template-parts/billet/part-billet-logo.php';
-
     const DEFAULT_LOGO = LegalMain::LEGAL_URL . '/assets/img/legal-blank.svg';
 
     const ORDER_VALUE = 'legal-logo';
 
+    const FIELD = [
+        'about' => 'review-about',
+    ];
+
+    const ABOUT = [
+        'font' => 'about-font',
+
+        'logo' => 'about-logo',
+    ];
+
     public static function get( $billet )
     {
+        $font = '';
+
+        $src = self::DEFAULT_LOGO;
+
+        $group = get_field( self::FIELD[ 'about' ], $billet['id'] );
+
+        if ( $group )
+        {
+            $font = $group[ self::ABOUT[ 'font' ] ];
+
+            if ( !empty( $group[ self::ABOUT[ 'logo' ] ] ) )
+            {
+                $src = $group[ self::ABOUT[ 'logo' ] ];
+            }
+        }
+
         $args['review'] = BilletMain::href( $billet['url']['review'] );
 
         $args['review']['label'] = ( !empty( $billet['filter']['review']['label'] ) ? $billet['filter']['review']['label'] : __( 'Review', ToolLoco::TEXTDOMAIN ) );
 
-        $args['review'][ 'font' ] = get_field( 'billet-font', $billet['id'] );
+        // $args['review'][ 'font' ] = get_field( 'billet-font', $billet['id'] );
+
+        $args['review'][ 'font' ] = $font;
 
         $args['index'] = $billet['index'];
 
@@ -22,16 +48,22 @@ class BilletLogo
 
         $args['logo'] = BilletMain::href( $billet['url']['logo'] );
 
-        $src = get_field( 'billet-logo-url', $billet['id'] );
+        // $src = get_field( 'billet-logo-url', $billet['id'] );
 
-        $args['logo']['src'] = ( !empty( $src ) ? $src : self::DEFAULT_LOGO );
+        // $args['logo']['src'] = ( !empty( $src ) ? $src : self::DEFAULT_LOGO );
+
+        $args['logo']['src'] = $src;
 
         return $args;
     }
 
+    const TEMPLATE = [
+        'logo' => LegalMain::LEGAL_PATH . '/template-parts/billet/part-billet-logo.php'
+    ];
+
     public static function render( $billet )
     { 
-        load_template( self::TEMPLATE, false, self::get( $billet ) );
+        load_template( self::TEMPLATE[ 'logo' ], false, self::get( $billet ) );
     }
 }
 
