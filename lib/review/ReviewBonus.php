c3d97b37596be7d7b44b4e26e73c1a4ed5da0f59
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-25 14:22:35 +0300

    mega billet

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -6,7 +6,7 @@ class ReviewBonus
         'review-bonus' => [
 			'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-bonus.css',
 
-			'ver' => '1.1.7',
+			'ver' => '1.1.8',
 		],
 
         'review-billet' => [
