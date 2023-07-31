255ff6d1ca5accdd224cc37894d5f43b0838a31e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-31 16:24:33 +0300

    mega billet

--- lib/bonus/BonusMain.php
+++ lib/bonus/BonusMain.php
@@ -290,15 +290,15 @@ class BonusMain
 
 		// $posts = self::get_posts_date( $atts );
 		
-		// $active_partners = self::get_posts_date( $atts, self::MODE[ 'partner' ], self::DURATION[ 'actual' ] );
+		$active_partners = self::get_posts_date( $atts, self::MODE[ 'partner' ], self::DURATION[ 'actual' ] );
 
-		// $active_no_partners = self::get_posts_date( $atts, self::MODE[ 'no-partner' ], self::DURATION[ 'actual' ] );
+		$active_no_partners = self::get_posts_date( $atts, self::MODE[ 'no-partner' ], self::DURATION[ 'actual' ] );
 
-		// $expired_all = self::get_posts_date( $atts, self::MODE[ 'all' ], self::DURATION[ 'expired' ] );
+		$expired_all = self::get_posts_date( $atts, self::MODE[ 'all' ], self::DURATION[ 'expired' ] );
 		
-		$posts = self::get_posts_date( $atts, self::MODE[ 'all' ], self::DURATION[ 'expired' ] );
+		// $posts = self::get_posts_date( $atts, self::MODE[ 'all' ], self::DURATION[ 'expired' ] );
 
-		// $posts = array_merge( $active_partners, $active_no_partners, $expired_all );
+		$posts = array_merge( $active_partners, $active_no_partners, $expired_all );
 
 		// LegalDebug::debug( [
 		// 	'count' => count( $posts ),
