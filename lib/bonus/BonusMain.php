226cab37c186fe6d5b7f22b7e047420fa310cad7
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-31 15:22:05 +0300

    mega billet

--- lib/bonus/BonusMain.php
+++ lib/bonus/BonusMain.php
@@ -92,9 +92,9 @@ class BonusMain
 			'>'
 		);
 		
-		$query = $query_filter->createWpQuery( self::get_args( $atts ) );
+		// $query = $query_filter->createWpQuery( self::get_args( $atts ) );
 
-		// $query = $query_filter->createWpQuery( self::get_args( $atts, 'partner' ) );
+		$query = $query_filter->createWpQuery( self::get_args( $atts, 'partner' ) );
 
 		// $query = $query_filter->createWpQuery( self::get_args( $atts, 'no-partner' ) );
 
