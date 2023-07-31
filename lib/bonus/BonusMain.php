f72e8ecbc309fb37427cbe073a8fa43d4874cf47
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-07-31 15:31:43 +0300

    mega billet

--- lib/bonus/BonusMain.php
+++ lib/bonus/BonusMain.php
@@ -99,9 +99,9 @@ class BonusMain
 			'>'
 		);
 		
-		// $query = $query_filter->createWpQuery( self::get_args( $atts ) );
+		$query = $query_filter->createWpQuery( self::get_args( $atts ) );
 
-		$query = $query_filter->createWpQuery( self::get_args( $atts, 'partner' ) );
+		// $query = $query_filter->createWpQuery( self::get_args( $atts, 'partner' ) );
 
 		// $query = $query_filter->createWpQuery( self::get_args( $atts, 'no-partner' ) );
 
@@ -249,9 +249,9 @@ class BonusMain
 		
 		// $posts = get_posts( self::get_args( $atts, 'partner' ) );
 
-		$posts = get_posts( self::get_args( $atts, 'no-partner' ) );
+		// $posts = get_posts( self::get_args( $atts, 'no-partner' ) );
 
-		// $posts = self::get_posts_date( $atts );
+		$posts = self::get_posts_date( $atts );
 
 		LegalDebug::debug( [
 			'count' => count( $posts ),
