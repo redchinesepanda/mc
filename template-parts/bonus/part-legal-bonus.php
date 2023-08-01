303696a20f15df1881ca4194502771f8649681e9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-08-01 12:44:28 +0300

    mega billet

--- template-parts/bonus/part-legal-bonus.php
+++ template-parts/bonus/part-legal-bonus.php
@@ -11,11 +11,11 @@
 			<div class="bonus-list-item item-<?php echo $item[ 'id' ]; ?>">
 				<?php if ( !empty( $item[ 'preview' ] ) ) : ?>
 					<a class="item-image-link" href="<?php echo $item[ 'preview' ][ 'href' ]; ?>">
-						<img class="item-image  image-<?php echo $item[ 'preview' ][ 'id' ]; ?>" src="<?php echo $item[ 'preview' ][ 'src' ]; ?>" width="<?php echo $item[ 'preview' ][ 'width' ]; ?>" height="<?php echo $item[ 'preview' ][ 'height' ]; ?>" />
+						<img class="item-image image-<?php echo $item[ 'preview' ][ 'id' ]; ?>" src="<?php echo $item[ 'preview' ][ 'src' ]; ?>" width="<?php echo $item[ 'preview' ][ 'width' ]; ?>" height="<?php echo $item[ 'preview' ][ 'height' ]; ?>" />
 					</a>
 				<?php endif; ?>
 				<div class="list-item-about">
-					<img class="item-logo image-<?php echo $item[ 'preview' ][ 'id' ]; ?>" src="<?php echo $item[ 'logo' ][ 'src' ]; ?>" width="<?php echo $item[ 'logo' ][ 'width' ]; ?>" height="<?php echo $item[ 'logo' ][ 'height' ]; ?>" />
+					<img class="item-logo image-<?php echo $item[ 'logo' ][ 'id' ]; ?>" src="<?php echo $item[ 'logo' ][ 'src' ]; ?>" width="<?php echo $item[ 'logo' ][ 'width' ]; ?>" height="<?php echo $item[ 'logo' ][ 'height' ]; ?>" />
 					<div class="item-about-title">
 						<a class="item-image-link" href="<?php echo $item[ 'title' ][ 'href' ]; ?>"><?php echo $item[ 'title' ][ 'label' ]; ?></a>
 					</div>
