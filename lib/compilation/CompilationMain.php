5aba16a1dbf4a74b60b031c4f376bc526a12db0a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-08-04 14:33:03 +0300

    mega billet

--- lib/compilation/CompilationMain.php
+++ lib/compilation/CompilationMain.php
@@ -196,10 +196,13 @@ class CompilationMain
         'about' => 'review-about',
     ];
 
-    const META_KEY = [
-        // 'rating' => 'billet-title-rating',
-        
+
+    const ABOUT = [
         'rating' => 'about-rating',
+    ];
+
+    const META_KEY = [
+        'rating' => 'billet-title-rating',
 
         'profit' => 'billet-profit-items',
     ];
@@ -304,7 +307,7 @@ class CompilationMain
     
                 'legal_rating' =>
                 [
-                    'key' => self::FIELD[ 'about' ] . '_' . self::META_KEY[ 'rating' ],
+                    'key' => self::FIELD[ 'about' ] . '_' . self::ABOUT[ 'rating' ],
 
                     'value'   => [ '' ],
         			
