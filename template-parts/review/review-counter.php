363c9ce72521664466b96685ae8ee9bab0d6aa5e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-08-08 11:32:11 +0300

    mega billet

--- template-parts/review/review-counter.php
+++ template-parts/review/review-counter.php
@@ -4,7 +4,7 @@
 </div>
 <div class="review-counter-set">
 	<?php foreach( $args[ 'items' ] as $id => $item ) : ?>
-		<div class="set-item-wrapper" >
+		<div class="set-item-wrapper">
 			<div class="set-item set-item-<?php echo $id; ?>" >
 				<div class="set-item-info">
 					<div class="item-value"><?php echo $item[ 'value' ]; ?></div>
@@ -21,13 +21,15 @@
 </div>
 <?php if ( !empty( $args[ 'items_overall' ] ) ) : ?>
 	<div class="review-counter-overall">
-		<?php foreach( $args[ 'items_overall' ] as $overall_id => $overall_item ) : ?>
-			<div class="set-item set-item-<?php echo $args[ 'amount' ] + $overall_id; ?>" >
-				<div class="set-item-info">
-					<div class="item-value"><?php echo $overall_item[ 'value' ]; ?></div>
-					<div class="item-label"><?php echo $overall_item[ 'label' ]; ?></div>
+		<div class="set-item-wrapper">
+			<?php foreach( $args[ 'items_overall' ] as $overall_id => $overall_item ) : ?>
+				<div class="set-item set-item-<?php echo $args[ 'amount' ] + $overall_id; ?>" >
+					<div class="set-item-info">
+						<div class="item-value"><?php echo $overall_item[ 'value' ]; ?></div>
+					</div>
 				</div>
-			</div>
-		<?php endforeach; ?>
+				<div class="item-label"><?php echo $overall_item[ 'label' ]; ?></div>
+			<?php endforeach; ?>
+		</div>
 	</div>
 <?php endif; ?>
