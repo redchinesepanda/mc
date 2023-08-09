68b283c71c8be12ede7297bc1f597d212fc3984e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-08-09 14:53:45 +0300

    mega billet

--- lib/review/ReviewBonus.php
+++ lib/review/ReviewBonus.php
@@ -261,6 +261,12 @@ class ReviewBonus
 
 		foreach ( $nodes as $id => $node )
 		{
+			LegalDebug::debug( [
+				'ReviewBonus' => 'get_content',
+
+				'$node->textContent' => substr( $node->textContent, 0, 30 ),
+			] );
+
 			$class = $node->getAttribute( 'class' );
 
 			// $permission_title = ( strpos( $class, self::BONUS_CLASS[ 'title' ] ) !== false );
@@ -281,19 +287,19 @@ class ReviewBonus
 			
 			$permission_replace = self::get_permission_replace( $permission_node, $permission_next );
 
-			LegalDebug::debug( [
-				'ReviewBonus' => 'get_content',
+			// LegalDebug::debug( [
+			// 	'ReviewBonus' => 'get_content',
 
-				'$node->textContent' => substr( $node->textContent, 0, 30 ),
+			// 	'$node->textContent' => substr( $node->textContent, 0, 30 ),
 	
-				// 'permission_node' => $permission_node,
+			// 	// 'permission_node' => $permission_node,
 				
-				// 'permission_previous' => $permission_previous,
+			// 	// 'permission_previous' => $permission_previous,
 
-				// 'permission_next' => $permission_next,
+			// 	// 'permission_next' => $permission_next,
 
-				// 'permission_replace' => $permission_replace,
-			] );
+			// 	// 'permission_replace' => $permission_replace,
+			// ] );
 
 			// $no_review = self::check_no_review( $class );
 			
