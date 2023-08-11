0af51b56e5d614101a7e838095d0abbff9fe1742
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-08-11 11:31:58 +0300

    mega billet

--- lib/review/ReviewTitle.php
+++ lib/review/ReviewTitle.php
@@ -89,7 +89,7 @@ class ReviewTitle
 	{
 		$xpath = new DOMXPath( $dom );
 
-		return $xpath->query( '//body/h1[contains(@class, \'' . self::CLASSES[ 'date' ] . '\')] | //body/h2[contains(@class, \'' . self::CLASSES[ 'date' ] . '\')]' );
+		return $xpath->query( '//body/*[contains(@class, \'' . self::CLASSES[ 'date-year' ] . '\')] | //body/*[contains(@class, \'' . self::CLASSES[ 'date-month-year' ] . '\')]' );
 	}
 
 	public static function modify_content( $content )
