57cadc4ae9e677c1c62fba1d22d394e8342bbfda
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-11 14:17:11 +0300

    mega billet

--- lib/dom/LegalDOM.php
+++ lib/dom/LegalDOM.php
@@ -2,20 +2,6 @@
 
 class LegalDOM
 {
-	public static function get_dom( $content )
-	{
-		$dom = new DOMDocument();
-
-		if ( !empty( $content ) )
-		{
-			// $dom->loadHTML( $content, LIBXML_NOERROR );
-			
-			$dom->loadHTML( $content, LIBXML_NOERROR | LIBXML_HTML_NOIMPLIED );
-		}
-
-		return $dom;
-	}
-
 	// public static function get_dom( $content )
 	// {
 	// 	$dom = new DOMDocument();
@@ -24,27 +10,41 @@ class LegalDOM
 	// 	{
 	// 		// $dom->loadHTML( $content, LIBXML_NOERROR );
 			
-	// 		// $dom->loadHTML( $content, LIBXML_NOERROR | LIBXML_HTML_NOIMPLIED );
-			
-	// 		$dom->loadHTML( '<div>' . $content . '</div>', LIBXML_NOERROR | LIBXML_HTML_NOIMPLIED );
-
-	// 		$container = $dom->getElementsByTagName( 'div' )->item( 0 );
-
-	// 		$container = $container->parentNode->removeChild( $container );
-
-	// 		while ( $dom->firstChild )
-	// 		{
-	// 			$dom->removeChild( $dom->firstChild );
-	// 		}
-
-	// 		while ( $container->firstChild )
-	// 		{
-	// 			$dom->appendChild( $container->firstChild );
-	// 		}
+	// 		$dom->loadHTML( $content, LIBXML_NOERROR | LIBXML_HTML_NOIMPLIED );
 	// 	}
 
 	// 	return $dom;
 	// }
+
+	public static function get_dom( $content )
+	{
+		$dom = new DOMDocument();
+
+		if ( !empty( $content ) )
+		{
+			// $dom->loadHTML( $content, LIBXML_NOERROR );
+			
+			// $dom->loadHTML( $content, LIBXML_NOERROR | LIBXML_HTML_NOIMPLIED );
+			
+			$dom->loadHTML( '<div>' . $content . '</div>', LIBXML_NOERROR | LIBXML_HTML_NOIMPLIED );
+
+			$container = $dom->getElementsByTagName( 'div' )->item( 0 );
+
+			$container = $container->parentNode->removeChild( $container );
+
+			while ( $dom->firstChild )
+			{
+				$dom->removeChild( $dom->firstChild );
+			}
+
+			while ( $container->firstChild )
+			{
+				$dom->appendChild( $container->firstChild );
+			}
+		}
+
+		return $dom;
+	}
 
 	public static function appendHTML( DOMNode $parent, $content )
 	{
