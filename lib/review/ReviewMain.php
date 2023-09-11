a1a968efaedf10388e163e865344753f0fb8641f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-11 11:37:15 +0300

    mega billet

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -156,15 +156,15 @@ class ReviewMain
 
         ReviewBonus::register();
 
-        // ReviewHowTo::register();
+        ReviewHowTo::register();
 
-        // ReviewBanner::register();
+        ReviewBanner::register();
 
-        // ReviewAuthor::register();
+        ReviewAuthor::register();
 
-        // ReviewCounter::register();
+        ReviewCounter::register();
 
-        ReviewList::register();
+        // ReviewList::register();
 
         ReviewOffers::register();
 
