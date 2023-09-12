653e416c7407215b51a1be6f7b5127158c016790
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-12 16:02:33 +0300

    mega billet

--- single.php
+++ single.php
@@ -13,6 +13,9 @@ get_header();
 <section class="legal-section-header">
 	<?php echo BaseHeader::render(); ?>
 </section>
+<section class="legal-section-breadcrumbs">
+	<?php echo LegalBreadcrumbsMain::render(); ?>
+</section>
 <section class="legal-section-content">
 	<?php echo BonusTemplateSingle::render(); ?>
 </section>
