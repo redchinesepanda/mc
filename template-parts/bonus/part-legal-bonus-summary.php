600f7936575397130f3502237becfe7532dac6b6
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-15 10:39:25 +0300

    bonus full

--- template-parts/bonus/part-legal-bonus-summary.php
+++ template-parts/bonus/part-legal-bonus-summary.php
@@ -1,6 +1,6 @@
 <div class="legal-bonus-summary">
 	<?php foreach ( $args as $key => $item ) : ?>
-		<div class="bonus-summary-item bonus-summary-<?php echo $key; ?>-label">">
+		<div class="bonus-summary-item bonus-summary-<?php echo $key; ?>-label">
 			<div class="summary-<?php echo $key; ?>-label"><?php echo $item[ 'label' ]; ?></div>
 			<div class="summary-<?php echo $key; ?>-value"><?php echo $item[ 'value' ]; ?></div>
 		</div>
