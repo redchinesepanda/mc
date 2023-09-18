525410eea0a29cf9d689b6c54b52aa09c776b4d3
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-18 13:14:21 +0300

    bonus full

--- template-parts/bonus/part-legal-bonus-duration.php
+++ template-parts/bonus/part-legal-bonus-duration.php
@@ -2,6 +2,6 @@
 	<span class="duration-label-title"><?php echo $args[ 'title' ]; ?></span>
 	<?php if ( !empty( $args[ 'duration' ] ) ) : ?>
 		<span class="duration-label-prefix"><?php echo $args[ 'prefix' ]; ?></span>
-		<span class="duration-label-duration"><?php echo $args[ 'duration' ]; ?></span>
+		<span class="duration-label-value"><?php echo $args[ 'duration' ]; ?></span>
 	<?php endif; ?>
 </div>
