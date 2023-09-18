d11c3bd0b2e6bc19cb38aa519c6fb35e38f1b41b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-18 14:15:19 +0300

    bonus full

--- template-parts/bonus/part-legal-bonus-featured.php
+++ template-parts/bonus/part-legal-bonus-featured.php
@@ -1,5 +1,5 @@
-<div class="full-main-featured">
-	<a class="main-featured-image-link" href="<?php echo $args[ 'preview' ][ 'href' ]; ?>">
-		<img class="main-featured-image main-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" />
+<div class="legal-bonus-featured">
+	<a class="bonus-featured-image-link" href="<?php echo $args[ 'preview' ][ 'href' ]; ?>">
+		<img class="bonus-featured-image bonus-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" />
 	</a>
 </div>
