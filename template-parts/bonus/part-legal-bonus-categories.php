682b74b21ebee2a062a1c8a39738724f4f142948
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-19 14:29:09 +0300

    bonus full

--- template-parts/bonus/part-legal-bonus-categories.php
+++ template-parts/bonus/part-legal-bonus-categories.php
@@ -1,5 +1,7 @@
-<div class="legal-bonus-categories">
-	<?php foreach ( $args[ 'items' ] as $item ) : ?>
-		<a class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?>" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
-	<?php endforeach; ?>
-</div>
+<?php if ( !empty( $args[ 'items' ] ) ) : ?>
+	<div class="legal-bonus-categories">
+		<?php foreach ( $args[ 'items' ] as $item ) : ?>
+			<a class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?>" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
+		<?php endforeach; ?>
+	</div>
+<?php endif;?>
