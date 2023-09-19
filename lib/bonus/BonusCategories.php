ac2ae4e7bd4e9580b788ab7bead5f64fbc0d1930
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-19 14:26:44 +0300

    bonus full

---
+++ lib/bonus/BonusCategories.php
@@ -0,0 +1,92 @@
+<?php
+
+class BonusCategories
+{
+	const CSS = [
+        'bonus-categories' => [
+            'path' => LegalMain::LEGAL_URL . '/assets/css/bonus/legal-bonus-categories.css',
+
+            'ver'=> '1.0.0',
+        ],
+    ];
+
+    public static function register_style()
+    {
+        if ( BonusMain::check() ) {
+            ToolEnqueue::register_style( self::CSS );
+        }
+    }
+
+	public static function register()
+    {
+        $handler = new self();
+
+        add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
+    }
+
+    public static function get_categories( $id )
+    {
+        return wp_get_post_categories(
+            $id,
+
+            [ 'fields' => 'ids,names' ]
+        );
+    }
+
+    public static function get_items( $categories )
+    {
+        $items = 0;
+
+        if ( !empty( $categories ) )
+        {
+            foreach ( $categories as $category )
+            {
+                $items[] = [
+                    'id' => $category->term_id,
+                    
+                    'href' => get_category_link( $category->term_id ),
+    
+                    'label' => $category->name,
+                ];
+            }
+        }
+
+        return $items;
+    }
+
+    public static function get()
+    {
+        $id = BonusMain::get_id();
+        
+		if ( empty( $id ) )
+		{
+			return [];
+        }
+
+		return [
+			'items' => self::get_items( self::get_categories( $id ) ),
+		];
+    }
+
+    const TEMPLATE = [
+        'bonus-categories' => LegalMain::LEGAL_PATH . '/template-parts/bonus/part-legal-bonus-categories.php',
+    ];
+
+    public static function render()
+    {
+		if ( !BonusMain::check() )
+        {
+            return '';
+        }
+
+        ob_start();
+
+        load_template( self::TEMPLATE[ 'bonus-categories' ], false, self::get() );
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+}
+
+?>
---
+++ template-parts/bonus/part-legal-bonus-categories.php
@@ -0,0 +1,5 @@
+<div class="legal-bonus-categories">
+	<?php foreach ( $args[ 'items' ] as $item ) : ?>
+		<a class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?>" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
+	<?php endforeach; ?>
+</div>
