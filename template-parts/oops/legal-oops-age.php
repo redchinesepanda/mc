d0362921aef532637ff85d637e1394da6cb8f986
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-20 16:17:11 +0300

    bonus full

--- template-parts/oops/legal-oops-age.php
+++ template-parts/oops/legal-oops-age.php
@@ -2,7 +2,9 @@
 	<div class="legal-oops-age">
 		<span class="oops-age-title"><?php echo $args[ 'title' ]; ?></span>
 		<span class="oops-age-description"><?php echo $args[ 'description' ]; ?></span>
-		<div class="oops-age-button-yes"><?php echo $args[ 'button-yes' ][ 'label' ]; ?></div>
+		<div class="oops-age-button-yes">
+			<span class="age-button-yes-link"><?php echo $args[ 'button-yes' ][ 'label' ]; ?></span>
+		</div>
 		<div class="oops-age-button-no">
 			<a class="age-button-no-link" href="<?php echo $args[ 'button-no' ][ 'href' ]; ?>" rel="nofollow"><?php echo $args[ 'button-no' ][ 'label' ]; ?></a>
 		</div>
