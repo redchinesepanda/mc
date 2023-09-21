a8e36c1cb14dc2dd0b093950e034c70f6da0cd7d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-21 15:17:05 +0300

    bonus full

--- lib/tools/ToolStats.php
+++ lib/tools/ToolStats.php
@@ -11,9 +11,9 @@ class ToolStats
 
 	public static function af_redirect( $post_id, $target_url, $redirect_type )
 	{
-		$url_path = get_post_permalink( $post->ID );
+		$url_path = get_post_permalink( $post_id );
 
-		$url_ref_click = get_field( self::FIELD[ 'bonus-affilate-primary' ], $id );
+		$url_ref_click = get_field( self::FIELD[ 'bonus-affilate-primary' ], $post_id );
 
 		LegalDebug::die( [
 			'function' => 'ToolStats::af_redirect',
