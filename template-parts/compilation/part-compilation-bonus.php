ae57148a7cd72d9c1b9d9318369c516403b9c419
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-25 12:21:29 +0300

    bonus full

--- template-parts/compilation/part-compilation-bonus.php
+++ template-parts/compilation/part-compilation-bonus.php
@@ -5,6 +5,6 @@ LegalDebug::debug( $args );
 ?>
 <div class="legal-compilation-bonus compilation-<?php echo $args['settings']['id']; ?>">
 	<?php foreach( $args['billets'] as $billet ) : ?>
-		<?php BilletMain::render( $billet ); ?>
+		<?php BilletMain::render_bonus( $billet ); ?>
 	<?php endforeach; ?>
 </div>
