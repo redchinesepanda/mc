e00615b8bb9a3a4fe0811768f8330193cb5012cd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-28 13:00:02 +0300

    bonus full

--- lib/wpml/WPMLMain.php
+++ lib/wpml/WPMLMain.php
@@ -182,6 +182,8 @@ class WPMLMain
 
     public static function register()
     {
+        $handler = new self();
+
         WPMLLangSwitcher::register();
 
         WPMLTrid::register();
@@ -189,6 +191,33 @@ class WPMLMain
         WPMLLanguageMismatch::register();
 
         // WPMLMedia::register();
+
+        add_filter('wpml_hreflangs', [ $handler, 'legal_hreflang' ] );
+    }
+
+    public static function legal_hreflang( $hreflang_items )
+    {
+        LegalDebug::debug( [
+            'hreflang_items' => $hreflang_items,
+        ] );
+        // if (empty($hreflang_items['x-default']) && is_singular()) {
+        //     $post_type = 'post_' . get_post_type();
+        //     $translations = apply_filters('wpml_get_element_translations', [], apply_filters('wpml_element_trid', false, get_the_ID(), $post_type), $post_type);
+        //     if (!empty($translations)) {
+        //         foreach ($translations as $lang => $item) {
+        //             if (empty($item->source_language_code)) {
+        //                 $x_default = $lang;
+        //                 break;
+        //             }
+        //         }
+        //     }
+      
+        //     if (isset($x_default, $hreflang_items[$x_default])) {
+        //         $hreflang_items['x-default'] = $hreflang_items[$x_default];
+        //     }
+        // }
+      
+        return $hreflang_items;
     }
 }
 
