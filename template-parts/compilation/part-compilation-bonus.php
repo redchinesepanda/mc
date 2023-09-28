86158ca11385642145b18b1e8f0789d6e9f61d28
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-28 16:14:48 +0300

    bonus full

--- template-parts/compilation/part-compilation-bonus.php
+++ template-parts/compilation/part-compilation-bonus.php
@@ -9,71 +9,69 @@ LegalDebug::debug( $args );
 	<?php endforeach; ?>
 </div>
 
-<div class="legal-compilation-bonus">
-    <div class="legal-compilation-logo">
-        <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
-            <img src="https://match.center/wp-content/uploads/image-64c7f7d930f0e-image-64c7.png" alt="William Hill" draggable="false">
-        </a>
-    </div>
-    <div class="legal-compilation-peculiarity">
-        <div class="legal-compilation-peculiarity-bonys">
-            <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">William Hill: PayPal Betting Site with the Best Football Coverage</a>
-        </div>
-        <div class="legal-compilation-peculiarity-text">
-            18+ To receive 2 x £10 free bets a new user must make a bet of at least £10 qualifying bet on any sport at odds of 1/2 or greater. Only the first single or accumulator bets count.
-        </div>
-    </div>
-    <div class="legal-compilation-review"><a href="https://match.center/william-hill/" draggable="false" class="underline">READ REVIEW</a></div>
-    <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
-</div>
-
-
-<div class="legal-compilation-bonus">
-    <div class="legal-compilation-logo">
-        <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
-            <img src="https://match.center/wp-content/uploads/image-64ae979a7dc17-image-64ae-e1692011317830.webp" alt="William Hill" draggable="false">
-        </a>
-    </div>
-    <div class="legal-compilation-peculiarity">
-        <div class="legal-compilation-peculiarity-bonys">
-            <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">ESC Online</a>
-        </div>
-        <div class="legal-compilation-peculiarity-text">
-            boasts exceptional football, horse racing,
-        </div>
-    </div>
-    <div class="legal-compilation-review"><a href="https://match.center/william-hill/" draggable="false" class="underline">READ REVIEW</a></div>
-    <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
-</div>
-
-<div class="legal-compilation-bonus">
-    <div class="legal-compilation-logo">
-        <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
-            <img src="https://match.center/wp-content/uploads/image-64c7f8ef3ce98-image-64c7.png" alt="William Hill" draggable="false">
-        </a>
-    </div>
-    <div class="legal-compilation-peculiarity">
-        <div class="legal-compilation-peculiarity-bonys">
-            <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">Bwin</a>
-        </div>
-        
-    </div>
-    <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
-</div>
-
-<div class="legal-compilation-bonus">
-    <div class="legal-compilation-logo">
-        <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
-            <img src="https://match.center/wp-content/uploads/image-64c7f96c4e14e-image-64c7.png" alt="William Hill" draggable="false">
-        </a>
-    </div>
-    <div class="legal-compilation-peculiarity">
-        <div class="legal-compilation-peculiarity-bonys">
-            <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">Solverde</a>
-        </div>
-        <div class="legal-compilation-peculiarity-text">
-            The Bet Builder feature allows for unique football
-        </div>
-    </div>
-    <a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
+<div class="legal-compilation-bonus-set">
+	<div class="legal-compilation-bonus">
+		<div class="legal-compilation-logo">
+			<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
+				<img src="https://match.center/wp-content/uploads/image-64c7f7d930f0e-image-64c7.png" alt="William Hill" draggable="false">
+			</a>
+		</div>
+		<div class="legal-compilation-peculiarity">
+			<div class="legal-compilation-peculiarity-bonys">
+				<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">William Hill: PayPal Betting Site with the Best Football Coverage</a>
+			</div>
+			<div class="legal-compilation-peculiarity-text">
+				18+ To receive 2 x £10 free bets a new user must make a bet of at least £10 qualifying bet on any sport at odds of 1/2 or greater. Only the first single or accumulator bets count.
+			</div>
+		</div>
+		<div class="legal-compilation-review"><a href="https://match.center/william-hill/" draggable="false" class="underline">READ REVIEW</a></div>
+		<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
+	</div>
+	<!-- <div class="legal-compilation-bonus">
+		<div class="legal-compilation-logo">
+			<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
+				<img src="https://match.center/wp-content/uploads/image-64ae979a7dc17-image-64ae-e1692011317830.webp" alt="William Hill" draggable="false">
+			</a>
+		</div>
+		<div class="legal-compilation-peculiarity">
+			<div class="legal-compilation-peculiarity-bonys">
+				<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">ESC Online</a>
+			</div>
+			<div class="legal-compilation-peculiarity-text">
+				boasts exceptional football, horse racing,
+			</div>
+		</div>
+		<div class="legal-compilation-review"><a href="https://match.center/william-hill/" draggable="false" class="underline">READ REVIEW</a></div>
+		<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
+	</div>
+	<div class="legal-compilation-bonus">
+		<div class="legal-compilation-logo">
+			<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
+				<img src="https://match.center/wp-content/uploads/image-64c7f8ef3ce98-image-64c7.png" alt="William Hill" draggable="false">
+			</a>
+		</div>
+		<div class="legal-compilation-peculiarity">
+			<div class="legal-compilation-peculiarity-bonys">
+				<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">Bwin</a>
+			</div>
+			
+		</div>
+		<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
+	</div>
+	<div class="legal-compilation-bonus">
+		<div class="legal-compilation-logo">
+			<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
+				<img src="https://match.center/wp-content/uploads/image-64c7f96c4e14e-image-64c7.png" alt="William Hill" draggable="false">
+			</a>
+		</div>
+		<div class="legal-compilation-peculiarity">
+			<div class="legal-compilation-peculiarity-bonys">
+				<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">Solverde</a>
+			</div>
+			<div class="legal-compilation-peculiarity-text">
+				The Bet Builder feature allows for unique football
+			</div>
+		</div>
+		<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
+	</div> -->
 </div>
