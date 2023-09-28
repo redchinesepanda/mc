ffde825c3686651c4001c584bfc754fad39064fa
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-28 16:19:22 +0300

    bonus full

--- template-parts/billet/part-billet-bonus.php
+++ template-parts/billet/part-billet-bonus.php
@@ -5,3 +5,20 @@ LegalDebug::debug( [
 ] );
 
 ?>
+<div class="legal-compilation-bonus legal-compilation-bonus-">
+	<div class="legal-compilation-logo">
+		<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" draggable="false">
+			<img src="https://match.center/wp-content/uploads/image-64c7f7d930f0e-image-64c7.png" alt="William Hill" draggable="false">
+		</a>
+	</div>
+	<div class="legal-compilation-peculiarity">
+		<div class="legal-compilation-peculiarity-bonys">
+			<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">William Hill: PayPal Betting Site with the Best Football Coverage</a>
+		</div>
+		<div class="legal-compilation-peculiarity-text">
+			18+ To receive 2 x £10 free bets a new user must make a bet of at least £10 qualifying bet on any sport at odds of 1/2 or greater. Only the first single or accumulator bets count.
+		</div>
+	</div>
+	<div class="legal-compilation-review"><a href="https://match.center/william-hill/" draggable="false" class="underline">READ REVIEW</a></div>
+	<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-button" draggable="false">VISIT SITE</a>
+</div>
