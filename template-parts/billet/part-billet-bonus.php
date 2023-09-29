60ee020a0e34215b0a696828ccee774a444ea80c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-09-29 09:58:59 +0300

    bonus full

--- template-parts/billet/part-billet-bonus.php
+++ template-parts/billet/part-billet-bonus.php
@@ -13,11 +13,9 @@ LegalDebug::debug( [
 	</div>
 	<div class="legal-compilation-peculiarity">
 		<div class="legal-compilation-peculiarity-bonys">
-			<a href="/go/william-hill-uk/" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false">William Hill: PayPal Betting Site with the Best Football Coverage</a>
+			<a href="<?php echo $args[ 'url' ][ 'title' ]; ?>" rel="nofollow" target="_blank" class="legal-compilation-peculiarity-bonys underline" draggable="false"><?php echo $args[ 'title' ]; ?></a>
 		</div>
-		<div class="legal-compilation-peculiarity-text">
-			18+ To receive 2 x £10 free bets a new user must make a bet of at least £10 qualifying bet on any sport at odds of 1/2 or greater. Only the first single or accumulator bets count.
-		</div>
+		<div class="legal-compilation-peculiarity-text"><?php echo $args[ 'bonus' ][ 'description' ]; ?></div>
 	</div>
 	<div class="legal-compilation-review">
 		<a href="<?php echo $args[ 'billet-logo' ][ 'review' ][ 'href' ]; ?>" draggable="false" class="underline"><?php echo $args[ 'billet-logo' ][ 'review' ][ 'label' ]; ?></a>
