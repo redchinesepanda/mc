231974c8fbdd325e23c768c080ce24d1341bf89c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-02 14:43:52 +0300

    bonus full

--- template-parts/billet/center/part-billet-title.php
+++ template-parts/billet/center/part-billet-title.php
@@ -1,10 +1,10 @@
 <?php
 
-LegalDebug::debug( [
-    'part' => 'part-billet-title.php',
+// LegalDebug::debug( [
+//     'part' => 'part-billet-title.php',
 
-    'args' => $args,
-] );
+//     'args' => $args,
+// ] );
 
 ?>
 <div class="billet-title">
