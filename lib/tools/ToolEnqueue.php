c0419e3a9e6d6119aaca1d0229c2001bb0fdc96d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-02 09:41:09 +0300

    bonus full

--- lib/tools/ToolEnqueue.php
+++ lib/tools/ToolEnqueue.php
@@ -108,13 +108,13 @@ class ToolEnqueue
         global $wp_scripts;
 
         foreach( $wp_scripts->queue as $script ) :
-           $result['scripts'][] =  $wp_scripts->registered[$script]->src . ";";
+           $result['scripts'][ $script ] =  $wp_scripts->registered[$script]->src . ";";
         endforeach;
     
         // Print all loaded Styles (CSS)
 
         global $wp_styles;
-        
+
         foreach( $wp_styles->queue as $style ) :
            $result['styles'][] =  $wp_styles->registered[$style]->src . ";";
         endforeach;
