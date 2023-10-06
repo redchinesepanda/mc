4bbdfb955f4a816deb3e799c77ee40bff10943d5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-06 14:08:23 +0300

    bonus full

---
+++ lib/template/TemplateNotFound.php
@@ -0,0 +1,53 @@
+<?php
+
+class TemplateNotFound
+{
+	// const CSS = [
+    //     'legal-template-bonus' => [
+	// 		'path' => LegalMain::LEGAL_URL . '/assets/css/template/template-bonus.css',
+
+	// 		'ver' => '1.0.7',
+	// 	],
+    // ];
+
+	// public static function register_style()
+    // {
+    //     ToolEnqueue::register_style( self::CSS );
+    // }
+
+	// public static function register()
+    // {
+    //     $handler = new self();
+
+	// 	add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
+    // }
+
+	public static function check()
+    {
+		$permission_category = ToolNotFound::check();
+
+		return $permission_category;
+    }
+
+	const TEMPLATE = [
+        'legal-template-notfound' => LegalMain::LEGAL_PATH . '/template-parts/template/legal-template-notfound.php',
+    ];
+
+	public static function render()
+    {
+		if ( !self::check() )
+        {
+            return '';
+        }
+
+        ob_start();
+		
+		load_template( self::TEMPLATE[ 'legal-template-notfound' ], false, [] );
+
+        $output = ob_get_clean();
+
+        return $output;
+    }
+}
+
+?>
---
+++ template-parts/template/legal-template-notfound.php
@@ -0,0 +1,31 @@
+<?php
+
+get_header();
+
+?>
+<section class="legal-section-header">
+	<?php echo BaseHeader::render(); ?>
+</section>
+<section class="legal-section-breadcrumbs">
+	<?php echo LegalBreadcrumbsMain::render(); ?>
+</section>
+<section class="legal-section-content">
+	<?php echo NotFoundMain::render(); ?>
+</section>
+<section id="theme-bottom-section" class="legal-section-author">
+	<?php echo ReviewAuthor::render(); ?>
+</section>
+<section class="legal-section-footer">
+	<?php echo BaseFooter::render(); ?>
+
+	<?php echo OopsMain::render(); ?>
+
+	<?php echo OopsCookie::render(); ?>
+
+	<?php echo OopsAge::render(); ?>
+</section>
+<?php
+
+get_footer();
+
+?>
