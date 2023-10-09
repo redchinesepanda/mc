ddc3fd98ca12c6c2be838ce05ae041539cc4da7b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-09 11:46:25 +0300

    bonus full

--- template-parts/billet/part-billet-logo.php
+++ template-parts/billet/part-billet-logo.php
@@ -2,7 +2,7 @@
     <div class="billet-order">#<?php echo $args['index']; ?></div>
 <?php endif; ?>
 <a class="legal-logo <?php echo $args['logo']['class']; ?> check-oops" href="<?php echo $args['logo']['href']; ?>" rel="nofollow">
-    <img src="<?php echo $args['logo']['src'] ?>" alt="billet logo" />
+    <img src="<?php echo $args['logo']['src'] ?>" alt="<?php echo $args['logo']['alt'] ?>" />
 </a>
 <a class="legal-review <?php echo $args['review']['class']; ?> <?php echo $args['review']['font']; ?>" href="<?php echo $args['review']['href']; ?>">
     <?php echo $args['review']['label']; ?>
