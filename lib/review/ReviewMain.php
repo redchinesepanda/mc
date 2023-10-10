9a518ed81132b05a8c9c6d6160f8e491ed3e1a47
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-10 14:54:44 +0300

    bonus full

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -88,7 +88,7 @@ class ReviewMain
         'review-table' => [
             'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-table.css',
 
-            'ver' => '1.0.9',
+            'ver' => '1.1.0',
         ],
     ];
 
