99e77a64aed5ff6cbcafb5e373dbcf6f179781d3
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-16 10:15:44 +0300

    bonus full

--- lib/review/ReviewTable.php
+++ lib/review/ReviewTable.php
@@ -120,7 +120,7 @@ class ReviewTable
 					LegalDebug::debug( [
 						'function' => 'ReviewTable::get_content',
 		
-						'textContent' => substr( $call->textContent, 0, 30 ),
+						'textContent' => substr( $row->textContent, 0, 30 ),
 					] );
 				}
 			}
