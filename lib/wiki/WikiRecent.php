1a57a3842d7ea380873d6135d3096acfe943a423
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-19 13:59:17 +0300

    bonus full

--- lib/wiki/WikiRecent.php
+++ lib/wiki/WikiRecent.php
@@ -22,11 +22,93 @@ class WikiRecent
 		add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
     }
 
+	const TAXONOMY = [
+		'category' => 'category',
+	];
+
+	const CATEGORY = [
+		'wiki-tag'
+	];
+
+	public static function query_recent( $id = 0 )
+	{
+		return [
+			'numberposts' => 6,
+
+            'post_type' => [ 'post' ],
+
+            // 'suppress_filters' => 0,
+
+            'exclude' => $id,
+
+            'tax_query' => [
+                [
+                    'taxonomy' => self::TAXONOMY[ 'category' ],
+
+                    'field' => 'slug',
+
+                    'terms' => self::CATEGORY,
+
+					'operator' => 'IN',
+				],
+            ],
+
+            'orderby' => [ 'menu_order' => 'ASC', 'modified' => 'DESC' ],
+		];
+	}
+
+	public static function parse_posts_recent( $posts )
+	{
+		$items = [];
+
+		if ( !empty( $posts ) )
+		{
+			foreach ( $posts as $post )
+			{
+				$group = get_field( self::FIELD[ 'about' ], $post->ID );
+
+				$items[] = [
+					'href' => get_post_permalink( $post->ID ),
+					
+					'title' => $post->post_title,
+				];
+				
+			}
+		}
+
+		return $items;
+	}
+
+	public static function get_recent_items()
+	{
+		$post = get_post();
+
+		if ( !empty( $post ) )
+		{
+			$query = self::query_recent( $post->ID );
+
+			$posts = get_posts( $query );
+
+			return self::parse_posts_recent( $posts );
+		}
+		
+		return [];
+	}
+
+	public static function get_recent_args()
+	{
+		return [
+			'title' => __( WikiMain::TEXT[ 'recent-articles' ], ToolLoco::TEXTDOMAIN ),
+
+			'items' => self::get_recent_items(),
+		];
+	}
+
 	const TEMPLATE = [
         'legal-wiki-recent' => LegalMain::LEGAL_PATH . '/template-parts/wiki/part-legal-wiki-recent.php',
     ];
 
-	public static function render( $args = [] )
+	public static function render()
     {
 		if ( !WikiMain::check() )
         {
@@ -35,7 +117,7 @@ class WikiRecent
 
         ob_start();
 
-        load_template( self::TEMPLATE[ 'legal-wiki-recent' ], false, $args );
+        load_template( self::TEMPLATE[ 'legal-wiki-recent' ], false, self::get_recent() );
 
         $output = ob_get_clean();
 
