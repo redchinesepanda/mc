60276c94b738a3dcf7231e4d99e3998c7872edf8
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-19 16:30:21 +0300

    bonus full

--- template-parts/wiki/part-legal-wiki-featured.php
+++ template-parts/wiki/part-legal-wiki-featured.php
@@ -1,5 +1,3 @@
 <div class="legal-wiki-featured">
-	<a class="wiki-featured-image-link" href="<?php echo $args[ 'preview' ][ 'href' ]; ?>">
-		<img class="wiki-featured-image wiki-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" />
-	</a>
+	<img class="wiki-featured-image wiki-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" />
 </div>
