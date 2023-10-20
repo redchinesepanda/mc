a16583692c03d652bfd0d357da0b07fd484269d5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-20 15:31:25 +0300

    bonus full

--- lib/LegalComponents.php
+++ lib/LegalComponents.php
@@ -106,15 +106,15 @@ class LegalComponents
 		LegalDebug::debug( [
 			'function' => 'check_plugins',
 
-			'check_post_type_page' => $check_post_type_page,
+			'check_post_type_page' => self::check_post_type_page(),
 
-			'check_taxonomy' => $check_taxonomy,
+			'check_taxonomy' => self::check_taxonomy(),
 
-			'LegalMain::check' => $LegalMain::check,
+			'LegalMain::check' => LegalMain::check(),
 
-			'check_post_type_post' => $check_post_type_post,
+			'check_post_type_post' => self::check_post_type_post(),
 
-			'check_not_found' => $check_not_found,
+			'check_not_found' => self::check_not_found(),
 		] );
         
         return (
