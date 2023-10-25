a085f39936534a1bf42463cbc5793eb351fce949
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-25 13:59:41 +0300

    bonus full

--- lib/notion/NotionMain.php
+++ lib/notion/NotionMain.php
@@ -9,6 +9,24 @@ class NotionMain
 		add_action( 'updated_post_meta', [ $handler, 'billet_list' ], 10, 4 );
 	}
 
+	const ACF_FIELD = [
+		'parts' => 'billet-list-parts',
+	]
+
+	const BILLET_LIST_PARTS = [
+		'icon' => 'billet-list-part-icon',
+
+		'direction' => 'billet-list-part-direction',
+
+		'feature' => 'billet-list-part-feature',
+
+		'items' => 'billet-list-part-items',
+	]
+
+	const BILLET_LIST_PART_ITEMS = [
+		'title' => 'billet-list-part-item-title',
+	]
+
 	const META_FIELD = [
 		'list' => 'notion_billet_list',
 	];
@@ -17,6 +35,8 @@ class NotionMain
 	{
 		if ( self::META_FIELD[ 'list' ] == $meta_key )
 		{
+			$notion_lists = (array) json_decode( $meta_value, true );
+
 			LegalDebug::die( [
 				'function' => 'NotionMain::billet_list',
 
@@ -26,8 +46,23 @@ class NotionMain
 
 				// 'meta_key' => $meta_key,
 
-				'meta_value_json_decode' => json_decode( $meta_value, true ),
+				'notion_lists' => $notion_lists,
 			] );
+
+			foreach ( $notion_lists as $notion_list )
+			{
+				$row = [
+					const::BILLET_LIST_PARTS[ 'icon' ] => $notion_list[ const::BILLET_LIST_PARTS[ 'icon' ] ],
+	
+					const::BILLET_LIST_PARTS[ 'direction' ]   => $notion_list[ const::BILLET_LIST_PARTS[ 'direction' ] ],
+	
+					const::BILLET_LIST_PARTS[ 'feature' ]  => $notion_list[ const::BILLET_LIST_PARTS[ 'feature' ] ],
+					
+					const::BILLET_LIST_PARTS[ 'items' ]  => [],
+				];
+				
+				add_row( self::ACF_FIELD[ 'parts' ], $row );
+			}
 		}
 	}
 }
