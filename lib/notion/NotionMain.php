7d26e3311ee78467eb3fd65cd3102cd153c39a78
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-25 14:54:19 +0300

    bonus full

--- lib/notion/NotionMain.php
+++ lib/notion/NotionMain.php
@@ -84,11 +84,11 @@ class NotionMain
 			foreach ( $lists as $list )
 			{
 				$row = [
-					self::BILLET_LIST_PARTS_KEY[ 'icon' ] => $list[ self::BILLET_LIST_PARTS_KEY[ 'icon' ] ],
+					self::BILLET_LIST_PARTS_KEY[ 'icon' ] => $list[ self::BILLET_LIST_PARTS[ 'icon' ] ],
 	
-					self::BILLET_LIST_PARTS_KEY[ 'direction' ]   => $list[ self::BILLET_LIST_PARTS_KEY[ 'direction' ] ],
+					self::BILLET_LIST_PARTS_KEY[ 'direction' ]   => $list[ self::BILLET_LIST_PARTS[ 'direction' ] ],
 	
-					self::BILLET_LIST_PARTS_KEY[ 'feature' ]  => $list[ self::BILLET_LIST_PARTS_KEY[ 'feature' ] ],
+					self::BILLET_LIST_PARTS_KEY[ 'feature' ]  => $list[ self::BILLET_LIST_PARTS[ 'feature' ] ],
 					
 					// self::BILLET_LIST_PARTS[ 'items' ]  => [],
 				];
