e4adfbdac3e9499e75e0e3a4b0802af8de6e7b92
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-25 15:10:07 +0300

    bonus full

--- lib/notion/NotionMain.php
+++ lib/notion/NotionMain.php
@@ -113,24 +113,30 @@ class NotionMain
 
 			foreach ( $lists as $list )
 			{
-				add_row( self::ACF_KEY[ 'parts' ], self::get_row( $list ), $post_id );
+				$row = self::get_row( $list );
+
+				add_row( self::ACF_KEY[ 'parts' ], $row, $post_id );
+
+				LegalDebug::debug( [
+					'row' => $row;
+				] );
 			}
 
 			$field = get_field( self::ACF_FIELD[ 'parts' ], $post_id );
 			
-			// LegalDebug::die( [
-			// 	'function' => 'NotionMain::billet_list',
+			LegalDebug::die( [
+				'function' => 'NotionMain::billet_list',
 
-			// 	'meta_id' => $meta_id,
+				'meta_id' => $meta_id,
 
-			// 	'post_id' => $post_id,
+				'post_id' => $post_id,
 
-			// 	'meta_key' => $meta_key,
+				'meta_key' => $meta_key,
 
-			// 	'lists' => $lists,
+				'lists' => $lists,
 
-			// 	'field' => $field,
-			// ] );
+				'field' => $field,
+			] );
 		}
 	}
 
