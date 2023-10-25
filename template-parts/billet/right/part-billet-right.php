5f10538aac864907b06b044b94228c2001730a37
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-25 09:39:24 +0300

    bonus full

--- template-parts/billet/right/part-billet-right.php
+++ template-parts/billet/right/part-billet-right.php
@@ -1,9 +1,7 @@
 <?php BilletBonus::render( $args ); ?>
 <?php if ( empty( $args[ 'license' ] ) ) : ?>
     <div class="bonus-button">
-        <a class="legal-play <?php echo $args[ 'play' ][ 'class' ]; ?> check-oops" href="<?php echo $args[ 'play' ][ 'href' ]; ?>" rel="nofollow">
-            <?php echo $args[ 'play' ][ 'label' ]; ?>
-        </a>
+        <a class="legal-play <?php echo $args[ 'play' ][ 'class' ]; ?> check-oops" href="<?php echo $args[ 'play' ][ 'href' ]; ?>" <?php echo BilletMain::render_nofollow( $args[ 'nofollow' ] ); ?>><?php echo $args[ 'play' ][ 'label' ]; ?></a>
     </div>
 <?php else : ?>
     <div class="billet-license">
