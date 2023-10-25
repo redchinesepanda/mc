12f3c8a33ffad97d9382673df71db671befc7afa
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-25 13:36:13 +0300

    bonus full

--- lib/notion/NotionMain.php
+++ lib/notion/NotionMain.php
@@ -15,20 +15,20 @@ class NotionMain
 
 	public static function billet_list( $meta_id, $post_id, $meta_key, $meta_value )
 	{
-		// if ( self::META_FIELD[ 'list' ] == $meta_key )
-		// {
-		// 	LegalDebug::debug( [
-		// 		'function' => 'NotionMain::billet_list',
+		if ( self::META_FIELD[ 'list' ] == $meta_key )
+		{
+			LegalDebug::debug( [
+				'function' => 'NotionMain::billet_list',
 
-		// 		'meta_id' => $meta_id,
+				'meta_id' => $meta_id,
 
-		// 		'post_id' => $post_id,
+				'post_id' => $post_id,
 
-		// 		'meta_key' => $meta_key,
+				'meta_key' => $meta_key,
 
-		// 		'meta_value' => $meta_value,
-		// 	] );
-		// }
+				'meta_value' => $meta_value,
+			] );
+		}
 	}
 }
 
