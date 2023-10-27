c90ead17f2aac802b0c95e6ad875d7d01d71e947
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-27 13:29:59 +0300

    notion list

--- template-parts/billet/part-billet-item-bonus.php
+++ template-parts/billet/part-billet-item-bonus.php
@@ -1,6 +1,6 @@
 <div class="legal-compilation-bonus <?php echo $args[ 'selector' ]; ?> font-<?php echo $args[ 'font' ]; ?>" style="background-color: <?php echo $args[ 'color' ]; ?>;">
 	<div class="legal-compilation-logo">
-		<a class="<?php echo $args[ 'logo' ][ 'class' ]; ?> check-oops" href="<?php echo $args[ 'logo' ][ 'href' ]; ?>" rel="nofollow">
+		<a class="<?php echo $args[ 'logo' ][ 'class' ]; ?> check-oops" href="<?php echo $args[ 'logo' ][ 'href' ]; ?>" <?php echo BilletMain::render_nofollow( $args[ 'logo' ][ 'nofollow' ] ); ?>>
 			<img src="<?php echo $args[ 'logo' ][ 'src' ]; ?>" alt="<?php echo $args[ 'logo' ][ 'alt' ]; ?>" width="138" height="45">
 		</a>
 	</div>
