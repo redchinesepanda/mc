4e782e14b5e3514ede994fe28c484620bad3ab36
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-10-30 15:30:01 +0300

    notion list

--- lib/wpml/WPMLTrid.php
+++ lib/wpml/WPMLTrid.php
@@ -26,7 +26,7 @@ class WPMLTrid
             FROM `wp_icl_translations`
             INNER JOIN `wp_posts` ON `element_id` = `ID`
             WHERE
-                `element_type` IN ( 'post_page', 'post_legal_bk_review' )
+                `element_type` IN ( 'post_page', 'legal_billet' )
                 AND `post_status` = 'publish'
             GROUP BY `trid`
             ORDER BY `legal_title`, `legal_elements`";
