3018122a2ff788827b17d3fc9d7a6ce9ed20abce
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-02 13:23:28 +0300

    notion list

--- template-parts/forecast/part-legal-forecast-preview-style.php
+++ template-parts/forecast/part-legal-forecast-preview-style.php
@@ -8,7 +8,7 @@
 
 ?>
 <?php foreach( $args[ 'items' ] as $id => $item ) : ?>
-	.legal-forecast-list legal-forecast-block-<?php echo $item[ 'id' ]; ?> .block-prewiew {
+	.legal-forecast-list .legal-forecast-block-<?php echo $item[ 'id' ]; ?> .block-prewiew {
 		background-image: url('<?php echo $item[ 'preview' ][ 'src' ] ?>');
 	}
 <?php endforeach; ?>
