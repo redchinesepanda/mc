80b4d57cae319cf840e3148336a498a7d6a37e40
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-09 16:17:33 +0300

    notion list

--- lib/bonus/BonusDuration.php
+++ lib/bonus/BonusDuration.php
@@ -117,13 +117,13 @@ class BonusDuration
 	
 				update_field( self::FIELD[ 'bonus-expire' ], $date, $post->ID );
 
-				// LegalDebug::debug( [
-				//     'function' => 'BonusAbout::affiliate_migrate',
+				LegalDebug::debug( [
+				    'function' => 'BonusAbout::affiliate_migrate',
 
-				//     'date_time' => $date_time,
+				    'date_time' => $date_time,
 
-				//     'value' => $value,
-				// ] );
+				    'value' => $value,
+				] );
 			}
         }
     }
