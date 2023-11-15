8865939840afb8c53cc29d21875a301dd5fabf48
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-15 13:08:36 +0300

    notion list

--- template-parts/wiki/part-legal-wiki-featured.php
+++ template-parts/wiki/part-legal-wiki-featured.php
@@ -1,3 +1,5 @@
-<div class="legal-wiki-featured">
-	<img class="wiki-featured-image wiki-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" />
-</div>
+<?php if ( !empty( $args[ 'preview' ] ) ) : ?>
+	<div class="legal-wiki-featured">
+		<img class="wiki-featured-image wiki-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" />
+	</div>
+<?php endif; ?>
