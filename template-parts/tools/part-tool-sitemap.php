8eb2f298cf34a4f72491e02250f686a77f99b3d2
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-17 11:52:15 +0300

    notion list

--- template-parts/tools/part-tool-sitemap.php
+++ template-parts/tools/part-tool-sitemap.php
@@ -1,7 +1,9 @@
 <?php if ( !empty( $args[ 'settings' ][ 'title' ] ) ) : ?>
 	<div class="legal-sitemap-item">
 		<span class="sitemap-item-title"><?php echo $args[ 'settings' ][ 'title' ]; ?></span>
-		<?php echo ToolSitemap::render_items( $args[ 'items' ] ); ?>
+		<?php foreach( $args as $item ) : ?>
+			<a href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
+		<?php endforeach; ?>
 	</div>
 <?php else : ?>
 	<?php echo ToolSitemap::render_items( $args[ 'items' ] ); ?>
