ef26cd49f201657780d4ad45643db93cea12c731
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-20 13:21:33 +0300

    notion list

--- template-parts/review/review-page.php
+++ template-parts/review/review-page.php
@@ -1,11 +1,22 @@
-<div class="legal-bonus-single">
-	<div class="legal-section-anchors">
-		<?php echo ReviewAnchors::render(); ?>
+<section class="legal-section-about">
+	<?php echo ReviewAnchors::render(); ?>
+</section>
+<section class="legal-section-anchors">
+	<?php echo ReviewAnchors::render(); ?>
+</section>
+<section class="legal-section-group">
+	<?php echo ReviewGroup::render(); ?>
+</section>
+<section class="legal-section-content">
+	<div class="legal-bonus-single">
+		<div class="legal-section-anchors">
+			<?php echo ReviewAnchors::render(); ?>
+		</div>
+		<div class="legal-section-group">
+			<?php echo ReviewGroup::render(); ?>
+		</div>
+		<div class="legal-bonus-main">
+			<?php echo ReviewContent::render(); ?>
+		</div>
 	</div>
-	<div class="legal-section-group">
-		<?php echo ReviewGroup::render(); ?>
-	</div>
-	<div class="legal-bonus-main">
-		<?php echo ReviewContent::render(); ?>
-	</div>
-</div>
+</section>
