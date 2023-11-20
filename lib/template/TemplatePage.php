b49c876b26ff51ae728815c9fbf521cd757e1336
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-20 12:51:11 +0300

    notion list

--- lib/template/TemplatePage.php
+++ lib/template/TemplatePage.php
@@ -2,6 +2,26 @@
 
 class TemplatePage
 {
+	const CSS = [
+        'legal-template-page' => [
+			'path' => LegalMain::LEGAL_URL . '/assets/css/template/template-page.css',
+
+			'ver' => '1.0.0',
+		],
+    ];
+
+	public static function register_style()
+    {
+        ToolEnqueue::register_style( self::CSS );
+    }
+
+	public static function register()
+    {
+        $handler = new self();
+
+		add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
+    }
+
 	const TEMPLATE = [
         'legal-template-page' => LegalMain::LEGAL_PATH . '/template-parts/template/legal-template-page.php',
     ];
