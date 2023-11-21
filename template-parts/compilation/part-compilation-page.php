34ba0483cf97481aa0b0e5cf185da1f9aabaf912
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-21 10:59:54 +0300

    notion list

---
+++ template-parts/compilation/part-compilation-page.php
@@ -0,0 +1,16 @@
+<section class="legal-section-anchors">
+	<?php echo ReviewAnchors::render(); ?>
+</section>
+<section class="legal-section-group">
+	<?php echo ReviewGroup::render(); ?>
+</section>
+<section class="legal-section-review-content">
+	<div class="legal-bonus-single">
+		<div class="legal-bonus-main">
+			<?php echo ReviewContent::render(); ?>
+		</div>
+	</div>
+</section>
+<section class="legal-section-offers">
+	<?php echo ReviewOffers::prepare_offers_bottom(); ?>
+</section>
