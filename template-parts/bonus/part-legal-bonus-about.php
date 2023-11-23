9ce8c6390bc37c2249a081042df774856d7a076a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-23 11:52:57 +0300

    notion list

--- template-parts/bonus/part-legal-bonus-about.php
+++ template-parts/bonus/part-legal-bonus-about.php
@@ -1,13 +1,15 @@
-<div class="legal-bonus-about">
-	<div class="bonus-about-data">
-		<h1 class="about-data-title"><?php echo $args[ 'title' ] ?></h1>
-		<span class="about-data-description"><?php echo $args[ 'description' ] ?></span>
+<div class="legal-bonus-about-wrapper">
+	<div class="legal-bonus-about">
+		<div class="bonus-about-data">
+			<h1 class="about-data-title"><?php echo $args[ 'title' ] ?></h1>
+			<span class="about-data-description"><?php echo $args[ 'description' ] ?></span>
+		</div>
+		<div class="bonus-about-action">
+			<img class="about-action-logo" src="<?php echo $args[ 'logo' ][ 'src' ] ?>" alt="<?php echo $args[ 'logo' ][ 'alt' ] ?>" width="130">
+			<?php echo BonusAbout::render_button(); ?>
+		</div>
 	</div>
-	<div class="bonus-about-action">
-		<img class="about-action-logo" src="<?php echo $args[ 'logo' ][ 'src' ] ?>" alt="<?php echo $args[ 'logo' ][ 'alt' ] ?>" width="130">
-		<?php echo BonusAbout::render_button(); ?>
-	</div>
+	<div class="legal-bonus-about-overlay1"></div>
+	<div class="legal-bonus-about-overlay2"></div>
+	<div class="legal-bonus-about-overlay3"></div>
 </div>
-<div class="legal-bonus-about-overlay1"></div>
-<div class="legal-bonus-about-overlay2"></div>
-<div class="legal-bonus-about-overlay3"></div>
