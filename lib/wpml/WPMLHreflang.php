91188f717472fbb86137b24b71516fd42a617e02
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-23 13:43:04 +0300

    notion list

---
+++ lib/wpml/WPMLHreflang.php
@@ -0,0 +1,49 @@
+<?php
+
+class WPMLHreflang
+{
+	public static function register()
+    {
+        $handler = new self();
+
+		add_filter( 'wpml_hreflangs', [ $handler, 'legal_hreflangs' ] );
+    }
+
+	public static function legal_hreflangs( $hreflang_items )
+	{
+		if ( empty( $hreflang_items[ 'x-default' ] ) && is_singular() )
+		{
+			LegalDebug::debug(
+				'function' => 'WPMLHreflang::legal_hreflangs',
+
+				'message' => 'x-default is empty',
+			);
+
+			// $post_type = 'post_' . get_post_type();
+
+			// $translations = apply_filters('wpml_get_element_translations', [], apply_filters('wpml_element_trid', false, get_the_ID(), $post_type), $post_type);
+
+			// if ( !empty( $translations ) )
+			// {
+			// 	foreach ( $translations as $lang => $item )
+			// 	{
+			// 		if ( empty( $item->source_language_code ) )
+			// 		{
+			// 			$x_default = $lang;
+
+			// 			break;
+			// 		}
+			// 	}
+			// }
+	  
+			// if ( isset( $x_default, $hreflang_items[ $x_default ] ) )
+			// {
+			// 	$hreflang_items[ 'x-default' ] = $hreflang_items[ $x_default ];
+			// }
+		}
+	  
+		return $hreflang_items;
+	}
+}
+
+?>
