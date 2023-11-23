f1eff6ae02265e875b2d3ba78eae1cad70b10d94
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-23 16:07:16 +0300

    notion list

--- template-parts/base/part-header-main.php
+++ template-parts/base/part-header-main.php
@@ -1,16 +1,18 @@
 <?php if ( !empty( $args ) ) : ?>
-	<div class="legal-header">
-		<a class="legal-logo" href="<?php echo $args[ 'href' ]; ?>">
-			<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center">
-		</a>
-		<div class="legal-header-control">
-		</div>
-		<?php if ( !empty( $args[ 'items' ] ) ) : ?>
-			<div class="legal-menu">
-				<?php foreach( $args[ 'items' ] as $item ) : ?>
-					<?php echo BaseHeader::render_item( $item ); ?>
-				<?php endforeach; ?>
+	<div class="legal-header-wrapper">
+		<div class="legal-header">
+			<a class="legal-logo" href="<?php echo $args[ 'href' ]; ?>">
+				<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center">
+			</a>
+			<div class="legal-header-control">
 			</div>
-		<?php endif; ?>
+			<?php if ( !empty( $args[ 'items' ] ) ) : ?>
+				<div class="legal-menu">
+					<?php foreach( $args[ 'items' ] as $item ) : ?>
+						<?php echo BaseHeader::render_item( $item ); ?>
+					<?php endforeach; ?>
+				</div>
+			<?php endif; ?>
+		</div>
 	</div>
 <?php endif; ?>
