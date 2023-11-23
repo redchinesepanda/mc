350b452838e1ead477578529affe434a1b5bb266
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-23 12:11:03 +0300

    notion list

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ defined( 'TVE_EDITOR_URL' ) || define( 'TVE_EDITOR_URL', get_template_directory_
 add_action( 'wp_enqueue_scripts', function () {
 	$parent_style = 'parent-style';
 
-	// wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [ $parent_style ], wp_get_theme()->get( 'Version' ) );
 
@@ -15,7 +15,7 @@ add_action( 'wp_enqueue_scripts', function () {
 
 	// ToolEnqueue::dequeue_style( TemplateMain::DEQUEUE );
 
-	wp_dequeue_style( $parent_style );
+	// wp_dequeue_style( $parent_style );
 } );
 
 require_once( 'lib/LegalMain.php' );
