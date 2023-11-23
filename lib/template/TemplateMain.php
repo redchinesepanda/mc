9bd11648c9bcce38b45f6960f0a2eb51ab586a77
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-23 12:29:41 +0300

    notion list

--- lib/template/TemplateMain.php
+++ lib/template/TemplateMain.php
@@ -33,6 +33,10 @@ class TemplateMain
         wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     
         wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [ $parent_style ], wp_get_theme()->get( 'Version' ) );
+
+        wp_dequeue_style( 'child-style' );
+
+        wp_dequeue_style( 'parent-style' );
     }
 
     const DEQUEUE = [
