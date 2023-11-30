2a7b70322c4d9fb149faae333bf0f2fe9fad01d9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-11-30 11:54:29 +0300

    notion list

--- lib/review/ReviewTable.php
+++ lib/review/ReviewTable.php
@@ -10,7 +10,7 @@ class ReviewTable
         self::HANDLE[ 'table' ] => [
             'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-table.css',
 
-            'ver' => '1.1.7',
+            'ver' => '1.1.8',
         ],
     ];
 
