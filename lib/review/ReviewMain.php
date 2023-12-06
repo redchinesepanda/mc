8f1c0abb58b6b15ae1aba4ba89968bc6f97ce035
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-06 13:23:07 +0300

    notion list

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -104,6 +104,16 @@ class ReviewMain
         // '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">'
     ];
 
+    public static function style_attributes( $html, $handle )
+    {
+        if ( 'bootstrap-main' === $handle )
+        {
+            return str_replace( '>', 'integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous"', $html );
+        }
+
+        return $html;
+    }
+
     public static function register_style( $styles = [] )
     {
         if ( self::check() ) {
@@ -174,6 +184,8 @@ class ReviewMain
         add_action( 'wp_enqueue_scripts', [ $handler, 'register_style' ] );
 
         add_action( 'wp_head', [ $handler, 'print' ] );
+
+        add_filter( 'style_loader_tag', [ $handler, 'style_attributes' ], 10, 2 );
 
         // add_filter( 'content_save_pre' , [ $handler, 'encoding' ], 10, 1);
 
