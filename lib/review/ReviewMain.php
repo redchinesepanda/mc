832ce8f4641b07bab866005cb4cbc66047f64e02
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-06 13:36:52 +0300

    notion list

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -95,22 +95,24 @@ class ReviewMain
             'ver' => '1.0.1',
         ],
 
-        'bootstrap-main' => [
-            'path' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css',
+        // <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-            'ver' => '1.0.0',
-        ],
+        // 'bootstrap-main' => [
+        //     'path' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css',
+
+        //     'ver' => '1.0.0',
+        // ],
     ];
 
-    public static function style_attributes( $html, $handle )
-    {
-        if ( 'bootstrap-main' === $handle )
-        {
-            return str_replace( ">", "media='all' integrity='sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx' crossorigin='anonymous'>", $html );
-        }
+    // public static function style_attributes( $html, $handle )
+    // {
+    //     if ( 'bootstrap-main' === $handle )
+    //     {
+    //         return str_replace( ">", "media='all' integrity='sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx' crossorigin='anonymous'>", $html );
+    //     }
 
-        return $html;
-    }
+    //     return $html;
+    // }
 
     public static function register_style( $styles = [] )
     {
