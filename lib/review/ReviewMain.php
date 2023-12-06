0516f4fef72b6a8c647065ed74e1905fc940a67b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-06 13:25:57 +0300

    notion list

--- lib/review/ReviewMain.php
+++ lib/review/ReviewMain.php
@@ -108,6 +108,10 @@ class ReviewMain
     {
         if ( 'bootstrap-main' === $handle )
         {
+            LegalDebug::debug( [
+                $html,
+            ] );
+
             return str_replace( "media='all'", "media='all' integrity='sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx' crossorigin='anonymous'", $html );
         }
 
