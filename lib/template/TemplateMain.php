5f6eedc2362a7b1f2cfa326fd5ce60f5cc6d41c1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-07 09:36:04 +0300

    notion list

--- lib/template/TemplateMain.php
+++ lib/template/TemplateMain.php
@@ -118,7 +118,7 @@ class TemplateMain
 
 		add_action( 'tcb_lightspeed_has_optimized_assets', [ $handler, 'tcb_optimized_assets' ] );
 
-        remove_action( 'wp_head', [ '\TCB\Lightspeed\Hooks', 'insert_optimization_script' ] );
+        remove_action( 'wp_head', [ '\TCB\Lightspeed\Hooks', 'insert_optimization_script' ], - 23 );
 
         TemplatePage::register();
 
