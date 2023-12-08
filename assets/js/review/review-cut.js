55b0fdee80ecc48aa5a6ae3ee6dc8d98a040079a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-08 15:34:57 +0300

    notion list

--- assets/js/review/review-cut.js
+++ assets/js/review/review-cut.js
@@ -11,7 +11,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		// } );
 	}
 
-	function prepareControl ( setID )
+	function prepareControl ( setID, element )
 	{
 		console.log( 'setID: ' + setID );
 
