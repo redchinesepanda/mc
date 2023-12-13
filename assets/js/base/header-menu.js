351f170718c1e3578268d8206fc3a54ddccaa899
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-13 10:21:53 +0300

    notion list

--- assets/js/base/header-menu.js
+++ assets/js/base/header-menu.js
@@ -66,7 +66,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		
 		group.forEach( appendAll, element );
 
-		subMenu.appendChild( element );
+		this.appendChild( element );
 
 		// console.log( this );
 
