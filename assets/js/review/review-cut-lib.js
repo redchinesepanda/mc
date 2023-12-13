2dc4177f0315fa98778d11d5e6151031e08ecdbd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-13 14:13:41 +0300

    notion list

--- assets/js/review/review-cut-lib.js
+++ assets/js/review/review-cut-lib.js
@@ -1,96 +1,96 @@
 // lib-cookie start
 
-// let legalCut = {
-// 	id : 1,
+let legalCut = {
+	id : 1,
 
-// 	cutSetId : 1,
+	cutSetId : 1,
 
-// 	// toggleItem: function ( element )
+	// toggleItem: function ( element )
 
-// 	toggleItem : function ( element )
-// 	{
-// 		element.classList.toggle( 'legal-active' );
-// 	},
+	toggleItem : function ( element )
+	{
+		element.classList.toggle( 'legal-active' );
+	},
 
-// 	// toggleDataset: function ( event )
+	// toggleDataset: function ( event )
 
-// 	toggleDataset : function ( event )
-// 	{
-// 		event.currentTarget.classList.toggle( 'legal-active' );
+	toggleDataset : function ( event )
+	{
+		event.currentTarget.classList.toggle( 'legal-active' );
 
-// 		event.currentTarget.parentElement.querySelectorAll(
-// 			'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
-// 		)
-// 		.forEach( this.toggleItem );
-// 	}, 
+		event.currentTarget.parentElement.querySelectorAll(
+			'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+		)
+		.forEach( this.toggleItem );
+	}, 
 
-// 	// prepareControl : function ( element )
+	// prepareControl : function ( element )
 
-// 	prepareControl ( element )
-// 	{
-// 		element.dataset.cutSetId = this.id;
+	prepareControl ( element )
+	{
+		element.dataset.cutSetId = this.id;
 
-// 		console.log( 'this.id: ' + this.id );
+		console.log( 'this.id: ' + this.id );
 		
-// 		console.log( 'this.cutSetId: ' + this.cutSetId );
+		console.log( 'this.cutSetId: ' + this.cutSetId );
 		
-// 		if ( element.classList.contains( 'legal-cut-control' ) )
-// 		{
-// 			element.addEventListener( 'click', this.toggleDataset, false );
-
-// 			this.id++;
-
-// 			this.cutSetId++;
-// 		}
-// 	}
-// };
-
-let legalCut = ( function()
-{
-	"use strict";
-
-	return {
-		cutSetId : 0,
-
-		name : 0,
-
-		// toggleItem: function ( element )
-
-		toggleItem : function ( element )
+		if ( element.classList.contains( 'legal-cut-control' ) )
 		{
-			element.classList.toggle( 'legal-active' );
-		},
+			element.addEventListener( 'click', this.toggleDataset, false );
 
-		// toggleDataset: function ( event )
+			this.id++;
 
-		toggleDataset : function ( event )
-		{
-			event.currentTarget.classList.toggle( 'legal-active' );
-
-			event.currentTarget.parentElement.querySelectorAll(
-				'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
-			)
-			.forEach( this.toggleItem );
-		}, 
-
-		// prepareControl : function ( element )
-
-		prepareControl : function ( element )
-		{
-			// element.dataset.cutSetId = this.cutSetId;
-			
-			element.dataset.cutSetId = this.name;
-			
-			if ( element.classList.contains( 'legal-cut-control' ) )
-			{
-				element.addEventListener( 'click', this.toggleDataset, false );
-
-				// this.cutSetId++;
-				
-				this.name++;
-			}
+			this.cutSetId++;
 		}
-	};
-} )();
+	}
+};
+
+// let legalCut = ( function()
+// {
+// 	"use strict";
+
+// 	return {
+// 		cutSetId : 0,
+
+// 		name : 0,
+
+// 		// toggleItem: function ( element )
+
+// 		toggleItem : function ( element )
+// 		{
+// 			element.classList.toggle( 'legal-active' );
+// 		},
+
+// 		// toggleDataset: function ( event )
+
+// 		toggleDataset : function ( event )
+// 		{
+// 			event.currentTarget.classList.toggle( 'legal-active' );
+
+// 			event.currentTarget.parentElement.querySelectorAll(
+// 				'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+// 			)
+// 			.forEach( this.toggleItem );
+// 		}, 
+
+// 		// prepareControl : function ( element )
+
+// 		prepareControl : function ( element )
+// 		{
+// 			// element.dataset.cutSetId = this.cutSetId;
+			
+// 			element.dataset.cutSetId = this.name;
+			
+// 			if ( element.classList.contains( 'legal-cut-control' ) )
+// 			{
+// 				element.addEventListener( 'click', this.toggleDataset, false );
+
+// 				// this.cutSetId++;
+				
+// 				this.name++;
+// 			}
+// 		}
+// 	};
+// } )();
 
 // lib-cookie end
