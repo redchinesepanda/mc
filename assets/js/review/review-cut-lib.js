1adaf4eac8730d2a1aa3acc13ab87acb8e651e66
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-13 14:03:29 +0300

    notion list

--- assets/js/review/review-cut-lib.js
+++ assets/js/review/review-cut-lib.js
@@ -1,45 +1,82 @@
 // lib-cookie start
 
-let LegalCut = ( function()
-{
-	"use strict";
+let LegalCut = {
+	cutSetId : 0,
 
-	return {
-		cutSetId : 0,
+	// toggleItem: function ( element )
 
-		// toggleItem: function ( element )
+	toggleItem : function ( element )
+	{
+		element.classList.toggle( 'legal-active' );
+	},
 
-		toggleItem : function ( element )
+	// toggleDataset: function ( event )
+
+	toggleDataset : function ( event )
+	{
+		event.currentTarget.classList.toggle( 'legal-active' );
+
+		event.currentTarget.parentElement.querySelectorAll(
+			'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+		)
+		.forEach( this.toggleItem );
+	}, 
+
+	// prepareControl : function ( element )
+
+	prepareControl : function ( element )
+	{
+		element.dataset.cutSetId = this.cutSetId;
+		
+		if ( element.classList.contains( 'legal-cut-control' ) )
 		{
-			element.classList.toggle( 'legal-active' );
-		},
+			element.addEventListener( 'click', this.toggleDataset, false );
 
-		// toggleDataset: function ( event )
-
-		toggleDataset : function ( event )
-		{
-			event.currentTarget.classList.toggle( 'legal-active' );
-
-			event.currentTarget.parentElement.querySelectorAll(
-				'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
-			)
-			.forEach( this.toggleItem );
-		}, 
-
-		// prepareControl : function ( element )
-
-		prepareControl : function ( element )
-		{
-			element.dataset.cutSetId = this.cutSetId;
-			
-			if ( element.classList.contains( 'legal-cut-control' ) )
-			{
-				element.addEventListener( 'click', this.toggleDataset, false );
-
-				this.cutSetId++;
-			}
+			this.cutSetId++;
 		}
-	};
-} )();
+	}
+};
+
+// let LegalCut = ( function()
+// {
+// 	"use strict";
+
+// 	return {
+// 		cutSetId : 0,
+
+// 		// toggleItem: function ( element )
+
+// 		toggleItem : function ( element )
+// 		{
+// 			element.classList.toggle( 'legal-active' );
+// 		},
+
+// 		// toggleDataset: function ( event )
+
+// 		toggleDataset : function ( event )
+// 		{
+// 			event.currentTarget.classList.toggle( 'legal-active' );
+
+// 			event.currentTarget.parentElement.querySelectorAll(
+// 				'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+// 			)
+// 			.forEach( this.toggleItem );
+// 		}, 
+
+// 		// prepareControl : function ( element )
+
+// 		prepareControl : function ( element )
+// 		{
+// 			element.dataset.cutSetId = this.cutSetId;
+			
+// 			if ( element.classList.contains( 'legal-cut-control' ) )
+// 			{
+// 				element.addEventListener( 'click', this.toggleDataset, false );
+
+// 				this.cutSetId++;
+// 			}
+// 		}
+// 	};
+// } )();
 
 // lib-cookie end
