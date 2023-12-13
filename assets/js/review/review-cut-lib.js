74c9ba56e375cd930d3688a7033dfed977be6d81
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-13 12:33:10 +0300

    notion list

---
+++ assets/js/review/review-cut-lib.js
@@ -0,0 +1,37 @@
+// lib-cookie start
+
+let LegalCut = ( function()
+{
+	"use strict";
+
+	return {
+		toggleItem: function ( element )
+		{
+			element.classList.toggle( 'legal-active' );
+		},
+
+		toggleDataset: function ( event )
+		{
+			event.currentTarget.classList.toggle( 'legal-active' );
+
+			event.currentTarget.parentElement.querySelectorAll(
+				'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+			)
+			.forEach( toggleItem );
+		}, 
+
+		prepareControl : function ( element )
+		{
+			element.dataset.cutSetId = this.id;
+			
+			if ( element.classList.contains( 'legal-cut-control' ) )
+			{
+				element.addEventListener( 'click', toggleDataset, false );
+
+				this.id++;
+			}
+		}
+	};
+} )();
+
+// lib-cookie end
---
+++ assets/js/review/review-cut-main.js
@@ -0,0 +1,45 @@
+// review-cut-js start
+
+document.addEventListener( 'DOMContentLoaded', function ()
+{
+	// function toggleItem( element )
+	// {
+	// 	element.classList.toggle( 'legal-active' );
+	// }
+
+	// function toggleDataset( event )
+	// {
+	// 	event.currentTarget.classList.toggle( 'legal-active' );
+
+	// 	event.currentTarget.parentElement.querySelectorAll(
+	// 		'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+	// 	)
+	// 	.forEach( toggleItem );
+	// }
+
+	// function prepareControl( element )
+	// {
+	// 	element.dataset.cutSetId = setID;
+		
+	// 	if ( element.classList.contains( 'legal-cut-control' ) )
+	// 	{
+	// 		element.addEventListener( 'click', toggleDataset, false );
+
+	// 		setID++;
+	// 	}
+	// }
+
+	let setData = {
+		id : 0
+	};
+	
+	document.querySelectorAll(
+		// '.tcb-post-content > .legal-cut-item, .tcb-post-content > .legal-cut-control'
+		
+		'.tcb-post-content > .legal-cut-item, .tcb-post-content > .legal-cut-control, .legal-menu .legal-cut-item, .legal-menu .legal-cut-control'
+	)
+	// .forEach( prepareControl );
+	.forEach( LegalCut.prepareControl, setData );
+} );
+
+// review-cut-js end
