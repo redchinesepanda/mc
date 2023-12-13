0a7ef2d52ceb4e4924273dda8e6507a71bfc0f9f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-13 14:18:35 +0300

    notion list

--- assets/js/review/review-cut-main.js
+++ assets/js/review/review-cut-main.js
@@ -2,19 +2,46 @@
 
 document.addEventListener( 'DOMContentLoaded', function ()
 {
+	function toggleItem( element )
+	{
+		element.classList.toggle( 'legal-active' );
+	}
+
+	function toggleDataset( event )
+	{
+		event.currentTarget.classList.toggle( 'legal-active' );
+
+		event.currentTarget.parentElement.querySelectorAll(
+			'.legal-cut-item[data-cut-set-id="' + event.currentTarget.dataset.cutSetId + '"]'
+		)
+		.forEach( toggleItem );
+	}
+
+	function prepareControl( element )
+	{
+		element.dataset.cutSetId = setID;
+		
+		if ( element.classList.contains( 'legal-cut-control' ) )
+		{
+			element.addEventListener( 'click', toggleDataset, false );
+
+			setID++;
+		}
+	}
+
+	let setID = 0;
+
 	// let setData = {
 	// 	id : 0
 	// };
-
-	let cut = legalCut;
 	
 	document.querySelectorAll(
 		// '.tcb-post-content > .legal-cut-item, .tcb-post-content > .legal-cut-control'
 		
 		'.tcb-post-content > .legal-cut-item, .tcb-post-content > .legal-cut-control, .legal-menu .legal-cut-item, .legal-menu .legal-cut-control'
 	)
-	// .forEach( prepareControl );
-	.forEach( cut.prepareControl );
+	.forEach( prepareControl );
+	// .forEach( LegalCut.prepareControl, setData );
 } );
 
 // review-cut-js end
