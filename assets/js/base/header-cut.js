df4b5aed333cbae11cd41f577431fdadfed2ae1e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-14 12:34:44 +0300

    notion list

--- assets/js/base/header-cut.js
+++ assets/js/base/header-cut.js
@@ -23,7 +23,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
     function prepareItems( element )
 	{
-		// console.log( element.querySelectorAll( elements.item.selectors ).length );
+		console.log( element.querySelectorAll( elements.item.selectors ).length );
 
         if ( element.querySelectorAll( elements.item.selectors ).length > 6 )
         {
@@ -40,7 +40,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
     function forgetItems( element )
 	{
-		console.log( element.querySelectorAll( elements.item.selectors ) );
+		// console.log( element.querySelectorAll( elements.item.selectors ) );
 
         element.querySelectorAll( elements.item.selectors ).forEach( forgetItem );
 	}
