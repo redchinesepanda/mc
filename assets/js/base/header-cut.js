48cc85318282060647974893d639660d616bf6dc
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-14 12:47:29 +0300

    notion list

--- assets/js/base/header-cut.js
+++ assets/js/base/header-cut.js
@@ -61,7 +61,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		// },
 
 		cutControl : {
-			selectors : ':scope > .legal-cut-control'
+			selectors : ':scope .legal-cut-control'
 		}
 	};
 
