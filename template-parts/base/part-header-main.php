57dd5591095ebd211d3b2d8110ba6ac703231948
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-14 15:34:21 +0300

    notion list

--- template-parts/base/part-header-main.php
+++ template-parts/base/part-header-main.php
@@ -1,13 +1,6 @@
 <?php if ( !empty( $args ) ) : ?>
 	<div class="legal-header-wrapper">
 		<div class="legal-header">
-			<!-- <a class="legal-logo" href="<?php echo $args[ 'href' ]; ?>">
-				<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/mc-logo.png" width="213" height="21" alt="Match.Center">
-				<picture>
-            		<source srcset="/wp-content/themes/thrive-theme-child/assets/img/base/header/header-logo-mc-mobile.svg" alt="Match.Center" media="(max-width: 767px)">
-            		<img src="/wp-content/themes/thrive-theme-child/assets/img/base/header/header-logo-mc-desktop.svg" alt="Match.Center">
-        		</picture>
-			</a> -->
 			<?php echo BaseHeader::render_logo( $args[ 'logo' ] ); ?>
 			<div class="legal-header-control">
 			</div>
