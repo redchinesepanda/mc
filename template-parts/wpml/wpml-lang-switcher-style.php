09743e125b450ae9583e3759326f90c0531be517
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-15 12:35:52 +0300

    notion list

--- template-parts/wpml/wpml-lang-switcher-style.php
+++ template-parts/wpml/wpml-lang-switcher-style.php
@@ -1,6 +1,9 @@
 .lang-switcher .locale-<?php echo $args['active']['id']; ?> {
     background-image: url('<?php echo $args['active']['src']; ?>');
 }
+.lang-switcher .lang-current .lang-title::before {
+    background-image: url('<?php echo $args['active']['src']; ?>');
+}
 <?php foreach( $args['languages'] as $lang ) : ?>
     .lang-switcher .locale-<?php echo $lang['id']; ?> {
         background-image: url('<?php echo $lang['src']; ?>');
