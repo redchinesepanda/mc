1ab29a225c3a3f1461b65ee6a5c1850be0d4273e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-19 11:07:50 +0300

    notion list

--- lib/billet/center/BilletList.php
+++ lib/billet/center/BilletList.php
@@ -30,32 +30,6 @@ class BilletList
         return [];
     }
 
-    public static function parse_items( $items )
-    {
-        return array_column( $items, self::ITEM[ 'title' ] );
-    }
-    
-    public static function parse_lists( $lists )
-    {
-        $result = [];
-
-        if ( $lists )
-        {
-            foreach ( $lists as $list )
-            {
-                $result[] = [
-                    'part-icon' => $list[ self::LIST[ 'icon' ] ],
-
-                    'part-direction' => $list[ self::LIST[ 'direction' ] ],
-
-                    'part-items' => self::parse_items( $list[ self::LIST[ 'items' ] ] ),
-                ];
-            }
-        }
-
-        return $result;
-    }
-
     public static function check_list_feature_has( $list, $feature )
 	{
 		return strpos( $list[ 'default_locale' ], $value ) !== false;
@@ -63,20 +37,8 @@ class BilletList
 
     public static function filter_lists_feature_has( $lists, $features )
     {
-        // LegalDebug::debug( [
-        //     'function' => 'BilletList::filter_lists_feature_has',
-
-        //     'features' => $features,
-        // ] );
-
         return array_filter( $lists, function( $list ) use ( $features )
         {
-            // LegalDebug::debug( [
-            //     'function' => 'BilletList::array_filter',
-    
-            //     'list' => $list,
-            // ] );
-
             if ( empty( $list[ self::LIST[ 'feature' ] ] ) )
             {
                 return false;
@@ -104,10 +66,34 @@ class BilletList
         return array_filter( $lists, [ $handler, 'check_feature_empty' ] );
     }
 
+    public static function parse_items( $items )
+    {
+        return array_column( $items, self::ITEM[ 'title' ] );
+    }
+    
+    public static function parse_lists( $lists )
+    {
+        $result = [];
+
+        if ( $lists )
+        {
+            foreach ( $lists as $list )
+            {
+                $result[] = [
+                    'part-icon' => $list[ self::LIST[ 'icon' ] ],
+
+                    'part-direction' => $list[ self::LIST[ 'direction' ] ],
+
+                    'part-items' => self::parse_items( $list[ self::LIST[ 'items' ] ] ),
+                ];
+            }
+        }
+
+        return $result;
+    }
+
     public static function get( $billet )
     {
-        // $args = [];
-
         $lists = get_field( self::FIELD[ 'lists' ], $billet[ 'id' ] );
 
         $features = self::get_features( $billet );
@@ -119,61 +105,12 @@ class BilletList
             $result = self::filter_lists_feature_has( $lists, $features );
         }
 
-        // LegalDebug::debug( [
-        //     'function' => 'BilletList::get',
-
-        //     'result' => $result,
-        // ] );
-
         if ( empty( $result ) )
         {
             $result = self::filter_lists_feature_empty( $lists );
         }
-
-        // return self::parse_lists( $result, $billet );
         
-        return self::parse_lists( $result ); 
-
-        // if ( $lists )
-        // {
-        //     foreach ( $lists as $key => $list )
-        //     {
-        //         // LegalDebug::debug( [
-        //         //     'function' => 'BilletList::get',
-
-        //         //     'parts' => $parts,
-
-        //         //     'parts_json_encode' => json_encode( $parts ),
-        //         // ] );
-
-        //         $display = self::check_list( $billet, $list );
-
-        //         if ( $display )
-        //         {
-        //             // $args[ $key ][ 'part-icon' ] = $part[ self::PART[ 'icon' ] ];
-    
-        //             // $args[ $key ]['part-direction' ] = $part[ self::PART[ 'direction' ] ];
-    
-        //             // $items = $part[ self::PART[ 'items' ] ];
-    
-        //             // if ( $items ) {
-        //             //     foreach ( $items as $item ) {
-        //             //         $args[ $key ][ 'part-items' ][] = $item[ self::ITEM[ 'title' ] ];
-        //             //     }
-        //             // }
-
-        //             $args[ $key ] = [
-        //                 'part-icon' => $list[ self::LIST[ 'icon' ] ],
-
-        //                 'part-direction' => $list[ self::LIST[ 'direction' ] ],
-
-        //                 'part-items' => self::parse_items( $list[ self::LIST[ 'items' ] ] ),
-        //             ];
-        //         }
-        //     }
-        // }
-
-        // return $args;
+        return self::parse_lists( $result );
     }
 
     const TEMPLATE = [
@@ -182,8 +119,6 @@ class BilletList
 
     public static function render( $billet )
     { 
-        // load_template( self::TEMPLATE[ 'lists' ], false, self::get( $billet ) );
-
         return self::render_main( self::TEMPLATE[ 'lists' ], self::get( $billet ) );
     }
 
