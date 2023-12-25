3b6cd5b0e8132482d699bb734e839616b86454c4
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2023-12-25 13:14:45 +0300

    notion list

--- lib/review/ReviewTable.php
+++ lib/review/ReviewTable.php
@@ -14,9 +14,24 @@ class ReviewTable
         ],
     ];
 
+	const CSS_NEW = [
+        self::HANDLE[ 'table' ] => [
+            'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-table-new.css',
+
+            'ver' => '1.0.0',
+        ],
+    ];
+
 	public static function register_style()
     {
-        ReviewMain::register_style( self::CSS );
+		if ( TemplateMain::check_code() )
+		{
+			ReviewMain::register_style( self::CSS_NEW );
+		}
+		else
+		{
+			ReviewMain::register_style( self::CSS );
+		}
     }
 
 	public static function register_functions()
