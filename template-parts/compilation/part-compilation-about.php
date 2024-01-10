a099826dbc89a80d241979c992b67311e673eef9
sergeykalabin <sergey.kalabin@legalbet.com>
2024-01-10 16:44:45 +0400

    commit frontend

--- template-parts/compilation/part-compilation-about.php
+++ template-parts/compilation/part-compilation-about.php
@@ -7,26 +7,24 @@
 ?>
 <!-- Compilation About Template -->
 <?php if ( !empty( $args ) ) : ?>
-	<div class="legal-section-main-screen">
-		<div class="legal-main-screen-wrapper">
-			<div class="legal-main-screen">
-				<div class="block-title-text">
-					<h1><?php echo $args[ 'title' ]; ?></h1>
+    <div class="compilation-about-wrapper">
+        <div class="compilation-about">
+            <div class="block-title-text">
+                <h1><?php echo $args[ 'title' ]; ?></h1>
 
-					<?php foreach ( $args[ 'content' ] as $item ) : ?>
+                <?php foreach ( $args[ 'content' ] as $item ) : ?>
 
-						<p class="text <?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p>
+                    <p class="text <?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p>
 
-					<?php endforeach; ?>
+                <?php endforeach; ?>
 
-					<span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0"></span>
-				</div>
+                <span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0"></span>
+            </div>
 
-				<div class="block-frame">
-					<img class="frame" src="https://test.match.center/wp-content/themes/thrive-theme-child/assets/img/compilation/compilation-frame-start-screen.png">
-				</div>
+            <div class="block-frame">
+                <img class="frame" src="https://test.match.center/wp-content/themes/thrive-theme-child/assets/img/compilation/compilation-frame-start-screen.png">
+            </div>
 
-			</div>
-		</div>
-	</div>
+        </div>
+    </div>
 <?php endif; ?>
