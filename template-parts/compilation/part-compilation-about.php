85adeafe6ec2aeaea0862dda8398ecec0fe94cd7
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-10 14:07:41 +0300

    notion list

--- template-parts/compilation/part-compilation-about.php
+++ template-parts/compilation/part-compilation-about.php
@@ -6,16 +6,18 @@ LegalDebug::debug( [
 
 ?>
 <!-- Compilation About Template -->
+<?php if ( !empty( $args ) ) : ?>
 <div class="legal-section-main-screen">
     <div class="legal-main-screen-wrapper">
         <div class="legal-main-screen">
-
             <div class="block-title-text">
-                <h1>Apps de Apostas Desportivas em Portugal</h1>
+                <h1><?php echo $args[ 'title' ]; ?></h1>
 
-                <p class="text">As apps de apostas desportivas são uma alternativa para os utilizadores que desejam apostar por meio de dispositivos móveis, tais como telemóveis e tablets. A app de apostas facilita o acesso dos utilizadores, pois permitem que eles façam apostas a qualquer momento de diferentes lugares, sem precisar de um computador de mesa ou um laptop.</p>
+				<?php foreach ( $args[ 'content' ] as $item ) : ?>
 
-                <p class="text legal-cut-item">Uma app de apostas desportivas não é interessante apenas para os clientes, mas para os sites de apostas também. É mais engajamento e lucro, pois são mais apostadores, mesmo com os dispositivos móveis. Por isso, são várias apps de apostas disponíveis cá em Portugal. Continue a ler este nosso artigo para conhecer um pouco mais destas aplicações de apostas online.</p>
+                	<p class="text"><?php echo $item; ?></p>
+
+				<?php endforeach; ?>
 
                 <span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0"></span>
             </div>
@@ -27,3 +29,4 @@ LegalDebug::debug( [
         </div>
     </div>
 </div>
+<?php endif; ?>
