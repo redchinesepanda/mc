ab7d797bffc2263f1b912ad883f5efd59e7743ed
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-11 12:38:36 +0300

    notion list

--- template-parts/compilation/part-compilation-about.php
+++ template-parts/compilation/part-compilation-about.php
@@ -11,22 +11,16 @@ LegalDebug::debug( [
         <div class="compilation-about">
             <div class="block-title-text">
                 <h1><?php echo $args[ 'title' ]; ?></h1>
-
                 <?php foreach ( $args[ 'content' ] as $item ) : ?>
-
                     <p class="text <?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p>
-
                 <?php endforeach; ?>
-
-                <span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0">Read more</span>
+				<?php if ( !empty( $args[ 'read-more' ] ) ) : ?>
+                	<span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0">Read more</span>
+				<?php endif; ?>
             </div>
-
-			<?php if ( !empty( $args[ 'read-more' ] ) ) : ?>
-				<div class="block-frame">
-					<img class="frame" src="https://test.match.center/wp-content/themes/thrive-theme-child/assets/img/compilation/compilation-frame-start-screen.png">
-				</div>
-			<?php endif; ?>
-
+			<div class="block-frame">
+				<img class="frame" src="https://test.match.center/wp-content/themes/thrive-theme-child/assets/img/compilation/compilation-frame-start-screen.png">
+			</div>
         </div>
     </div>
 <?php endif; ?>
