b01a4e579139afd3df5f225ed439aca224fb2eb5
sergeykalabin <sergey.kalabin@legalbet.com>
2024-01-12 13:45:23 +0400

    commit frontend

--- template-parts/compilation/part-compilation-about.php
+++ template-parts/compilation/part-compilation-about.php
@@ -9,17 +9,17 @@
 <?php if ( !empty( $args[ 'title' ] ) ) : ?>
     <div class="compilation-about-wrapper">
         <div class="compilation-about">
-            <div class="block-title-text">
+            <div class="about-section-content">
                 <h1><?php echo $args[ 'title' ]; ?></h1>
                 <?php foreach ( $args[ 'content' ] as $item ) : ?>
-                    <p class="text <?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p>
+                    <p class="<?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p>
                 <?php endforeach; ?>
 				<?php if ( !empty( $args[ 'read-more' ] ) ) : ?>
                 	<span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0">Read more</span>
 				<?php endif; ?>
             </div>
-			<div class="block-frame">
-				<img class="frame" src="https://test.match.center/wp-content/themes/thrive-theme-child/assets/img/compilation/compilation-frame-start-screen.png">
+			<div class="about-section-image">
+				<img class="section-image-item" src="https://test.match.center/wp-content/themes/thrive-theme-child/assets/img/compilation/compilation-frame-start-screen.png">
 			</div>
         </div>
     </div>
