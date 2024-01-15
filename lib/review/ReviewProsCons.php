0c2b1bcada75d366abc2695f7d3e12808eb7953e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-15 10:37:28 +0300

    notion list

--- lib/review/ReviewProsCons.php
+++ lib/review/ReviewProsCons.php
@@ -15,7 +15,7 @@ class ReviewProsCons
 		// 	'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-pros-cons-new.css',
 
 		// 	'ver' => '1.0.0',
-		// ],
+		// ], 
     ];
 
 	// public static function register_style()
