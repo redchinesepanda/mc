579be9501f54fd2bafb8cbafbeb0feb1438b4ab4
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-15 10:42:55 +0300

    notion list

--- lib/review/ReviewList.php
+++ lib/review/ReviewList.php
@@ -10,9 +10,24 @@ class ReviewList
         ],
     ];
 
+    const CSS_NEW = [
+        'review-list' => [
+            'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-list-new.css',
+
+            'ver' => '1.0.0',
+        ],
+    ];
+
     public static function register_style()
     {
-        ReviewMain::register_style( self::CSS );
+        if ( TemplateMain::check_code() )
+		{
+			ReviewMain::register_style( self::CSS_NEW );
+		}
+		else
+		{
+			ReviewMain::register_style( self::CSS );
+		}
     }
 
     public static function register_inline_style()
