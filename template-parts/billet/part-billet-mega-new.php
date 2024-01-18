4c403daa3fdb8ce33380ac0b53a41c3286375a8c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-18 14:33:26 +0300

    notion list

--- template-parts/billet/part-billet-mega-new.php
+++ template-parts/billet/part-billet-mega-new.php
@@ -10,6 +10,9 @@
 	</style>
 	<div class="billet-mega-about">
 		<div class="mega-about-logo"></div>
+		<?php if( !empty( $args[ 'author' ][ 'name' ] ) ) : ?>
+			<div class="mega-about-name"><?php echo $args[ 'author' ][ 'name' ] ?></div>
+		<?php endif; ?>
 		<?php if( !$args[ 'no-controls' ] ) : ?>
 			<a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="mega-about-afillate check-oops" <?php echo BilletMain::render_nofollow( $args[ 'afillate' ][ 'nofollow' ] ); ?>><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
 			<?php if ( !empty( $args[ 'review' ][ 'href' ] ) ) : ?>
@@ -17,7 +20,6 @@
 			<?php endif; ?>
 		<?php endif; ?>
 		<?php if( !empty( $args[ 'author' ] ) ) : ?>
-			<div class="mega-about-name"><?php echo $args[ 'author' ][ 'name' ] ?></div>
 			<div class="mega-about-post"><?php echo $args[ 'author' ][ 'post' ] ?></div>
 			<?php if( !empty( $args[ 'author' ][ 'items' ] ) ) : ?>
 				<div class="mega-about-links">
