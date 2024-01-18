671cb931b8bfc13cf8070535dee67c8d14b6eabd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-18 12:21:11 +0300

    notion list

--- lib/notion/NotionYoast.php
+++ lib/notion/NotionYoast.php
@@ -9,10 +9,6 @@ class NotionYoast
 		add_filter( 'wpseo_title', [ $handler, 'wp_kama_wpseo_title_filter' ], 10, 2 );
 
 		add_filter( 'wpseo_metadesc', [ $handler, 'wp_kama_wpseo_metadesc_filter' ], 10, 2 );
-
-		LegalDebug::debug( [
-			'$handler' => $handler, 
-		] );
 	}
 
 	const META_FIELD = [
