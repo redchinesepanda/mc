07be14cf3718a3e6fa2f84e8b1530bacd5bc4a23
sergeykalabin <sergey.kalabin@legalbet.com>
2024-01-23 19:18:42 +0400

    commit frontend

--- lib/review/ReviewCut.php
+++ lib/review/ReviewCut.php
@@ -10,9 +10,29 @@ class ReviewCut
 		],
     ];
 
-    public static function register_style()
+	const CSS_NEW = [
+        'review-cut-new' => [
+			'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-cut-new.css',
+
+			'ver' => '1.0.0',
+		],
+    ];
+
+ /*    public static function register_style()
     {
 		ReviewMain::register_style( self::CSS );
+    } */
+
+	public static function register_style()
+    {
+		if ( TemplateMain::check_code() )
+		{
+			ReviewMain::register_style( self::CSS_NEW );
+		}
+		else
+		{
+			ReviewMain::register_style( self::CSS );
+		}
     }
 
 	const JS = [
