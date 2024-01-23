f13a425727dbbd8a904c0e7a1131fa51771871e1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-23 11:41:38 +0300

    notion list

--- assets/js/review/review-gallery-swiper.js
+++ assets/js/review/review-gallery-swiper.js
@@ -4,102 +4,26 @@ document.addEventListener( 'DOMContentLoaded', function ()
 {
 	function handleSwipe( element )
 	{
-		// console.log( 'handleSwipe' );
-		
-		// console.log( element.dataset ); 
-
-		let x = element.dataset.touchendX - element.dataset.touchstartX;
-
-		// let y = element.dataset.touchendY - element.dataset.touchstartY;
-
-		// let xy = Math.abs( x / y );
-
-		// let yx = Math.abs( y / x );
-
-		// console.log( x );
-
-		if ( x < 0 )
+		if ( element.dataset.touchendX - element.dataset.touchstartX < 0 )
 		{
 			element.dispatchEvent( events.swipeForward( element.dataset.id ) );
 
 			console.log( events.swipeForward( element.dataset.id ) );
 
-			console.log( "left" );
+			console.log( "right" );
 		}
 		else
 		{
-			console.log( "right" );
+			console.log( "left" );
 		}
-
-		// if (
-		// 	Math.abs( x ) > element.dataset.treshold
-			
-		// 	|| Math.abs( y ) > element.dataset.treshold
-		// )
-		// {
-		// 	if ( yx <= element.dataset.limit )
-		// 	{
-		// 		if ( x < 0 )
-		// 		{
-		// 			console.log( "left" );
-		// 		}
-		// 		else
-		// 		{
-		// 			console.log( "right" );
-		// 		}
-		// 	}
-		// 	if ( xy <= element.dataset.limit )
-		// 	{
-		// 		if ( y < 0 )
-		// 		{
-		// 			console.log( "top" );
-		// 		}
-		// 		else
-		// 		{
-		// 			console.log( "bottom" );
-		// 		}
-		// 	}
-		// }
-		// else
-		// {
-		// 	console.log( "tap" );
-		// }
 	}
-
-	function getTouches( event )
-	{
-		console.log( event.touches );
-
-		return event.touches;
-	}  
 
 	function handleTouchStart( event )
 	{
 		event.preventDefault();
 		
 		event.currentTarget.dataset.touchstartX = event.changedTouches[0].screenX;
-
-		// event.currentTarget.dataset.touchstartY = event.changedTouches[0].screenY;
-
-		// console.log( 'handleTouchStart' );
-
-		// console.log( event.currentTarget.dataset );
-		
-		// console.log( event.changedTouches );
-		
-		// console.log( getTouches( event ).clientX );
-
-		// getTouches( event );
 	}
-
-	// function handleTouchMove( event )
-	// {
-	// 	console.log( 'handleTouchMove' );
-
-	// 	// getTouches( event );
-
-	// 	handleSwipe( event.currentTarget );
-	// }
 
 	function handleTouchEnd( event )
 	{
@@ -107,36 +31,14 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 		event.currentTarget.dataset.touchendX = event.changedTouches[0].screenX;
 
-		// event.currentTarget.dataset.touchendY = event.changedTouches[0].screenY;
-
-		// console.log( 'handleTouchEnd' );
-		
-		// console.log( event.currentTarget.dataset );
-
-		// console.log( event.changedTouches );
-
-		// console.log( getTouches( event ).clientX );
-
-		// getTouches( event );
-
 		handleSwipe( event.currentTarget );
 	}
 
 	function initDataset( element )
 	{
-		// element.dataset.pageWidth = window.innerWidth || document.body.clientWidth;
-
-		// element.dataset.treshold = Math.max( 1, Math.floor( 0.01 * ( element.dataset.pageWidth ) ) );
-
 		element.dataset.touchstartX = 0;
 
-		// element.dataset.touchstartY = 0;
-
 		element.dataset.touchendX = 0;
-
-		// element.dataset.touchendY = 0;
-
-		// element.dataset.limit = Math.tan( 45 * 1.5 / 180 * Math.PI );
 	}
 
 	function setTouch( element )
@@ -146,88 +48,6 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		element.addEventListener( 'touchstart', handleTouchStart, false );
 
 		element.addEventListener( 'touchend', handleTouchEnd, false );
-		
-		// element.addEventListener( 'touchmove', handleTouchMove, false );
-	}
-
-	// let pageWidth = window.innerWidth || document.body.clientWidth;
-
-	// let treshold = Math.max( 1, Math.floor( 0.01 * ( pageWidth ) ) );
-
-	// let touchstartX = 0;
-
-	// let touchstartY = 0;
-
-	// let touchendX = 0;
-
-	// let touchendY = 0;
-
-	// const limit = Math.tan( 45 * 1.5 / 180 * Math.PI );
-
-	// const gestureZone = document.getElementById( 'modalContent' );
-
-	// gestureZone.addEventListener( 'touchstart', function( event )
-	// {
-	// 	touchstartX = event.changedTouches[0].screenX;
-	// 	touchstartY = event.changedTouches[0].screenY;
-	// }, false );
-
-	// gestureZone.addEventListener( 'touchend', function( event )
-	// {
-	// 	touchendX = event.changedTouches[0].screenX;
-	// 	touchendY = event.changedTouches[0].screenY;
-	// 	handleGesture( event );
-	// }, false );
-
-	// function handleGesture(e)
-	// {
-	// 	let x = touchendX - touchstartX;
-
-	// 	let y = touchendY - touchstartY;
-
-	// 	let xy = Math.abs( x / y );
-
-	// 	let yx = Math.abs( y / x );
-
-	// 	if ( Math.abs( x ) > treshold || Math.abs( y ) > treshold )
-	// 	{
-	// 		if ( yx <= limit )
-	// 		{
-	// 			if ( x < 0 )
-	// 			{
-	// 				console.log( "left" );
-	// 			}
-	// 			else
-	// 			{
-	// 				console.log( "right" );
-	// 			}
-	// 		}
-	// 		if ( xy <= limit )
-	// 		{
-	// 			if ( y < 0 )
-	// 			{
-	// 				console.log( "top" );
-	// 			}
-	// 			else
-	// 			{
-	// 				console.log( "bottom" );
-	// 			}
-	// 		}
-	// 	}
-	// 	else
-	// 	{
-	// 		console.log( "tap" );
-	// 	}
-	// }
-
-	function setSwipeForward( element )
-	{
-		element.addEventListener( 'swipeforward', scrollForward );
-	}
-
-	function setSwipeBackward( element )
-	{
-		element.addEventListener( 'swipebackward', scrollForward );
 	}
 
 	const events = {
@@ -310,10 +130,6 @@ document.addEventListener( 'DOMContentLoaded', function ()
 	function slider( element )
 	{
 		element.querySelectorAll( selectors.imageset ).forEach( setTouch );
-	
-		element.querySelectorAll( selectors.imageset ).forEach( setSwipeForward );
-
-		element.querySelectorAll( selectors.imageset ).forEach( setSwipeBackward );
 	}
 
 	document.querySelectorAll( selectors.imagesetWrapper ).forEach( slider );
