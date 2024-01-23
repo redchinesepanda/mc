19cfaee4cb123af6fef4533ac10a16e38d6bd75b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-23 16:32:09 +0300

    notion list

--- assets/js/review/review-gallery-pagination.js
+++ assets/js/review/review-gallery-pagination.js
@@ -1,12 +1,46 @@
 // review-gallery-pagination-js start
 
+let reviewGalleyPagination = ( function()
+{
+	"use strict";
+
+	return {
+		pageForward : 'pageforward',
+
+		pageForwardEvent : function( id )
+		{
+			return new CustomEvent(
+				this.pageForward,
+
+				{
+					detail: {
+						id: () => id
+					},
+				}
+			)
+		},
+
+		pageBackward : 'pagebackward',
+
+		pageBackwardEvent : function( id )
+		{
+			return new CustomEvent(
+				'pagebackward',
+
+				{
+					detail: {
+						id: () => id
+					},
+				}
+			)
+		}
+	};
+} )();
+
 document.addEventListener( 'DOMContentLoaded', function ()
 {
-	// function pageChange( element, selector )
 	function pageChange( element, sibling )
 	{
-		// let sibling = element.querySelector( selector );
-
 		if ( sibling !== null )
 		{
 			element.querySelector( selectors.paginationItemActive ).classList.remove( classes.paginationItemActive );
@@ -17,26 +51,11 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 	function pageForward( event )
 	{
-		// let pageForward = event.currentTarget.querySelector( selectors.paginationItemNext );
-
-		// if ( pageForward !== null )
-		// {
-		// 	event.currentTarget.querySelector( selectors.paginationItemActive ).classList.remove( classes.paginationItemActive );
-
-		// 	pageForward.classList.add( classes.paginationItemActive );
-		// }
-
-		// pageChange( event.currentTarget, selectors.paginationItemNext );
-		
 		pageChange( event.currentTarget, event.currentTarget.querySelector( selectors.paginationItemActive ).nextElementSibling );
 	}
 
 	function pageBackward( event )
 	{
-		// console.log( event.currentTarget.querySelector( selectors.paginationItemActive ).previousSibling );
-
-		// console.log( event.currentTarget.querySelector( selectors.paginationItemActive ).previousElementSibling );
-
 		pageChange( event.currentTarget, event.currentTarget.querySelector( selectors.paginationItemActive ).previousElementSibling );
 	}
 	
@@ -53,54 +72,20 @@ document.addEventListener( 'DOMContentLoaded', function ()
 	{
 		element.querySelectorAll( selectors.offScreen ).forEach( addPaginationItem, this );
 
-		this.querySelector( selectors.imagesetPagination ).addEventListener( 'pageforward', pageForward, false );
+		// this.querySelector( selectors.imagesetPagination ).addEventListener( 'pageforward', pageForward, false );
+		
+		this.querySelector( selectors.imagesetPagination ).addEventListener( reviewGalleyPagination.pageForward, pageForward, false );
 
 		this.querySelector( selectors.imagesetPagination ).addEventListener( 'pagebackward', pageBackward, false );
 	}
 
 	function checkOffscreen( element )
 	{
-		// console.log( 'checkOffscreen' );
-
-		// console.log( 'element left: ' + element.getBoundingClientRect().left );
-
-		// console.log( 'element right: ' + element.getBoundingClientRect().right );
-
-		// console.log( 'wrapper right: ' + this.getBoundingClientRect().right );
-
 		if ( element.getBoundingClientRect().right > this.getBoundingClientRect().right )
 		{
 			element.classList.add( classes.offScreen );
 		}
 	}
-
-	const events = {
-		pageForward : function( id )
-		{
-			return new CustomEvent(
-				'pageforward',
-
-				{
-					detail: {
-						id: () => id
-					},
-				}
-			)
-		},
-
-		pageBackward : function( id )
-		{
-			return new CustomEvent(
-				'pagebackward',
-
-				{
-					detail: {
-						id: () => id
-					},
-				}
-			)
-		}
-	};
 
 	const selectors = {
 		imageset : '.tcb-post-content .legal-imageset',
@@ -132,23 +117,13 @@ document.addEventListener( 'DOMContentLoaded', function ()
 			left: 0,
 
 			behavior: 'smooth'
-		} );
-
-		// console.log( this ); 
+		} ); 
 
 		element.querySelectorAll( selectors.imagesetItem ).forEach( checkOffscreen, this );
-
-		// initDataset( element );
-
-		// element.addEventListener( 'touchstart', handleTouchStart, false );
-
-		// element.addEventListener( 'touchend', handleTouchEnd, false );
 	}
 
 	function slider( element )
 	{
-		// console.log( element.classList );
-
 		element.querySelectorAll( selectors.imageset ).forEach( setPagination, element );
 
 		element.querySelectorAll( selectors.imageset ).forEach( initPagination, element );
