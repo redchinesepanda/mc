60dda0b25ec5b49d3ae8d15bf95433cefd71cf81
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-25 12:41:20 +0300

    new billet

---
+++ template-parts/billet/center/part-billet-title-new.php
@@ -0,0 +1,19 @@
+<?php
+
+// LegalDebug::debug( [
+//     'part' => 'part-billet-title.php',
+
+//     'args' => $args,
+// ] );
+
+?>
+<div class="billet-title">
+    <?php if ( $args['order'] == BilletTitle::ORDER_TYPE ) : ?>
+        <div class="billet-order">#<?php echo $args['index']; ?></div>
+    <?php endif; ?>
+    <a class="legal-title <?php echo $args['class']; ?> check-oops" href="<?php echo $args['href']; ?>" <?php echo BilletMain::render_nofollow( $args[ 'nofollow' ] ); ?>><?php echo $args['label']; ?></a>
+    <?php if ( !empty( $args['rating'] ) ): ?>
+        <div class="billet-title-rating"><?php echo $args['rating']; ?></div>
+    <?php endif; ?>
+    <?php echo BilletAchievement::render_achievement( $args ); ?>
+</div>
