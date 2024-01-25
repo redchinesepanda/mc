c03ebfd4e7e079776a09b24fc961cac0e6b634ee
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-25 13:48:13 +0300

    new billet

--- template-parts/billet/center/part-billet-title-new.php
+++ template-parts/billet/center/part-billet-title-new.php
@@ -11,15 +11,17 @@
     <a class="legal-logo <?php echo $args['logo']['logo']['class']; ?> check-oops" href="<?php echo $args['logo']['logo']['href']; ?>" <?php echo BilletMain::render_nofollow( $args['logo'][ 'logo' ][ 'nofollow' ] ); ?>>
         <img src="<?php echo $args['logo']['logo']['src'] ?>" alt="<?php echo $args['logo']['alt'] ?>" />
     </a>
-    <?php if ( $args[ 'title' ]['order'] == BilletTitle::ORDER_TYPE ) : ?>
-        <div class="billet-order">#<?php echo $args[ 'title' ]['index']; ?></div>
-    <?php endif; ?>
-    <a class="legal-title <?php echo $args[ 'title' ]['class']; ?> check-oops" href="<?php echo $args[ 'title' ]['href']; ?>" <?php echo BilletMain::render_nofollow( $args[ 'title' ][ 'nofollow' ] ); ?>><?php echo $args[ 'title' ]['label']; ?></a>
-    <?php if ( !empty( $args[ 'title' ]['rating'] ) ): ?>
-        <div class="billet-title-rating"><?php echo $args[ 'title' ]['rating']; ?></div>
-    <?php endif; ?>
-    <?php if ( !empty( $args['logo']['review']['href'] ) ) : ?>
-        <a class="legal-review <?php echo $args['logo']['review']['class']; ?> <?php echo $args['logo']['review']['font']; ?>" href="<?php echo $args['logo']['review']['href']; ?>"><?php echo $args['logo']['review']['label']; ?></a>
-    <?php endif; ?>
+    <div class="title-data">
+        <?php if ( $args[ 'title' ]['order'] == BilletTitle::ORDER_TYPE ) : ?>
+            <div class="billet-order">#<?php echo $args[ 'title' ]['index']; ?></div>
+        <?php endif; ?>
+        <a class="legal-title <?php echo $args[ 'title' ]['class']; ?> check-oops" href="<?php echo $args[ 'title' ]['href']; ?>" <?php echo BilletMain::render_nofollow( $args[ 'title' ][ 'nofollow' ] ); ?>><?php echo $args[ 'title' ]['label']; ?></a>
+        <?php if ( !empty( $args[ 'title' ]['rating'] ) ): ?>
+            <div class="billet-title-rating"><?php echo $args[ 'title' ]['rating']; ?></div>
+        <?php endif; ?>
+        <?php if ( !empty( $args['logo']['review']['href'] ) ) : ?>
+            <a class="legal-review <?php echo $args['logo']['review']['class']; ?> <?php echo $args['logo']['review']['font']; ?>" href="<?php echo $args['logo']['review']['href']; ?>"><?php echo $args['logo']['review']['label']; ?></a>
+        <?php endif; ?>
+    </div>
     <?php //echo BilletAchievement::render_achievement( $args ); ?>
 </div>
