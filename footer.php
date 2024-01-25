85c300601eaf429307d35db9859ad65164b955af
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-25 22:06:34 +0300

    new billet

---
+++ footer.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+ * Thrive Themes - https://thrivethemes.com
+ *
+ * @package thrive-theme
+ */
+ 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Silence is golden!
+}
+
+?>
+
+<?php echo OopsCookie::render(); ?>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
