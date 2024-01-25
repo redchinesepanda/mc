c66dce8eb260b0b97d2a90aa183d558c09455dd1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-01-25 13:26:02 +0300

    new billet

--- template-parts/billet/center/part-billet-title-new.php
+++ template-parts/billet/center/part-billet-title-new.php
@@ -8,12 +8,18 @@
 
 ?>
 <div class="billet-title">
+    <a class="legal-logo <?php echo $args['logo']['class']; ?> check-oops" href="<?php echo $args['logo']['href']; ?>" <?php echo BilletMain::render_nofollow( $args[ 'logo' ][ 'nofollow' ] ); ?>>
+        <img src="<?php echo $args['logo']['src'] ?>" alt="<?php echo $args['logo']['alt'] ?>" />
+    </a>
     <?php if ( $args[ 'title' ]['order'] == BilletTitle::ORDER_TYPE ) : ?>
         <div class="billet-order">#<?php echo $args[ 'title' ]['index']; ?></div>
     <?php endif; ?>
     <a class="legal-title <?php echo $args[ 'title' ]['class']; ?> check-oops" href="<?php echo $args[ 'title' ]['href']; ?>" <?php echo BilletMain::render_nofollow( $args[ 'title' ][ 'nofollow' ] ); ?>><?php echo $args[ 'title' ]['label']; ?></a>
     <?php if ( !empty( $args[ 'title' ]['rating'] ) ): ?>
         <div class="billet-title-rating"><?php echo $args[ 'title' ]['rating']; ?></div>
+    <?php endif; ?>
+    <?php if ( !empty( $args['review']['href'] ) ) : ?>
+        <a class="legal-review <?php echo $args['review']['class']; ?> <?php echo $args['review']['font']; ?>" href="<?php echo $args['review']['href']; ?>"><?php echo $args['review']['label']; ?></a>
     <?php endif; ?>
     <?php //echo BilletAchievement::render_achievement( $args ); ?>
 </div>
