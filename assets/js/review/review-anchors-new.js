a127a7e455cb8ff28faeb0440895e8ea0353f126
sergeykalabin <sergey.kalabin@legalbet.com>
2024-02-07 21:33:42 +0400

    commit frontend

--- assets/js/review/review-anchors-new.js
+++ assets/js/review/review-anchors-new.js
@@ -1,51 +1,5 @@
 // review-anchors-js start
 
-/* document.addEventListener( 'DOMContentLoaded', function ()
-{
-    function toggleItems( event )
-	{
-		event.currentTarget.parentElement.classList.toggle( 'legal-active' );
-	}
-
-	const args = [
-		{
-			'selector' : '.anchors-title',
-
-			'event' : 'click',
-
-			'action' : toggleItems
-		},
-	];
-
-	function toggleInit()
-	{
-		args.forEach( function ( arg ) {
-			let anchorsTitle = document.querySelector( arg.selector );
-
-			if ( anchorsTitle != null )
-			{
-				if ( window.matchMedia( '( max-width: 767px )' ).matches )
-				{
-					anchorsTitle.addEventListener( arg.event, arg.action, false );
-	
-					// document.querySelector( arg.selector ).addEventListener( arg.event, arg.action, false );
-				} else {
-					anchorsTitle.removeEventListener( arg.event, arg.action, false );
-	
-					// document.querySelector( arg.selector ).removeEventListener( arg.event, arg.action, false );
-				}
-			}
-		} );
-	}
-
-	toggleInit();
-
-	window.addEventListener( 'resize', toggleInit, false );
-} ); */
-
-// review-anchors-js end
-
-
 document.addEventListener( 'DOMContentLoaded', function ()
 {
 	
@@ -60,3 +14,5 @@ document.addEventListener( 'DOMContentLoaded', function ()
 	})
 	
 } );
+
+// review-anchors-js end
