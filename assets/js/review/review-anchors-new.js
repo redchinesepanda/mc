a4fcdf07f5408133063f3d5fc1944054025beec2
sergeykalabin <sergey.kalabin@legalbet.com>
2024-02-07 19:49:52 +0400

    commit frontend

--- assets/js/review/review-anchors-new.js
+++ assets/js/review/review-anchors-new.js
@@ -53,13 +53,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		/*--------------------Swiper--------------*/
 		new Swiper(el, {
 			loop: true,
-			/* pagination: {
-				el: '.swiper-pagination',
-			},
-			navigation: {
-				nextEl:'.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			}, */
+		
 		});
 	})
 	
