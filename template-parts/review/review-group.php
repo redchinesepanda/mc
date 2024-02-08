aa28cefc3317ed92f8fb5ecfff3234165546520f
sergeykalabin <sergey.kalabin@legalbet.com>
2024-02-08 13:52:12 +0400

    commit frontend

--- template-parts/review/review-group.php
+++ template-parts/review/review-group.php
@@ -1,10 +1,16 @@
 <nav class="group" style="padding-bottom: 30px;">
     <?php if( !empty( $args[ 'other' ] ) ) : ?>
         <div class="review-group">
-            <span class="group-item"><?php echo $args[ 'current' ][ 'label' ]; ?></span>
-            <?php foreach( $args[ 'other' ] as $item ) : ?>
-                <a class="group-item" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
-            <?php endforeach; ?>
+
+            <div class="swiper"> <!--новый тег для swiper -->
+                <div class="swiper-wrapper"> <!--новый тег для swiper -->
+                    <span class="group-item swiper-slide"><?php echo $args[ 'current' ][ 'label' ]; ?></span>
+                    <?php foreach( $args[ 'other' ] as $item ) : ?>
+                        <a class="group-item swiper-slide" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+
         </div>
     <?php endif; ?>
 </nav>
