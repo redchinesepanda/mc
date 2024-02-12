0a9d47a4a3336f3d3e5790f559bfa51c55581407
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-12 14:52:41 +0300

    new billet

--- lib/billet/center/BilletAchievement.php
+++ lib/billet/center/BilletAchievement.php
@@ -144,11 +144,11 @@ class BilletAchievement
     {
         // return $title[ 'achievement' ] == self::TYPE_DISABLED;
 
-        LegalDebug::debug( [
-            'BilletAchievemnet' => 'check_disabled',
+        // LegalDebug::debug( [
+        //     'BilletAchievemnet' => 'check_disabled',
 
-            '$filter' => $filter,
-        ] );
+        //     '$filter' => $filter,
+        // ] );
         
         return $filter[ 'achievement' ] == self::TYPE_DISABLED;
     }
@@ -175,15 +175,17 @@ class BilletAchievement
 
     public static function render_style( $args )
     {
+        $billet = BilletMain::get( $args );
+
+        $title = BilletTitle::get( $billet ); 
+
         LegalDebug::debug( [
             'BilletAchievement' => 'render_style',
 
             '$args' => $args,
+
+            '$billet' => $billet,
         ] );
-
-        $billet = BilletMain::get( $args );
-
-        $title = BilletTitle::get( $billet ); 
 
         if ( self::check_disabled( $title ) )
         {
