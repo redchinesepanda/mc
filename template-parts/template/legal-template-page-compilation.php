bd8b0df8ed30a9b581547227badf561dd89f225b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-13 16:45:22 +0300

    new billet

--- template-parts/template/legal-template-page-compilation.php
+++ template-parts/template/legal-template-page-compilation.php
@@ -12,7 +12,7 @@ get_header();
 <section class="legal-section-about">
 	<?php echo CompilationAbout::render(); ?>
 </section>
-<?php echo CompilationTemplatePage::render(); ?>
+<?php echo CompilationPage::render(); ?>
 <section id="theme-bottom-section" class="legal-section-author">
 	<?php echo ReviewAuthor::render(); ?>
 </section>
