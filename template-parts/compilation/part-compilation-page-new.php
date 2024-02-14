25ec57e665e95d205580f65a4d05655655b47feb
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-14 13:58:04 +0300

    new billet

--- template-parts/compilation/part-compilation-page-new.php
+++ template-parts/compilation/part-compilation-page-new.php
@@ -5,10 +5,11 @@
 	<?php echo ReviewAnchors::render(); ?>
 </section>
 <section class="legal-section-content">
-	<div class="legal-bonus-single">
-		<div class="legal-bonus-main">
+	<div class="legal-compilation-page">
+		<div class="legal-compilation-page-main">
 			<?php echo ReviewContent::render(); ?>
 		</div>
+		<div class="legal-compilation-page-sidebar"></div>
 	</div>
 </section>
 <section class="legal-section-offers">
