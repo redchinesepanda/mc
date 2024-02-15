a1fdfbdf78d0a85f111255c62ebb371eb41a8e01
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-15 12:15:09 +0300

    new billet

--- assets/js/review/review-about.js
+++ assets/js/review/review-about.js
@@ -2,13 +2,36 @@
 
 document.addEventListener( 'DOMContentLoaded', function ()
 {
+	function appendItem( element )
+	{
+		element.appendChild( document.querySelector( selectors.reviewAboutRight ) );
+	}
+
 	function checkState( event )
 	{
 		console.log( window.scrollY );
+
+		if ( window.scrollY > 0 && !localStorage.getItem( 'reviewAboutScroll' ) )
+		{
+			localStorage.setItem( 'reviewAboutScroll', true );
+
+			document.querySelectorAll( selectors.sidebar ).forEach( appendItem );
+		}
+
+		if ( window.scrollY == 0 )
+		{
+			localStorage.setItem( 'reviewAboutScroll', false );
+
+			document.querySelectorAll( selectors.reviewAbout ).forEach( appendItem );
+		}
 	}
 
 	const selectors = {
-		aboutRight : '.review-about .about-right'
+		reviewAbout : '.review-about',
+
+		reviewAboutRight : '.review-about .about-right',
+
+		sidebar : '.legal-review-page-sidebar'
 	};
 
 	document.addEventListener( 'scroll', checkState, false );
