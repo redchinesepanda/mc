13209bb9a516e4276228d4ca22dee3f746e5af6c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-15 11:37:48 +0300

    new billet

--- template-parts/review/review-about-new.php
+++ template-parts/review/review-about-new.php
@@ -1,9 +1,11 @@
 <?php if( !empty( $args[ 'title' ] ) ) : ?>
     <div class="review-about-wrapper <?php echo $args[ 'class' ]; ?>">
         <div class="review-about <?php echo $args[ 'font' ]; ?> <?php echo $args[ 'class' ]; ?>">
-            <div class="achievement-name">
-                <span class="achievement-bonus-link"><?php echo $args[ 'achievement' ][ 'term' ]; ?></span>
-            </div>
+            <?php if( !empty( $args['achievement'] ) ) : ?>
+                <div class="achievement-name">
+                    <span class="achievement-bonus-link"><?php echo $args[ 'achievement' ][ 'term' ]; ?></span>
+                </div>
+            <?php endif; ?>
             <div class="about-left">
                 <div class="about-logo"></div>
             </div>
