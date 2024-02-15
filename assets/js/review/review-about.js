bddd3473eb3df5eb6a0959f895292596d23e21da
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-15 12:36:50 +0300

    new billet

--- assets/js/review/review-about.js
+++ assets/js/review/review-about.js
@@ -4,19 +4,11 @@ document.addEventListener( 'DOMContentLoaded', function ()
 {
 	function moveBack( element )
 	{
-		console.log( element );
-
-		console.log( document.querySelector( selectors.sidebarBonus ) );
-
 		element.appendChild( document.querySelector( selectors.sidebarBonus ) );
 	}
 
 	function moveToSidebar( element )
 	{
-		console.log( element );
-
-		console.log( document.querySelector( selectors.reviewAboutBonus ) );
-
 		element.appendChild( document.querySelector( selectors.reviewAboutBonus ) );
 	}
 
@@ -24,32 +16,18 @@ document.addEventListener( 'DOMContentLoaded', function ()
 	{
 		console.log( 'checkState' );
 
-		// console.log( 'window.scrollY: ' + window.scrollY );
-
-		// console.log( 'reviewAboutScroll: ' + localStorage.getItem( 'reviewAboutScroll' ) );
-
 		let state = localStorage.getItem( 'reviewAboutScroll' );
-
-		// console.log( 'state != 1: ' + ( state != 1 ) );
 
 		if ( window.scrollY > 0 && state != 1 )
 		{
 			localStorage.setItem( 'reviewAboutScroll', 1 );
 
-			// console.log( 'reviewAboutScroll move: ' + localStorage.getItem( 'reviewAboutScroll' ) );
-
 			document.querySelectorAll( selectors.sidebar ).forEach( moveToSidebar );
 		}
-
-		// console.log( '== 0: ' + ( window.scrollY == 0 ) );
-
-		// console.log( 'state == 1: ' + ( localStorage.getItem( 'reviewAboutScroll' ) == 1 ) );
 
 		if ( window.scrollY == 0 && state == 1 )
 		{
 			localStorage.setItem( 'reviewAboutScroll', 0 );
-
-			// console.log( 'reviewAboutScroll back: ' + localStorage.getItem( 'reviewAboutScroll' ) );
 
 			document.querySelectorAll( selectors.reviewAbout ).forEach( moveBack );
 		}
