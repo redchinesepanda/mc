ffd4ff598c373d6fb2b77f8eaab7fe873bae6a4f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-15 12:23:34 +0300

    new billet

--- assets/js/review/review-about.js
+++ assets/js/review/review-about.js
@@ -29,6 +29,10 @@ document.addEventListener( 'DOMContentLoaded', function ()
 			// document.querySelectorAll( selectors.sidebar ).forEach( moveBonusToSidebar );
 		}
 
+		console.log( 'window.scrollY: ' + ( window.scrollY == 0 ) );
+
+		console.log( 'window.scrollY: ' + ( localStorage.getItem( 'reviewAboutScroll' ) == true ) );
+
 		if ( window.scrollY == 0 && localStorage.getItem( 'reviewAboutScroll' ) == true )
 		{
 			localStorage.setItem( 'reviewAboutScroll', false );
