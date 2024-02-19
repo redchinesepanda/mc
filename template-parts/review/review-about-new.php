5f875644cab233df001e792eda10069a6cb0dbc4
sergeykalabin <sergey.kalabin@legalbet.com>
2024-02-19 11:36:19 +0400

    commit frontend

--- template-parts/review/review-about-new.php
+++ template-parts/review/review-about-new.php
@@ -16,10 +16,10 @@
                 <?php if( !empty( $args['bonus'][ 'name' ] ) ) : ?>
                     <div class="review-bonus-title"><?php echo $args[ 'bonus' ][ 'name' ]; ?></div>
                 <?php endif; ?>
+                <a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="legal-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
                 <?php if( empty( $args['mode'] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
                     <div class="review-bonus-description"><?php echo $args[ 'bonus' ][ 'description' ]; ?></div>
                 <?php endif; ?>
-                <a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="legal-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
             </div>
         </div>
     </div>
