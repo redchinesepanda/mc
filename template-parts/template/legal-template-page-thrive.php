22b42ec9cbdd4e3091ce1bd2ee345265661d8176
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-19 13:10:45 +0300

    new billet

--- template-parts/template/legal-template-page-thrive.php
+++ template-parts/template/legal-template-page-thrive.php
@@ -1,7 +1,10 @@
-<?php
-
-// thrive_template()->render();
-
-?>
+<?php get_header(); ?>
+<section class="legal-section-header">
+	<?php echo BaseHeader::render(); ?>
+</section>
+<section class="legal-section-breadcrumbs">
+	<?php echo LegalBreadcrumbsMain::render(); ?>
+</section>
 <h1>Legal Page Thrive</h1>
+<?php get_footer(); ?>
 <!-- Legal Page Thrive -->
