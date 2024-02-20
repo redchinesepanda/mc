f1cc240eac375ac7a23a05f8417a09ba6d202387
sergeykalabin <sergey.kalabin@legalbet.com>
2024-02-20 13:47:57 +0400

    commit frontend

--- template-parts/review/review-about-new.php
+++ template-parts/review/review-about-new.php
@@ -14,9 +14,6 @@
             </div>
             <div class="about-right">
                 <?php if( !empty( $args['bonus'][ 'name' ] ) ) : ?>
-                    <span class="review-bonus-head">Bonus</span>
-                <?php endif; ?>
-                <?php if( !empty( $args['bonus'][ 'name' ] ) ) : ?>
                     <div class="review-bonus-title"><?php echo $args[ 'bonus' ][ 'name' ]; ?></div>
                 <?php endif; ?>
                 <a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="legal-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
@@ -24,7 +21,7 @@
                     <div class="review-bonus-description legal-cut-item" data-cut-set-id="0"><?php echo $args[ 'bonus' ][ 'description' ]; ?></div>
                 <?php endif; ?>
                 <?php if( empty( $args['mode'] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
-                    <span class="legal-cut-control" data-review-default="Show T&C" data-review-active="Hide T&C" data-cut-set-id="0"></span>
+                    <span class="legal-cut-control" data-content-default="Show T&C" data-content-active="Hide T&C" data-cut-set-id="0">Show T&C</span>
                 <?php endif; ?>
             </div>
         </div>
