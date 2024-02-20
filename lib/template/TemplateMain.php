4c026a7c4bee99e4dcd8c96920ff34d9778f6710
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-20 12:27:55 +0300

    new billet

--- lib/template/TemplateMain.php
+++ lib/template/TemplateMain.php
@@ -20,6 +20,10 @@ class TemplateMain
 
     public static function check_code()
     {
+        LeglalDebug::debug( [
+            WPMLMain::current_language(),
+        ] );
+        
         return in_array( WPMLMain::current_language(), self::CURRENT_LANGUAGE );
     }
 
