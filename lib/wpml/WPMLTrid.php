a0c8516b9cb7ae842162f18682dc2b09dbd14b6b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-02-20 16:46:37 +0300

    new billet

--- lib/wpml/WPMLTrid.php
+++ lib/wpml/WPMLTrid.php
@@ -13,6 +13,24 @@ class WPMLTrid
         self::debug( $message );
     }
 
+    public static function get_query( $position = 0, $limit = 100 )
+    {
+        return "SELECT
+                DISTINCT `wp_icl_translations`.`trid` AS `legal_trid`,
+                COUNT( `wp_icl_translations`.`element_id` ) AS `legal_elements`,
+                `wp_icl_translations`.`element_id` AS `legal_element_id`,
+                `wp_posts`.`post_title` AS `legal_title`,
+                GROUP_CONCAT( `language_code` ) as 'legal_language_codes'
+            FROM `wp_icl_translations`
+            INNER JOIN `wp_posts` ON `element_id` = `ID`
+            WHERE
+                `element_type` = 'post_page'
+                AND `post_status` = 'publish'
+            GROUP BY `trid`
+            ORDER BY `legal_title`, `legal_elements`
+            LIMIT $position, 100";
+    }
+
     public static function get()
     {
         global $wpdb;
@@ -30,23 +48,24 @@ class WPMLTrid
         //         AND `post_status` = 'publish'
         //     GROUP BY `trid`
         //     ORDER BY `legal_title`, `legal_elements`";
+
+        $posts = [];
+
+        $position = 0;
+
+        $limit = 100;
         
-        $query = "SELECT
-                DISTINCT `wp_icl_translations`.`trid` AS `legal_trid`,
-                COUNT( `wp_icl_translations`.`element_id` ) AS `legal_elements`,
-                `wp_icl_translations`.`element_id` AS `legal_element_id`,
-                `wp_posts`.`post_title` AS `legal_title`,
-                GROUP_CONCAT( `language_code` ) as 'legal_language_codes'
-            FROM `wp_icl_translations`
-            INNER JOIN `wp_posts` ON `element_id` = `ID`
-            WHERE
-                `element_type` = 'post_page'
-                AND `post_status` = 'publish'
-            GROUP BY `trid`
-            ORDER BY `legal_title`, `legal_elements`
-            LIMIT 0, 100";
+        do
+        {
+            $result = $wpdb->get_results( self::get_query( $position, $limit ) );
+
+            $posts = array_merge( $posts, $result );
+
+            $position += $limit;
+        }
+        while ( !empty( $result ) );
         
-        $posts = $wpdb->get_results( $query );
+        // $posts = $wpdb->get_results( $query );
 
         return $posts;
     }
