dc2bf670558c19b6fb9be8a886e47eafbeb379ce
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-04 13:20:35 +0300

    new billet

--- template-parts/template/legal-template-notfound.php
+++ template-parts/template/legal-template-notfound.php
@@ -9,9 +9,6 @@ get_header();
 <section class="legal-section-content">
 	<?php echo NotFoundMain::render(); ?>
 </section>
-<section id="theme-bottom-section" class="legal-section-author">
-	<?php echo ReviewAuthor::render(); ?>
-</section>
 <?php
 
 get_footer();
