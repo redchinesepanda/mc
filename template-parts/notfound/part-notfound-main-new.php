6af34f5e01b1e1f2ef31f5fb6f4dc5e01ab2bc52
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-05 14:23:13 +0300

    new billet

--- template-parts/notfound/part-notfound-main-new.php
+++ template-parts/notfound/part-notfound-main-new.php
@@ -1,10 +1,10 @@
-<div class="page404">
+<div class="legal-notfound">
 	<div class="svg-before-text-block">
-		<div class="svg-oops-smile"></div>
-		<h1 class="block-groups_title"><?php echo $args[ 'title' ]; ?></h1>
+		<div class="svg-oops-notfound"></div>
+		<h1 class="notfound-title"><?php echo $args[ 'title' ]; ?></h1>
 	</div>
-	<p><?php echo $args[ 'description' ]; ?></p>
-	<div class="menu-avaible-404">
+	<p class="notfound-description"><?php echo $args[ 'description' ]; ?></p>
+	<div class="menu-notfound">
 		<?php foreach( $args[ 'items' ] as $item ) : ?>
 			<a class="menu-item" href="<?php echo $item[ 'href' ]; ?>" target="_blank"><?php echo $item[ 'label' ]; ?></a>
 		<?php endforeach; ?>
