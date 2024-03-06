bc59afabefb5a34305e9d01654aaf95d584da1c1
sergeykalabin <sergey.kalabin@legalbet.com>
2024-03-06 19:46:54 +0300

    commit frontend

--- template-parts/review/review-bonus.php
+++ template-parts/review/review-bonus.php
@@ -1,24 +1,26 @@
 <?php if( !empty( $args ) ) : ?>
-	<div class="bonus-title">
-		<style type="text/css">
-			.legal-bonus.item-<?php echo $args[ 'index' ]; ?> .bonus-title-image {
-				background-image: url( '<?php echo $args[ 'src' ]; ?>' );
-			}
-		</style>
-		<?php if( !empty( $args['src'] ) ) : ?>
-			<div class="bonus-title-image"></div>
-		<?php endif; ?>
-		<?php if( !empty( $args['title'][ 'text' ] ) ) : ?>
-			<a class="bonus-title-link check-oops" href="<?php echo $args['title'][ 'href' ]; ?>"><?php echo $args['title'][ 'text' ]; ?></a>
-		<?php endif; ?>
-	</div>
-	<h3 class="bonus-name">
-		<?php echo $args[ 'name' ][ 'text' ]; ?>
-	</h3>
-	<div class="bonus-get">
-		<a class="bonus-get-link check-oops" href="<?php echo $args['get'][ 'href' ]; ?>" target="_blank" rel="nofollow">
-			<?php echo $args['get'][ 'text' ]; ?>
-		</a>
+	<div class="bonus-header">
+		<div class="bonus-title">
+			<style type="text/css">
+				.legal-bonus.item-<?php echo $args[ 'index' ]; ?> .bonus-title-image {
+					background-image: url( '<?php echo $args[ 'src' ]; ?>' );
+				}
+			</style>
+			<?php if( !empty( $args['src'] ) ) : ?>
+				<div class="bonus-title-image"></div>
+			<?php endif; ?>
+			<?php if( !empty( $args['title'][ 'text' ] ) ) : ?>
+				<a class="bonus-title-link check-oops" href="<?php echo $args['title'][ 'href' ]; ?>"><?php echo $args['title'][ 'text' ]; ?></a>
+			<?php endif; ?>
+		</div>
+		<h3 class="bonus-name">
+			<?php echo $args[ 'name' ][ 'text' ]; ?>
+		</h3>
+		<div class="bonus-get">
+			<a class="bonus-get-link check-oops" href="<?php echo $args['get'][ 'href' ]; ?>" target="_blank" rel="nofollow">
+				<?php echo $args['get'][ 'text' ]; ?>
+			</a>
+		</div>
 	</div>
 	<?php if ( !empty( $args[ 'content' ] ) ) : ?>
 		<div class="bonus-content <?php echo $args[ 'class' ]; ?>">
