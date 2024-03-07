44978276af6ca17cd836640740317b042b8f5810
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-07 11:51:51 +0300

    new billet

--- lib/bonus/BonusPreview.php
+++ lib/bonus/BonusPreview.php
@@ -94,9 +94,9 @@ class BonusPreview
 
 	public static function get_nodes_shortcode( $dom )
 	{
-		return LegalDOM::get_nodes( $dom, "//text()[contains(., 'legal-bonus terms')]" );
+		// return LegalDOM::get_nodes( $dom, "//text()[contains(., 'legal-bonus terms')]" );
 		
-		// return LegalDOM::get_nodes( $dom, "//text()[contains(., '[" . self::SHORTCODE[ 'preview' ] . "')]" );
+		return LegalDOM::get_nodes( $dom, "//text()[contains(., '[" . self::SHORTCODE[ 'preview' ] . "')]" );
 	}
 
 	public static function insert_anchors( $dom )
