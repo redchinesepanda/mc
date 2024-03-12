da64a88d402463ac7b77bdff2968666abaa084da
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-12 13:46:26 +0300

    new billet

--- template-parts/base/part-header-item.php
+++ template-parts/base/part-header-item.php
@@ -1,6 +1,10 @@
 <?php if ( !empty( $args ) ) : ?>
 	<div class="menu-item <?php echo $args[ 'class' ]; ?>">
-		<a class="item-title" href="<?php echo $args[ 'href' ]; ?>" <?php echo $args[ 'data' ]; ?>><?php echo $args[ 'title' ]; ?></a>
+		<?php if ( $args[ 'href' ] == '#' ) : ?>
+			<span class="item-title" <?php echo $args[ 'data' ]; ?>><?php echo $args[ 'title' ]; ?></span>
+		<?php else : ?>
+			<a class="item-title" href="<?php echo $args[ 'href' ]; ?>" <?php echo $args[ 'data' ]; ?>><?php echo $args[ 'title' ]; ?></a>
+		<?php endif; ?>
 		<?php if ( !empty( $args[ 'children' ] ) ) : ?>
 			<div class="sub-menu">
 				<?php foreach( $args[ 'children' ] as $item ) : ?>
