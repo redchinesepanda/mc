573390f70024c33c0ab192d40b12bcc4e9c55e24
sergeykalabin <sergey.kalabin@legalbet.com>
2024-03-12 10:24:27 +0300

    commit frontend

--- template-parts/compilation/part-compilation-attention.php
+++ template-parts/compilation/part-compilation-attention.php
@@ -1,3 +1,6 @@
-<div class="compilation-attention <?php echo $args['type']; ?>">
-    <?php echo $args['text']; ?>
+<div class="compilation-attention <?php echo $args[ 'type' ]; ?>">
+    <?php if ( $args[ 'type' ] == CompilationMain::TYPE[ 'tooltip' ] ) : ?>
+        <span class="legal-tooltip-close"></span>
+    <?php endif; ?>
+    <?php echo $args[ 'text' ]; ?>
 </div>
