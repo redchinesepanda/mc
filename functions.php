680a0ecc09516640655e3993a6f47fa37088942c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-13 14:00:25 +0300

    new billet

--- functions.php
+++ functions.php
@@ -6,11 +6,6 @@ LegalMain::register();
 
 add_action( 'do_robotstxt', 'wp_kama_do_robotstxt_action' );
 
-/**
- * Function for `do_robotstxt` action-hook.
- * 
- * @return void
- */
 function wp_kama_do_robotstxt_action()
 {
 	echo 'test';
