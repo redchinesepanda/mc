08e25f3c21768eec97246008830928e2d1cb1e53
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-13 13:57:00 +0300

    new billet

--- functions.php
+++ functions.php
@@ -4,4 +4,18 @@ require_once( 'lib/LegalMain.php' );
 
 LegalMain::register();
 
+add_action( 'do_robotstxt', 'wp_kama_do_robotstxt_action' );
+
+/**
+ * Function for `do_robotstxt` action-hook.
+ * 
+ * @return void
+ */
+function wp_kama_do_robotstxt_action()
+{
+	echo 'test';
+
+	die();
+}
+
 ?>
