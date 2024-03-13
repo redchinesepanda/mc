3759941ee7dc3e3a56405387694301a967fa2229
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-13 15:12:21 +0300

    new billet

--- functions.php
+++ functions.php
@@ -1,20 +1,5 @@
 <?php
 
-// add_action( 'do_robotstxt', 'my_robotstxt' );
-// function my_robotstxt(){
-
-// 	$lines = [
-// 		'User-agent: *',
-// 		'Disallow: /wp-admin/',
-// 		'Disallow: /wp-includes/',
-// 		'',
-// 	];
-
-// 	echo implode( "\r\n", $lines );
-
-// 	die; // обрываем работу PHP
-// }
-
 require_once( 'lib/LegalMain.php' );
 
 LegalMain::register();
