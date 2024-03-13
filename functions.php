a9f97e8ab98faf3fe0fdc535f38d8de798f94a9c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-13 13:25:00 +0300

    new billet

--- functions.php
+++ functions.php
@@ -1,38 +1,7 @@
 <?php
 
-// defined( 'TVE_EDITOR_URL' ) || define( 'TVE_EDITOR_URL', get_template_directory_uri() . '/architect/' );
-
-// add_action( 'wp_enqueue_scripts', function () {
-// 	$parent_style = 'parent-style';
-
-// 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-
-// 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', [ $parent_style ], wp_get_theme()->get( 'Version' ) );
-// } );
-
 require_once( 'lib/LegalMain.php' );
 
 LegalMain::register();
-
-add_action( 'do_robotstxt', 'wp_kama_robots_txt' );
-
-function wp_kama_robots_txt(){
-
-	$lines = [
-		'User-agent: *',
-		'Disallow: /wp-admin/',
-		'Disallow: /wp-includes/',
-		'',
-	];
-
-	echo implode( "\r\n", $lines );
-
-	die; // обрываем работу PHP
-}
-
-// if( !function_exists( 'tve_load_global_variables' ) )
-// {
-// 	function tve_load_global_variables() {}
-// }
 
 ?>
