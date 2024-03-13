dcd3eb798d1d6775b65e19938d298b7988cdbad9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-13 16:00:46 +0300

    new billet

--- lib/tools/ToolSitemapXML.php
+++ lib/tools/ToolSitemapXML.php
@@ -10,7 +10,7 @@ class ToolSitemapXML
 	public static function register()
     {
         LegalDebug::debug( [
-            'ToolSitemapXML',
+            'ToolSitemapXML' => 'register',
         ] );
 
         $handler = new self(); 
