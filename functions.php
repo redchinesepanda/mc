b851afa758213d07f21c67903718cbb7acf022c5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-13 14:00:59 +0300

    new billet

--- functions.php
+++ functions.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once( 'lib/LegalMain.php' );
-
-LegalMain::register();
-
-add_action( 'do_robotstxt', 'wp_kama_do_robotstxt_action' );
-
 function wp_kama_do_robotstxt_action()
 {
 	echo 'test';
 
 	die();
 }
+
+require_once( 'lib/LegalMain.php' );
+
+LegalMain::register();
+
+add_action( 'do_robotstxt', 'wp_kama_do_robotstxt_action' );
 
 ?>
