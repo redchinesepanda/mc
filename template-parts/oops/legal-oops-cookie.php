ecd87f170cb2f743b820a712519669ae35d3018a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-14 12:10:56 +0300

    new billet

--- template-parts/oops/legal-oops-cookie.php
+++ template-parts/oops/legal-oops-cookie.php
@@ -1,8 +1,10 @@
 <div class="legal-oops-cookie-wrapper">
 	<div class="legal-oops-cookie">
 		<span class="oops-cookie-title"><?php echo $args[ 'title' ]; ?></span>
-		<span class="oops-cookie-description"><?php echo $args[ 'description' ]; ?></span>
-		<a class="oops-cookie-privacy" href="<?php echo $args[ 'privacy' ][ 'href' ]; ?>"><?php echo $args[ 'privacy' ][ 'label' ]; ?></a>
+		<div class="oops-cookie-info">
+			<span class="oops-cookie-description"><?php echo $args[ 'description' ]; ?></span>
+			<a class="oops-cookie-privacy" href="<?php echo $args[ 'privacy' ][ 'href' ]; ?>"><?php echo $args[ 'privacy' ][ 'label' ]; ?></a>
+		</div>
 		<div class="oops-cookie-button"><?php echo $args[ 'label' ]; ?></div>
 	</div>
 </div>
