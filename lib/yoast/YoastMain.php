16533af88ccdbf798496d49306c883ee139529ce
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-20 13:52:28 +0300

    new billet

--- lib/yoast/YoastMain.php
+++ lib/yoast/YoastMain.php
@@ -28,6 +28,8 @@ class YoastMain
         \add_filter( 'wpseo_force_creating_and_using_attachment_indexables', '__return_true' );
 
         add_action( 'wpseo_register_extra_replacements', [ $handler, 'register_my_plugin_extra_replacements' ] );
+
+        add_action( 'wp_loaded', [ $handler, 'wpwc_fix_yoast_seo_robots_txt' ] );
     }
 
     /**
