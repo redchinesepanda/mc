065fc9d65d11a3b303274ddfdd9c4c75b92e8fa7
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-23 00:53:28 +0300

    new billet

--- lib/wpml/WPMLDomain.php
+++ lib/wpml/WPMLDomain.php
@@ -19,7 +19,16 @@ class WPMLDomain
 	
 	function wp_kama_option_filter( $value, $option )
 	{
+		LegalDebug::debug( [
+			'WPMLDomain' => 'wp_kama_option_filter',
 
+			'default_language' => $default_language,
+
+			'option' => $option,
+
+			'value' => $value,
+		] );
+		
 		if ( $option == self::OPTIONS[ 'icl-sitepress-settings' ] )
 		{
 			$default_language = ToolNotFound::get_default_language();
