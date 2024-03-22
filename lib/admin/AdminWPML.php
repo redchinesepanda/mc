f65df9680db4c2007065bf751bdb00245940d365
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-22 14:37:41 +0300

    new billet

--- lib/admin/AdminWPML.php
+++ lib/admin/AdminWPML.php
@@ -8,19 +8,19 @@ class AdminWPML
 		
         // add_action( 'wpml_admin_language_switcher_items', [ $handler, 'mc_wpml_admin_language_switcher_items' ] );
         
-		add_action( 'admin_bar_menu', [ $handler, 'mc_admin_bar_menu' ] );
+		// add_action( 'admin_bar_menu', [ $handler, 'mc_admin_bar_menu' ] );
 	}
 
-	public static function mc_admin_bar_menu( $wp_admin_bar )
-	{
-		LegalDebug::debug( [
-			'AdminWPML' =>'mc_admin_bar_menu',
+	// public static function mc_admin_bar_menu( $wp_admin_bar )
+	// {
+	// 	LegalDebug::debug( [
+	// 		'AdminWPML' =>'mc_admin_bar_menu',
 
-			'$wp_admin_bar' => $wp_admin_bar,
-		] );
+	// 		'$wp_admin_bar' => $wp_admin_bar,
+	// 	] );
 
-		return $wp_admin_bar;
-	}
+	// 	return $wp_admin_bar;
+	// }
 
 	public static function mc_wpml_admin_language_switcher_items( $items )
 	{
