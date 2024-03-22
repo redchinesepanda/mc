55c48327072ea720418ddd256f8f2613b9fada07
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-23 00:05:42 +0300

    new billet

--- lib/wpml/WPMLDomain.php
+++ lib/wpml/WPMLDomain.php
@@ -57,42 +57,54 @@ class WPMLDomain
 		LegalDebug::debug( [
 			'WPMLDomain' => 'get_posts',
 
-			'default_language' => $sitepress->get_setting( 'default_language' ),
-
-			'urls' => $sitepress->get_setting( 'urls' ),
-
-			'language_negotiation_type' => $sitepress->get_setting( 'language_negotiation_type' ),
-
-			'WPML_LANGUAGE_NEGOTIATION_TYPE_DIRECTORY' => WPML_LANGUAGE_NEGOTIATION_TYPE_DIRECTORY,
-
-			// 'current_language' => WPMLMain::current_language(),
+			'check_change_default_language' => self::check_change_default_language(),
 
 			'get_default_language' => ToolNotFound::get_default_language(),
 
-			'check_default_language' => self::check_default_language(),
+			'default_language' => $sitepress->get_setting( 'default_language' ),
+
+			// 'urls' => $sitepress->get_setting( 'urls' ),
+
+			// 'language_negotiation_type' => $sitepress->get_setting( 'language_negotiation_type' ),
+
+			// 'WPML_LANGUAGE_NEGOTIATION_TYPE_DIRECTORY' => WPML_LANGUAGE_NEGOTIATION_TYPE_DIRECTORY,
+
+			// 'current_language' => WPMLMain::current_language(),
 		] );
 
-		if ( self::check_default_language() )
+		$default_language = ToolNotFound::get_default_language();
+
+		if ( self::check_change_default_language() )
 		{
-			$sitepress->set_setting(
+			$sitepress->set_setting
+			(
 				self::SETTINGS[ 'default-language' ],
 				
 				// WPMLMain::current_language(),
 
-				ToolNotFound::get_default_language(),
+				// ToolNotFound::get_default_language(),
+
+				$default_language,
 				
 				true
 			);
 		}
 		else
 		{
-			$sitepress->set_setting( 'default_language', 'en', true );
+			$sitepress->set_setting
+			(
+				self::SETTINGS[ 'default-language' ],
+				
+				$default_language,
+				
+				true
+			);
 		}
     }
 	
-	public static function check_default_language()
+	public static function check_change_default_language()
 	{
-		return ToolNotFound::check_one_restricted_language();
+		return ToolNotFound::check_domain();
 	}
 }
 
