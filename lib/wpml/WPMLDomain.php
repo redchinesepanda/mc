14307a230ac2a5f234bf7864a2863623d65f6990
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-23 00:15:23 +0300

    new billet

--- lib/wpml/WPMLDomain.php
+++ lib/wpml/WPMLDomain.php
@@ -8,9 +8,9 @@ class WPMLDomain
 
 		// add_action( 'update_option_icl_sitepress_settings', [ $handler,'prevent_update_option' ], 10, 3 );
 		
-		add_action( 'update_option_' . self::OPTIONS[ 'wpml-settings' ], [ $handler,'prevent_update_option' ], 10, 3 );
+		// add_action( 'update_option_' . self::OPTIONS[ 'wpml-settings' ], [ $handler,'prevent_update_option' ], 10, 3 );
 
-		// add_action( 'update_option', [ $handler,'prevent_update_option' ], 10, 3 );
+		add_action( 'update_option', [ $handler,'prevent_update_option' ], 10, 3 );
 	}
 
 	public static function register()
@@ -28,22 +28,22 @@ class WPMLDomain
 
 	function prevent_update_option( $old_value, $value, $option )
 	{
-		// if ( $option == self::OPTIONS[ 'wpml-settings' ] )
-		// {
-			// LegalDebug::die( [
-			// 	'WPMLDomain' => 'prevent_update_option',
+		if ( $option == self::OPTIONS[ 'wpml-settings' ] )
+		{
+			LegalDebug::die( [
+				'WPMLDomain' => 'prevent_update_option',
 	
-			// 	'old_value' => $old_value,
+				'old_value' => $old_value,
 	
-			// 	'value' => $value,
+				'value' => $value,
 	
-			// 	'option' => $option,
-			// ] );
+				'option' => $option,
+			] );
 
 			return $old_value;
-		// }
+		}
 
-		// return $value;
+		return $value;
 	}
 
 	const SETTINGS = [
