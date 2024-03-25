2988e8d77b73085922ed6caf01bbb02eebe9ed8c
sergeykalabin <sergey.kalabin@legalbet.com>
2024-03-25 17:39:29 +0300

    commit frontend

--- template-parts/bonus/part-legal-bonus-categories.php
+++ template-parts/bonus/part-legal-bonus-categories.php
@@ -1,11 +1,17 @@
 <?php if ( !empty( $args[ 'items' ] ) ) : ?>
 	<div class="legal-bonus-categories">
-		<?php foreach ( $args[ 'items' ] as $item ) : ?>
-			<?php if ( !empty( $item[ 'anchor' ] ) ) : ?>
-				<a class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?>" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
-			<?php else: ?>
-				<span class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?>"><?php echo $item[ 'label' ]; ?></span>
-			<?php endif; ?>
-		<?php endforeach; ?>
+		<!-- новый тег для swiper -->
+		<div class="swiper">
+			<!-- новый тег для swiper -->
+			<div class="swiper-wrapper">
+				<?php foreach ( $args[ 'items' ] as $item ) : ?>
+					<?php if ( !empty( $item[ 'anchor' ] ) ) : ?>
+						<a class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?> swiper-slide" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
+					<?php else: ?>
+						<span class="bonus-categories-item bonus-categories-item-<?php echo $item[ 'id' ]; ?> swiper-slide"><?php echo $item[ 'label' ]; ?></span>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			</div>
+		</div>
 	</div>
 <?php endif;?>
