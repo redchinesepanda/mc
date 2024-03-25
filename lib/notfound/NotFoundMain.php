8ceefc86107811648d5c0923509cecc46d0ef114
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-25 11:18:41 +0300

    new billet

--- lib/notfound/NotFoundMain.php
+++ lib/notfound/NotFoundMain.php
@@ -40,6 +40,11 @@ class NotFoundMain
 		}
     }
 
+	public static function check_not_notfound()
+    {
+        return !is_404();
+    }
+
 	public static function check()
     {
         return is_404();
