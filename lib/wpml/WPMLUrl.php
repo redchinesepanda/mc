f392d70ee836310c75f413869775ddd0ce489748
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-03-26 15:37:52 +0300

    new billet

--- lib/wpml/WPMLUrl.php
+++ lib/wpml/WPMLUrl.php
@@ -2,14 +2,14 @@
 
 class WPMLUrl
 {
-	const ELEMENT = [
-		'anchor' => '<a',
-	];
+	// const ELEMENT = [
+	// 	'anchor' => '<a',
+	// ];
 
-	public static function check_contains_anchor()
-    {
-        return LegalComponents::check_contains( self::ELEMENT[ 'anchor' ] );
-    }
+	// public static function check_contains_anchor()
+    // {
+    //     return LegalComponents::check_contains( self::ELEMENT[ 'anchor' ] );
+    // }
 
 	public static function check_url_modify()
 	{
@@ -24,44 +24,61 @@ class WPMLUrl
 		{
 			$handler = new self();
 
-			add_action( 'the_content', [ $handler, 'modify_content' ] );
+			add_filter( 'stylesheet_directory_uri', 'modify_stylesheet_directory_uri', 10, 3 );
+
+			// add_action( 'the_content', [ $handler, 'modify_content' ] );
 		}
     }
 
-	public static function modify_content( $content )
+	public static function modify_stylesheet_directory_uri( $stylesheet_dir_uri, $stylesheet, $theme_root_uri )
 	{
-		$dom = LegalDOM::get_dom( $content ); 
+		LegalDebug::debug( [
+			'WPMLUrl' =>'modify_stylesheet_directory_uri',
 
-		self::modify_anchors( $dom );
+			'$stylesheet_dir_uri' => $stylesheet_dir_uri,
 
-		return $dom->saveHTML( $dom );
+            '$stylesheet' => $stylesheet,
+			
+            '$theme_root_uri' => $theme_root_uri,
+        ] );
+
+		return $stylesheet_dir_uri;
 	}
 
-	public static function get_nodes_anchor( $dom )
-	{
-		return LegalDOM::get_nodes( $dom, "//a" );
-	}
+	// public static function modify_content( $content )
+	// {
+	// 	$dom = LegalDOM::get_dom( $content ); 
 
-	public static function modify_anchors( $dom )
-	{
-		$nodes = self::get_nodes_anchor( $dom );
+	// 	self::modify_anchors( $dom );
 
-		if ( $nodes->length == 0 )
-		{
-			return false;
-		}
+	// 	return $dom->saveHTML( $dom );
+	// }
 
-		foreach ( $nodes as $node )
-        {
-			$href = $node->getAttribute( 'href' );
+	// public static function get_nodes_anchor( $dom )
+	// {
+	// 	return LegalDOM::get_nodes( $dom, "//a" );
+	// }
 
-			// $href = str_replace( 'http://', '', $href );
+	// public static function modify_anchors( $dom )
+	// {
+	// 	$nodes = self::get_nodes_anchor( $dom );
 
-			// $node->setAttribute( 'href', $href );
-		}
+	// 	if ( $nodes->length == 0 )
+	// 	{
+	// 		return false;
+	// 	}
 
-		return true;
-	}
+	// 	foreach ( $nodes as $node )
+    //     {
+	// 		$href = $node->getAttribute( 'href' );
+
+	// 		// $href = str_replace( 'http://', '', $href );
+
+	// 		// $node->setAttribute( 'href', $href );
+	// 	}
+
+	// 	return true;
+	// }
 }
 
 ?>
