01ba0261381b3cb71ee1e0b9f8fe6375700a6cd5
sergeykalabin <sergey.kalabin@legalbet.com>
2024-03-27 12:25:56 +0300

    commit frontend

--- template-parts/compilation/part-compilation-main.php
+++ template-parts/compilation/part-compilation-main.php
@@ -3,15 +3,11 @@
     <?php if ( !empty( $args['settings']['title']['text'] ) ) : ?>
         <div class="compilation-title  <?php echo $args['settings']['title']['class']; ?>"><?php echo $args['settings']['title']['text']; ?></div>
     <?php endif; ?>
-    <div class="legal-compilation-info">
-        <?php if ( !empty( $args[ 'settings' ][ 'date' ][ 'value' ] ) ) : ?>
+    <div class="legal-compilation-info"><?php if ( !empty( $args[ 'settings' ][ 'date' ][ 'value' ] ) ) : ?>
             <div class="compilation-date"><?php echo $args[ 'settings' ][ 'date' ][ 'label' ]; ?>: <?php echo $args[ 'settings' ][ 'date' ][ 'value' ]; ?></div>
-        <?php endif; ?>
-        <?php echo CompilationMain::render_attention_tooltip( $args['settings']['attention'] ); ?>
-        <?php echo CompilationMain::render_attention( $args['settings']['attention'], CompilationMain::POSITION[ 'below' ] ); ?>
-    </div>
+            <?php endif; ?><?php echo CompilationMain::render_attention_tooltip( $args['settings']['attention'] ); ?><?php echo CompilationMain::render_attention( $args['settings']['attention'], CompilationMain::POSITION[ 'below' ] ); ?></div>
     <?php foreach( $args['billets'] as $billet ) : ?>
         <?php echo BilletMain::render_billet( $billet ); ?>
     <?php endforeach; ?>
     <?php echo CompilationMain::render_attention( $args['settings']['attention'], CompilationMain::POSITION[ 'bottom' ] ); ?>
-</div>
+</div> 
