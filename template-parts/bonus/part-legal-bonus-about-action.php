f903ad757ec5f92c8bb6c02ab60c44ea6bb22733
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-01 09:00:49 +0300

    new billet

--- template-parts/bonus/part-legal-bonus-about-action.php
+++ template-parts/bonus/part-legal-bonus-about-action.php
@@ -1,5 +1,5 @@
 <div class="bonus-about-action">
-	<span class="about-action-label"><?php echo $args[ 'bonus' ][ 'label' ] ?></span>
-	<span class="about-action-label"><?php echo $args[ 'bonus' ][ 'value' ] ?></span>
+	<span class="about-action-label"><?php echo $args[ 'label' ] ?></span>
+	<span class="about-action-label"><?php echo $args[ 'value' ] ?></span>
 	<?php echo BonusAbout::render_button(); ?>
 </div>
