21cf6b74821af647bdff9d4aac386b5d3180b90e
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-01 15:34:31 +0300

    commit frontend

--- template-parts/bonus/part-legal-bonus-featured.php
+++ template-parts/bonus/part-legal-bonus-featured.php
@@ -1,5 +1,5 @@
 <div class="legal-bonus-featured">
-	<a class="bonus-featured-image-link" href="<?php echo $args[ 'preview' ][ 'href' ]; ?>">
+	<a class="bonus-featured-image-link" href="<?php echo $args[ 'preview' ][ 'href' ]; ?>" rel="nofollow">
 		<img class="bonus-featured-image bonus-featured-image-<?php echo $args[ 'preview' ][ 'id' ]; ?>" src="<?php echo $args[ 'preview' ][ 'src' ]; ?>" width="<?php echo $args[ 'preview' ][ 'width' ]; ?>" height="<?php echo $args[ 'preview' ][ 'height' ]; ?>" loading="lazy">
 	</a>
 </div>
