c22e6a235232ae9a35a7b9de507feb8b17e7573f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-05 09:55:55 +0300

    multisite

--- template-parts/wpml/wpml-lang-switcher-main.php
+++ template-parts/wpml/wpml-lang-switcher-main.php
@@ -5,23 +5,25 @@ LegalDebug::debug( [
 ] );
 
 ?>
-<div class="lang-switcher">
-    <div class="lang-current lang-item <?php echo $args[ 'active' ][ 'class' ]; ?>">
-        <div class="lang-title"><?php echo $args[ 'active' ][ 'title' ]; ?></div>
-        <?php if ( $args[ 'active' ][ 'suffix' ] ) : ?>
-            <a class="lang-title-suffix" href="<?php echo $args[ 'active' ][ 'href' ]; ?>"><?php echo $args[ 'active' ][ 'suffix' ]; ?></a>
-        <?php endif; ?>
+<?php if ( !empty( $args ) ) : ?>
+    <div class="lang-switcher">
+        <div class="lang-current lang-item <?php echo $args[ 'active' ][ 'class' ]; ?>">
+            <div class="lang-title"><?php echo $args[ 'active' ][ 'title' ]; ?></div>
+            <?php if ( $args[ 'active' ][ 'suffix' ] ) : ?>
+                <a class="lang-title-suffix" href="<?php echo $args[ 'active' ][ 'href' ]; ?>"><?php echo $args[ 'active' ][ 'suffix' ]; ?></a>
+            <?php endif; ?>
+        </div>
+        <div id="lang-menu-1" class="menu-avaible">
+            <?php if ( !empty( $args[ 'languages' ] ) ) : ?>
+                <?php foreach( $args[ 'languages' ] as $lang ) : ?>
+                    <a class='lang-avaible lang-item' href="<?php echo $lang[ 'href' ]; ?>" target="_blank">
+                        <div class="lang-image locale-<?php echo $lang[ 'id' ]; ?>"></div>
+                        <div class="lang-title">
+                            <?php echo $lang[ 'title' ]; ?>
+                        </div>
+                    </a>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </div>
     </div>
-    <div id="lang-menu-1" class="menu-avaible">
-        <?php if ( !empty( $args[ 'languages' ] ) ) : ?>
-            <?php foreach( $args[ 'languages' ] as $lang ) : ?>
-                <a class='lang-avaible lang-item' href="<?php echo $lang[ 'href' ]; ?>" target="_blank">
-                    <div class="lang-image locale-<?php echo $lang[ 'id' ]; ?>"></div>
-                    <div class="lang-title">
-                        <?php echo $lang[ 'title' ]; ?>
-                    </div>
-                </a>
-            <?php endforeach; ?>
-        <?php endif; ?>
-    </div>
-</div> 
+<?php endif; ?>
