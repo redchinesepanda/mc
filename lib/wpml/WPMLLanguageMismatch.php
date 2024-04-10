d2f2a0a576c357eb3af047e0b96acff3c134d059
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-10 15:34:18 +0300

    new billet

--- lib/wpml/WPMLLanguageMismatch.php
+++ lib/wpml/WPMLLanguageMismatch.php
@@ -4,9 +4,9 @@ class WPMLLanguageMismatch
 {
 	public static function register_functions()
 	{
-		// $handler = new self();
+		$handler = new self();
 
-		// add_filter( 'permalink_manager_detected_post_id', [ $handler, 'fix_language_mismatch' ], 9, 3 );
+		add_filter( 'permalink_manager_detected_post_id', [ $handler, 'fix_language_mismatch' ], 9, 3 );
 
 		// LegalDebug::debug( [
 		// 	'WPMLLanguageMismatch' => 'register_functions',
@@ -26,17 +26,30 @@ class WPMLLanguageMismatch
 
 	public static function fix_language_mismatch( $item_id, $uri_parts, $is_term = false )
 	{
-		// LegalDebug::debug( [
-		// 	'WPMLLanguageMismatch' => 'fix_language_mismatch',
+		LegalDebug::debug( [
+			'WPMLLanguageMismatch' => 'fix_language_mismatch',
 
-		// 	'item_id' => $item_id,
+			'item_id' => $item_id,
 
-        //     'uri_parts' => $uri_parts,
+            'uri_parts' => $uri_parts,
 
-        //     'is_term' => $is_term,
-		// ] );
+            'is_term' => $is_term,
+		] );
 
-		return 0;
+		$post = get_post( $item_id );
+
+		if ( $post )
+		{
+			$url = get_post_permalink( $post->ID );
+
+			LegalDebug::debug( [
+				'WPMLLanguageMismatch' => 'fix_language_mismatch',
+	
+				'url' => $url,
+			] );
+		}
+
+		return $item_id;
 	}
 
 	// {
