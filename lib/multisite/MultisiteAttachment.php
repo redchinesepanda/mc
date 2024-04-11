8a3e22b6d850cf65ca63f399ca62532262bcdf42
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-12 00:02:24 +0300

    multisite

--- lib/multisite/MultisiteAttachment.php
+++ lib/multisite/MultisiteAttachment.php
@@ -177,7 +177,7 @@ class MultisiteAttachment
 	{
 		$inserted_attachment_id = false;
 
-		$post_moved_id = MultisiteMeta::get_post_moved( $post[ 'ID' ] );
+		$post_moved_id = MultisiteMeta::get_moved( $post[ 'ID' ] );
 
 		$path = self::get_path( $post[ 'ID' ] );
 
