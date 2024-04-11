f43409015c68477754c60464e6fec5237310408c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-11 11:55:32 +0300

    new billet

--- lib/tools/ToolRewrite.php
+++ lib/tools/ToolRewrite.php
@@ -26,7 +26,7 @@ class ToolRewrite
 	
 			add_action( 'template_redirect', [ $handler, 'debug_404_template_redirect' ], 99999 );
 	
-			// add_filter ( 'template_include', [ $handler, 'debug_404_template_dump' ] );
+			add_filter ( 'template_include', [ $handler, 'debug_404_template_dump' ] );
 	
 			// add_filter( 'post_type_link', [ $handler, 'review_link' ], 10, 4 );
 	
