126e364f89a9e41b9b474ef1bd6b6555fef3d55c
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-11 14:36:43 +0300

    commit frontend

--- lib/review/ReviewAbout.php
+++ lib/review/ReviewAbout.php
@@ -236,10 +236,11 @@ class ReviewAbout
     public static function get_name( $group, $mode, $bonus_exception )
     {
         // заккоментил Калабин. Проверка вывода названия бонуса в сайдбаре
-       /*  if ( $mode == 'mini' || $bonus_exception )
+        // if ( $mode == 'mini' || $bonus_exception )
+        if ( $mode == 'mini' && $bonus_exception )
         {
             return $group[ 'about-title' ];
-        } */
+        }
 
         return $group[ 'about-bonus' ];
     }
