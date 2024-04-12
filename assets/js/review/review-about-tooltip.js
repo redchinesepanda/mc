97af2d21a20b8113b4f03b93d072493165ef8c71
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-12 12:56:49 +0300

    commit frontend

--- assets/js/review/review-about-tooltip.js
+++ assets/js/review/review-about-tooltip.js
@@ -30,7 +30,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 		this.querySelectorAll( selectors.tooltip ).forEach( prepareModal, element );
 
-		this.querySelectorAll( selectors.tooltip ).forEach( prepareClose, element );
+		/* this.querySelectorAll( selectors.tooltip ).forEach( prepareClose, element ); */
 
 		this.querySelectorAll( selectors.tooltipClose ).forEach( prepareClose, element );
 
