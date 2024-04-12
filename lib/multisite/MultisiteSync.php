3398d65c7429add55b4d09d3497e4a65f120e18b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-12 12:29:20 +0300

    multisite

--- lib/multisite/MultisiteSync.php
+++ lib/multisite/MultisiteSync.php
@@ -33,45 +33,77 @@ class MultisiteSync
 		add_filter( 'save_post_' . self::POST_TYPE[ 'billet' ], [ $handler, 'set_attachments' ], 10, 2 );
 	}
 
+	public static function get_subfield_names( $subfields )
+	{
+		return array_column( $subfields, 'name' );
+	}
+
+	public static function get_field_names( $field, $subfields = [] )
+	{
+		$field_names = [];
+
+		$subfield_names = self::get_subfield_names( $subfields );
+
+		foreach ( $subfield_names as $subfield_name )
+		{
+			$field_names[] = sprintf(
+				self::PATTERNS[ 'group-field' ],
+				
+				$field[ 'name' ],
+	
+				$subfield_name
+			);
+		}
+
+		return $field_names;
+	}
+
 	public static function set_attachments( $post_id, $post )
     {
-		$field_name = sprintf(
-			self::PATTERNS[ 'group-field' ],
-			
-			self::FIELDS[ 'about' ][ 'name' ],
+		$field_names = self::get_field_names( self::FIELDS[ 'about' ], self::FIELD_ABOUT );
 
-			self::FIELD_ABOUT[ 'logo' ][ 'name' ]
-		);
+		// $field_name = sprintf(
+		// 	self::PATTERNS[ 'group-field' ],
+			
+		// 	self::FIELDS[ 'about' ][ 'name' ],
+
+		// 	self::FIELD_ABOUT[ 'logo' ][ 'name' ]
+		// );
 
 		LegalDebug::debug( [
 			'MultisiteSync' => 'set_attachments',
 
 			'post_id' => $post_id,
 
-			'field_name' => $field_name,
+			// 'field_name' => $field_name,
+
+			'field_names' => $field_names,
 		] );
 
-		if ( $origin_post_id = MultisiteACF::get_field_raw( $field_name, $post_id ) )
+		foreach ( $field_names as $field_name )
 		{
-			if ( $post_moved_id = MultisitePost::get_post_moved_id( $origin_post_id ) )
+			if ( $origin_post_id = MultisiteACF::get_field_raw( $field_name, $post_id ) )
 			{
-				MultisiteACF::update_field( $field_name, $post_moved_id, $post_id );
+				if ( $post_moved_id = MultisitePost::get_post_moved_id( $origin_post_id ) )
+				{
+					MultisiteACF::update_field( $field_name, $post_moved_id, $post_id );
+				}
+	
+				// if ( $post_moved_id = MultisiteMeta::get_post_moved_id( $origin_post_id ) )
+				// {
+				// 	LegalDebug::debug( [
+				// 		'MultisiteSync' => 'set_attachments',
+			
+				// 		'post_moved_id' => $post_moved_id,
+				// 	] );
+				// }
+	
+				LegalDebug::debug( [
+					'MultisiteSync' => 'set_attachments',
+	
+					'origin_post_id' => $origin_post_id,
+				] );
 			}
-
-			// if ( $post_moved_id = MultisiteMeta::get_post_moved_id( $origin_post_id ) )
-			// {
-			// 	LegalDebug::debug( [
-			// 		'MultisiteSync' => 'set_attachments',
-		
-			// 		'post_moved_id' => $post_moved_id,
-			// 	] );
-			// }
-
-			LegalDebug::die( [
-				'MultisiteSync' => 'set_attachments',
-
-				'origin_post_id' => $origin_post_id,
-			] );
 		}
 
 		LegalDebug::die( [
