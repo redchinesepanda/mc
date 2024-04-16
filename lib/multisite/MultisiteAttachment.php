f67488087a8f5a6de0c8e139110a4201fc7363e9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-16 16:39:12 +0300

    multisite

--- lib/multisite/MultisiteAttachment.php
+++ lib/multisite/MultisiteAttachment.php
@@ -177,15 +177,15 @@ class MultisiteAttachment
 
 		$result = sprintf( self::PATTERNS[ 'shortcode' ], self::SHORTCODES[ 'gallery' ], $atts );
 
-		// LegalDebug::debug( [
-		// 	'MultisiteAttachment' => 'get_gallery_shortcodes',
+		LegalDebug::debug( [
+			'MultisiteAttachment' => 'get_gallery_shortcodes',
 
-		// 	'match' => $match,
+			'match' => $match,
 
-		// 	'atts' => $atts,
+			'atts' => $atts,
 
-		// 	'result' => $result,
-        // ] );
+			'result' => $result,
+        ] );
 
 		return $result;
 	}
