1cc95f85a80a44596b4ff261dd78ff94a954eeee
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-16 11:16:52 +0300

    new billet

--- template-parts/compilation/part-compilation-about.php
+++ template-parts/compilation/part-compilation-about.php
@@ -12,7 +12,8 @@
             <div class="about-section-content">
                 <h1><?php echo $args[ 'title' ]; ?></h1>
                 <?php foreach ( $args[ 'content' ] as $item ) : ?>
-                    <p class="<?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p>
+                    <!-- <p class="<?php echo $item[ 'class' ]; ?>"><?php echo $item[ 'text' ]; ?></p> -->
+                    <?php echo $item[ 'text' ]; ?>
                 <?php endforeach; ?>
 				<?php if ( !empty( $args[ 'read-more' ] ) ) : ?>
                 	<span class="legal-cut-control" data-content-default="Read more" data-content-active="Hide" data-cut-set-id="0">Read more</span>
