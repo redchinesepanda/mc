0cdee857dee8380401283ab09a8effcf05cc2c0a
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-17 16:12:32 +0300

    commit frontend

--- assets/js/forecast/legal-forecast-vote.js
+++ assets/js/forecast/legal-forecast-vote.js
@@ -1,80 +1,24 @@
 // legal-forecast-vote-js start
 
-document.addEventListener( 'DOMContentLoaded', function ()
-{
-	/* function toggleSet( element )
-	{
-		element.classList.toggle( classes.active );
-	}
+document.addEventListener( 'DOMContentLoaded', function () {
 
-	function toggleModal( event )
-	{
-		event.currentTarget.closest( selectors.aboutAchievement ).querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) ).forEach( toggleSet );
-	}
-
-	function prepareClose( element )
-	{
-		element.dataset.tooltipSet = this.dataset.tooltipSet;
-
-		element.addEventListener( 'click', toggleModal, false );
-	}
-
-	function prepareModal( element )
-	{
-		element.dataset.tooltipSet = this.dataset.tooltipSet;
-	}
-	
-	function prepareTooltip( element )
-	{
-		element.dataset.tooltipSet = this.dataset.tooltipSet;
-
-		this.querySelectorAll( selectors.tooltip ).forEach( prepareModal, element );
-
-		this.querySelectorAll( selectors.tooltipClose ).forEach( prepareClose, element );
-
-		element.addEventListener( 'click', toggleModal, false );
-	}
-
-	function prepareElements( element, index )
-	{
-		element.dataset.tooltipSet = index;
-
-		element.querySelectorAll( selectors.tooltipOpen ).forEach( prepareTooltip, element );
-	}
-
-	const classes = {
+    const classes = {
 		active: 'legal-active',
 	};
+	
+    const selectors = {
+		voteLabel: '.wp-polls .form_radio_btn input[type="radio"]:checked + label',
 
-	const selectors = {
-		tooltipOpen: '.review-about .achievement-name',
-		
-		tooltipClose: '.review-about .achievement-tooltip-close',
-
-		tooltip: '.review-about .achievement-tooltip-background',
-
-		aboutAchievement: '.review-about .about-achievement',
-
-		tooltipSet: function ( id ) {
-			return '[data-tooltip-set="' + id + '"]';
-		}
+		voteButtonSend: '.wp-polls .Button-my-style'
 	};
 
-	document.querySelectorAll( selectors.aboutAchievement ).forEach( prepareElements ); */
-
-	// Не оптимизированно, но работает - закрытие по клику на фон. Начало
-	let tooltipBackground = document.querySelector( selectors.tooltip );
-
-	function closeBackgroundTooltip() {
-		if( tooltipBackground.classList.contains( classes.active ) ) {
-			tooltipBackground.classList.remove( classes.active );
-		}
+    function buttonDisable( event ) {
+        console.log("скрипт подключился")
+		document.querySelector( selectors.voteButtonSend ).classList.toggle( classes.active );
 	}
 
-    console.log('Js подключен')
+    document.querySelector( selectors.voteLabel ).addEventListener( 'click', buttonDisable );
 
-	tooltipBackground.addEventListener( 'click', closeBackgroundTooltip );
-	// Не оптимизированно, но работает - закрытие по клику на фон. Конец
 } );
 
 // legal-forecast-vote-js end 
