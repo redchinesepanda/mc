2e31cd54cbda98915eb1734e641f7f9369dd6db0
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-17 15:16:06 +0300

    multisite

--- lib/multisite/MultisiteAttachmentSync.php
+++ lib/multisite/MultisiteAttachmentSync.php
@@ -180,9 +180,9 @@ class MultisiteAttachmentSync
 			}
 		}
 
-		// LegalDebug::debug( [
-		// 	'MultisiteAttachmentSync' => 'set_attachments',
-		// ] );
+		LegalDebug::die( [
+			'MultisiteAttachmentSync' => 'set_attachments',
+		] );
     }
 }
 
