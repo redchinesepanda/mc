e84a1daf40ab002a91106a969b165fd501479781
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-18 15:00:58 +0300

    multisite

--- lib/multisite/MultisitePostSync.php
+++ lib/multisite/MultisitePostSync.php
@@ -165,37 +165,37 @@ class MultisitePostSync
 		
 		$fields = MultisiteTermSync::get_fields( $post_id, $field_names );
 
-		LegalDebug::debug( [
-			'MultisiteTermSync' => 'set_terms',
+		// LegalDebug::debug( [
+		// 	'MultisiteTermSync' => 'set_terms',
 
-			'fields' => $fields,
-		] );
+		// 	'fields' => $fields,
+		// ] );
 
 		foreach ( $fields as $field_name => $field_value )
 		{
-			LegalDebug::debug( [
-				'MultisiteTermSync' => 'set_terms',
+			// LegalDebug::debug( [
+			// 	'MultisiteTermSync' => 'set_terms',
 
-				'field_name' => $field_name,
+			// 	'field_name' => $field_name,
 
-				'field_value' => $field_value,
-			] );
+			// 	'field_value' => $field_value,
+			// ] );
 
 			if ( $field_value_sync = MultisiteTermSync::get_field_value_sync( $field_name, $field_value ) )
 			{
-				LegalDebug::debug( [
-					'MultisiteTermSync' => 'set_terms',
+				// LegalDebug::debug( [
+				// 	'MultisiteTermSync' => 'set_terms',
 
-					'field_value_sync' => $field_value_sync,
-				] );
+				// 	'field_value_sync' => $field_value_sync,
+				// ] );
 
 				MultisiteACF::update_field( $field_name, $field_value_sync, $post_id );
 
-				LegalDebug::debug( [
-					'MultisiteTermSync' => 'set_terms',
+				// LegalDebug::debug( [
+				// 	'MultisiteTermSync' => 'set_terms',
 
-					'get_field_raw' => MultisiteACF::get_field_raw( $field_name, $post_id ),
-				] );
+				// 	'get_field_raw' => MultisiteACF::get_field_raw( $field_name, $post_id ),
+				// ] );
 			}
 		}
 
