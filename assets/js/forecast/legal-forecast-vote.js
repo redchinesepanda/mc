3b6d80914e8ecb4797ffad7b04f4bb360c894792
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-18 12:21:40 +0300

    commit frontend

--- assets/js/forecast/legal-forecast-vote.js
+++ assets/js/forecast/legal-forecast-vote.js
@@ -4,26 +4,38 @@ document.addEventListener( 'DOMContentLoaded', function ()
 {
     const classes = {
 		active: 'legal-active',
+
+		wrapperText: 'wrapper-text'
 	};
 	
     const selectors = {
 		voteLabel: '.wp-polls .form_radio_btn label',
 
-		voteButtonSend: '.wp-polls .Button-my-style'
+		voteButtonSend: '.wp-polls .Button-my-style',
+
+		voteList: '.wp-polls .wp-polls-ans li'
 	};
 
     function buttonDisable( event )
 	{
-    /*     console.log('скрипт подключился'); */
-
+        // console.log('скрипт подключился');
 		document.querySelector( selectors.voteButtonSend ).classList.add( classes.active );
 	}
-
-   /*  document.querySelector( selectors.voteLabel ).addEventListener( 'click', buttonDisable ); */
 
 	document.querySelectorAll( selectors.voteLabel ).forEach(i => {
 		i.addEventListener( 'click', buttonDisable );  
   	}); 
+
+	function wrap( parent ) {
+		const wrapper = document.createElement('div');
+		wrapper.classList = classes.wrapperText;
+		parent.childNodes.forEach(ch => wrapper.appendChild(ch));
+		console.log(parent.childNodes);
+		parent.appendChild(wrapper);
+	}
+	
+	document.querySelectorAll( selectors.voteList ).forEach(li => wrap( li ));
+	
 } );
 
 // legal-forecast-vote-js end
