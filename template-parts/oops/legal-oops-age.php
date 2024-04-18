55693388d9272590abc0b577250918ce98bac013
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-18 17:37:03 +0300

    commit frontend

--- template-parts/oops/legal-oops-age.php
+++ template-parts/oops/legal-oops-age.php
@@ -2,11 +2,11 @@
 	<div class="legal-oops-age">
 		<span class="oops-age-title"><?php echo $args[ 'title' ]; ?></span>
 		<span class="oops-age-description"><?php echo $args[ 'description' ]; ?></span>
-		<div class="oops-age-button-yes">
-			<span class="age-button-yes-link"><?php echo $args[ 'button-yes' ][ 'label' ]; ?></span>
-		</div>
 		<div class="oops-age-button-no">
 			<a class="age-button-no-link" href="<?php echo $args[ 'button-no' ][ 'href' ]; ?>" rel="nofollow"><?php echo $args[ 'button-no' ][ 'label' ]; ?></a>
+		</div>
+		<div class="oops-age-button-yes">
+			<span class="age-button-yes-link"><?php echo $args[ 'button-yes' ][ 'label' ]; ?></span>
 		</div>
 		<span class="oops-age-after"><?php echo $args[ 'after' ]; ?></span>
 		<span class="oops-age-you-shure"><?php echo $args[ 'you-shure' ]; ?>We are sorry, we cannot allow minors access to our website.</span>
