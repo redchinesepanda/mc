500825a2ede3813c65bcf7e5a132f8e8c563dc75
sergeykalabin <sergey.kalabin@legalbet.com>
2024-04-18 16:00:49 +0300

    commit frontend

--- assets/js/forecast/legal-forecast-vote.js
+++ assets/js/forecast/legal-forecast-vote.js
@@ -20,7 +20,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		// linkView: '.wp-polls a'
 	};
 
-    function buttonDisable( event )
+ /*    function buttonDisable( event )
 	{
         // console.log('скрипт подключился');
 		document.querySelector( selectors.voteButtonSend ).classList.add( classes.active );
@@ -28,20 +28,21 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 	document.querySelectorAll( selectors.voteLabel ).forEach(i => {
 		i.addEventListener( 'click', buttonDisable );  
-  	}); 
+  	}); */ 
 
-	/* function inputCheck( event ) {
+	function inputCheck( event ) {
 		if (event.checked) {
 			console.log('is checked');
+			document.querySelector( selectors.voteButtonSend ).classList.add( classes.active );
 		} else {
 			console.log('Checkbox is not checked');
-			document.querySelector( selectors.voteButtonSend ).classList.add( classes.active );
+			// document.querySelector( selectors.voteButtonSend ).classList.add( classes.active );
 		};
 	};
 
 	document.querySelectorAll( selectors.voteInput ).forEach(i => {
 		i.addEventListener( 'click', inputCheck );
-  	}); */
+  	});
 	
 } );
 
