e8f5fc5f617042e44ba8cdd250dab514095d1edb
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-23 12:08:29 +0300

    multisite

--- lib/multisite/MultisitePostSync.php
+++ lib/multisite/MultisitePostSync.php
@@ -194,27 +194,27 @@ class MultisitePostSync
 
 		if ( $post_parent_id = self::get_parent( $post_id ) )
 		{
-			LegalDebug::debug( [
-				'MultisitePostSync' => 'set_posts',
+			// LegalDebug::debug( [
+			// 	'MultisitePostSync' => 'set_posts',
 	
-				'post_parent_id' => $post_parent_id,
-			] );
+			// 	'post_parent_id' => $post_parent_id,
+			// ] );
 
 			if ( $post_parent_id_sync = MultisiteTermSync::get_field_value_sync( 'post_parent', $post_parent_id ) )
 			{
-				LegalDebug::debug( [
-					'MultisitePostSync' => 'set_posts',
+				// LegalDebug::debug( [
+				// 	'MultisitePostSync' => 'set_posts',
 		
-					'post_parent_id_sync' => $post_parent_id_sync,
-				] );
+				// 	'post_parent_id_sync' => $post_parent_id_sync,
+				// ] );
 
 				self::set_parent( $post_id, $post_parent_id_sync );
 			}
 		}
 
-		LegalDebug::die( [
-			'MultisitePostSync' => 'set_posts',
-		] );
+		// LegalDebug::die( [
+		// 	'MultisitePostSync' => 'set_posts',
+		// ] );
     }
 
 	public static function get_parent( $post_id )
@@ -227,13 +227,13 @@ class MultisitePostSync
 		{
 			$post_parent = $post->post_parent;
 
-			LegalDebug::debug( [
-				'MultisitePostSync' => 'set_posts',
+			// LegalDebug::debug( [
+			// 	'MultisitePostSync' => 'set_posts',
 	
-				'post_id' => $post_id,
+			// 	'post_id' => $post_id,
 	
-				'post_parent' => $post_parent,
-			] );
+			// 	'post_parent' => $post_parent,
+			// ] );
 
 			if ( !empty( $post_parent ) )
 			{
