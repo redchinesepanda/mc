8bf9d711c99c78599647ebd73e1f7094084423b3
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-23 12:04:07 +0300

    multisite

--- lib/multisite/MultisitePost.php
+++ lib/multisite/MultisitePost.php
@@ -219,15 +219,15 @@ class MultisitePost
 
 		$post = self::prepare_post( $post, $blog_id, $post_moved_id );
 
-		LegalDebug::die( [
-			'MultisitePost' => 'add_post',
+		// LegalDebug::debug( [
+		// 	'MultisitePost' => 'add_post',
 
-			'post_id' => $post_id,
+		// 	'post_id' => $post_id,
 
-			'ID' => !empty( $post[ 'ID' ] ) ? $post[ 'ID' ] : 'unset',
+		// 	'ID' => !empty( $post[ 'ID' ] ) ? $post[ 'ID' ] : 'unset',
 
-			'post' => $post,
-		] );
+		// 	'post' => $post,
+		// ] );
 
 		$inserted_post_id = wp_insert_post( $post );
 
