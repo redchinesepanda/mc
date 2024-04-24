04e8fd6b798a552b03135b344acf93a2400845c1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-04-24 14:01:36 +0300

    new billet

--- assets/js/billet/billet-ajax-get-description.js
+++ assets/js/billet/billet-ajax-get-description.js
@@ -7,6 +7,10 @@ let MCAjaxBilletActions = ( function()
 		{
 			const xhttp = new XMLHttpRequest();
 
+			let id = data.id;
+
+			let billetId = data.billetId;
+
 			xhttp.onload = function() {
 				// document.getElementById( "demo" ).innerHTML = this.responseText;
 
@@ -14,12 +18,16 @@ let MCAjaxBilletActions = ( function()
 
 				console.log( parsed );
 
+				console.log( id );
+
 				document.querySelector( '.billet-footer' ).innerHTML = parsed.description;
 			}
 
 			// xhttp.open( "GET", MCAjax.ajax_url, true );
 			
-			xhttp.open( "GET", MCAjax.ajax_url + "?action=mc_ajax_get_description&post_id=2484975", true );
+			// xhttp.open( "GET", MCAjax.ajax_url + "?action=mc_ajax_get_description&post_id=2484975", true );
+			
+			xhttp.open( "GET", MCAjax.ajax_url + "?action=mc_ajax_get_description&post_id=" + billetId, true );
 
 			// xhttp.setRequestHeader( "Content-type", "application/x-www-form-urlencoded" );
 			
@@ -30,11 +38,33 @@ let MCAjaxBilletActions = ( function()
     }
 } )();
 
-function prepareBillet( element )
+function getDescription( event )
 {
-	let id = element.id.replace( 'billet-', '' );
+	MCAjaxBilletActions.ajaxGetDescription( {
+		id : event.currentTarget.dataset.id,
 
-	console.log( id );
+		billetId : event.currentTarget.dataset.billetId,
+	} );
+}
+
+function prepareBillet( billet )
+{
+	
+
+	console.log( billetId );
+
+	let showTnCButton = billet.querySelector( selectors.showTnCButton );
+
+	if ( showTnCButton != null )
+	{
+		showTnCButton.dataset.id = billet.id;
+
+		let billetId = billet.id.replace( 'billet-', '' );
+		
+		showTnCButton.dataset.billetId = billetId;
+
+		showTnCButton.addEventListener( 'click', getDescription, { once: true } );
+	}
 }
 document.addEventListener( 'DOMContentLoaded', function ()
 {
@@ -45,7 +75,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		showTnCButton: '.billet-footer-control',
 	};
 
-	document.querySelectorAll( selectors.aboutAchievement ).forEach( prepareBillet );
+	document.querySelectorAll( selectors.billet ).forEach( prepareBillet );
 
 	MCAjaxBilletActions.ajaxGetDescription( {} );
 
