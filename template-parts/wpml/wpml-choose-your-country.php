367e9997d727d66a72251122d8859a1bab209269
sergeykalabin <sergey.kalabin@legalbet.com>
2024-05-13 11:44:26 +0300

    commit frontend

--- template-parts/wpml/wpml-choose-your-country.php
+++ template-parts/wpml/wpml-choose-your-country.php
@@ -1,179 +1,181 @@
-<h1>Choose Your Country</h1>
-<h2>Africa, Middle East, and India</h2>
-<div class="choose-you-country">
-	<a class="lang-item" href="https://match.center/ng/" target="_blank" draggable="false">
-		<div class="lang-image locale-70"></div>
-		<div class="lang-title">Nigeria</div>
-	</a>
-	<a class="lang-item" href="https://match.center/za/" target="_blank" draggable="false">
-		<div class="lang-image locale-75"></div>
-		<div class="lang-title">South Africa</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ke/" target="_blank" draggable="false">
-		<div class="lang-image locale-77"></div>
-		<div class="lang-title">Kenya</div>
-	</a>
-	<a class="lang-item" href="https://match.center/in/" target="_blank" draggable="false">
-		<div class="lang-image locale-94"></div>
-		<div class="lang-title">India</div>
-	</a>
-</div>
-<h2>Asia</h2>
-<div class="choose-you-country">
-	<a class="lang-item" href="https://match.center/kz/" target="_blank" draggable="false">
-		<div class="lang-image locale-66"></div>
-		<div class="lang-title">Казахстан</div>
-	</a>
-	<a class="lang-item" href="https://match.center/kz-kz/" target="_blank" draggable="false">
-		<div class="lang-image locale-91"></div>
-		<div class="lang-title">Қазақстан</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ph/" target="_blank" draggable="false">
-		<div class="lang-image locale-84"></div>
-		<div class="lang-title">Philippines</div>
-	</a>
-</div>
-<h2>Europe</h2>
-<div class="choose-you-country">
-	<a class="lang-item" href="https://match.center/by/" target="_blank" draggable="false">
-		<div class="lang-image locale-67"></div>
-		<div class="lang-title">Беларусь</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ie/" target="_blank" draggable="false">
-		<div class="lang-image locale-71"></div>
-		<div class="lang-title">Ireland</div>
-	</a>
-	<a class="lang-item" href="https://match.center/es/" target="_blank" draggable="false">
-		<div class="lang-image locale-2"></div>
-		<div class="lang-title">España</div>
-	</a>
-	<a class="lang-item" href="https://match.center" target="_blank" draggable="false">
-		<div class="lang-image locale-1"></div>
-		<div class="lang-title">United Kingdom</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ua/" target="_blank" draggable="false">
-		<div class="lang-image locale-72"></div>
-		<div class="lang-title">Україна</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ua-ru/" target="_blank" draggable="false">
-		<div class="lang-image locale-72"></div>
-		<div class="lang-title">Украина</div>
-	</a>
-	<a class="lang-item" href="https://match.center/pt/" target="_blank" draggable="false">
-		<div class="lang-image locale-86"></div>
-		<div class="lang-title">Portugal</div>
-	</a>
-	<a class="lang-item" href="https://match.center/at/" target="_blank" draggable="false">
-		<div class="lang-image locale-87"></div>
-		<div class="lang-title">Österreich</div>
-	</a>
-	<a class="lang-item" href="https://match.center/hr/" target="_blank" draggable="false">
-		<div class="lang-image locale-22"></div>
-		<div class="lang-title">Hrvatska</div>
-	</a>
-	<a class="lang-item" href="https://match.center/cz/" target="_blank" draggable="false">
-		<div class="lang-image locale-89"></div>
-		<div class="lang-title">Česká Republika</div>
-	</a>
-	<a class="lang-item" href="https://match.center/pl/" target="_blank" draggable="false">
-		<div class="lang-image locale-40"></div>
-		<div class="lang-title">polska</div>
-	</a>
-	<a class="lang-item" href="https://match.center/se/" target="_blank" draggable="false">
-		<div class="lang-image locale-88"></div>
-		<div class="lang-title">Sverige</div>
-	</a>
-	<a class="lang-item" href="https://match.center/de/" target="_blank" draggable="false">
-		<div class="lang-image locale-3"></div>
-		<div class="lang-title">Deutschland</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ro/" target="_blank" draggable="false">
-		<div class="lang-image locale-44"></div>
-		<div class="lang-title">Română</div>
-	</a>
-	<a class="lang-item" href="https://match.center/mt/" target="_blank" draggable="false">
-		<div class="lang-image locale-34"></div>
-		<div class="lang-title">Malta</div>
-	</a>
-	<a class="lang-item" href="https://match.center/rs/" target="_blank" draggable="false">
-		<div class="lang-image locale-90"></div>
-		<div class="lang-title">Republika Srbija</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ch/" target="_blank" draggable="false">
-		<div class="lang-image locale-93"></div>
-		<div class="lang-title">Schweiz</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ch-fr/" target="_blank" draggable="false">
-		<div class="lang-image locale-93"></div>
-		<div class="lang-title">Suisse (FR)</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ch-it/" target="_blank" draggable="false">
-		<div class="lang-image locale-93"></div>
-		<div class="lang-title">Svizzera (IT)</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ch-en/" target="_blank" draggable="false">
-		<div class="lang-image locale-93"></div>
-		<div class="lang-title">Switzerland (EN)</div>
-	</a>
-	<a class="lang-item" href="https://match.center/dk/" target="_blank" draggable="false">
-		<div class="lang-image locale-95"></div>
-		<div class="lang-title">Denmark</div>
-	</a>
-	<a class="lang-item" href="https://match.center/nl/" target="_blank" draggable="false">
-		<div class="lang-image locale-37"></div>
-		<div class="lang-title">Nederland</div>
-	</a>
-</div>
-<h2>Latin America and the Caribbean</h2>
-<div class="choose-you-country">
-	<a class="lang-item" href="https://match.center/mx/" target="_blank" draggable="false">
-		<div class="lang-image locale-69"></div>
-		<div class="lang-title">México</div>
-	</a>
-	<a class="lang-item" href="https://match.center/pe/" target="_blank" draggable="false">
-		<div class="lang-image locale-79"></div>
-		<div class="lang-title">Perú</div>
-	</a>
-	<a class="lang-item" href="https://match.center/co/" target="_blank" draggable="false">
-		<div class="lang-image locale-80"></div>
-		<div class="lang-title">Colombia</div>
-	</a>
-	<a class="lang-item" href="https://match.center/py/" target="_blank" draggable="false">
-		<div class="lang-image locale-83"></div>
-		<div class="lang-title">Paraguay</div>
-	</a>
-	<a class="lang-item" href="https://match.center/br/" target="_blank" draggable="false">
-		<div class="lang-image locale-85"></div>
-		<div class="lang-title">Brasil</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ar/" target="_blank" draggable="false">
-		<div class="lang-image locale-5"></div>
-		<div class="lang-title">Argentina</div>
-	</a>
-	<a class="lang-item" href="https://match.center/cl/" target="_blank" draggable="false">
-		<div class="lang-image locale-92"></div>
-		<div class="lang-title">Chile</div>
-	</a>
-</div>
-<h2>Oceania</h2>
-<div class="choose-you-country">
-	<a class="lang-item" href="https://match.center/au/" target="_blank" draggable="false">
-		<div class="lang-image locale-74"></div>
-		<div class="lang-title">Australia</div>
-	</a>
-	<a class="lang-item" href="https://match.center/nz/" target="_blank" draggable="false">
-		<div class="lang-image locale-78"></div>
-		<div class="lang-title">New Zealand</div>
-	</a>
-</div>
-<h2>The United States, Canada, and Puerto Rico</h2>
-<div class="choose-you-country">
-	<a class="lang-item" href="https://match.center/ca/" target="_blank" draggable="false">
-		<div class="lang-image locale-8"></div>
-		<div class="lang-title">Canada (En)</div>
-	</a>
-	<a class="lang-item" href="https://match.center/ca-fr/" target="_blank" draggable="false">
-		<div class="lang-image locale-8"></div>
-		<div class="lang-title">Canada (FR)</div>
-	</a>
+<div class="legal-choose-you-country">
+	<h1>Choose Your Country</h1>
+	<div class="choose-you-country-title">Africa, Middle East, and India</div>
+	<div class="choose-you-country-block">
+		<a class="lang-item" href="https://match.center/ng/" target="_blank" draggable="false">
+			<div class="lang-image locale-70"></div>
+			<div class="lang-title">Nigeria</div>
+		</a>
+		<a class="lang-item" href="https://match.center/za/" target="_blank" draggable="false">
+			<div class="lang-image locale-75"></div>
+			<div class="lang-title">South Africa</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ke/" target="_blank" draggable="false">
+			<div class="lang-image locale-77"></div>
+			<div class="lang-title">Kenya</div>
+		</a>
+		<a class="lang-item" href="https://match.center/in/" target="_blank" draggable="false">
+			<div class="lang-image locale-94"></div>
+			<div class="lang-title">India</div>
+		</a>
+	</div>
+	<div class="choose-you-country-title">Asia</div>
+	<div class="choose-you-country-block">
+		<a class="lang-item" href="https://match.center/kz/" target="_blank" draggable="false">
+			<div class="lang-image locale-66"></div>
+			<div class="lang-title">Казахстан</div>
+		</a>
+		<a class="lang-item" href="https://match.center/kz-kz/" target="_blank" draggable="false">
+			<div class="lang-image locale-91"></div>
+			<div class="lang-title">Қазақстан</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ph/" target="_blank" draggable="false">
+			<div class="lang-image locale-84"></div>
+			<div class="lang-title">Philippines</div>
+		</a>
+	</div>
+	<div class="choose-you-country-title">Europe</div>
+	<div class="choose-you-country-block">
+		<a class="lang-item" href="https://match.center/by/" target="_blank" draggable="false">
+			<div class="lang-image locale-67"></div>
+			<div class="lang-title">Беларусь</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ie/" target="_blank" draggable="false">
+			<div class="lang-image locale-71"></div>
+			<div class="lang-title">Ireland</div>
+		</a>
+		<a class="lang-item" href="https://match.center/es/" target="_blank" draggable="false">
+			<div class="lang-image locale-2"></div>
+			<div class="lang-title">España</div>
+		</a>
+		<a class="lang-item" href="https://match.center" target="_blank" draggable="false">
+			<div class="lang-image locale-1"></div>
+			<div class="lang-title">United Kingdom</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ua/" target="_blank" draggable="false">
+			<div class="lang-image locale-72"></div>
+			<div class="lang-title">Україна</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ua-ru/" target="_blank" draggable="false">
+			<div class="lang-image locale-72"></div>
+			<div class="lang-title">Украина</div>
+		</a>
+		<a class="lang-item" href="https://match.center/pt/" target="_blank" draggable="false">
+			<div class="lang-image locale-86"></div>
+			<div class="lang-title">Portugal</div>
+		</a>
+		<a class="lang-item" href="https://match.center/at/" target="_blank" draggable="false">
+			<div class="lang-image locale-87"></div>
+			<div class="lang-title">Österreich</div>
+		</a>
+		<a class="lang-item" href="https://match.center/hr/" target="_blank" draggable="false">
+			<div class="lang-image locale-22"></div>
+			<div class="lang-title">Hrvatska</div>
+		</a>
+		<a class="lang-item" href="https://match.center/cz/" target="_blank" draggable="false">
+			<div class="lang-image locale-89"></div>
+			<div class="lang-title">Česká Republika</div>
+		</a>
+		<a class="lang-item" href="https://match.center/pl/" target="_blank" draggable="false">
+			<div class="lang-image locale-40"></div>
+			<div class="lang-title">polska</div>
+		</a>
+		<a class="lang-item" href="https://match.center/se/" target="_blank" draggable="false">
+			<div class="lang-image locale-88"></div>
+			<div class="lang-title">Sverige</div>
+		</a>
+		<a class="lang-item" href="https://match.center/de/" target="_blank" draggable="false">
+			<div class="lang-image locale-3"></div>
+			<div class="lang-title">Deutschland</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ro/" target="_blank" draggable="false">
+			<div class="lang-image locale-44"></div>
+			<div class="lang-title">Română</div>
+		</a>
+		<a class="lang-item" href="https://match.center/mt/" target="_blank" draggable="false">
+			<div class="lang-image locale-34"></div>
+			<div class="lang-title">Malta</div>
+		</a>
+		<a class="lang-item" href="https://match.center/rs/" target="_blank" draggable="false">
+			<div class="lang-image locale-90"></div>
+			<div class="lang-title">Republika Srbija</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ch/" target="_blank" draggable="false">
+			<div class="lang-image locale-93"></div>
+			<div class="lang-title">Schweiz</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ch-fr/" target="_blank" draggable="false">
+			<div class="lang-image locale-93"></div>
+			<div class="lang-title">Suisse (FR)</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ch-it/" target="_blank" draggable="false">
+			<div class="lang-image locale-93"></div>
+			<div class="lang-title">Svizzera (IT)</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ch-en/" target="_blank" draggable="false">
+			<div class="lang-image locale-93"></div>
+			<div class="lang-title">Switzerland (EN)</div>
+		</a>
+		<a class="lang-item" href="https://match.center/dk/" target="_blank" draggable="false">
+			<div class="lang-image locale-95"></div>
+			<div class="lang-title">Denmark</div>
+		</a>
+		<a class="lang-item" href="https://match.center/nl/" target="_blank" draggable="false">
+			<div class="lang-image locale-37"></div>
+			<div class="lang-title">Nederland</div>
+		</a>
+	</div>
+	<div class="choose-you-country-title">Latin America and the Caribbean</div>
+	<div class="choose-you-country-block">
+		<a class="lang-item" href="https://match.center/mx/" target="_blank" draggable="false">
+			<div class="lang-image locale-69"></div>
+			<div class="lang-title">México</div>
+		</a>
+		<a class="lang-item" href="https://match.center/pe/" target="_blank" draggable="false">
+			<div class="lang-image locale-79"></div>
+			<div class="lang-title">Perú</div>
+		</a>
+		<a class="lang-item" href="https://match.center/co/" target="_blank" draggable="false">
+			<div class="lang-image locale-80"></div>
+			<div class="lang-title">Colombia</div>
+		</a>
+		<a class="lang-item" href="https://match.center/py/" target="_blank" draggable="false">
+			<div class="lang-image locale-83"></div>
+			<div class="lang-title">Paraguay</div>
+		</a>
+		<a class="lang-item" href="https://match.center/br/" target="_blank" draggable="false">
+			<div class="lang-image locale-85"></div>
+			<div class="lang-title">Brasil</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ar/" target="_blank" draggable="false">
+			<div class="lang-image locale-5"></div>
+			<div class="lang-title">Argentina</div>
+		</a>
+		<a class="lang-item" href="https://match.center/cl/" target="_blank" draggable="false">
+			<div class="lang-image locale-92"></div>
+			<div class="lang-title">Chile</div>
+		</a>
+	</div>
+	<div class="choose-you-country-title">Oceania</div>
+	<div class="choose-you-country-block">
+		<a class="lang-item" href="https://match.center/au/" target="_blank" draggable="false">
+			<div class="lang-image locale-74"></div>
+			<div class="lang-title">Australia</div>
+		</a>
+		<a class="lang-item" href="https://match.center/nz/" target="_blank" draggable="false">
+			<div class="lang-image locale-78"></div>
+			<div class="lang-title">New Zealand</div>
+		</a>
+	</div>
+	<div class="choose-you-country-title">The United States, Canada, and Puerto Rico</div>
+	<div class="choose-you-country-block">
+		<a class="lang-item" href="https://match.center/ca/" target="_blank" draggable="false">
+			<div class="lang-image locale-8"></div>
+			<div class="lang-title">Canada (En)</div>
+		</a>
+		<a class="lang-item" href="https://match.center/ca-fr/" target="_blank" draggable="false">
+			<div class="lang-image locale-8"></div>
+			<div class="lang-title">Canada (FR)</div>
+		</a>
+	</div>
 </div>
