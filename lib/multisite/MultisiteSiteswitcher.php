f290c0c82ca1e5c578f13af67e1e32670e7cf8cd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-14 12:50:03 +0300

    multisite

--- lib/multisite/MultisiteSiteswitcher.php
+++ lib/multisite/MultisiteSiteswitcher.php
@@ -4,11 +4,11 @@ class MultisiteSiteswitcher
 {
 	public static function register_functions_debug()
 	{
-		LegalDebug::debug( [
-			'MultisiteSiteswitcher' =>'register_functions_debug',
+		// LegalDebug::debug( [
+		// 	'MultisiteSiteswitcher' =>'register_functions_debug',
 
-			'get_sites_list' => self::get_sites_list(),
-		] );
+		// 	'get_sites_list' => self::get_sites_list(),
+		// ] );
 	}
 
 	public static function get_sites_list()
