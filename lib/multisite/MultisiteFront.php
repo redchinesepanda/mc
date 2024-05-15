b852cd7a954b7e48732f5010a6549f2e9bee4f53
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-15 12:20:45 +0300

    multisite

--- lib/multisite/MultisiteFront.php
+++ lib/multisite/MultisiteFront.php
@@ -14,11 +14,22 @@ class MultisiteFront
 
 	public static function filter_function_name_11( $locale )
 	{
+		$current_blog_id = MultisiteBlog::get_current_blog_id();
+
+        $blog_locale = MultisiteBlog::get_blog_option( $current_blog_id, MultisiteSiteOptions::OPTIONS[ 'blog-locale' ] );
+		
 		LegalDebug::debug( [
 			'MultisiteFront' => 'filter_function_name_11',
 
+			'blog_locale' => $blog_locale,
+
 			'locale' => $locale,
 		] );
+
+        if ( !empty( $blog_locale ) )
+        {
+            return $blog_locale;
+        }
 
 		return $locale;
 	}
