861e9a757577fbc7c380930a9086c92af874d381
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-17 09:30:38 +0300

    multisite

--- lib/multisite/MultisiteHreflang.php
+++ lib/multisite/MultisiteHreflang.php
@@ -22,11 +22,61 @@ class MultisiteHreflang
 		] );
     }
 
-	public static function get_group_items( $post_id )
+	public static function group_items_query( $terms )
 	{
+		return [
+			'posts_per_page' => -1,
+			
+			'post_type' => 'page',
+
+			'tax_query' => [
+				[
+					'taxonomy' => WPMLTranslationGroups::TAXONOMY[ 'translation_group' ],
+
+					'terms' => $terms,
+
+					'field' => 'slug',
+
+					'operator' => 'IN',
+				],
+			],
+		];
+	}
+
+	public static function get_group_items( $terms )
+	{
+		$items = [];
+
+		$posts = get_posts( self::group_items_query( $terms ) );
+
+		foreach ( $posts as $post )
+		{
+			$items[] = get_post_permalink( $post->ID );
+		}
+
+		return $items;
+	}
+
+	public static function get_group_items_all( $post_id )
+	{
+		$current_blog_id = MultisiteBlog::get_current_blog_id();
+
 		$translation_groups = WPMLTranslationGroups::get_translation_group( $post_id );
 
-		return [];
+		$blogs = MultisiteBlog::get_other_sites();
+
+		$items = [];
+
+		foreach ( $blogs as $blog )
+		{
+			MultisiteBlog::set_blog( $blog->blog_id );
+
+			$blog_locale = MultisiteBlog::get_blog_option( $blog->blog_id, MultisiteSiteOptions::OPTIONS[ 'blog-locale' ] );
+
+			$items[ $blog_locale ] = get_group_items( $translation_groups );
+		}
+
+		return $items;
 	}
 }
 
