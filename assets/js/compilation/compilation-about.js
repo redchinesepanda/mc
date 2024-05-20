13b3ad6c8a2f5594e5dfc4d62d7197afb9335f6c
sergeykalabin <sergey.kalabin@legalbet.com>
2024-05-20 18:56:24 +0300

    commit frontend

---
+++ assets/js/compilation/compilation-about.js
@@ -0,0 +1,35 @@
+// compilation-about-js start
+
+document.addEventListener( 'DOMContentLoaded', function ()
+{
+
+	function choosePicture() {
+
+		console.log('скрипт подключился');
+
+		const wrapper = document.querySelector( selectors.compilationAbout );
+
+		const casinoWrapper = document.querySelector( selectors.compilationAboutCasino );
+
+		const picture = document.querySelector( selectors.compilationAboutPicture );
+
+		if ( wrapper.closest( casinoWrapper ) ) {
+			picture.srcset='/assets/img/compilation/compilation-bookmaker.svg';
+		} else {
+			return true;
+		}
+	}
+
+	const selectors = {
+
+		compilationAbout: '.compilation-about-wrapper',
+
+		compilationAboutCasino: '.legal-casino',
+
+		compilationAboutPicture: '.compilation-about-wrapper picture source',
+	};
+	
+	choosePicture();
+} );
+
+// compilation-about-js end
