96bfce77dcd6fbf829f2a9df9c1b0e3b72a4873a
sergeykalabin <sergey.kalabin@legalbet.com>
2024-05-21 14:51:53 +0300

    commit frontend

--- template-parts/tabs/legal-tabs-info.php
+++ template-parts/tabs/legal-tabs-info.php
@@ -19,17 +19,13 @@
                 <div class="legal-tab-display">
                     <div class="legal-tab-content empty-tab-content"></div>
                     <div class="legal-tab-content empty-tab-content"></div>
-                    <div class="legal-tab-content legal-content-3">We focus on providing reviews that are relevant to our readers, based on factors such as
-                        geographic location, preferred payment methods, and game types.
+                    <div class="legal-tab-content legal-content-0 legal-active">We are professionals and enthusiasts in the online gambling industry, with years of experience in evaluating and recommending the best betting sites.
                     </div>
-                    <div class="legal-tab-content legal-content-2">Our reviews are based on objective criteria, such as site features, user experience, and
-                        customer service, ensuring that our readers can make informed decisions.
+                    <div class="legal-tab-content legal-content-1">We provide only honest and transparent information about the sites we review, ensuring that our readers can trust our recommendations.
                     </div>
-                    <div class="legal-tab-content legal-content-1">We provide only honest and transparent information about the sites we review, ensuring
-                        that our readers can trust our recommendations.
+                    <div class="legal-tab-content legal-content-2">Our reviews are based on objective criteria, such as site features, user experience, and customer service, ensuring that our readers can make informed decisions.
                     </div>
-                    <div class="legal-tab-content legal-content-0 legal-active">We are professionals and enthusiasts in the online gambling industry, with years of
-                        experience in evaluating and recommending the best betting sites.
+                    <div class="legal-tab-content legal-content-3">We focus on providing reviews that are relevant to our readers, based on factors such as geographic location, preferred payment methods, and game types.
                     </div>
                 </div>
             </div>
