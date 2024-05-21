80bb4f7169cda7dc8b2f48b40de489dea144ee9f
sergeykalabin <sergey.kalabin@legalbet.com>
2024-05-21 09:01:03 +0300

    commit frontend

---
+++ template-parts/tabs/legal-tabs-info.php
@@ -0,0 +1,38 @@
+<section class="legal-section-what-is-mc">
+    <div class="what-is-mc-wrapper">
+        <div class="legal-what-is-mc">
+            <h2>What is Match.Center?</h2>
+            <p>Our aim is to be the go-to source for players looking for reliable and trustworthy
+                information about online gambling sites. At Match.Center, we are committed to providing
+                the ultimate guides and reviews on online gambling. But what does it mean – ‘Ultimate’?
+                Here are our core values:
+            </p>
+            <div class="legal-tabs">
+                <div class="legal-tab-menu">
+
+                    <div class="legal-tab-title legal-active" data-content="0" data-tabs="legal-tabs-0">Expertise</div>
+                    <div class="legal-tab-title" data-content="1" data-tabs="legal-tabs-0">Transparency</div>
+                    <div class="legal-tab-title" data-content="2" data-tabs="legal-tabs-0">Fairness</div>
+                    <div class="legal-tab-title" data-content="3" data-tabs="legal-tabs-0">Relevance</div>
+
+                </div>
+
+                <div class="legal-tab-display">
+                    <div class="legal-tab-content legal-content-3">We focus on providing reviews that are relevant to our readers, based on factors such as
+                        geographic location, preferred payment methods, and game types.
+                    </div>
+                    <div class="legal-tab-content legal-content-2">Our reviews are based on objective criteria, such as site features, user experience, and
+                        customer service, ensuring that our readers can make informed decisions.
+                    </div>
+                    <div class="legal-tab-content legal-content-1">We provide only honest and transparent information about the sites we review, ensuring
+                        that our readers can trust our recommendations.
+                    </div>
+                    <div class="legal-tab-content legal-content-0">We are professionals and enthusiasts in the online gambling industry, with years of
+                        experience in evaluating and recommending the best betting sites.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
