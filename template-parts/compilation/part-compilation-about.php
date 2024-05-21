2f23b26604d58aff60b7760d6cb81dc45012bda9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-21 15:54:52 +0300

    new billet

--- template-parts/compilation/part-compilation-about.php
+++ template-parts/compilation/part-compilation-about.php
@@ -30,7 +30,7 @@
             <?php if ( !empty( $args[ 'content' ] ) && !empty( $args[ 'image' ] ) ) : ?>
                 <div class="about-section-image">
                     <picture>
-                    <source srcset="http://old.match.center/wp-content/themes/mc-theme/assets/img/base/header/header-logo-mc-mobile.svg" media="(max-width: 959px)" width="48" height="48">
+                        <source srcset="/wp-content/themes/mc-theme/assets/img/legal-blank.svg" media="(max-width: 959px)" width="1" height="1"> 
                         <img src="<?php echo $args[ 'image' ][ 'src' ]; ?>" width="<?php echo $args[ 'image' ][ 'width' ]; ?>" height="<?php echo $args[ 'image' ][ 'height' ]; ?>" alt="Compilation about" loading="lazy">
                     </picture>
                 </div>
