7940b31c9f910b57704f856560563ffca7fce64e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-23 10:47:45 +0300

    new billet

--- functions.php
+++ functions.php
@@ -19,4 +19,25 @@ function my_acf_json_save_point( $path )
 
 add_filter( 'acf/settings/save_json', 'my_acf_json_save_point' );
 
+function custom_acf_json_save_paths( $paths, $post )
+{
+    // if ( $post['title'] === 'Theme Settings' ) {
+    //     $paths = array( get_stylesheet_directory() . '/options-pages' );
+    // }
+
+    // if ( $post['title'] === 'Theme Settings Fields' ) {
+    //     $paths = array( get_stylesheet_directory() . '/field-groups' );
+    // }
+
+	LegalDebug::die( [
+        'functions.php' =>'my_acf_json_save_point',
+
+        'paths' => $paths,
+    ] );
+
+    return $paths;
+}
+
+add_filter( 'acf/json/save_paths', 'custom_acf_json_save_paths', 10, 2 );
+
 ?>
