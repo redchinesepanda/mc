e2d153f28292f862df3467302853ffb8946374bd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-23 11:36:36 +0300

    new billet

--- lib/multisite/MultisiteACF.php
+++ lib/multisite/MultisiteACF.php
@@ -127,6 +127,63 @@ class MultisiteACF
 
 		return true;
 	}
+
+	// https://wp-kama.ru/plugin/acf/local-json
+
+	// add_filter( 'acf/settings/load_json', 'my_acf_json_load_point' );
+	
+	// function my_acf_json_load_point( $paths ) {
+	
+	// 	// // remove original path (optional)
+	// 	// unset( $paths[0] );
+	
+	// 	// // append path
+	// 	// $paths[] = get_stylesheet_directory() . '/my-custom-folder';
+	
+	// 	LegalDebug::debug( [
+	// 		'functions.php' =>'my_acf_json_load_point',
+	
+	// 		'paths' => $paths,
+	// 	] );
+	
+	// 	return $paths;
+	// }
+	
+	// add_filter( 'acf/settings/save_json', 'my_acf_json_save_point' );
+	
+	// function my_acf_json_save_point( $path )
+	// {
+	// 	// return get_stylesheet_directory() . '/my-custom-folder';
+	
+	// 	LegalDebug::debug( [
+	// 		'functions.php' =>'my_acf_json_save_point',
+	
+	// 		'path' => $path,
+	// 	] );
+	
+	// 	return $path;
+	// }
+	
+	// add_filter( 'acf/json/save_paths', 'custom_acf_json_save_paths', 10, 2 );
+	
+	// function custom_acf_json_save_paths( $paths, $post )
+	// {
+	// 	// if ( $post['title'] === 'Theme Settings' ) {
+	// 	//     $paths = array( get_stylesheet_directory() . '/options-pages' );
+	// 	// }
+	
+	// 	// if ( $post['title'] === 'Theme Settings Fields' ) {
+	// 	//     $paths = array( get_stylesheet_directory() . '/field-groups' );
+	// 	// }
+	
+	// 	LegalDebug::debug( [
+	// 		'functions.php' =>'custom_acf_json_save_paths',
+	
+	// 		'paths' => $paths,
+	// 	] );
+	
+	// 	return $paths;
+	// }
 }
 
 ?>
