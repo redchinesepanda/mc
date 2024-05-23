effd324f386c48bcd0d0cbc51960349e1f8d9f93
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-23 10:44:18 +0300

    new billet

--- functions.php
+++ functions.php
@@ -2,6 +2,21 @@
 
 require_once( 'lib/LegalMain.php' );
 
-LegalMain::register(); 
+LegalMain::register();
+
+function my_acf_json_save_point( $path )
+{
+    // return get_stylesheet_directory() . '/my-custom-folder';
+
+	LegalDebug::debug( [
+        'functions.php' =>'my_acf_json_save_point',
+
+        'path' => $path,
+    ] );
+
+	return $path;
+}
+
+add_filter( 'acf/settings/save_json', 'my_acf_json_save_point' );
 
 ?>
