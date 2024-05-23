00fcc185d159c714874a5d772d39893d1cfd458a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-23 12:04:21 +0300

    new billet

--- lib/wpml/WPMLLangSwitcher.php
+++ lib/wpml/WPMLLangSwitcher.php
@@ -195,19 +195,25 @@ class WPMLLangSwitcher
 
         $languages = self::get_all();
 
+        $multisite_languages = MultisiteSiteSwitcher::get_languages();
+
+        $multisite_sites_list = MultisiteSiteswitcher::get_sites_list();
+
         // $post = get_post();
 
         // $items = MultisiteHreflang::get_group_items_all( $post->ID );
 
-        // LegalDebug::debug( [
-        //     'WPMLLangSwitcher' => 'get',
+        LegalDebug::debug( [
+            'WPMLLangSwitcher' => 'get',
 
-        //     'languages' => $languages,
+            'languages' => $languages,
 
-        //     // 'items' => $items,
+            // 'items' => $items,
 
-        //     'get_sites_list' => MultisiteSiteswitcher::get_sites_list(),
-        // ] );
+            'multisite_sites_list' => $multisite_sites_list,
+
+            'multisite_languages' => $multisite_languages,
+        ] );
 
         if ( empty( $languages ) )
         {
