006f6dfff89e2e8c184e2ffefc8844eb7d92b276
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-05-27 13:59:08 +0300

    new billet

--- lib/wpml/WPMLLangSwitcher.php
+++ lib/wpml/WPMLLangSwitcher.php
@@ -231,7 +231,7 @@ class WPMLLangSwitcher
                 
                 // $languages = $multisite_languages;
                 
-                $languages = array_merge( $languages, $multisite_languages );
+                $languages = MultisiteSiteSwitcher::get_combined_languages( $languages, $multisite_languages );
             }
         }
 
