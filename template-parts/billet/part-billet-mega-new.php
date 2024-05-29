b388342c9a396d410d9a3a4001393d8f2561e8f8
sergeykalabin <sergey.kalabin@legalbet.com>
2024-05-29 17:02:27 +0300

    commit frontend

--- template-parts/billet/part-billet-mega-new.php
+++ template-parts/billet/part-billet-mega-new.php
@@ -49,6 +49,9 @@
 		<?php endif; ?>
 		<?php echo $args[ 'content' ]; ?>
 	</div>
+	<div class="billet-mega-footer">
+	Um cassino que funcionava no regime normal em todo Portugal durante 40 anos entrou no mercado online em 2017, e as apostas desportivas apareceram em 2020. A casa de apostas foi a patrocinadora principal das camisolas das equipas da primeira liga portuguesa, entre as quais CD Santa Clara, Paços de Ferreira e Rio Ave.<?php echo $args[ 'footer' ] ?>
+		</div>
 	<?php if( !empty( $args[ 'footer' ] ) ) : ?>
 		<div class="billet-mega-footer">
 			<?php echo $args[ 'footer' ] ?>
