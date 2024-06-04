42620cae62da41d9f1b87978e3ddfe73b893d8d9
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-06-04 13:34:41 +0300

    new billet

--- lib/multisite/MultisiteHreflang.php
+++ lib/multisite/MultisiteHreflang.php
@@ -93,23 +93,23 @@ class MultisiteHreflang
 
 		$language_code = WPMLMain::get_language_code( $post->ID );
 
-		LegalDebug::debug( [
-			'MultisiteHreflang' => 'get_post_uri',
+		// LegalDebug::debug( [
+		// 	'MultisiteHreflang' => 'get_post_uri',
 
-			'auto_update_uri' => get_post_meta( $post->ID, "auto_update_uri", true ),
+		// 	'auto_update_uri' => get_post_meta( $post->ID, "auto_update_uri", true ),
 
-			'get_post_permalink' => get_post_permalink( $post ),
+		// 	'get_post_permalink' => get_post_permalink( $post ),
 
-			'get_permalink' => get_permalink( $post ),
+		// 	'get_permalink' => get_permalink( $post ),
 
-			'$post' => $post->ID,
+		// 	'$post' => $post->ID,
 
-			'$url' => $url,
+		// 	'$url' => $url,
 
-			'$post_path' => $post_path,
+		// 	'$post_path' => $post_path,
 
-			'$language_code' => $language_code,
-		] );
+		// 	'$language_code' => $language_code,
+		// ] );
 
 		if ( $language_code == 'en' )
         {
