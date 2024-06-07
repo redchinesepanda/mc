9d762154655c83b7798d7336ff59b6f12f0ed5fb
sergeykalabin <sergey.kalabin@legalbet.com>
2024-06-07 10:24:29 +0300

    commit frontend

--- assets/js/tabs/tabs-main.js
+++ assets/js/tabs/tabs-main.js
@@ -1,7 +1,5 @@
 // tabs-main-js start
 
-import { spoilerinit } from '/assets/js/billet/billet-footer.js';
-
 document.addEventListener( 'DOMContentLoaded', function ()
 {
     function tabToggle( event )
