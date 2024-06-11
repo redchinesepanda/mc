53fec5f3caeee6270300d9654ad9f3d7b832cfee
sergeykalabin <sergey.kalabin@legalbet.com>
2024-06-11 12:25:17 +0300

    frontend optimisation

--- assets/js/review/review-anchors-new.js
+++ assets/js/review/review-anchors-new.js
@@ -112,33 +112,6 @@ document.addEventListener( 'DOMContentLoaded', function ()
    
     window.addEventListener( events.scroll, initScroll );
 
-	// заполнение ширины контейнера свайпером старт
-
-	/* function overflow(e) {
-		return e.scrollWidth > e.offsetWidth || e.scrollHeight > e.offsetHeight;
-	} */
-
-	function defineOverflow( str ) {
-		if ( !str ) {
-            return;
-        };
-		// console.log(`${str} Элемент найден`);
-		return overflow(str) === false ? str.parentNode.classList.add( classes.shortStr ) : false;
-	/* 	if (overflow(str)) {
-			console.log('Текст не умещается');
-		} else {
-			console.log('Текст умещается');
-			str.parentNode.classList.add( classes.shortStr );
-		}; */
-	};
-
-	// defineOverflow( document.querySelector(selectors.stringSwiper ) );
-
-	// document.querySelector( '.compilation-about .swiper-wrapper' ).forEach( defineOverflow );
-
-	// заполнение ширины контейнера свайпером конец
-
-
 	// const buttonToTop = document.querySelector( selectors.buttonToTop );
    
     // window.addEventListener("scroll", function () {
