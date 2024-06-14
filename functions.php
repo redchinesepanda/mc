663a90658f65d3e9e9403fcc5b6d329ce3c7e77c
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-06-14 12:19:47 +0300

    new billet

--- functions.php
+++ functions.php
@@ -4,4 +4,17 @@ require_once( 'lib/LegalMain.php' );
 
 LegalMain::register();
 
+add_filter( 'wpo_purge_cache_hooks', function( $actions )
+{
+    // $actions[] = 'my_custom_action';
+
+	LegalDebug::debug( [
+        'functions.php' => 'wpo_purge_cache_hooks',
+
+        'actions' => $actions,
+    ] );
+
+    return $actions;
+} );
+
 ?>
