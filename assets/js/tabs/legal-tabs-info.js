41c4afe45faea008a5327797c1773a4302128377
sergeykalabin <sergey.kalabin@legalbet.com>
2024-06-14 17:03:55 +0300

    frontend optimisation

--- assets/js/tabs/legal-tabs-info.js
+++ assets/js/tabs/legal-tabs-info.js
@@ -2,7 +2,22 @@
 
 document.addEventListener( 'DOMContentLoaded', function ()
 {
-    console.log('Скрипт legal-tabs-info подключён')
+    // console.log('Скрипт legal-tabs-info подключён')
+
+    const selectors = {
+
+		tabContent : '.legal-tabs-info .legal-tab-content.legal-active',
+
+
+	};
+
+    const tabInfoContent = document.querySelector( selectors.tabContent );
+
+    let heightTextTab = tabInfoContent.scrollHeight;
+
+    console.log( tabInfoContent.scrollHeight );
+
+    tabInfoContent('data-height', 'heightTextTab');
 
     
 
