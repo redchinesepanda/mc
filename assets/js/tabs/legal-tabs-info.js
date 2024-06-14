365513143bc69275ea55d34c72c5f5aae67e959b
sergeykalabin <sergey.kalabin@legalbet.com>
2024-06-14 16:50:41 +0300

    frontend optimisation

---
+++ assets/js/tabs/legal-tabs-info.js
@@ -0,0 +1,11 @@
+// legal-tabs-info-js start
+
+document.addEventListener( 'DOMContentLoaded', function ()
+{
+    console.log('Скрипт legal-tabs-info подключён')
+
+    
+
+} );
+
+// legal-tabs-info-js end
