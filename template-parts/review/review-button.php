1fa0316b700201d910e21d37443c999c7c056c6e
sergeykalabin <sergey.kalabin@legalbet.com>
2024-06-19 12:34:25 +0300

    frontend optimizathion

--- template-parts/review/review-button.php
+++ template-parts/review/review-button.php
@@ -1,3 +1,3 @@
-<a href="<?php echo $args[ 'href' ]; ?>" class="legal-afillate check-oops" rel="nofollow">
+<a href="<?php echo $args[ 'href' ]; ?>" class="legal-afillate check-oops" target="_blank" rel="nofollow">
 	<?php echo $args[ 'text' ]; ?>
 </a>
