ae282e84dd0a1f5b07eff642d32520efcec6afc6
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-06-24 13:08:34 +0300

    new billet

--- lib/yoast/YoastOG.php
+++ lib/yoast/YoastOG.php
@@ -34,6 +34,8 @@ class YoastOG
 
 			// 'image_container' => $image_container,
 
+			'has_post_thumbnail' => has_post_thumbnail(),
+
 			'get_images' => $image_container->get_images(),
 
 			'has_images' => $image_container->has_images(),
