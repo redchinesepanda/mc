ec034b3cb3a267d40ac3295e898a5a00fac74f01
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-04 11:30:10 +0300

    new billet

--- lib/base/BaseHeader.php
+++ lib/base/BaseHeader.php
@@ -992,11 +992,11 @@ class BaseHeader
 
 		$urls = self::replace_urls_group( $home_urls_replaced, $cross_urls_replaced );
 
-		// LegalDebug::debug( [
-		// 	'BaseHeader' =>'replace_urls',
+		LegalDebug::debug( [
+			'BaseHeader' =>'replace_urls',
 
-		// 	'urls' => $urls,
-		// ] );
+			'urls' => $urls,
+		] );
 
 		return $urls;
 	}
