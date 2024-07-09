710f32269acf78c0aa233e33e2549a8e2d000142
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-09 14:41:38 +0300

    new billet

--- template-parts/tools/part-tool-forbidden.php
+++ template-parts/tools/part-tool-forbidden.php
@@ -1,10 +1,20 @@
-<div id="main-wrapper" class="container">
-   <div class="container" role="main">
-        <div class="row">
-			<div class="article-container">
-				<header><h1 class="entry-title"><?php echo $args[ 'title' ] ?></h1></header>
-				<div class="entry-content"><p><?php echo $args[ 'description' ] ?></p>
+<!doctype html>
+<html>
+	<head>
+		<link rel="profile" href="https://gmpg.org/xfn/11">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title><?php echo $args[ 'title' ] ?></title>
+	</head>
+	<body>
+		<div id="main-wrapper" class="container">
+		<div class="container" role="main">
+				<div class="row">
+					<div class="article-container">
+						<header><h1 class="entry-title"><?php echo $args[ 'title' ] ?></h1></header>
+						<div class="entry-content"><p><?php echo $args[ 'description' ] ?></p>
+					</div>
+				</div>
 			</div>
-        </div>
-    </div>
-</div>
+		</div>
+	</body>
+</html>
