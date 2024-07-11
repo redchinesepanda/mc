706ae24c6d1fb34cef014c989e64ee0b2d3eab4a
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-11 13:51:38 +0300

    new billet

--- header.php
+++ header.php
@@ -25,7 +25,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<meta name="google-site-verification" content="qru4JAh1lV7MfZ-yILC_Eh-rQX_3hzYxbE2fAWYsmyc">
 		<meta name="google-site-verification" content="2dAywc7Sx2HoaFUblkGSpagh3bqZc4-BBhS6tQ2WB40" />
 		<meta name="google-site-verification" content="5DLHAOxxRfrKYK3wIUaq0xciDnh6pb9Lv9MJQLhAV2E" />
-		<meta name="google-site-verification" content="mEkrJzr2kWNt0bwY4KBe-esDddNfY6LHIHoQwqgXUAA" />
 		<meta name="google-site-verification" content="KmZVf5Emmz8s9HLvmrqcExlPZwHfYZrdAF0KMD1D2RY" />
 		<meta name="google-site-verification" content="CrvtIqKDtaSvdPanDaB0sgmPewuoJIqFrZ5QDPhostw" />
 		<meta name="google-site-verification" content="fcqNvu0qA7WE-eyxfNul5kC9y_c1lWQvGzDc5b06uQg" />
