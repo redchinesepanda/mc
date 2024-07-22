32b1fe1e2eef37d46778f91c543b3aa6056c3542
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-22 13:44:33 +0300

    new billet

--- lib/brand/BrandFilter.php
+++ lib/brand/BrandFilter.php
@@ -20,6 +20,13 @@ class BrandFilter
         'type' => 'brand_type',
     ];
 
+	public static function check_add_bulk_actions()
+	{
+		return MultisiteMain::check_multisite()
+		
+			&& MultisiteBlog::check_main_blog();
+	}
+
 	public static function register_functions_admin()
 	{
 		if ( MultisiteMain::check_multisite() )
@@ -28,11 +35,82 @@ class BrandFilter
 			{
 				$handler = new self();
 
-				add_filter( 'edit_post_' . self::POST_TYPE[ 'billet' ], [ $handler, 'set_brand_type' ], 10, 2 );
+				// add_filter( 'edit_post_' . self::POST_TYPE[ 'billet' ], [ $handler, 'set_brand_type' ], 10, 2 );
 		
 				add_action( 'restrict_manage_posts', [ $handler, 'brand_type_filter' ] );
 			}
 		}
+
+		if ( self::check_add_bulk_actions() )
+        {
+            $handler = new self();
+
+			// 'edit_post_' . self::POST_TYPE[ 'billet' ]
+
+            add_filter( 'bulk_actions-edit_post_' . self::POST_TYPE[ 'billet' ], [ $handler, 'add_brand_type_item' ] );
+
+            add_filter( 'handle_bulk_actions-edit-page', [ $handler, 'handle_brand_type_item' ], 10, 3);
+
+            add_action('admin_notices', [ $handler, 'notify_brand_type_item' ] );
+        }
+    }
+
+	const ACTION = [
+        'set-brand-type'=> 'set-brand-type',
+
+        'done-brand-type'=> 'done-brand-type',
+    ];
+
+	public static function notify_brand_type_item()
+    {
+    	if ( ! empty( $_REQUEST[ self::ACTION[ 'done-brand-type' ] ] ) )
+        {
+    		$num_changed = (int) $_REQUEST[ self::ACTION[ 'done-brand-type' ] ];
+
+    		// printf( '<div id="message" class="updated notice is-dismissable"><p>' . __('Published %d posts.', 'txtdomain') . '</p></div>', $num_changed );
+
+    		$message = sprintf( ToolLoco::translate( 'Brand type set for %d posts' ), $num_changed );
+
+            $args = [
+                'message' => $message,
+			];
+
+            self::print_notices( $args );
+    	}
+    }
+
+	public static function redirect_clean( $redirect )
+	{
+		return remove_query_arg( self::ACTION, $redirect );
+	}
+
+	public static function handle_brand_type_item( $redirect_url, $action, $post_ids )
+    {
+        if ( $action == self::ACTION[ 'set-brand-type' ] )
+        {
+            $redirect_url = self::redirect_clean( $redirect_url );
+    	
+    		foreach ( $post_ids as $post_id )
+            {
+    			$post = get_post( $post_id );
+
+                if ( $post )
+                {
+                    self::set_brand_type( $post_id, $post );
+                }
+    		}
+
+    		$redirect_url = add_query_arg( self::ACTION[ 'done-brand-type' ], count( $post_ids ), $redirect_url );
+    	}
+
+    	return $redirect_url;
+    }
+
+    public static function add_translation_group_item( $bulk_actions )
+    {
+    	$bulk_actions[ self::ACTION[ 'set-brand-type' ] ] = ToolLoco::translate( 'Set Brand Type' );
+
+    	return $bulk_actions;
     }
 
 	public static function brand_type_filter( $post_type = '' )
@@ -147,6 +225,15 @@ class BrandFilter
 			// 	'term_ids' => $term_ids,
 			// ] );
 		}
+    }
+
+	const TEMPLATE = [
+        'brand-type-notices' => LegalMain::LEGAL_PATH . '/template-parts/brand/part-brand-type-notices.php',
+    ];
+
+    public static function print_notices( $args )
+    {
+        LegalComponents::print_main( self::TEMPLATE[ 'brand-type-notices' ], $args );
     }
 }
 
