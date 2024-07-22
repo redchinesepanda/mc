01728fa24e66c63e3df8f525872d283d0c6ac43f
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-22 10:24:56 +0300

    new billet

---
+++ lib/admin/AdminPage.php
@@ -0,0 +1,24 @@
+<?php
+
+class AdminPage
+{
+	public static function register()
+    {
+        $handler = new self();
+
+        add_action( 'wp_list_pages', [ $handler, 'list_pages_filter' ], 10, 3 );
+    }
+
+	function list_pages_filter( $output, $parsed_args, $pages )
+	{
+		LegalDebug::debug( [
+			'AdminPage' => 'list_pages_filter',
+
+			'pages' => $pages,
+		] );
+		
+		return $output;
+	}
+}
+
+?>
