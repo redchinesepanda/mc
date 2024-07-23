66cb1e50f21e5032c1f6359100f16848f56907b2
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-23 12:23:44 +0300

    new billet

--- functions.php
+++ functions.php
@@ -11,7 +11,20 @@ add_filter( 'redirection_request_headers', function( $headers )
 	] );
 
 	return $headers;
-} );
+}, 10, 1 );
+
+add_filter( 'redirection_request_header', function( $value, $name )
+{
+	LegalDebug::debug( [
+		'NotFoundMain' => 'redirection_request_headers-1',
+
+		'value' => $value,
+
+		'name' => $name,
+	] );
+
+	return $value;
+}, 10, 2 );
 
 require_once( 'lib/LegalMain.php' );
 
