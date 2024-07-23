25aa443bff55544fb6c87f0b287253fcde1c4a94
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-23 12:07:11 +0300

    new billet

--- lib/notfound/NotFoundMain.php
+++ lib/notfound/NotFoundMain.php
@@ -55,12 +55,16 @@ class NotFoundMain
 
 	public static function check()
     {
+		global $wp_query;
+		
 		LegalDebug::debug( [
 			'NotFoundMain' => 'check-1',
 
 			'is_page_template' => is_page_template( '404.php' ),
 
 			'is_404' => is_404(),
+
+			'query_vars' => $wp_query->query_vars,
 		] );
 
         return is_404();
