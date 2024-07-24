34774c5a1c95a2a30a0f283ea5c48bdb2429a1a5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-24 12:38:19 +0300

    new billet

--- lib/multisite/MultisiteMeta.php
+++ lib/multisite/MultisiteMeta.php
@@ -435,6 +435,16 @@ class MultisiteMeta
 		// if ( ! empty( $post_moved ) && ( is_array( $post_moved ) || is_numeric( $post_moved ) ) )
 		
 		// if ( ! empty( $post_moved ) && ( is_array( $post_moved ) ) )
+
+		LegalDebug::debug( [
+			'MultisiteMeta' => 'get_post_moved-1',
+
+			'post_moved' => $post_moved,
+
+			'gettype' => gettype( $post_moved ),
+
+			'in_array' => in_array( gettype( $post_moved ), [ 'integer', 'array' ] ),
+		] );
 		
 		if ( ! empty( $post_moved ) && ( in_array( gettype( $post_moved ), [ 'integer', 'array' ] ) ) )
 		{
