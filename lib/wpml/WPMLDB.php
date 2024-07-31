8944be54823226b1a82850e251fef38ad06a33ce
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-07-31 14:20:58 +0300

    new billet

--- lib/wpml/WPMLDB.php
+++ lib/wpml/WPMLDB.php
@@ -371,6 +371,24 @@ class WPMLDB
 
 		if ( ! empty( $trid_items_db ) )
 		{
+			LegalDebug::debug( [
+				'WPMLBD' => 'get_hreflang-1',
+
+				'trid_items_db-count' => count( $trid_items_db ),
+
+				'trid_items_db' => $trid_items_db,
+			] );
+
+			$trid_items_db = WPMLMain::exclude( $trid_items_db );
+
+			LegalDebug::debug( [
+				'WPMLBD' => 'get_hreflang-2',
+
+				'trid_items_db-count' => count( $trid_items_db ),
+
+				'trid_items_db' => $trid_items_db,
+			] );
+
 			return self::parse_hreflang_items( $trid_items_db );
 		}
 
