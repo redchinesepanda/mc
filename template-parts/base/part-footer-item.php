7dfbc31f213d0d8f1ce55be65b3e8cb49e1f50d1
sergeykalabin <sergey.kalabin@legalbet.com>
2024-07-31 16:10:38 +0300

    develop links in footer

--- template-parts/base/part-footer-item.php
+++ template-parts/base/part-footer-item.php
@@ -2,6 +2,8 @@
 	<div class="menu-item <?php echo $args[ 'class' ]; ?>">
 		<?php if ( $args[ 'href' ] == '#' ) : ?>
 			<span class="item-title" ><?php echo $args[ 'title' ]; ?></span>
+		<?php elseif ( $args[ 'href' ] == 'https://www.ukclubsport.com/' ) : ?>
+			<a class="item-title odds" href="<?php echo $args[ 'href' ]; ?>" rel="nofollow noreferrer" target="_blank"><?php echo $args[ 'title' ]; ?></a>
 		<?php else : ?>
 			<a class="item-title" href="<?php echo $args[ 'href' ]; ?>"><?php echo $args[ 'title' ]; ?></a>
 		<?php endif; ?>
