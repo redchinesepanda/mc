768eee1e5e53adf46662a3ed35a4d70132200988
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-08-05 14:15:47 +0300

    new billet

--- template-parts/wpml/wpml-lang-switcher-main.php
+++ template-parts/wpml/wpml-lang-switcher-main.php
@@ -5,20 +5,20 @@
 // ] );
 
 ?>
-<?php if ( !empty( $args ) ) : ?>
+<?php if ( ! empty( $args ) ) : ?>
     <div class="lang-switcher">
         <div class="lang-current lang-item legal-new">
             <div class="lang-title">
-                <?php if ( !empty( $args[ 'active' ] ) ) : ?>
+                <?php if ( ! empty( $args[ 'active' ] ) ) : ?>
                     <span><?php echo $args[ 'active' ][ 'title' ]; ?></span>
                 <?php endif; ?>
             </div>
             <!-- <img class="lang-image" src="<?php echo $args[ 'active' ][ 'src' ]; ?>" width="17" height="17" alt="<?php echo $args[ 'active' ][ 'title' ]; ?>" loading="lazy"> -->
-            <?php if ( !empty( $args[ 'choose-your-country' ] ) ) : ?>
+            <?php if ( ! empty( $args[ 'choose-your-country' ] ) ) : ?>
                 <a class="lang-title-suffix" href="<?php echo $args[ 'choose-your-country' ][ 'href' ]; ?>"><?php echo $args[ 'choose-your-country' ][ 'suffix' ]; ?></a>
             <?php endif; ?>
         </div>
-        <?php if ( !empty( $args[ 'languages' ] ) ) : ?>
+        <?php if ( ! empty( $args[ 'languages' ] ) ) : ?>
             <div id="lang-menu-1" class="menu-avaible">
                 <?php foreach( $args[ 'languages' ] as $lang ) : ?>
                     <a class='lang-avaible lang-item' href="<?php echo $lang[ 'href' ]; ?>" target="_blank">
