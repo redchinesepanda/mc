729e4411915eb02436fe902b99674529febf7120
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-08-13 14:05:14 +0300

    new billet

--- lib/multisite/MultisiteBlog.php
+++ lib/multisite/MultisiteBlog.php
@@ -51,8 +51,13 @@ class MultisiteBlog
 		restore_current_blog();
 	}
 
-	public static function get_site( $id )
+	public static function get_site( $id = '' )
 	{
+		if ( empty ( $id ) )
+		{
+			$id = MultisiteBlog::get_current_blog_id();
+		}
+
 		return get_site( $id );
 	}
 
