4e70920c9b9617ee706f56f328dd8b3798988cbb
sergeykalabin <sergey.kalabin@legalbet.com>
2024-08-20 11:38:39 +0300

    commit frontend

--- template-parts/review/review-about-bonus.php
+++ template-parts/review/review-about-bonus.php
@@ -6,7 +6,7 @@
         <?php endif; ?>
         <a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="legal-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
         <?php if( empty( $args[ 'mode' ] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
-            <div class="review-bonus-description legal-cut-item" data-cut-set-id="0"><?php echo $args[ 'bonus' ][ 'description' ]; ?></div>
+            <div class="review-bonus-description legal-cut-item <?php echo $args[ 'tnc-class' ] ?>" data-cut-set-id="0"><?php echo $args[ 'bonus' ][ 'description' ]; ?></div>
         <?php endif; ?>
         <?php if( empty( $args[ 'mode' ] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
             <span class="legal-cut-control" data-review-default="<?php echo $args[ 'text' ][ 'show' ]; ?>" data-review-active="<?php echo $args[ 'text' ][ 'hide' ]; ?>" data-cut-set-id="0"></span>
