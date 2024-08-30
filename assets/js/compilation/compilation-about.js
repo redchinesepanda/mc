9e2b7f92049e55ead86e801b56f8871fbdfbda0e
sergeykalabin <sergey.kalabin@legalbet.com>
2024-08-30 09:43:08 +0300

    commit frontend

--- assets/js/compilation/compilation-about.js
+++ assets/js/compilation/compilation-about.js
@@ -98,7 +98,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
 		defineOverflow( document.querySelector( selectors.stringSwiper ) );
 	}
 
-	setTimeout( prepareCalc, 300 );
+	setTimeout( prepareCalc, 500 );
 
 	// заполнение ширины контейнера свайпером конец new
 
