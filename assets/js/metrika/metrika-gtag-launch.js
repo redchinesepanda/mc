c738056fa4bddd8a40d92f139840c8de83be9f34
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-09-03 13:38:31 +0300

    new billet

--- assets/js/metrika/metrika-gtag-launch.js
+++ assets/js/metrika/metrika-gtag-launch.js
@@ -17,18 +17,19 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 	function gtagRunInitNoCookieUserId()
 	{
+		let userId = 'legal_user_id' + Date.now();
+
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
 		gtag('config', 'UA-224707123-1');
-
-		let userId = 'legal_user_id' + Date.now();
-
-		console.log( 'userId:');
-
-		console.log( userId );
-
 		gtag('config', '{{ gaData.id }}', { 'user_id': userId });
+		gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'ad_user_data': 'granted',
+            'ad_personalization': 'granted',
+            'analytics_storage': 'denied'
+        });
 	}
 
 	if ( MetrikaLib.checkCookie() )
