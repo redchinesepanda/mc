643d95535759588cc2ce2082568a0fbf3aac0d45
sergeykalabin <sergey.kalabin@legalbet.com>
2024-09-05 12:48:54 +0300

    commit frontend

--- template-parts/wpml/wpml-choose-your-country.php
+++ template-parts/wpml/wpml-choose-your-country.php
@@ -14,21 +14,9 @@
 			<div class="lang-image locale-77"></div>
 			<div class="lang-title">Kenya</div>
 		</a>
-		<a class="lang-item" href="/in/" target="_blank" draggable="false">
-			<div class="lang-image locale-94"></div>
-			<div class="lang-title">India</div>
-		</a>
 	</div>
 	<div class="choose-you-country-title">Asia</div>
 	<div class="choose-you-country-block">
-		<!-- <a class="lang-item" href="/kz/" target="_blank" draggable="false">
-			<div class="lang-image locale-66"></div>
-			<div class="lang-title">Казахстан</div>
-		</a> -->
-		<!-- <a class="lang-item" href="/kz-kz/" target="_blank" draggable="false">
-			<div class="lang-image locale-91"></div>
-			<div class="lang-title">Қазақстан</div>
-		</a> -->
 		<a class="lang-item" href="/ph/" target="_blank" draggable="false">
 			<div class="lang-image locale-84"></div>
 			<div class="lang-title">Philippines</div>
@@ -52,50 +40,18 @@
 			<div class="lang-image locale-1"></div>
 			<div class="lang-title">United Kingdom</div>
 		</a>
-		<!-- <a class="lang-item" href="/ua/" target="_blank" draggable="false">
-			<div class="lang-image locale-72"></div>
-			<div class="lang-title">Україна</div>
-		</a>
-		<a class="lang-item" href="/ua-ru/" target="_blank" draggable="false">
-			<div class="lang-image locale-72"></div>
-			<div class="lang-title">Украина</div>
-		</a> -->
 		<a class="lang-item" href="/pt/" target="_blank" draggable="false">
 			<div class="lang-image locale-86"></div>
 			<div class="lang-title">Portugal</div>
 		</a>
-		<!-- <a class="lang-item" href="/at/" target="_blank" draggable="false">
-			<div class="lang-image locale-87"></div>
-			<div class="lang-title">Österreich</div>
-		</a> -->
 		<a class="lang-item" href="/hr/" target="_blank" draggable="false">
 			<div class="lang-image locale-22"></div>
 			<div class="lang-title">Hrvatska</div>
 		</a>
-		<!-- <a class="lang-item" href="/cz/" target="_blank" draggable="false">
-			<div class="lang-image locale-89"></div>
-			<div class="lang-title">Česká Republika</div>
-		</a> -->
-		<!-- <a class="lang-item" href="/pl/" target="_blank" draggable="false">
-			<div class="lang-image locale-40"></div>
-			<div class="lang-title">Polska</div>
-		</a> -->
 		<a class="lang-item" href="/se/" target="_blank" draggable="false">
 			<div class="lang-image locale-88"></div>
 			<div class="lang-title">Sverige</div>
 		</a>
-		<!-- <a class="lang-item" href="/de/" target="_blank" draggable="false">
-			<div class="lang-image locale-3"></div>
-			<div class="lang-title">Deutschland</div>
-		</a> -->
-		<!-- <a class="lang-item" href="/ro/" target="_blank" draggable="false">
-			<div class="lang-image locale-44"></div>
-			<div class="lang-title">Română</div>
-		</a> -->
-		<!-- <a class="lang-item" href="/mt/" target="_blank" draggable="false">
-			<div class="lang-image locale-34"></div>
-			<div class="lang-title">Malta</div>
-		</a> -->
 		<a class="lang-item" href="/rs/" target="_blank" draggable="false">
 			<div class="lang-image locale-90"></div>
 			<div class="lang-title">Republika Srbija</div>
@@ -120,10 +76,6 @@
 			<div class="lang-image locale-95"></div>
 			<div class="lang-title">Denmark</div>
 		</a>
-		<!-- <a class="lang-item" href="/nl/" target="_blank" draggable="false">
-			<div class="lang-image locale-37"></div>
-			<div class="lang-title">Nederland</div>
-		</a> -->
 	</div>
 	<div class="choose-you-country-title">Latin America and the Caribbean</div>
 	<div class="choose-you-country-block">
@@ -147,26 +99,11 @@
 			<div class="lang-image locale-85"></div>
 			<div class="lang-title">Brasil</div>
 		</a>
-		<!-- <a class="lang-item" href="/ar/" target="_blank" draggable="false">
-			<div class="lang-image locale-5"></div>
-			<div class="lang-title">Argentina</div>
-		</a> -->
 		<a class="lang-item" href="/cl/" target="_blank" draggable="false">
 			<div class="lang-image locale-92"></div>
 			<div class="lang-title">Chile</div>
 		</a>
 	</div>
-	<!-- <div class="choose-you-country-title">Oceania</div>
-	<div class="choose-you-country-block">
-		<a class="lang-item" href="/au/" target="_blank" draggable="false">
-			<div class="lang-image locale-74"></div>
-			<div class="lang-title">Australia</div>
-		</a>
-		<a class="lang-item" href="/nz/" target="_blank" draggable="false">
-			<div class="lang-image locale-78"></div>
-			<div class="lang-title">New Zealand</div>
-		</a>
-	</div> -->
 	<div class="choose-you-country-title">The United States, Canada, and Puerto Rico</div>
 	<div class="choose-you-country-block">
 		<a class="lang-item" href="/ca/" target="_blank" draggable="false">
