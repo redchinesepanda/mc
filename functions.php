aaa21dae6cf64e4dd46ff18e6370e994067545b6
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-09-19 13:39:12 +0300

    new billet

--- functions.php
+++ functions.php
@@ -5,7 +5,11 @@ require_once( 'lib/LegalMain.php' );
 LegalMain::register();
 
 function add_my_icons($file) {
-    $file = get_stylesheet_directory().'/path_to_my/icons.json';
+    // $file = get_stylesheet_directory().'/path_to_my/icons.json';
+    
+	$file = get_stylesheet_directory().'/assets/font/mc-icons-sports/mc-icons-sports.json';
+
+	// assets\font\mc-icons-sports\mc-icons-sports.json
 
 	print_r( $file );
 
@@ -15,7 +19,12 @@ function add_my_icons($file) {
 add_filter( 'jvm_richtext_icons_iconset_file', 'add_my_icons');
 
 function add_my_css($cssfile) {
-    $cssfile = get_stylesheet_directory_uri().'/path_to_my/cssfile.css';
+    // $cssfile = get_stylesheet_directory_uri().'/path_to_my/cssfile.css';
+    
+	$cssfile = get_stylesheet_directory_uri().'/assets/font/mc-icons-sports/mc-icons-sports.css';
+
+	// assets\font\mc-icons-sports\mc-icons-sports.css
+
     return $cssfile;
 }
 
