deb8c1eaec793cb1b58f463b29ec969537e615c3
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-09-20 12:29:02 +0300

    new billet

--- assets/js/admin/admin-tinymce-iconpicker.js
+++ assets/js/admin/admin-tinymce-iconpicker.js
@@ -1,3 +1,7 @@
 tinymce.PluginManager.add( 'tinymce_iconpicker', function( editor, url ) {
-	editor.addButton( 'tinymce_iconpicker', {} );
+	editor.addButton( 'tinymce_iconpicker', {
+		title: 'Insert Button',
+
+    	icon: 'icon code-sample'
+	} );
 } );
