37abb0d98ed7be1a74f238a113d38e27aa88f0bf
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-09-20 13:56:05 +0300

    new billet

--- lib/admin/AdminTinyMCEIconPicker.php
+++ lib/admin/AdminTinyMCEIconPicker.php
@@ -2,31 +2,6 @@
 
 class AdminTinyMCEIconPicker
 {
-	const CSS = [
-        'front-tinymce-iconpicker' => [
-            'path' => LegalMain::LEGAL_URL . '/assets/css/admin/admin-tinymce-iconpicker.css',
-
-            'ver'=> '1.2.1',
-        ],
-
-		'front-tinymce-iconpicker-icons' => [
-			'path' => LegalMain::LEGAL_URL . '/assets/font/font-mc-icons-sports.css',
-
-			'ver' => '1.0.0',
-		],
-
-		'front-tinymce-iconpicker-font-mc-icons-sports' => [
-			'path' => LegalMain::LEGAL_URL . '/assets/font/font-main.css',
-
-			'ver' => '1.0.1',
-		],
-    ];
-
-	public static function register_style()
-    {
-		ToolEnqueue::register_style( self::CSS );
-    }
-
 	const CSS_ADMIN = [
         'admin-tinymce-iconpicker' => LegalMain::LEGAL_URL . '/assets/css/admin/admin-tinymce-iconpicker.css',
 
