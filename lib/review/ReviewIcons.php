259d3b65f888a42b7792e7731a69b440f26df4fd
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-09-20 14:03:01 +0300

    new billet

--- lib/review/ReviewIcons.php
+++ lib/review/ReviewIcons.php
@@ -10,7 +10,7 @@ class ReviewIcons
         ],
 
         'review-font-mc-icons-sports-icons' => [
-			'path' => LegalMain::LEGAL_URL . '/assets/font/font-mc-icons-sports.css',
+			'path' => LegalMain::LEGAL_URL . '/assets/font/mc-icons-sports/mc-icons-sports.css',
 
 			'ver' => '1.0.0',
 		],
