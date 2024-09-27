b07af7bb6cd9c02fa85ca7b4b814d1e12c9068fa
sergeykalabin <sergey.kalabin@legalbet.com>
2024-09-27 13:28:22 +0300

    commit frontend

--- template-parts/billet/right/part-billet-bonus.php
+++ template-parts/billet/right/part-billet-bonus.php
@@ -7,11 +7,13 @@
 // ] );
 
 ?>
-<div class="bonus-title">
-    <a class="legal-bonus <?php echo $args[ 'class' ]; ?> check-oops" href="<?php echo $args[ 'href' ]; ?>" <?php echo BilletMain::render_nofollow( $args[ 'nofollow' ] ); ?>>
-        <?php echo $args[ 'title' ]; ?>
-    </a>
-</div>
+<?php if ( !empty( $args[ 'title' ] ) ): ?>
+    <div class="bonus-title">
+        <a class="legal-bonus <?php echo $args[ 'class' ]; ?> check-oops" href="<?php echo $args[ 'href' ]; ?>" <?php echo BilletMain::render_nofollow( $args[ 'nofollow' ] ); ?>>
+            <?php echo $args[ 'title' ]; ?>
+        </a>
+    </div>
+<?php endif; ?>
 <?php if ( !empty( $args[ 'description' ] ) ): ?>
     <div class="bonus-description">
         <span><?php echo $args[ 'description' ]; ?></span>
