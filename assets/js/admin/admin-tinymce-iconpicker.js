cb868e53029c78e2729058464cd17bd15fa30ea1
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-02 14:14:58 +0300

    new billet

--- assets/js/admin/admin-tinymce-iconpicker.js
+++ assets/js/admin/admin-tinymce-iconpicker.js
@@ -8,84 +8,6 @@ tinymce.PluginManager.add('tinymce_iconpicker', function (editor, url) {
 
 		onclick: function ()
 		{
-			// editor.windowManager.open(
-			// 	{
-			// 		title: 'Insert icon',
-
-			// 		width: 500,
-
-			// 		height: 300,
-
-			// 		body: [
-
-			// 			{
-			// 				type: 'listbox',
-
-			// 				name: 'class',
-
-			// 				label: 'Choose Icon',
-
-			// 				'values': [
-			// 					{ text: "American football", value: "icon-american-football" },
-
-			// 					{ text: "Asian", value: "icon-asian" },
-
-			// 					{ text: "Athletics", value: "icon-athletics" }
-			// 				]
-			// 			},
-
-			// 			// {
-			// 			// 	type: 'htmlpanel',
-
-			// 			// 	html: 'Panel content goes here.'
-			// 			// },
-
-			// 			{
-			// 				type: "container",
-
-			// 				label: "MC Icon",
-
-			// 				layout: "flex",
-
-			// 				direction: "row",
-
-			// 				items: [
-			// 					{ type: "textbox", name: "mc-textbox-name" },
-
-			// 					{
-			// 						type: "button",
-
-			// 						text: "Choose",
-
-			// 						onclick() {
-			// 							// (TI_Picker.target = jQuery(this.$el).prev()), TI_Picker.showLightbox(null);
-
-			// 							console.log( 'container button' );
-
-			// 							console.log( this.$el.previousElementSibling );
-
-			// 							console.log( this.$el.children );
-
-			// 							console.log( this.$el.previousSibling );
-
-			// 							console.log( this.$el );
-			// 						},
-			// 					}
-			// 				],
-			// 			}
-			// 		],
-
-			// 		onsubmit: function ( element )
-			// 		{
-			// 			// let $content = '<a href="' + e.data.url + '" class="btn' + (e.data.style !== 'default' ? ' ' + e.data.style : '') + '"' + (!!e.data.newtab ? ' target="_blank"' : '') + '>' + e.data.label + '</a>';
-
-			// 			let content = '<span class="mc-icon-container"><i class="mc-icon mc-icons-sports ' + element.data.class + '"></i></span>';
-
-			// 			editor.insertContent( content );
-			// 		}
-			// 	}
-			// );
-			
 			editor.windowManager.open(
 				{
 					title: 'Insert icon',
@@ -94,79 +16,64 @@ tinymce.PluginManager.add('tinymce_iconpicker', function (editor, url) {
 
 					height: 300,
 
-					body: {
-						items: [
-							{
-								type: "container",
+					body: [
 
-								label: "MC Icon Select",
+						{
+							type: 'listbox',
 
-								layout: "flex",
+							name: 'class',
 
-								direction: "row",
+							label: 'Choose Icon',
 
-								items: [
-									{ type: "textbox", name: "mc-textbox-name-select" },
+							'values': [
+								{ text: "American football", value: "icon-american-football" },
 
-									{
-										type: 'listbox',
-		
-										name: 'class',
-		
-										label: 'Choose Icon',
-		
-										'values': [
-											{ text: "American football", value: "icon-american-football" },
-		
-											{ text: "Asian", value: "icon-asian" },
-		
-											{ text: "Athletics", value: "icon-athletics" }
-										]
+								{ text: "Asian", value: "icon-asian" },
+
+								{ text: "Athletics", value: "icon-athletics" }
+							]
+						},
+
+						// {
+						// 	type: 'htmlpanel',
+
+						// 	html: 'Panel content goes here.'
+						// },
+
+						{
+							type: "container",
+
+							label: "MC Icon",
+
+							layout: "flex",
+
+							direction: "row",
+
+							items: [
+								{ type: "textbox", name: "mc-textbox-name" },
+
+								{
+									type: "button",
+
+									text: "Choose",
+
+									onclick() {
+										// (TI_Picker.target = jQuery(this.$el).prev()), TI_Picker.showLightbox(null);
+
+										console.log( 'container button' );
+
+										console.log( this.$el.previousElementSibling );
+
+										console.log( this.$el.children );
+
+										console.log( this.$el.previousSibling );
+
+										console.log( this.$el );
 									},
-								],
-							},
-
-							// {
-							// 	type: 'htmlpanel',
-
-							// 	html: 'Panel content goes here.'
-							// },
-
-							{
-								type: "container",
-
-								label: "MC Icon",
-
-								layout: "flex",
-
-								direction: "row",
-
-								items: [
-									{ type: "textbox", name: "mc-textbox-name" },
-
-									{
-										type: "button",
-
-										text: "Choose",
-
-										onclick() {
-											// (TI_Picker.target = jQuery(this.$el).prev()), TI_Picker.showLightbox(null);
-
-											console.log( 'container button' );
-
-											console.log( this.$el.previousElementSibling );
-
-											console.log( this.$el.children );
-
-											console.log( this.$el.previousSibling );
-
-											console.log( this.$el );
-										},
-									}
-								],
-							}
-						],
-					},
+								}
+							],
+						}
+					],
 
 					onsubmit: function ( element )
 					{
