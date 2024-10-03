8bfb0ce4679f561bfb0ae8e1ebe1d47789c402d8
sergeykalabin <sergey.kalabin@legalbet.com>
2024-10-03 16:19:37 +0300

    commit frontend

--- template-parts/review/review-anchors.php
+++ template-parts/review/review-anchors.php
@@ -10,18 +10,15 @@
 <?php if( !empty( $args[ 'items' ] ) ) : ?>
     <nav class="anchors">
         <div class="review-anchors">
-            <div class="anchors-title"><?php echo $args[ 'title' ]; ?></div>
-            <!-- новый тег для swiper -->
+            <!-- <div class="anchors-title"><?php echo $args[ 'title' ]; ?></div> -->
             <div class="swiper">
-                <!-- новый тег для swiper -->
                 <div class="swiper-wrapper">
-                <!-- новый класс .swiper-slide для swiper -->
                 <?php foreach( $args[ 'items' ] as $item ) : ?>
                     <a class="anchors-item swiper-slide" href="<?php echo $item[ 'href' ]; ?>"><?php echo $item[ 'label' ]; ?></a>
                 <?php endforeach; ?>
                 </div>
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev anchors-btn anchors-btn-prev"></div>
+                <div class="swiper-button-next anchors-btn anchors-btn-next"></div>
             </div>
         </div>
         <span class="legal-to-top"></span>
