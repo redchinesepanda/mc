1d48ce47a72d6ed44d039e0f0ba5c70ea3c352e2
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-08 13:35:00 +0300

    new billet

--- lib/review/ReviewIcons.php
+++ lib/review/ReviewIcons.php
@@ -3,22 +3,28 @@
 class ReviewIcons
 {
 	const CSS = [
-        'review-set-font-mc-icons-sports' => [
+        'review-icons-style' => [
             'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-icons.css',
 
             'ver'=> '1.0.0',
         ],
 
-        'review-font-mc-icons-sports-icons' => [
+        'review-icons-style-mc-icons-sports' => [
 			'path' => LegalMain::LEGAL_URL . '/assets/font/mc-icons-sports/mc-icons-sports.css',
 
 			'ver' => '1.0.0',
 		],
 
-		'review-font-mc-icons-sports-font' => [
-			'path' => LegalMain::LEGAL_URL . '/assets/font/font-main.css',
+		// 'review-icons-font-main' => [
+		// 	'path' => LegalMain::LEGAL_URL . '/assets/font/font-main.css',
 
-			'ver' => '1.0.1',
+		// 	'ver' => '1.0.1',
+		// ],
+
+		'review-icons-font-mc-icons-sports' => [
+			'path' => LegalMain::LEGAL_URL . '/assets/font/font-mc-icons-sports.css',
+
+			'ver' => '1.0.0',
 		],
     ];
 
