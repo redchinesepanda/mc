24a16931e56f4a014f4f884ce1590b31f848b24b
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-08 12:02:10 +0300

    new billet

--- template-parts/admin/part-icon-picker.php
+++ template-parts/admin/part-icon-picker.php
@@ -3,39 +3,24 @@
  * @var $icons
  */
 ?>
-<!-- <div class="mc-icon-picker"> -->
-	<h3>Choose icon</h3>
-
-	<a href="#" class="themify-icons-close_lightbox">
-		<i class="ti-close"></i>
-	</a>
-
-	<div class="themify-icons-lightbox_container">
-
-		<ul class="tf-icons-font-group">
-			<?php foreach( $args[ 'categories' ] as $category ) : ?>
-				<li>
-					<a href="#ti_picker-<?php echo $category['key']; ?>" class="external-link"><?php echo $category['label']; ?></a>
-				</li>
+<h3>Choose icon</h3>
+<ul class="icon-picker-categories">
+	<?php foreach( $args[ 'categories' ] as $category ) : ?>
+		<li>
+			<a class="picker-categories-item" href="#mc-category-<?php echo $category[ 'key' ]; ?>"><?php echo $category[ 'label' ]; ?></a>
+		</li>
+	<?php endforeach; ?>
+</ul>
+<?php foreach( $args[ 'categories' ] as $category ) : ?>
+	<section id="mc-category-<?php echo $category[ 'key' ]; ?>">
+		<h2 class="picker-category-header"><?php echo $category[ 'label' ]; ?></h2>
+		<div class="picker-category-items">
+			<?php foreach( $category[ 'icons' ] as $icon_name ) : ?>
+				<a href="#" data-icon="<?php echo $icon_name; ?>">
+					<i class="<?php echo $icon_name; ?>" aria-hidden="true"></i>
+					<?php echo $icon_name; ?>
+				</a>
 			<?php endforeach; ?>
-		</ul>
-
-		<?php foreach( $args[ 'categories' ] as $category ) : ?>
-			<section id="ti_picker-<?php echo $category['key']; ?>">
-				<h2 class="page-header"><?php echo $category['label']; ?></h2>
-				<div class="row">
-					<?php foreach( $category['icons'] as $icon_name ) : ?>
-						<a href="#" data-icon="<?php echo $icon_name; ?>">
-							<i class="<?php echo $icon_name; ?>" aria-hidden="true"></i>
-							<?php echo $icon_name; ?>
-						</a>
-					<?php endforeach; ?>
-				</div>
-			</section>
-		<?php endforeach; ?>
-
-	</div>
-
-	<!-- .themify-icons-lightbox_container -->
-<!-- </div> -->
-<!-- <div id="TI_Picker_overlay"></div> -->
+		</div>
+	</section>
+<?php endforeach; ?>
