7df96de578d6e5148a19dc0f5f1452595176784d
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-14 15:35:28 +0300

    new billet

--- template-parts/review/review-about-bonus.php
+++ template-parts/review/review-about-bonus.php
@@ -9,22 +9,24 @@
 ?>
 <?php if( !empty( $args[ 'title' ] ) ) : ?>
     <div class="about-right">
-        <?php if( !empty( $args[ 'bonus' ][ 'name' ] ) ) : ?>
-            <div class="review-bonus-title"><?php echo $args[ 'bonus' ][ 'name' ]; ?></div>
-        <?php endif; ?>
-        <a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="legal-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
-        <?php if( empty( $args[ 'mode' ] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
-            <div class="review-bonus-description legal-cut-item <?php echo $args[ 'tnc-class' ] ?>" data-cut-set-id="0"><p class="bonus-description-text"><?php echo $args[ 'bonus' ][ 'description' ]; ?></p></div>
-        <?php endif; ?>
-        <?php if( empty( $args[ 'mode' ] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
-            <span class="legal-cut-control" data-review-default="<?php echo $args[ 'text' ][ 'show' ]; ?>" data-review-active="<?php echo $args[ 'text' ][ 'hide' ]; ?>" data-cut-set-id="0"></span>
-        <?php endif; ?>
+        <div class="review-bonus-data">
+            <?php if( !empty( $args[ 'bonus' ][ 'name' ] ) ) : ?>
+                <div class="review-bonus-title"><?php echo $args[ 'bonus' ][ 'name' ]; ?></div>
+            <?php endif; ?>
+            <a href="<?php echo $args[ 'afillate' ][ 'href' ]; ?>" class="legal-afillate check-oops" style="" target="_blank" rel="nofollow"><?php echo $args[ 'afillate' ][ 'text' ]; ?></a>
+            <?php if( empty( $args[ 'mode' ] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
+                <div class="review-bonus-description legal-cut-item <?php echo $args[ 'tnc-class' ] ?>" data-cut-set-id="0"><p class="bonus-description-text"><?php echo $args[ 'bonus' ][ 'description' ]; ?></p></div>
+            <?php endif; ?>
+            <?php if( empty( $args[ 'mode' ] ) && !empty( $args[ 'bonus' ][ 'description' ] ) ) : ?>
+                <span class="legal-cut-control" data-review-default="<?php echo $args[ 'text' ][ 'show' ]; ?>" data-review-active="<?php echo $args[ 'text' ][ 'hide' ]; ?>" data-cut-set-id="0"></span>
+            <?php endif; ?>
+        </div>
         <?php if ( ! empty( $args[ 'afillate' ][ 'stats' ] ) ) : ?>
             <div class="review-bonus-stats">
                 <?php foreach ( $args[ 'afillate' ][ 'stats' ] as $stats_item ) : ?>
                     <div class="bonus-stats-items">
-                        <span class="stats-item-label">License</span>
-                        <span class="stats-item-value"><?php echo $args[ 'afillate' ][ 'stats' ][ 'license' ]; ?></span>
+                        <span class="stats-item-label"><?php echo $stats_item[ 'label' ]; ?></span>
+                        <span class="stats-item-value"><?php echo $stats_item[ 'value' ]; ?></span>
                     </div>
                 <?php endforeach; ?>
             </div>
