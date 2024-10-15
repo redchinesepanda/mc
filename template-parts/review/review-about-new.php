7b5d4e5a0ec5b140463e73f52098785d90df7c4e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-15 09:58:52 +0300

    new billet

--- template-parts/review/review-about-new.php
+++ template-parts/review/review-about-new.php
@@ -1,10 +1,10 @@
 <?php
 
-LegalDebug::debug( [
-    'template' => 'review-about-new.php',
+// LegalDebug::debug( [
+//     'template' => 'review-about-new.php',
 
-    'args' => $args,
-] );
+//     'args' => $args,
+// ] );
 
 ?>
 <?php if ( !empty( $args[ 'title' ] ) ) : ?>
