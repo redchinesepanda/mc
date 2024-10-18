5592c318cf56e4cd50961907bda25273756f59c5
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-18 12:12:49 +0300

    new billet

--- lib/review/ReviewAnchors.php
+++ lib/review/ReviewAnchors.php
@@ -183,7 +183,7 @@ class ReviewAnchors
 
             'set_title_id_manual' => $set_title_id_manual,
 
-            'set_title_id_manual-or-set_title_id_manual' => ( $set_title_id_manual || $set_title_id_manual ),
+            'set_title_id_manual-or-set_title_id_manual' => ( bool ) ( $set_title_id_manual || $set_title_id_manual ),
         ] );
 
         if ( $set_title_id_manual || $set_title_id_manual )
