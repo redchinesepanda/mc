13be7e503d5444387dc07fa3abb1125a7873a492
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-23 13:22:27 +0300

    new billet

--- assets/js/review/review-about-tooltip.js
+++ assets/js/review/review-about-tooltip.js
@@ -12,14 +12,10 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 	const args = [
 		{
-			// 'tooltipContainer' : '.review-about .about-achievement',
-			
 			'tooltipContainer' : '.review-about .about-achievement .achievement-item'
 		},
 
 		{
-			// 'tooltipContainer' : '.legal-review-page-sidebar .bonus-stats-items',
-			
 			'tooltipContainer' : '.review-bonus-stats .stats-item-tooltip'
 		}
 	];
@@ -33,25 +29,12 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 		tooltipClose: '.legal-tooltip-close',
 
-		// tooltipOpen: '.review-about .achievement-name',
-		
-		// tooltipClose: '.review-about .achievement-tooltip-close',
-
-		// tooltip: '.review-about .achievement-tooltip-background',
-
 		aboutAchievement: '.review-about .about-achievement',
-
-		// tooltipContainer: '.legal-review-page-sidebar .bonus-stats-items',
 
 		tooltipSet: function ( id ) {
 			return '[data-tooltip-set="' + id + '"]';
 		}
 	};
-
-	function addSet( element )
-	{
-		element.classList.add( classes.active );
-	}
 
 	function removeSet( element )
 	{
@@ -63,166 +46,35 @@ document.addEventListener( 'DOMContentLoaded', function ()
 		element.classList.toggle( classes.active );
 	}
 
-	// function toggleModal( event )
-	// {
-	// 	// event.currentTarget.closest( selectors.aboutAchievement ).querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) ).forEach( toggleSet );
-	// 	// toggleSet( event.currentTarget );
-
-	// 	console.log( 'toggleModal' );
-
-	// 	console.log( event.currentTarget );
-
-	// 	event.currentTarget.closest( selectors.tooltipContainer )
-	// 		.querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) )
-	// 		.forEach( toggleSet );
-	// }
-
 	function closeModal( event )
 	{
-		console.log( 'closeModal' );
-
-		// event.currentTarget.closest( selectors.aboutAchievement ).querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) ).forEach( toggleSet );
-		// toggleSet( event.currentTarget );
-
-		// console.log( event.target );
-
-		// console.log( event.currentTarget );
-
-		// event.currentTarget.closest( selectors.tooltipContainer )
-		// 	.querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) )
-		// 	.forEach( toggleSet );
-
-		// console.log( event.target.closest( selectors.tooltip ) );
-		
-		// event.target.closest( selectors.tooltip ).parentElement().classList.remove( classes.active );
+		// console.log( 'closeModal' );
 
 		removeSet( event.currentTarget.closest( selectors.tooltip ) );
 	}
 
-	function openModal( event )
+	function toggleModal( event )
 	{
-		console.log( 'openModal' );
+		// console.log( 'toggleModal' );
 
-		// if ( ! event.currentTarget.classList.contains( classes.active ) )
-		// {
-			// event.currentTarget.classList.add( classes.active );
+		toggleSet( event.currentTarget );
 
-			toggleSet( event.currentTarget );
-
-			toggleSet( event.currentTarget.parentElement.querySelector( selectors.tooltip ) );
-
-			// event.currentTarget.closest( selectors.aboutAchievement ).querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) ).forEach( toggleSet );
-			// toggleSet( event.currentTarget );
-	
-	
-			// console.log( event.target ); 
-	
-			// console.log( event.currentTarget );
-	
-			// event.currentTarget.closest( selectors.tooltipContainer )
-			// 	.querySelectorAll( selectors.tooltipSet( event.currentTarget.dataset.tooltipSet ) )
-			// 	.forEach( addSet );
-			
-			// event.currentTarget
-			// event.currentTarget
-			// 	.querySelectorAll( selectors.tooltip )
-			// 	.forEach( addSet );
-
-
-		// }
+		toggleSet( event.currentTarget.parentElement.querySelector( selectors.tooltip ) );
 	}
-
-	// function prepareClose( element )
-	// {
-	// 	console.log( 'prepareClose' );
-
-	// 	console.log( element );
-
-	// 	// element.dataset.tooltipSet = this.dataset.tooltipSet;
-
-	// 	// element.addEventListener( 'click', toggleModal, false );
-		
-	// 	element.addEventListener( 'click', closeModal, false );
-
-	// 	// console.log('prepareClose:', element);
-	// }
-
-	// function prepareModal( element )
-	// {
-	// 	console.log( 'prepareModal' );
-
-	// 	element.dataset.tooltipSet = this.dataset.tooltipSet;
-
-	// 	// console.log('prepareModal:', element);
-	// }
-	
-	// function prepareTooltip( element )
-	// {
-	// 	console.log( 'prepareTooltip' );
-
-	// 	console.log( element );
-
-	// 	console.log( element.dataset.tooltipSet );
-
-	// 	// if ( !element ) {
-    //     //     return;
-    //     // };
-
-	// 	// if ( element.classList.contains( classes.tooltipItem ) )
-	// 	// {
-	// 	// 	element.parentNode.classList.add( classes.tooltipContainer );
-	// 	// }
-
-	// 	element.addEventListener( 'click', openModal, false );
-
-	// 	// element.dataset.tooltipSet = this.dataset.tooltipSet;
-
-	// 	// this.querySelectorAll( selectors.tooltip ).forEach( prepareModal, element );
-		
-	// 	// element.querySelectorAll( selectors.tooltip ).forEach( prepareModal, element );
-
-	// 	// this.querySelectorAll( selectors.tooltipClose ).forEach( prepareClose, element );
-
-	// 	// console.log( selectors.tooltipClose );
-
-	// 	element.querySelectorAll( selectors.tooltipClose ).forEach( prepareClose, element );
-		
-	// 	// .addEventListener( 'click', toggleModal, false );
-	// }
 
 	function prepareElements( element )
 	{
-		console.log( 'prepareElements' );
+		// console.log( 'prepareElements' );
 
-		// console.log(`${element}, ${elementID}, ${this}`);
-		
-		// console.log( `${element}, ${elementID}` );
-
-		// console.log( element );
-
-		// element.dataset.tooltipSet = elementID;
-
-		// console.log( elementID );
-
-		// console.log( element.dataset.tooltipSet );
-
-		// if ( ! element ) {
-        //     return;
-        // };
-
-		// element.querySelectorAll( selectors.tooltipOpen ).forEach( prepareTooltip, element );
-		
-		// prepareTooltip( element );
-
-		element.addEventListener( 'click', openModal, false );
+		element.addEventListener( 'click', toggleModal, false );
 
 		element.parentElement.querySelector( selectors.tooltipClose ).addEventListener( 'click', closeModal, false );
-
-		// element.querySelectorAll( selectors.tooltipClose ).forEach( prepareClose, element );
 	}
 
-	function controlInit ( arg )
+	function controlInit( arg )
 	{	
+		// console.log( 'controlInit' );
+
 		// console.log( arg.tooltipContainer );
 
 		document.querySelectorAll( arg.tooltipContainer ).forEach( prepareElements );
@@ -230,7 +82,7 @@ document.addEventListener( 'DOMContentLoaded', function ()
 
 	function tooltipInit()
 	{
-		console.log( tooltipInit );
+		// console.log( 'tooltipInit' );
 
 		// console.log( args );
 
