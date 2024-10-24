bb8c6277bf0f333be140650cf046713e1e1b5b49
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-24 11:56:58 +0300

    new billet

--- template-parts/review/review-stats.php
+++ template-parts/review/review-stats.php
@@ -16,8 +16,10 @@
 			</div>
 			<div class="item-value-wrapper" data-value="<?php echo $item[ 'value' ]; ?>">
 				<div class="item-value"></div>
-				<span class="value-rating"><?php echo $item[ 'rating' ]; ?></span>
-				<span class="value-rating-max">/<?php echo $item[ 'rating-max' ]; ?></span>
+				<div class="item-data">
+					<span class="value-rating"><?php echo $item[ 'rating' ]; ?></span>
+					<span class="value-rating-max">/<?php echo $item[ 'rating-max' ]; ?></span>
+				</div>
 			</div>
 		</div>
 	<?php endforeach; ?>
