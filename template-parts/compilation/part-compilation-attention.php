3b75b4d784ce13c820209f4beeaf01ef0b210f4e
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-28 13:06:12 +0300

    new billet

--- template-parts/compilation/part-compilation-attention.php
+++ template-parts/compilation/part-compilation-attention.php
@@ -1,3 +1,12 @@
+<?php
+
+LegalDebug::debug( [
+    'template' => 'part-compilation-attention.php',
+
+    'args' => $args,
+] );
+
+?>
 <div class="compilation-attention <?php echo $args[ 'type' ]; ?>">
     <?php if ( $args[ 'type' ] == CompilationMain::TYPE[ 'tooltip' ] ) : ?>
         <span class="legal-tooltip-close"></span>
