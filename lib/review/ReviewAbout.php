39e181b06ee7318c358283f856cb0b7c6e1b5e8b
sergeykalabin <sergey.kalabin@legalbet.com>
2024-10-28 12:07:26 +0300

    deploy

--- lib/review/ReviewAbout.php
+++ lib/review/ReviewAbout.php
@@ -14,7 +14,7 @@ class ReviewAbout
         'review-about-new' => [
 			'path' => LegalMain::LEGAL_URL . '/assets/css/review/review-about-new.css',
 
-			'ver' => '1.0.2',
+			'ver' => '1.0.3',
 		],
     ];
 
@@ -51,13 +51,13 @@ class ReviewAbout
         'review-about' => [
 			'path' => LegalMain::LEGAL_URL . '/assets/js/review/review-about.js',
 
-			'ver' => '1.0.2',
+			'ver' => '1.0.3',
 		],
 
         'review-about-tooltip' => [
 			'path' => LegalMain::LEGAL_URL . '/assets/js/review/review-about-tooltip.js',
 
-			'ver' => '1.0.2',
+			'ver' => '1.0.3',
 		], 
     ];
 
