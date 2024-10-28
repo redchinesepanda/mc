4ad4c20c9e15fa55bf1fca33d30818c63fc71855
dmitriy.grechishkin <dmitriy.grechishkin@yandex.ru>
2024-10-28 13:10:13 +0300

    new billet

--- template-parts/compilation/part-compilation-attention.php
+++ template-parts/compilation/part-compilation-attention.php
@@ -8,9 +8,9 @@
 
 ?>
 <div class="compilation-attention <?php echo $args[ 'type' ]; ?>" data-tooltip-text="<?php echo $args[ 'text-data' ]; ?>">
+    <!-- <?php echo $args[ 'text' ]; ?> -->
+    <?php echo $args[ 'text-anchors' ]; ?>
     <?php if ( $args[ 'type' ] == CompilationMain::TYPE[ 'tooltip' ] ) : ?>
         <span class="legal-tooltip-close"></span>
     <?php endif; ?>
-    <!-- <?php echo $args[ 'text' ]; ?> -->
-    <?php echo $args[ 'text-anchors' ]; ?>
 </div>
